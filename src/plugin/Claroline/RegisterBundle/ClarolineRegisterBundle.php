744ea0d8f46498297da27471a9c94057e2815560
Stéphane Klein <stephaneklein221@gmail.com>
2011-08-24 10:56:43 +0200

    Move auto-registration logic into register plugin bundle.

--- src/plugin/Claroline/RegisterBundle/ClarolineRegisterBundle.php
+++ src/plugin/Claroline/RegisterBundle/ClarolineRegisterBundle.php
@@ -2,8 +2,17 @@
 
 namespace Claroline\RegisterBundle;
 
-use Claroline\PluginBundle\AbstractType\ClarolinePlugin;
+use Claroline\PluginBundle\AbstractType\ClarolineApplication;
+use Claroline\GUIBundle\Widget\ApplicationLauncher;
 
-class ClarolineRegisterBundle extends ClarolinePlugin
+class ClarolineRegisterBundle extends ClarolineApplication
 {
+    public function getLaunchers()
+    {
+        $launcher = new ApplicationLauncher('claroline_register_index', 
+                                            'register.application.name',
+                                            array('ROLE_ANONYMOUS'));
+
+        return array($launcher);
+    }
 }
--- src/plugin/Claroline/RegisterBundle/Controller/MainController.php
+++ src/plugin/Claroline/RegisterBundle/Controller/MainController.php
@@ -3,11 +3,31 @@
 namespace Claroline\RegisterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Claroline\UserBundle\Entity\User;
+use Claroline\UserBundle\Form\UserType;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ClarolineRegisterBundle::index.html.twig');
+        $user = new User();
+        $form = $this->createForm(new UserType(), $user);
+
+        $request = $this->get('request');
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+
+            if ($form->isValid())
+            {
+                $this->get('claroline.user.manager')->create($user);
+
+                return $this->redirect($this->generateUrl('claro_core_desktop'));
+            }
+        }
+
+        return $this->render('ClarolineRegisterBundle:Main:form.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
