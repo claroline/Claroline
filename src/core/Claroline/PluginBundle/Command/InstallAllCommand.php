3e96817b3e758a6b42ecc941b951e5c326cc62aa
Stéphane Klein <stephaneklein221@gmail.com>
2011-08-26 10:26:13 +0200

    Command-line added for raw installation of all the plugins. Other commands renamed.

---
+++ src/core/Claroline/PluginBundle/Command/InstallAllCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Claroline\PluginBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class InstallAllCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName('claroline:plugin:install_all')
+             ->setDescription('Registers all the plugins within "src/plugin".');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $pluginManager = $this->getContainer()->get('claroline.plugin.manager');
+        $pluginPath = $this->getContainer()->getParameter('claroline.plugin.directory');
+        
+        $output->writeln("Scanning plugin directory ('{$pluginPath}')...");
+
+        $pluginVendors = new \DirectoryIterator($pluginPath);
+
+        foreach ($pluginVendors as $vendor)
+        {
+            if ($vendor->isDir() && ! $vendor->isDot())
+            {
+                $vendorName = $vendor->getBasename();
+                $vendorPlugins = new \DirectoryIterator($vendor->getPathname());
+
+                foreach ($vendorPlugins as $plugin)
+                {
+                    if ($plugin->isDir() && ! $plugin->isDot())
+                    {
+                        $bundleName = $plugin->getBasename();
+                        $FQCN = "{$vendorName}\\{$bundleName}\\{$vendorName}{$bundleName}";
+                        $output->writeln("Installing plugin '{$FQCN}'...");
+                        $pluginManager->install($FQCN);
+                    }
+                }
+            }
+        }
+
+        $output->writeln('Done');
+    }
+}
--- src/core/Claroline/PluginBundle/Command/InstallCommand.php
+++ src/core/Claroline/PluginBundle/Command/InstallCommand.php
@@ -5,7 +5,7 @@ namespace Claroline\PluginBundle\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InstallerCommand extends AbstractPluginCommand
+class InstallCommand extends AbstractPluginCommand
 {
     protected function configure()
     {
