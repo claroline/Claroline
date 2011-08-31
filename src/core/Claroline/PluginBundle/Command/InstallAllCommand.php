fa931bb09848c40b980d50e7f8a2201c446f0397
Systho <systho@gmail.com>
2011-08-31 16:07:38 +0200

    forgot to import ArrayInput in PluginInstallAllCommand

--- src/core/Claroline/PluginBundle/Command/InstallAllCommand.php
+++ src/core/Claroline/PluginBundle/Command/InstallAllCommand.php
@@ -4,6 +4,7 @@ namespace Claroline\PluginBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
