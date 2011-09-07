005a28eb0d736e2d36fe2833369e241aa963648c
Stéphane Klein <stephaneklein221@gmail.com>
2011-09-07 17:28:07 +0200

    Defining AuthenticationController as a service.

--- src/core/Claroline/SecurityBundle/Controller/AuthenticationController.php
+++ src/core/Claroline/SecurityBundle/Controller/AuthenticationController.php
@@ -2,35 +2,56 @@
 
 namespace Claroline\SecurityBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\Security\Core\SecurityContext;
 
-class AuthenticationController extends Controller
+class AuthenticationController
 {
+    private $request;
+    private $twigEngigne;
+    
+    public function __construct(Request $request,
+                                TwigEngine $engine)
+    {
+        $this->request = $request;
+        $this->twigEngigne = $engine;
+    }
+    
     public function loginAction()
     {
-        if ($this->getRequest()->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        if ($this->request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
         {
-            $error = $this->getRequest()->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $this->request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
         }
         else
         {
-            $error = $this->getRequest()->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $this->request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
         }
 
-        return $this->render('ClarolineSecurityBundle:Authentication:login.html.twig', array(
-            'last_username' => $this->getRequest()->getSession()->get(SecurityContext::LAST_USERNAME),
-            'error'         => $error,
-        ));
+        $lastUsername = $this->request->getSession()->get(SecurityContext::LAST_USERNAME);
+        
+        return $this->twigEngigne->renderResponse(
+            'ClarolineSecurityBundle:Authentication:login.html.twig', 
+            array(
+                'last_username' => $lastUsername,
+                'error' => $error
+            )
+        );
     }
 
     public function checkAction()
     {
-        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
+        throw new \RuntimeException(
+            'You must configure the check path to be handled by the firewall '
+            . 'using form_login in your security firewall configuration.'
+        );
     }
 
     public function logoutAction()
     {
-        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+        throw new \RuntimeException(
+            'You must activate the logout in your security firewall configuration.'
+        );
     }
 }
