941a7eb0abe1a498cad532baa87717bb79e97d0c
Stéphane Klein <stephaneklein221@gmail.com>
2011-09-08 13:51:38 +0200

    Defining WorkspaceController as a service.

--- src/core/Claroline/WorkspaceBundle/Controller/WorkspaceController.php
+++ src/core/Claroline/WorkspaceBundle/Controller/WorkspaceController.php
@@ -2,110 +2,91 @@
 
 namespace Claroline\WorkspaceBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManager;
+use Claroline\WorkspaceBundle\Service\Manager\ACLWorkspaceManager;
 use Claroline\WorkspaceBundle\Entity\Workspace;
 use Claroline\WorkspaceBundle\Form\WorkspaceType;
 
-class WorkspaceController extends Controller
+class WorkspaceController
 {
-    /**
-     * @return \Claroline\WorkspaceBundle\Entity\ACLWorkspaceManager
-     */
-    public function getWorkspaceManager()
+    private $request;
+    private $session;
+    private $securityContext;
+    private $entityManager;
+    private $router;
+    private $formFactory;
+    private $twigEngine;
+    private $workspaceAclManager;
+    
+    public function __construct(Request $request,
+                                Session $session,
+                                SecurityContext $context,
+                                EntityManager $em,
+                                Router $router,
+                                FormFactory $factory,
+                                TwigEngine $engine,
+                                ACLWorkspaceManager $aclManager)
     {
-        /* TODO Can't this be injected ? 
-         * @see http://symfony2tips.blogspot.com/2011/02/dependencyinjection-for-controllers.html
-         */
-        return $this->get('claroline.workspace.acl_workspace_manager');
-    }
-
-    public function getUser()
-    {
-        return $this->get('security.context')->getToken()->getUser();
+        $this->request = $request;
+        $this->session = $session;
+        $this->securityContext = $context;
+        $this->entityManager = $em;
+        $this->router = $router;
+        $this->formFactory = $factory;
+        $this->twigEngine = $engine;
+        $this->workspaceAclManager = $aclManager;
     }
 
     public function newAction()
     {
         $workspace = new Workspace();
-        $form = $this->createForm(new WorkspaceType(), $workspace);
+        $form = $this->formFactory->create(new WorkspaceType(), $workspace);
 
-        return $this->render('ClarolineWorkspaceBundle:Workspace:form.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        return $this->twigEngine->renderResponse(
+            'ClarolineWorkspaceBundle:Workspace:form.html.twig', 
+            array('form' => $form->createView())
+        );
     }
     
     public function createAction()
     {
-        $workspaceManager = $this->getWorkspaceManager();
-
         $workspace = new Workspace();
-        $form = $this->createForm(new WorkspaceType(), $workspace);
-        $request = $this->getRequest();
-        $form->bindRequest($request);
-        $workspace->setOwner($this->getUser());
+        $form = $this->formFactory->create(new WorkspaceType(), $workspace);
+        $form->bindRequest($this->request);
+        $user = $this->securityContext->getToken()->getUser();
+        $workspace->setOwner($user);
 
         if ($form->isValid())
         {
-            $workspaceManager->create($workspace);
-
-            return $this->redirect($this->generateUrl('claro_core_desktop'));
+            $this->workspaceAclManager->create($workspace);
+            $route = $this->router->generate('claro_core_desktop');
+            
+            return new RedirectResponse($route);
         }
 
-        return $this->render('ClarolineWorkspaceBundle:Workspace:form.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        return $this->twigEngine->renderResponse(
+            'ClarolineWorkspaceBundle:Workspace:form.html.twig',
+            array('form' => $form->createView())
+        );
     }
     
     public function deleteAction($id)
     {
-        $workspaceManager = $this->getWorkspaceManager();
-
-        $workspaceRepo = $this->getDoctrine()->getRepository('ClarolineWorkspaceBundle:Workspace');
+        $workspaceEntity = 'ClarolineWorkspaceBundle:Workspace';
+        $workspaceRepo = $this->entityManager->getRepository($workspaceEntity);
         $workspace = $workspaceRepo->find($id);
-
-        $workspaceManager->delete($workspace);
+        $this->workspaceAclManager->delete($workspace);
         
-        $this->get('session')->setFlash('notice', 'Workspace successfully deleted');            
-
-        return $this->redirect($this->generateUrl('claro_core_desktop'));
+        $this->session->setFlash('notice', 'Workspace successfully deleted');            
+        $route = $this->router->generate('claro_core_desktop');
+        
+        return new RedirectResponse($route);
     }
-    /*
-    public function nodeAction()
-    {
-        // create ws
-        $ws = new Workspace();
-        $ws->setName('Workspace 1');
-
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($ws);
-        $em->flush();
-        */
-
-        /*
-        // retrieve ws 1
-        $em = $this->getDoctrine()->getEntityManager();
-        $ws = $em->find('Claroline\CoreBundle\Entity\Workspace', 1);
-        */
-
-        /*
-        // create test tree
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $parentNode = new \Claroline\CoreBundle\Entity\Node();
-        $childNode1 = new \Claroline\CoreBundle\Entity\Node();
-        $childNode2 = new \Claroline\CoreBundle\Entity\Node();
-
-        $parentNode->setName('Parent node');
-        $childNode1->setName('Child node 1');
-        $childNode2->setName('Child node 2');
-
-        $childNode1->setParent($parentNode);
-        $childNode2->setParent($parentNode);
-
-        $em->persist($parentNode);
-        $em->persist($childNode1);
-        $em->persist($childNode2);
-
-        $em->flush();
-    }*/
 }
