b34d2dd868c97bbce8806313fbf7943b0b3cc878
Stéphane Klein <stephaneklein221@gmail.com>
2011-09-12 15:03:17 +0200

    Adding DoctrineMigrationsBundle.

--- app/autoload.php
+++ app/autoload.php
@@ -8,9 +8,10 @@ $coreNamespaces = array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
     'Sensio'           => __DIR__.'/../vendor/bundles',
     'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
     'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine\\DBAL\\Migrations' => __DIR__.'/../vendor/doctrine-migrations/lib',
+    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
     'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
@@ -19,7 +20,6 @@ $coreNamespaces = array(
     'Gedmo'            => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
     'Claroline'        => array(__DIR__.'/../src/core', __DIR__.'/../src/plugin'),
 );
-
 
 $pluginNamespaces = array();
 if (file_exists(__DIR__.'/config/plugin/namespaces'))
