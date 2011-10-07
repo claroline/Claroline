f34c0183cc6fff5a1f1bd45a15c9b36eea3eb4fd
Systho <systho@gmail.com>
2011-10-07 12:25:34 +0200

    removed claroline:reinstall command which was basically only running uninstall then install. Fixing a bug making the core installer installing plugins

--- src/core/Claroline/InstallBundle/Command/InstallCommand.php
+++ src/core/Claroline/InstallBundle/Command/InstallCommand.php
@@ -5,6 +5,8 @@ namespace Claroline\InstallBundle\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class InstallCommand extends ContainerAwareCommand
 {
@@ -13,17 +15,33 @@ class InstallCommand extends ContainerAwareCommand
         parent::configure();
         $this->setName('claroline:install')
              ->setDescription('Installs the platform according to the config.');
+    
+        $this->addOption(
+            'with-plugins', 
+            'w', 
+            InputOption::VALUE_OPTIONAL, 
+            "When set to true, available plugins will be installed", 
+            false
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Launching installer...');
+        $delegateInput = new ArrayInput($input->getArguments());
 
         $manager = $this->getContainer()->get('claroline.install.core_installer');
         $manager->install();
         
         $command = $this->getApplication()->find('init:acl');        
-        $command->run($input, $output);
+        $command->run($delegateInput, $output);
+        
+        $options = $input->getOptions();
+        if(isset($options['with-plugins']))
+        {            
+            $pluginInstaller = $this->getApplication()->find('claroline:plugin:install_all');
+            $pluginInstaller->run($delegateInput, $output);
+        }
         
         $output->writeln('Done');
         
--- src/core/Claroline/InstallBundle/Command/UninstallCommand.php
+++ src/core/Claroline/InstallBundle/Command/UninstallCommand.php
@@ -5,6 +5,8 @@ namespace Claroline\InstallBundle\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class UninstallCommand extends ContainerAwareCommand
 {
@@ -13,14 +15,33 @@ class UninstallCommand extends ContainerAwareCommand
         parent::configure();
         $this->setName('claroline:uninstall')
              ->setDescription('uninstall the platform to a clear state.');
+        
+        $this->addOption(
+            'with-plugins', 
+            'w', 
+            InputOption::VALUE_OPTIONAL, 
+            "When set to true, available plugins will be installed", 
+            false
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Launching uninstaller...');
         
+        $delegateInput = new ArrayInput($input->getArguments());
+        
+        $options = $input->getOptions();
+        if(isset($options['with-plugins']))
+        {
+            
+                        
+            $pluginRemover = $this->getApplication()->find('claroline:plugin:remove_all');
+            $pluginRemover->run($delegateInput, $output);
+        }
+        
         $command = $this->getApplication()->find('claroline:security:drop_acl');        
-        $command->run($input, $output);
+        $command->run($delegateInput, $output);
 
         $manager = $this->getContainer()->get('claroline.install.core_installer');
         $manager->uninstall();
