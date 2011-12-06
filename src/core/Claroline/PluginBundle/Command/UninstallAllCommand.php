499e54e217365ce9b03df81018615934c053c22a
Stéphane Klein <stephaneklein221@gmail.com>
2011-12-06 11:30:08 +0100

    Fixing code duplication

--- src/core/Claroline/PluginBundle/Command/UninstallAllCommand.php
+++ src/core/Claroline/PluginBundle/Command/UninstallAllCommand.php
@@ -18,50 +18,11 @@ class UninstallAllCommand extends AbstractPluginCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $pluginInstaller = $this->getContainer()->get('claroline.plugin.installer');
-        $pluginDirs = array(
-            $this->getContainer()->getParameter('claroline.plugin.extension_directory'),
-            $this->getContainer()->getParameter('claroline.plugin.application_directory'),
-            $this->getContainer()->getParameter('claroline.plugin.tool_directory')
-        );
-        
-        foreach ($pluginDirs as $pluginDir)
+        if ($this->walkPluginDirectories('uninstallPlugin', $output))
         {
-            $output->writeln("Scanning plugin directory ('{$pluginDir}')...");
-
-            $pluginVendors = new \DirectoryIterator($pluginDir);
-
-            foreach ($pluginVendors as $vendor)
-            {
-                if ($vendor->isDir() && !$vendor->isDot())
-                {
-                    $vendorName = $vendor->getBasename();
-                    $vendorPlugins = new \DirectoryIterator($vendor->getPathname());
-
-                    foreach ($vendorPlugins as $plugin)
-                    {
-                        if ($plugin->isDir() && !$plugin->isDot())
-                        {
-                            $bundleName = $plugin->getBasename();
-                            $fqcn = "{$vendorName}\\{$bundleName}\\{$vendorName}{$bundleName}";
-                            
-                            if ($pluginInstaller->isInstalled($fqcn))
-                            {
-                                $output->writeln("Uninstalling plugin '{$fqcn}'...");
-                                $pluginInstaller->uninstall($fqcn);
-                            }
-                            else
-                            {
-                                $output->writeln("Plugin '{$fqcn}' is not installed. Aborting.");
-                            }
-                        }
-                    }
-                }
-            }
+            $this->resetCache($output);
         }
-
-        $output->writeln('Done');
         
-        $this->resetCache($output);
+        $output->writeln('Done');
     }
 }
