4087344e20a490ed83b70f0a0f76478f72ebd141
Stéphane Klein <stephaneklein221@gmail.com>
2012-01-17 13:18:17 +0100

    Finalizing context history browser (tests + config option)

--- src/core/Claroline/CommonBundle/Tests/History/BrowserTest.php
+++ src/core/Claroline/CommonBundle/Tests/History/BrowserTest.php
@@ -20,24 +20,121 @@ class BrowserTest extends WebTestCase
     {
         $this->session = self::createClient()->getContainer()->get('session');
         $this->request = $this->getMockedRequest();
-        $this->browser = new Browser($this->request, $this->session);
+        $this->browser = new Browser($this->request, $this->session, 4);       
     }
     
-    public function testKeepCurrentContextRequiresAValidKey()
+    public function testBrowserInitsAnArraySessionVariableToHandleHistory()
+    {
+        $this->assertTrue($this->session->has(Browser::HISTORY_SESSION_VARIABLE));
+        $this->assertTrue(is_array($this->session->get(Browser::HISTORY_SESSION_VARIABLE)));
+        $this->assertTrue(is_array($this->browser->getContextHistory()));
+    }
+    
+    public function testKeepCurrentContextIsOnlyAllowedForGetRequests()
     {
         $this->setExpectedException('Claroline\CommonBundle\Exception\ClarolineException');
-        $this->request->expects($this->any())
-            ->method('getMethod')
-            ->will($this->returnValue('GET'));
+        $browser = new Browser($this->getMockedRequest('POST'), $this->session, 4);
+        $browser->keepCurrentContext('Some context name');
+    }
+    
+    public function testKeepCurrentContextRequiresAValidContextName()
+    {
+        $this->setExpectedException('Claroline\CommonBundle\Exception\ClarolineException');
         $this->browser->keepCurrentContext('');
     }
     
-    private function getMockedRequest()
+    public function testBrowserBuildsCompleteContextsAndReturnsThemFromTheNewerToTheOlder()
     {
-        $request = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
-            ->disableOriginalConstructor()
-            ->getMock();
-
+        $browser = new Browser($this->getMockedRequest('GET', 'some/uri/1'), $this->session, 4);
+        $browser->keepCurrentContext('A');
+        $browser = new Browser($this->getMockedRequest('GET', 'some/uri/2'), $this->session, 4);
+        $browser->keepCurrentContext('B');
+        $browser = new Browser($this->getMockedRequest('GET', 'some/uri/3'), $this->session, 4);
+        $browser->keepCurrentContext('C');
+        
+        $history = $this->browser->getContextHistory();
+       
+        $this->assertEquals(3, count($history));
+        $this->assertEquals('C', $history[0]->getName());
+        $this->assertEquals('B', $history[1]->getName());
+        $this->assertEquals('A', $history[2]->getName());      
+        $this->assertEquals('some/uri/3', $history[0]->getUri());
+        $this->assertEquals('some/uri/2', $history[1]->getUri());
+        $this->assertEquals('some/uri/1', $history[2]->getUri());
+    }
+    
+    public function testBrowserKeepsOnlyOneInstanceOfAGivenContext()
+    {
+        $this->browser->keepCurrentContext('A'); 
+        $this->browser->keepCurrentContext('B');
+        $this->browser->keepCurrentContext('A');
+        $this->browser->keepCurrentContext('C');
+            
+        $history = $this->browser->getContextHistory();
+       
+        $this->assertEquals(3, count($history));
+        $this->assertEquals('C', $history[0]->getName());
+        $this->assertEquals('A', $history[1]->getName());
+        $this->assertEquals('B', $history[2]->getName());
+    }
+    
+    public function testBrowserDequeuesOlderElementWhenQueueMaxSizeIsReached()
+    {
+        $this->browser->keepCurrentContext('A'); 
+        $this->browser->keepCurrentContext('B');
+        $this->browser->keepCurrentContext('C');
+        $this->browser->keepCurrentContext('D');
+        $this->browser->keepCurrentContext('E');
+        
+        $history = $this->browser->getContextHistory();
+       
+        $this->assertEquals(4, count($history));
+        $this->assertEquals('E', $history[0]->getName());
+        $this->assertEquals('D', $history[1]->getName());
+        $this->assertEquals('C', $history[2]->getName());
+        $this->assertEquals('B', $history[3]->getName());
+    }
+    
+    public function testBrowserTruncatesSessionStoredQueueIfSizeConfigParamHasChangedToSmaller()
+    {
+        $this->browser->keepCurrentContext('A'); 
+        $this->browser->keepCurrentContext('B');
+        $this->browser->keepCurrentContext('C');
+        $this->browser->keepCurrentContext('D');
+        
+        $otherBrowserInstance = new Browser($this->request, $this->session, 2);
+        $history = $otherBrowserInstance->getContextHistory();
+        
+        $this->assertEquals(2, count($history));
+        $this->assertEquals('D', $history[0]->getName());
+        $this->assertEquals('C', $history[1]->getName());
+    }
+    
+    public function testGetLastContextReturnsNewerElementInHistoryIfAny()
+    {
+        $this->browser->keepCurrentContext('A'); 
+        $this->browser->keepCurrentContext('B');
+        
+        $context = $this->browser->getLastContext();
+        
+        $this->assertEquals('B', $context->getName());
+    }
+    
+    public function testGetLastContextReturnsNullIfNoContextAvailable()
+    {
+        $this->assertNull($this->browser->getLastContext());
+    }
+    
+    private function getMockedRequest($method = 'GET', $uri = 'some/uri')
+    {
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $request->expects($this->any())
+            ->method('getMethod')
+            ->will($this->returnValue($method));
+        $request->expects($this->any())
+            ->method('getUri')
+            ->will($this->returnValue($uri));
+        
         return $request;
     }
 }
