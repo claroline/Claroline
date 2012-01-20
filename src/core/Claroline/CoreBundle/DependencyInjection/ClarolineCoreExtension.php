da0b53ccdf0c490a4b0a490cdaf34b36f08b869a
Stéphane Klein <stephaneklein221@gmail.com>
2012-01-20 17:12:58 +0100

    Merging and renaming controllers/listeners services files

--- src/core/Claroline/CoreBundle/DependencyInjection/ClarolineCoreExtension.php
+++ src/core/Claroline/CoreBundle/DependencyInjection/ClarolineCoreExtension.php
@@ -3,9 +3,9 @@
 namespace Claroline\CoreBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ClarolineCoreExtension extends Extension
 {
@@ -13,14 +13,13 @@ class ClarolineCoreExtension extends Extension
     {
         $locator = new FileLocator(__DIR__ . '/../Resources/config/services');
         $loader = new YamlFileLoader($container, $locator);
+        $loader->load('listeners.yml');
+        $loader->load('controllers.yml');
         $loader->load('installation.yml');
         $loader->load('resource.yml');
         $loader->load('user.yml');
         $loader->load('security.yml');
         $loader->load('workspace.yml');
-        $loader->load('admin.yml');
-        $loader->load('desktop.yml');
-        $loader->load('home.yml');
         $loader->load('common.yml');
     }
 }
