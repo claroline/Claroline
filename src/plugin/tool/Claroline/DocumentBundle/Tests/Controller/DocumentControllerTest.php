1f6b1376bd0c24469caf52a68410e45ec4e19bf1
Stéphane Klein <stephaneklein221@gmail.com>
2012-02-08 10:41:27 +0100

    Fixing and refactoring document controller test

--- src/plugin/tool/Claroline/DocumentBundle/Tests/Controller/DocumentControllerTest.php
+++ src/plugin/tool/Claroline/DocumentBundle/Tests/Controller/DocumentControllerTest.php
@@ -6,96 +6,103 @@ use Claroline\CoreBundle\Testing\TransactionalTestCase;
 
 class DocumentControllerTest extends TransactionalTestCase
 {
-    private $dir;
+    /** @var string */
+    private $uploadDirectory;
 
-    public function __construct()
-    {
-        //path = __DIR__/../../../../../../../files
-        //path = __DIR__/../../../../../../../test/files   
-        parent::__construct();
-        $ds = DIRECTORY_SEPARATOR;
-        $this->dir = __DIR__ . "{$ds}..{$ds}..{$ds}..{$ds}..{$ds}..{$ds}..{$ds}..{$ds}test{$ds}files";
-    }
-
-    public function setUp()
+    /** @var string */
+    private $stubDirectory;
+    
+    protected function setUp()
     {
         parent::setUp();
-        $dirArray = $this->parseDirectory($this->dir);
-        $this->cleanDirectory($dirArray, $this->dir);
+        $this->uploadDirectory = $this->client
+            ->getContainer()
+            ->getParameter('claroline.files.directory');
+        $ds = DIRECTORY_SEPARATOR;
+        $this->stubDirectory = __DIR__ . "{$ds}..{$ds}stubs{$ds}";
+        $this->cleanUploadDirectory();
     }
 
     protected function tearDown()
     {
         parent::tearDown();
-        $dirArray = $this->parseDirectory($this->dir);
-        $this->cleanDirectory($dirArray, $this->dir);
+        $this->cleanUploadDirectory();
     }
 
-    public function testUploadThenDownloadThenDeleteDocument()
+    public function testUploadedFilesAppearInTheDocumentList()
     {
-        $ds = DIRECTORY_SEPARATOR;
-        $file = __DIR__ . $ds . ".." . $ds . "stubs" . $ds . "moveTest.txt";
-        //add file number 1 
-        $crawler = $this->client->request('GET', '/document/form');
-        $form = $crawler->filter('input[type=submit]')->form();
-        $crawler = $this->client->submit($form, array('Document_Form[file]' => $file));
-        //add file number 2
-        $file = __DIR__ . $ds . ".." . $ds . "stubs" . $ds . "otherMoveTest.txt";
-        $crawler = $this->client->request('GET', '/document/form');
-        $form = $crawler->filter('input[type=submit]')->form();
-        $crawler = $this->client->submit($form, array('Document_Form[file]' => $file));
-        //test
-        $crawler = $this->client->request('GET', '/document/list');
-        $this->assertEquals(2, $crawler->filter('#document_item')->count());
-        $this->assertEquals(3, count($this->parseDirectory($this->dir)));
-        //download file number 1
-        $crawler = $this->client->request('GET', '/document/list');
-        $link = $crawler->filter('#link_download')->eq(0)->link();
-        $crawler = $this->client->click($link);
-        $this->assertTrue($this->client->getResponse()->headers->contains('Content-Disposition', 'filename=moveTest.txt'));
-        //delete file number 2    
-        $crawler = $this->client->request('GET', '/document/list');
-        $link = $crawler->filter('#link_delete')->eq(1)->link();
-        $crawler = $this->client->click($link);
-        //count files
-        $crawler = $this->client->request('GET', '/document/list');
-        //test
-        $this->assertEquals(1, $crawler->filter('#document_item')->count());
-        $this->assertEquals(2, count($this->parseDirectory($this->dir)));
-        $lastFiles = $this->parseDirectory($this->dir);
-        $this->assertEquals("moveTest.txt", $lastFiles[0]);
-    }
+        $this->uploadFile("{$this->stubDirectory}moveTest.txt");
+        $this->uploadFile("{$this->stubDirectory}otherMoveTest.txt");
 
-    public function parseDirectory($dir)
+        $crawler = $this->client->request('GET', '/document/list');
+        $this->assertEquals(2, $crawler->filter('.document_item')->count());
+        $this->assertEquals(2, count($this->getUploadedFiles()));
+    }
+    
+    public function testUploadedFileCanBeDownloaded()
     {
-        $iterator = new \DirectoryIterator($dir);
-        $dirArray = array();
+        $this->uploadFile("{$this->stubDirectory}moveTest.txt");
+        
+        $crawler = $this->client->request('GET', '/document/list');
+        $link = $crawler->filter('.link_download')->eq(0)->link();
+        $crawler = $this->client->click($link);
+        
+        $this->assertTrue($this->client->getResponse()->headers->contains(
+            'Content-Disposition', 'attachment; filename=moveTest.txt')
+        );     
+        $this->assertEquals(
+            file_get_contents("{$this->stubDirectory}moveTest.txt"), 
+            $this->client->getResponse()->getContent()
+        );
+    }
+    
+    public function testUploadedFileCanBeDeleted()
+    {
+        $this->uploadFile("{$this->stubDirectory}moveTest.txt");
+        
+        $crawler = $this->client->request('GET', '/document/list');
+        $link = $crawler->filter('.link_delete')->eq(0)->link();
+        $this->client->click($link);
+        
+        $crawler = $this->client->request('GET', '/document/list');
+        $this->assertEquals(0, $crawler->filter('.document_item')->count());
+        $this->assertEquals(0, count($this->getUploadedFiles()));
+    }
+    
+    private function cleanUploadDirectory()
+    {
+        $iterator = new \DirectoryIterator($this->uploadDirectory);
 
         foreach ($iterator as $file)
         {
-            if ($file->isFile())
+            if ($file->isFile() && $file->getFilename() !== 'placeholder')
             {
-                $dirArray[] = $file->getFilename();
+                chmod($file->getPathname(), 0777);
+                unlink($file->getPathname());
             }
         }
-
-        return $dirArray;
     }
-
-    public function cleanDirectory($dirArray, $dir)
+  
+    private function uploadFile($filePath)
     {
-        $ds = DIRECTORY_SEPARATOR;
-        $indexCount = count($dirArray);
+        $crawler = $this->client->request('GET', '/document/form');
+        $form = $crawler->filter('input[type=submit]')->form();
+        $this->client->submit($form, array('Document_Form[file]' => $filePath));
+    }
+    
+    private function getUploadedFiles()
+    {
+        $iterator = new \DirectoryIterator($this->uploadDirectory);
+        $uploadedFiles = array();
 
-        for ($index = 0; $index < $indexCount; $index++)
+        foreach ($iterator as $file)
         {
-            if ($dirArray[$index] != 'placeholder')
+            if ($file->isFile() && $file->getFilename() !== 'placeholder')
             {
-                $pathName = $dir . $ds . $dirArray[$index];
-                chmod($pathName, 0777);
-                unlink($pathName);
+                $uploadedFiles[] = $file->getFilename();
             }
         }
-    }
 
+        return $uploadedFiles;
+    }
 }
