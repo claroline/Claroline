f40508c4e1f90d61ac1a6a99fd73b8937ff26861
Stéphane Klein <stephaneklein221@gmail.com>
2012-02-22 14:17:14 +0100

    Workspace controller refactoring (draft)

--- src/core/Claroline/CoreBundle/Controller/DesktopController.php
+++ src/core/Claroline/CoreBundle/Controller/DesktopController.php
@@ -9,25 +9,6 @@ class DesktopController extends Controller
 {
     public function indexAction()
     {
-        return $this->render(
-            'ClarolineCoreBundle:Desktop:desktop.html.twig',
-            array('workspaces_block' => $this->renderWorkspaceBlock())
-        );
-    }
-    
-    private function renderWorkspaceBlock()
-    {
-        $workspaceRoles = array();
-        $user = $this->get('security.context')->getToken()->getUser();
-        
-        if ($user instanceof User)
-        {
-            $workspaceRoles = $user->getWorkspaceRoleCollection(); 
-        }
-        
-        return $this->get('templating')->render(
-            'ClarolineCoreBundle:Desktop:workspaces_block.html.twig',
-            array('workspace_roles' => $workspaceRoles)
-        );
+        return $this->render('ClarolineCoreBundle:Desktop:desktop.html.twig');
     }
 }
