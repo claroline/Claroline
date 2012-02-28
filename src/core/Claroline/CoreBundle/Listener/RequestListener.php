d54eeb99dd9668659fa1e405dfeb87b222aa6557
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-02-28 15:53:43 +0100

    platform settings form is now working and tested

---
+++ src/core/Claroline/CoreBundle/Listener/RequestListener.php
@@ -0,0 +1,22 @@
+<?php
+namespace Claroline\CoreBundle\Listener;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class RequestListener
+{
+    protected $session;
+    protected $configHandler;
+    
+    public function __construct($session, $configHandler)
+    {
+        $this->session=$session;
+        $this->configHandler=$configHandler;
+    }
+    
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->session->setLocale($this->configHandler->getParameter('locale_language'));      
+    }
+}
