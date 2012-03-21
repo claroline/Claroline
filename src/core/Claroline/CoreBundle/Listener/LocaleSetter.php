ae0e419cb85c23e23754763d9c49b2a2bc6be00b
Stéphane Klein <stephaneklein221@gmail.com>
2012-03-21 11:42:13 +0100

    Renaming RequestListener to LocaleSetter

--- src/core/Claroline/CoreBundle/Listener/LocaleSetter.php
+++ src/core/Claroline/CoreBundle/Listener/LocaleSetter.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace Claroline\CoreBundle\Listener;
 
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
-class RequestListener
+class LocaleSetter
 {
     protected $session;
     protected $configHandler;
     
     public function __construct($session, $configHandler)
     {
-        $this->session=$session;
-        $this->configHandler=$configHandler;
+        $this->session = $session;
+        $this->configHandler = $configHandler;
     }
     
     public function onKernelRequest(GetResponseEvent $event)
