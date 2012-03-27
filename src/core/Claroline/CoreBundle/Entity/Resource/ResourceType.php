21a23b8efd035ee7dbf3737f37190cde878dea3b
Stéphane Klein <stephaneklein221@gmail.com>
2012-03-27 14:55:33 +0200

    Allowing plugins to declare specific resource types

--- src/core/Claroline/CoreBundle/Entity/Resource/ResourceType.php
+++ src/core/Claroline/CoreBundle/Entity/Resource/ResourceType.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace Claroline\CoreBundle\Entity\Resource;
 
-use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\Common\Collections\ArrayCollection;
+use Claroline\CoreBundle\Entity\Plugin;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Claroline\CoreBundle\Repository\ResourceTypeRepository")
  * @ORM\Table(name="claro_resource_type")
  */
 class ResourceType
@@ -45,14 +45,20 @@ class ResourceType
     private $resources;
     
     /**
-     * @ORM\Column(type="boolean")
+     * @ORM\Column(type="boolean", name="is_navigable")
      */
     private $isNavigable;
     
     /**
-     * @ORM\Column(type="boolean")
+     * @ORM\Column(type="boolean", name="is_listable")
      */
     private $isListable;
+    
+    /**
+     * @ORM\OneToOne(targetEntity="Claroline\CoreBundle\Entity\Plugin")
+     * @ORM\JoinColumn(name="plugin_id", referencedColumnName="id")
+     */
+    private $plugin;
         
     public function __construct()
     {
@@ -133,5 +139,15 @@ class ResourceType
     public function getListable()
     {
         return $this->isListable;
+    }
+    
+    public function setPlugin(Plugin $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+    
+    public function getPlugin()
+    {
+        return $this->plugin;
     }
 }
---
+++ src/core/Claroline/CoreBundle/Repository/ResourceTypeRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Claroline\CoreBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ResourceTypeRepository extends EntityRepository
+{
+    public function findPluginResourceTypes()
+    {
+        $dql = "
+            SELECT rt FROM Claroline\CoreBundle\Entity\Resource\ResourceType rt
+            WHERE rt.plugin IS NOT NULL
+        ";
+            
+        $query = $this->_em->createQuery($dql);
+            
+        return $query->getResult();       
+    }
+    
+    public function findPluginResourceTypeFqcns()
+    {
+        $sql = 'SELECT type FROM claro_resource_type WHERE plugin_id IS NOT NULL';
+        
+        return $this->_em
+            ->getConnection()
+            ->query($sql)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
