ae0f742495ea3246fa90bb361df4c83f8e4a1f6a
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-05-08 16:31:05 +0200

    pluginDependencyInjection class

--- src/plugin/extension/VendorX/ResourceXBundle/DependencyInjection/VendorXResourceXExtension.php
+++ src/plugin/extension/VendorX/ResourceXBundle/DependencyInjection/VendorXResourceXExtension.php
@@ -6,8 +6,9 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Claroline\CoreBundle\Library\Plugin\PluginDependencyInjection;
 
-class VendorXResourceXExtension extends Extension
+class VendorXResourceXExtension extends PluginDependencyInjection
 {
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -15,15 +16,6 @@ class VendorXResourceXExtension extends Extension
         $loader = new YamlFileLoader($container, $locator);
         $loader->load('services.yml');
         
-        $taggedServices = $container->findTaggedServiceIds("resource.manager");
-        
-        $serviceArray = $container->getParameter('resource.service.list');
-        
-        $names = array_keys($taggedServices);
-        
-        foreach($names as $name)
-        {
-            $serviceArray[$name]='';
-        }  
+        parent::setUp($container);
     }
 }
