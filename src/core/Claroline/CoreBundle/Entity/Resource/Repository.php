70b7d0e6b709584ca75a1b36a91dd2cbc546c1bb
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-05-11 09:19:40 +0200

    Repository class

---
+++ src/core/Claroline/CoreBundle/Entity/Resource/Repository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Claroline\CoreBundle\Entity\Resource;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="claro_repository")
+ */
+class Repository
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\ManyToMany(
+     *      targetEntity="Claroline\CoreBundle\Entity\Resource\AbstractResource", 
+     *      cascade={"persist"}
+     * )
+     * @ORM\JoinTable(name="claro_resource_repository",
+     *      joinColumns={@ORM\JoinColumn(name="repository_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="resource_id", referencedColumnName="id")}
+     * )
+     */
+    protected $resources;
+    
+    public function __construct()
+    {
+        $this->resources = new ArrayCollection();
+    }
+    
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getResources()
+    {
+        return $this->resources;
+    }
+    
+    public function addResource(AbstractResource $resource)
+    {
+        $this->$resource->add($resource);
+        $resource->getRepositories()->add($this);
+    }
+    
+    public function removeResource(AbstractResource $resource)
+    {
+        $this->$resource->removeElement($resource);
+        $resource->removeElement($this);
+    }
+}
