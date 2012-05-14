29004b6a90f22b173e7d30d04f52ddce8ab7cf2f
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-05-14 11:18:22 +0200

    checkstyle

--- src/core/Claroline/CoreBundle/Tests/DataFixtures/Additional/LoadFileData.php
+++ src/core/Claroline/CoreBundle/Tests/DataFixtures/Additional/LoadFileData.php
@@ -94,7 +94,8 @@ class LoadFileData extends AbstractFixture implements ContainerAwareInterface, O
 
         return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535),
             mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535),
-            mt_rand(0, 65535), mt_rand(0, 65535));
+            mt_rand(0, 65535), mt_rand(0, 65535)
+        );
     }
     
     public function getOrder()
--- src/plugin/extension/Claroline/HTMLPageBundle/Manager/HTMLPageManager.php
+++ src/plugin/extension/Claroline/HTMLPageBundle/Manager/HTMLPageManager.php
@@ -127,7 +127,8 @@ class HTMLPageManager implements ResourceInterface
 
         return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535),
             mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535),
-            mt_rand(0, 65535), mt_rand(0, 65535));
+            mt_rand(0, 65535), mt_rand(0, 65535)
+        );
     } 
     
     private function emptyDir($dir)
