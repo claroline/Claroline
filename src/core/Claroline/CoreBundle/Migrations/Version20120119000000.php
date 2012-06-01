2a4c3de0de70e4cb28bf8a5a10371ded165ca6a6
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-06-01 11:17:34 +0200

    set sharable to false by default

--- src/core/Claroline/CoreBundle/Migrations/Version20120119000000.php
+++ src/core/Claroline/CoreBundle/Migrations/Version20120119000000.php
@@ -214,7 +214,7 @@ class Version20120119000000 extends BundleMigration
         $table->addColumn('name', 'string', array('notnull' => false));
         $table->addColumn('count_instance', 'integer', array('notnull' => false));
         $table->addColumn('license_id', 'integer', array('notnull' => false));
-        $table->addColumn('is_sharable', 'boolean', array('notnull' => true));
+        $table->addColumn('is_sharable', 'boolean', array('notnull' => false));
         $this->addDiscriminator($table);
         
         $table->addForeignKeyConstraint(
