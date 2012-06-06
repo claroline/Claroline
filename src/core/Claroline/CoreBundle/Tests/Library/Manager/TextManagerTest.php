13a8ee58dd950d37dd7b7f74a48c92cef63df9f6
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-06-06 17:57:12 +0200

    testDefaultAction for textManager

--- src/core/Claroline/CoreBundle/Tests/Library/Manager/TextManagerTest.php
+++ src/core/Claroline/CoreBundle/Tests/Library/Manager/TextManagerTest.php
@@ -7,21 +7,38 @@ use Claroline\CoreBundle\Tests\DataFixtures\LoadResourceTypeData;
 
 class TextManagerTest extends FunctionalTestCase
 {
+    /** @ var EntityManager */
+    private $em;
+    
     protected function setUp()
     {
         parent::setUp();
         $this->loadUserFixture();
         $this->loadFixture(new LoadResourceTypeData());
         $this->client->followRedirects();
+        $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
     }
     
     public function testAdd()
     {
         $this->logUser($this->getFixtureReference('user/admin'));
-        $this->addText('HELLO WORLD');
-        $crawler = $this->client->request('GET', '/resource/directory');
-        
-        $this->assertEquals(1, $crawler->filter('.row_resource')->count());
+        $id = $this->addText('HELLO WORLD');
+        $crawler = $this->client->request('GET', '/resource/directory');        
+        $this->assertEquals(1, $crawler->filter('.row_resource')->count());        
+        $text = $this->em->getRepository('ClarolineCoreBundle:Resource\ResourceInstance')->find($id)->getResource();
+        $this->assertEquals('HELLO WORLD',$text->getLastRevision()->getContent());
+        $this->assertEquals(1, count($text));
+        $this->assertEquals(1, count($text->getRevisions()));
+        $this->assertEquals(1, count($text->getLastRevision()));
+    }
+    
+    public function testDefaultAction()
+    {
+        $this->logUser($this->getFixtureReference('user/admin'));
+        $id = $this->addText('HELLO WORLD');
+        $crawler = $this->client->request('GET', "/resource/click/text/{$id}");
+        $node = $crawler->filter('#content');
+        $this->assertTrue(strpos($node->text(), 'HELLO WORLD')!=false);
     }
     
     private function addText($txt)
@@ -33,7 +50,7 @@ class TextManagerTest extends FunctionalTestCase
         $form = $crawler->filter('input[type=submit]')->form();
         $crawler = $this->client->submit($form, array('text_form[text]' => $txt));
         $id = $crawler->filter(".row_resource")->last()->attr('data-resource_id');
-        
+
         return $id;
     }
 }
