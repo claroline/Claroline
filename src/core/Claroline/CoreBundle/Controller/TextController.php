35123cd321639129407b74ac04de440016aee1dc
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-06-06 13:56:57 +0200

    editAction for text (not complete yet)

---
+++ src/core/Claroline/CoreBundle/Controller/TextController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Claroline\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Claroline\CoreBundle\Form\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Claroline\CoreBundle\Entity\Resource\Text;
+
+class TextController extends Controller
+{
+    public function editAction($id)
+    {
+        $request = $this->get('request');
+        $user = $this->get('security.context')->getToken()->getUser();
+        $content = $request->request->get('content');
+        $em = $this->getDoctrine()->getEntityManager();
+        $old = $em->getRepository('ClarolineCoreBundle:Resource\Text')->find($id);
+        $version = $old->getVersion();
+        $text = new Text();
+        //$text->setUser($user);
+        $text->setText($content);
+        $text->setVersion(++$version);
+        $text->setName($old->getName());
+        $text->setSharable($old->isSharable());
+        $text->setLicense($old->getLicense());
+        $text->setParent($old);
+        $text->setResourceType($old->getResourceType());
+        $em->persist($text);
+        $em->flush();
+        
+        return new Response('edited');        
+    }
+}
