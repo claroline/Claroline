00557950d8209c4be4b7b4e9c354468b6b64bf2e
Stéphane Klein <stephaneklein221@gmail.com>
2012-06-15 10:01:06 +0200

    Code cleaning and refactoring (2 : DataFixtures dir)

--- src/core/Claroline/CoreBundle/DataFixtures/LoadLicensesData.php
+++ src/core/Claroline/CoreBundle/DataFixtures/LoadLicensesData.php
@@ -7,30 +7,32 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Claroline\CoreBundle\Entity\License;
 
+/**
+ * Licenses data fixture.
+ */
 class LoadLicensesData extends AbstractFixture implements OrderedFixtureInterface
 {
+    /**
+     * Loads three (dummy) license types.
+     *
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
-        $other = new License();
-        $other->setName("others");
-        
-        $dontKnow = new License();
-        $dontKnow->setName("don't know");
-        
-        $publicDomain = new License();
-        $publicDomain->setName("public domain");
-        
-        $allRightReserved = new License();
-        $allRightReserved->setName("all right reserved");
-        
-        $manager->persist($other);
-        $manager->persist($dontKnow);
-        $manager->persist($publicDomain);
-        $manager->persist($allRightReserved);
-        
-        $manager->flush();        
+        $licenseNames = array('public domain', 'all rights reserved', 'other');
+
+        foreach ($licenseNames as $licenseName) {
+            $license = new License();
+            $license->setName($licenseName);
+            $manager->persist($license);
+        }
+
+        $manager->flush();
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     public function getOrder()
     {
         return 3;
--- src/core/Claroline/CoreBundle/DataFixtures/LoadPlatformRolesData.php
+++ src/core/Claroline/CoreBundle/DataFixtures/LoadPlatformRolesData.php
@@ -8,31 +8,45 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Library\Security\PlatformRoles;
 
+/**
+ * Platform roles data fixture.
+ */
 class LoadPlatformRolesData extends AbstractFixture implements OrderedFixtureInterface
 {
+    /**
+     * Loads three roles used within the platform :
+     * - simple user
+     * - workspace creator
+     * - administrator
+     *
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
         $userRole = new Role();
         $userRole->setName(PlatformRoles::USER);
-        
+
         $creatorRole = new Role();
         $creatorRole->setName(PlatformRoles::WS_CREATOR);
         $creatorRole->setParent($userRole);
-        
+
         $adminRole = new Role();
         $adminRole->setName(PlatformRoles::ADMIN);
         $adminRole->setParent($creatorRole);
-        
+
         $manager->persist($userRole);
         $manager->persist($creatorRole);
         $manager->persist($adminRole);
         $manager->flush();
-        
+
         $this->addReference('role/user', $userRole);
         $this->addReference('role/ws_creator', $creatorRole);
         $this->addReference('role/admin', $adminRole);
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     public function getOrder()
     {
         return 1;
