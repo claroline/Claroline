544ca1250c0589e8dcaa7341380e176350ede2a5
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-06-18 08:53:18 +0200

    fix ResourceInstanceRepository getListableChildren method

--- src/core/Claroline/CoreBundle/Repository/ResourceInstanceRepository.php
+++ src/core/Claroline/CoreBundle/Repository/ResourceInstanceRepository.php
@@ -14,12 +14,12 @@ class ResourceInstanceRepository extends NestedTreeRepository
             WHERE re.lvl = 0
             AND re.workspace = {$ws->getId()}
             ";
-            
+
         $query = $this->_em->createQuery($dql);
-        
+
         return $query->getResult();
     }
-    
+
     public function getDirectoryDirectChildren($ri)
     {
        $dql = "
@@ -28,12 +28,12 @@ class ResourceInstanceRepository extends NestedTreeRepository
             WHERE rt.type = 'directory'
             AND ri.parent = {$ri->getId()}
         ";
-            
+
         $query = $this->_em->createQuery($dql);
-            
-        return $query->getResult();   
+
+        return $query->getResult();
     }
-    
+
     public function getNotDirectoryDirectChildren($ri)
     {
        $dql = "
@@ -42,26 +42,27 @@ class ResourceInstanceRepository extends NestedTreeRepository
             WHERE rt.type != 'directory'
             AND ri.parent = {$ri->getId()}
         ";
-            
+
         $query = $this->_em->createQuery($dql);
-            
-        return $query->getResult();   
+
+        return $query->getResult();
     }
-    
+
     public function getListableChildren($resourceInstance)
-    {        
+    {
         $dql = "
             SELECT ri FROM Claroline\CoreBundle\Entity\Resource\ResourceInstance ri
-            JOIN ri.parent par 
-            WHERE par.id = {$resourceInstance->getId()} 
-            AND ri.resourceType
+            JOIN ri.parent par
+            JOIN ri.abstractResource res
+            WHERE par.id = {$resourceInstance->getId()}
+            AND res.resourceType
             IN (SELECT rt FROM Claroline\CoreBundle\Entity\Resource\ResourceType rt
-                WHERE rt.isListable = 1) 
+                WHERE rt.isListable = 1)
             ";
-            
+
             $query = $this->_em->createQuery($dql);
-            
+
             return $query->getResult();
     }
-    
+
 }
