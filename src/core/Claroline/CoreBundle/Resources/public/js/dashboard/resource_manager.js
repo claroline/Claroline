8181ba9ca1cac7a7e0ab77d5cc89eb22cabd1dd6
Stéphane Klein <stephaneklein221@gmail.com>
2012-07-12 09:06:15 +0200

    Javascript coding conventions

--- src/core/Claroline/CoreBundle/Resources/public/js/dashboard/resource_manager.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/dashboard/resource_manager.js
@@ -5,10 +5,10 @@ $(function(){
         type: 'GET',
         url: Routing.generate('claro_resource_menus'),
         cache: false,
-        success: function(data){
+        success: function (data) {
             jsonmenu = JSON.parse(data);
         },
-        complete: function(){
+        complete: function () {
             createDivTree();
             createTree('#source_tree');
         }
@@ -24,9 +24,9 @@ $(function(){
     function createDivTree()
     {
         var content = ""
-        +"<div id='ct_form'></div><br>"
-        +"<div id='source_tree'></div>"
-        $('#ct_dialog').append(content)
+            +"<div id='ct_form'></div><br>"
+            +"<div id='source_tree'></div>";
+        $('#ct_dialog').append(content);
     }
 
     /**
@@ -42,18 +42,18 @@ $(function(){
                 url: Routing.generate('claro_resource_roots')
             },
             clickFolderMode: 1,
-            onLazyRead: function (node){
+            onLazyRead: function (node) {
                 node.appendAjax({
-                    url:Routing.generate('claro_resource_children', {
-                        'instanceId':node.data.key
+                    url: Routing.generate('claro_resource_children', {
+                        'instanceId': node.data.key
                     }),
-                    error: function (node, XMLHttpRequest, textStatus, errorThrown){
-                        if(XMLHttpRequest.status == 403){
-                            ClaroUtils.ajaxAuthenticationErrorHandler(function(){
+                    error: function (node, XMLHttpRequest, textStatus, errorThrown) {
+                        if (XMLHttpRequest.status == 403) {
+                            ClaroUtils.ajaxAuthenticationErrorHandler(function () {
                                 window.location.reload();
                             });
                         } else {
-                            alert("this node could not be loaded");
+                            alert('this node could not be loaded');
                         }
                     }
                 });
@@ -80,7 +80,6 @@ $(function(){
                 },
                 autoExpandMS: 1000,
                 preventVoidMoves: true,
-
                 onDragEnter: function (node, sourceNode) {
                     return true;
                 },
@@ -110,184 +109,175 @@ $(function(){
             e.preventDefault();
             var option = ClaroUtils.getCheckedValue(document.forms['move_resource_form']['options']);
             var route = {}
-            if('move' == option){
+            if ('move' == option) {
                 route = {
                     'name': 'claro_resource_move',
-                    'parameters':{
+                    'parameters': {
                         'instanceId': sourceNode.data.key,
                         'newParentId': node.data.key
-                        }
-                    };
-            ClaroUtils.sendRequest(route);
-            sourceNode.move(node, hitMode);
-            $('#ct_form').empty();
-        } else {
-            route = {
-                'name': 'claro_resource_add_workspace',
-                'parameters':{
-                    'instanceId': sourceNode.data.key,
-                    'instanceDestinationId': node.data.key,
-                    'options': option
+                    }
+                };
+                ClaroUtils.sendRequest(route);
+                sourceNode.move(node, hitMode);
+                $('#ct_form').empty();
+            } else {
+                route = {
+                    'name': 'claro_resource_add_workspace',
+                    'parameters': {
+                        'instanceId': sourceNode.data.key,
+                        'instanceDestinationId': node.data.key,
+                        'options': option
+                    }
                 }
-
+                ClaroUtils.sendRequest(route);
+                var newNode = {
+                    title: sourceNode.data.title,
+                    key: sourceNode.data.key,
+                    copy: sourceNode.data.copy,
+                    instanceCount: sourceNode.data.instanceCount,
+                    shareType: sourceNode.data.shareType,
+                    resourceId: sourceNode.data.resourceId
+                }
+                node.addChild(newNode);
+                $('#ct_form').empty();
             }
-        ClaroUtils.sendRequest(route);
-
-            var newNode = {
-                title: sourceNode.data.title,
-                key: sourceNode.data.key,
-                copy: sourceNode.data.copy,
-                instanceCount: sourceNode.data.instanceCount,
-                shareType: sourceNode.data.shareType,
-                resourceId: sourceNode.data.resourceId
-            }
-
-            node.addChild(newNode);
-            $('#ct_form').empty();
-        }
-    });
-}
-
-/**
-* Executes the desired action for a menu item.
-*
-* @param {Object} obj
-* @param {Object} node
-*/
-function executeMenuActions(obj, node)
-{
-    var submissionHandler = function(xhr){
-        if(xhr.getResponseHeader('Content-Type') == 'application/json'){
-            var JSONObject = JSON.parse(xhr.responseText);
-            var instance = JSONObject[0];
-            var newNode = {
-                title:instance.title,
-                key:instance.key,
-                copy:instance.copy,
-                instanceCount:instance.instanceCount,
-                shareType:instance.shareType,
-                resourceId:instance.resourceId
-            }
-
-            if (instance.type == 'directory'){
-                newNode.isFolder = true;
-            }
-
-            if(node.data.key != newNode.key){
-                node.appendAjax({
-                    url:Routing.generate('claro_resource_children', {
-                        'instanceId':node.data.key
-                    })
-                });
-                node.expand();
-            }
-            else{
-                node.data.title = newNode.title;
-                node.data.shareType = newNode.shareType;
-                node.render();
-            }
-
-            $('#ct_tree').show();
-            $('#ct_form').empty();
-        } else {
-            $('#ct_form').empty();
-            $('#ct_form').append(xhr.responseText);
-            $('#ct_form').find('form').submit(function (e) {
-                e.preventDefault();
-                var action = $('#ct_form').find('form').attr('action');
-                action = action.replace('_instanceId', node.data.key);
-                action = action.replace('_resourceId', node.data.resourceId);
-                var id = $('#ct_form').find('form').attr('id');
-                ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
-            })
-        }
+        });
     }
 
-    var executeAsync = function (obj, node, route) {
-
-        var removeNode = function(){
-            ClaroUtils.sendRequest(route, function(data, textStatus, jqXHR){
-                if(204 == jqXHR.status){
-                    node.remove();
+    /**
+     * Executes the desired action for a menu item.
+     *
+     * @param {Object} obj
+     * @param {Object} node
+     */
+    function executeMenuActions(obj, node)
+    {
+        var submissionHandler = function(xhr){
+            if (xhr.getResponseHeader('Content-Type') == 'application/json') {
+                var JSONObject = JSON.parse(xhr.responseText);
+                var instance = JSONObject[0];
+                var newNode = {
+                    title: instance.title,
+                    key: instance.key,
+                    copy: instance.copy,
+                    instanceCount: instance.instanceCount,
+                    shareType: instance.shareType,
+                    resourceId: instance.resourceId
                 }
-            })
-        }
 
-        var executeRequest = function(){
-            ClaroUtils.sendRequest(route, function(data){
-                $('#ct_tree').hide();
-                $('#ct_form').append(data);
-                $('#ct_form').find('form').submit(function(e){
+                if (instance.type == 'directory') {
+                    newNode.isFolder = true;
+                }
+
+                if (node.data.key != newNode.key) {
+                    node.appendAjax({
+                        url:Routing.generate('claro_resource_children', {
+                            'instanceId':node.data.key
+                        })
+                    });
+                    node.expand();
+                } else {
+                    node.data.title = newNode.title;
+                    node.data.shareType = newNode.shareType;
+                    node.render();
+                }
+
+                $('#ct_tree').show();
+                $('#ct_form').empty();
+            } else {
+                $('#ct_form').empty();
+                $('#ct_form').append(xhr.responseText);
+                $('#ct_form').find('form').submit(function (e) {
                     e.preventDefault();
                     var action = $('#ct_form').find('form').attr('action');
                     action = action.replace('_instanceId', node.data.key);
+                    action = action.replace('_resourceId', node.data.resourceId);
                     var id = $('#ct_form').find('form').attr('id');
                     ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
-                })
-            })
+                });
+            }
         }
 
-        switch(obj.name) {
-            case "delete":
-                removeNode(node, route);
-                break;
-            default:
-                executeRequest (node, route);
-                break;
+        var executeAsync = function (obj, node, route) {
+            var removeNode = function () {
+                ClaroUtils.sendRequest(route, function (data, textStatus, jqXHR) {
+                    if (204 == jqXHR.status) {
+                        node.remove();
+                    }
+                });
+            }
+
+            var executeRequest = function () {
+                ClaroUtils.sendRequest(route, function (data) {
+                    $('#ct_tree').hide();
+                    $('#ct_form').append(data);
+                    $('#ct_form').find('form').submit(function (e) {
+                        e.preventDefault();
+                        var action = $('#ct_form').find('form').attr('action');
+                        action = action.replace('_instanceId', node.data.key);
+                        var id = $('#ct_form').find('form').attr('id');
+                        ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
+                    });
+                })
+            }
+
+            switch (obj.name) {
+                case 'delete':
+                    removeNode(node, route);
+                    break;
+                default:
+                    executeRequest (node, route);
+                    break;
+            }
         }
+
+        var route = obj.route;
+        var compiledRoute = route.replace('_instanceId', node.data.key);
+        compiledRoute = compiledRoute.replace('_resourceId', node.data.resourceId);
+        obj.async ? executeAsync(obj, node, compiledRoute): window.location = compiledRoute;
     }
 
-    var route = obj.route;
-    var compiledRoute = route.replace('_instanceId', node.data.key);
-    compiledRoute = compiledRoute.replace('_resourceId', node.data.resourceId);
-    (obj.async) ? executeAsync(obj, node, compiledRoute): window.location = compiledRoute;
-}
-
-/**
-* Finds wich menu object was clicked on in the menu description.
-*
-* @param {Object} items the menu description.
-* @param {Object} node the target node.
-* @param {string} menuItem the menuItem name.
-*/
-function findMenuObject(items, node, menuItem)
-{
-    for (var property in items.items){
-        if(property == menuItem){
-            executeMenuActions(items.items[property], node);
-        } else {
-            if (items.items[property].hasOwnProperty('items')){
-                findMenuObject(items.items[property], node, menuItem);
+    /**
+     * Finds wich menu object was clicked on in the menu description.
+     *
+     * @param {Object} items the menu description.
+     * @param {Object} node the target node.
+     * @param {string} menuItem the menuItem name.
+     */
+    function findMenuObject(items, node, menuItem)
+    {
+        for (var property in items.items) {
+            if (property == menuItem) {
+                executeMenuActions(items.items[property], node);
+            } else {
+                if (items.items[property].hasOwnProperty('items')) {
+                    findMenuObject(items.items[property], node, menuItem);
+                }
             }
         }
     }
-}
 
-/**
-* Creates the context menu for a specific node
-*/
-function bindContextMenuTree(node)
-{
-    var type = node.data.type;
-
-    var menuDefaultOptions =
+    /**
+     * Creates the context menu for a specific node
+     */
+    function bindContextMenuTree(node)
     {
-        selector: '#node_'+node.data.key,
-        callback: function(key, options){
-            findMenuObject(jsonmenu[type], node, key);
-        }
-    }
+        var type = node.data.type;
 
-    menuDefaultOptions.items = jsonmenu[type].items;
-    $.contextMenu(menuDefaultOptions);
-    var additionalMenuOptions = $.extend(
-        menuDefaultOptions,
-        {
-            selector: '#dynatree-custom-claro-menu-'+node.data.key,
+        var menuDefaultOptions = {
+            selector: '#node_'+ node.data.key,
+            callback: function (key, options) {
+                findMenuObject(jsonmenu[type], node, key);
+            }
+        }
+
+        menuDefaultOptions.items = jsonmenu[type].items;
+        $.contextMenu(menuDefaultOptions);
+        var additionalMenuOptions = $.extend(menuDefaultOptions, {
+            selector: '#dynatree-custom-claro-menu-' + node.data.key,
             trigger: 'left'
-        }
-        );
+        });
 
-    $.contextMenu(additionalMenuOptions);
-}
+        $.contextMenu(additionalMenuOptions);
+    }
 });
