25c1b569ab077907e2b2f94b6677a8bf8e67148d
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-07-17 09:24:32 +0200

    fix a bug due to a missing '#' and a missing 'root' property

--- src/core/Claroline/CoreBundle/Resources/public/js/workspace/workspace_show.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/workspace/workspace_show.js
@@ -205,7 +205,7 @@ $(function(){
                                 e.preventDefault();
                                 var action = $('#ws_form').find('form').attr('action');
                                 action = action.replace('_instanceId', node.data.key);
-                                var id = $('ws_form').find('form').attr('id');
+                                var id = $('#ws_form').find('form').attr('id');
                                 ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
                             });
                         })
