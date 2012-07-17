ef505b7c6b1751f57dd253930d0780027bf9afbc
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-07-17 11:26:32 +0200

    js resource tooltip

--- src/core/Claroline/CoreBundle/Resources/public/js/workspace/workspace_show.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/workspace/workspace_show.js
@@ -104,6 +104,7 @@ $(function(){
                     "id": resourceTypes[i].id,
                     "key": resourceTypes[i].type,
                     "title": resourceTypes[i].type,
+                    "tooltip":  resourceTypes[i].type,
                     "shareType": 1,
                     "type": "resourceType",
                     "isFolder": true,
@@ -271,7 +272,7 @@ $(function(){
                     node.activate();
             },
             onCustomRender: function (node) {
-                var html = "<a id='ws_node_"+node.data.key+"' class='ws_dynatree-title' style='cursor:pointer;' href='#'> "+node.data.title+" share "+node.data.shareType+" </a>";
+                var html = "<a id='node_"+node.data.key+"' class='dynatree-title' style='cursor:pointer;' title='"+node.data.tooltip+"'href='#'> "+node.data.title+"</a>";
                 html += "<span class='ws_dynatree-custom-claro-menu' id='ws_dynatree-custom-claro-menu-"+node.data.key+"' style='cursor:pointer; color:blue;'> menu </span>";
                 return html;
             },
