c022e7cb9490384157fdd53447f5aa29a28f0e4e
Stéphane Klein <stephaneklein221@gmail.com>
2012-07-26 10:25:41 +0200

    Upgrading to Symfony 2.0.16

--- app/autoload.php
+++ app/autoload.php
@@ -57,10 +57,7 @@ $loader->registerNamespaceFallbacks(array(
 ));
 $loader->register();
 
-// Swiftmailer needs a special autoloader to allow
-// the lazy loading of the init file (which is expensive)
-require_once __DIR__.'/../vendor/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
+require __DIR__.'/../vendor/swiftmailer/lib/swift_required.php';
 
 AnnotationRegistry::registerLoader(function($class) use ($loader) {
     $loader->loadClass($class);
