8be2d8a069445e06f7490c560c49d2779e737b66
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-07-30 17:14:34 +0200

    some js cleaning

--- src/core/Claroline/CoreBundle/Resources/public/js/jquery/claroline/resource_manager.jquery.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/jquery/claroline/resource_manager.jquery.js
@@ -11,7 +11,7 @@ $(function(){
                 displayMode: 'classic',
                 checkbox: true,
                 resourcePickedHandler: function (resourceId){
-                    alert("DEFAULT SUBMIT HANDLER MUST BE CHANGED")
+                    alert("DEFAULT SUBMIT HANDLER MUST BE CHANGED");
                 }
             }, options);
             return this.each(function(){
@@ -22,12 +22,12 @@ $(function(){
                     Routing.generate('claro_resource_menus'),
                     function(data){
                         jsonmenu = JSON.parse(data);
-                        if('picker' == params.mode){
+                        if('picker' === params.mode){
                             for (var menu in jsonmenu) {
                                 delete jsonmenu[menu].items['delete'];
-                                delete jsonmenu[menu].items['properties'];
+                                delete jsonmenu[menu].items.properties;
                             }
-                            delete jsonmenu['directory'];
+                            delete jsonmenu.directory;
                         }
                     },
                     function(){
@@ -35,9 +35,8 @@ $(function(){
                             function(data){
                                 jsonroots = data;
                                 createDivTree(currentDiv);
-                                }
-                            )
-                        })
+                            });
+                        });
 
                 function createDivTree(div) {
                     ClaroUtils.sendRequest(Routing.generate('claro_resource_types'),
@@ -47,51 +46,51 @@ $(function(){
                                 workspaceroots: jsonroots
                             });
                             div.append(content);
-                            if (true == params.checkbox) {
+                            if (true === params.checkbox) {
                                 $('#ct_download').live('click', function(){
                                     var children = $('#source_tree').dynatree('getTree').getSelectedNodes();
                                     var parameters = {};
                                     for (var i in children) {
-                                        if(children[i].isTool != true) {
+                                        if(children[i].isTool !== true) {
                                             parameters[i] = children[i].data.instanceId;
-                                            if(children[i].getParent().data.isTool == true) {
+                                            if(children[i].getParent().data.isTool === true) {
                                                 parameters[i] = children[i].getParent().data.type+'_'+children[i].data.instanceId;
                                             }
                                         }
                                     }
-                                    parameters['type'] = params.displayMode;
+                                    parameters.type = params.displayMode;
                                     window.location = Routing.generate('claro_multi_export', parameters);
-                                })
+                                });
                             }
 
 
                             $('#ct_switch_mode').click(function(){
-                                (params.displayMode == 'classic') ? params.displayMode = 'linker': params.displayMode = 'classic';
-                                (params.displayMode == 'classic') ? params.checkbox = true: params.checkbox = false;
+                                (params.displayMode === 'classic') ? params.displayMode = 'linker': params.displayMode = 'classic';
+                                (params.displayMode === 'classic') ? params.checkbox = true: params.checkbox = false;
                                 $('#source_tree').dynatree('destroy');
                                 $('#source_tree').empty();
                                 createTree('#source_tree');
                             });
 
                             $('#ct_filter').click(function(){
-                                if (params.displayMode == 'classic') {
-                                    if ($('#select_root').val() != null){
+                                if (params.displayMode === 'classic') {
+                                    if ($('#select_root').val() !== null){
                                         filterNodes('workspaceId', $('#select_root').val());
                                     }
-                                    if ($('#select_type').val() != null) {
+                                    if ($('#select_type').val() !== null) {
                                         filterNodes('type', $('#select_type').val());
                                     }
                                     //it's obviously just a test and isn't working'
                                     //how to manage date ?
-                                    if ($('#rf_date_from').val() != null) {
+                                    if ($('#rf_date_from').val() !== null) {
                                         alert('this is a test and it\'s not working');
                                         filterNodes('dateCreation', $('#rf_date_from').val());
 
                                     }
                                 }
 
-                                if (params.displayMode == 'linker') {
-                                    if ($('#select_type').val() != null) {
+                                if (params.displayMode === 'linker') {
+                                    if ($('#select_type').val() !== null) {
                                         filterNodes('type', $('#select_type').val());
                                     }
 
@@ -116,7 +115,7 @@ $(function(){
                             if (node.data[filter].indexOf(searchArray) >= 0) {
                                 node.visitParents(function(node) {
                                     $(node.li).show();
-                                    return (node.parent != null);
+                                    return (node.parent !== null);
                                 }, true);
                                 return 'skip';
                             } else {
@@ -124,27 +123,27 @@ $(function(){
                             }
                         }
                     });
-                }
+                };
 
                 function createTree(treeId)
                 {
                     var initChildren = function (){
 
                         var children = {};
-                        if (params.displayMode == 'classic') {
+                        if (params.displayMode === 'classic') {
                             children = eval(jsonroots);
                         } else {
                             children = [];
                         }
                         return children;
-                    }
+                    };
 
                     var onLazyReadUrl = function(node){
                         var url = '';
-                        (params.displayMode == 'classic') ? url = Routing.generate('claro_resource_children', {'instanceId': node.data.instanceId})
+                        (params.displayMode === 'classic') ? url = Routing.generate('claro_resource_children', {'instanceId': node.data.instanceId})
                         : url = Routing.generate('claro_resources_list', {'resourceTypeId':node.parent.data.id, 'rootId':node.data.instanceId});
                         return url;
-                    }
+                    };
 
                     var initLinker = function(){
                         ClaroUtils.sendRequest(Routing.generate('claro_resource_types'), function(data){
@@ -162,7 +161,7 @@ $(function(){
                                     "isFolder": true,
                                     "isLazy": false,
                                     "isTool": true
-                                }
+                                };
                                 root.addChild(node);
 
                                 var rootChildren = eval(jsonroots);
@@ -173,7 +172,7 @@ $(function(){
                                     bindContextMenuTree(rootChildren[k]);
                                }
                             }
-                        })
+                        });
                     };
 
                     function dropNode(node, sourceNode, hitMode)
@@ -181,10 +180,10 @@ $(function(){
                         $('#ct_form').empty().append(moveForm);
                         $('#move_resource_form_submit').click(function (e) {
                             e.preventDefault();
-                            var option = ClaroUtils.getCheckedValue(document.forms['move_resource_form']['options']);
-                            var route = {}
+                            var option = ClaroUtils.getCheckedValue(document.forms.move_resource_form.options);
+                            var route = {};
 
-                            if ('move' == option) {
+                            if ('move' === option) {
                                 route = {
                                     'name': 'claro_resource_move',
                                     'parameters': {
@@ -202,7 +201,7 @@ $(function(){
                                         'resourceId': sourceNode.data.resourceId,
                                         'instanceDestinationId': node.data.instanceId
                                     }
-                                }
+                                };
                                 ClaroUtils.sendRequest(route);
                                 var newNode = {
                                     title: sourceNode.data.title,
@@ -214,7 +213,7 @@ $(function(){
                                     isFolder: sourceNode.data.isFolder,
                                     instanceId: sourceNode.data.instanceId,
                                     isLazy: true
-                                }
+                                };
                                 node.addChild(newNode);
                                 $('#ct_form').empty();
                             }
@@ -228,7 +227,7 @@ $(function(){
                             callback: function (key, options) {
                                 findMenuObject(jsonmenu[type], node, key);
                             }
-                        }
+                        };
 
                         menuDefaultOptions.items = jsonmenu[type].items;
                         $.contextMenu(menuDefaultOptions);
@@ -242,7 +241,7 @@ $(function(){
                         var executeMenuActions = function(obj, node)
                         {
                             var submissionHandler = function(xhr){
-                                if (xhr.getResponseHeader('Content-Type') == 'application/json') {
+                                if (xhr.getResponseHeader('Content-Type') === 'application/json') {
                                     var JSONObject = JSON.parse(xhr.responseText);
                                     var instance = JSONObject[0];
                                     var newNode = {
@@ -252,14 +251,14 @@ $(function(){
                                         instanceCount: instance.instanceCount,
                                         shareType: instance.shareType,
                                         resourceId: instance.resourceId
-                                    }
+                                    };
 
-                                    if (instance.type == 'directory') {
+                                    if (instance.type === 'directory') {
                                         newNode.isFolder = true;
                                     }
 
-                                    if (node.data.instanceId != newNode.instanceId) {
-                                        node.appendAjax({url: onLazyReadUrl(node)})
+                                    if (node.data.instanceId !== newNode.instanceId) {
+                                        node.appendAjax({url: onLazyReadUrl(node)});
                                         node.expand();
                                     } else {
                                         node.data.title = newNode.title;
@@ -278,21 +277,21 @@ $(function(){
                                         ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
                                     });
                                 }
-                            }
+                            };
 
                             var executeAsync = function (obj, node, route) {
                                 var removeNode = function () {
                                     ClaroUtils.sendRequest(route, function (data, textStatus, jqXHR) {
-                                        if (204 == jqXHR.status) {
+                                        if (204 === jqXHR.status) {
                                             node.remove();
                                         }
                                     });
-                                }
+                                };
 
                                 var executeRequest = function () {
                                     ClaroUtils.sendRequest(route, function (data) {
                                         $('#ct_tree').hide();
-                                        if(node.data.isTool == true){
+                                        if(node.data.isTool === true){
                                             $('#modal-body').append(Twig.render(select_workspace_form));
                                             $('#modal-body').append('he');
                                             $('#bootstrap-modal').modal('show');
@@ -304,29 +303,22 @@ $(function(){
                                             var id = $('#ct_form').find('form').attr('id');
                                             ClaroUtils.sendForm(action, document.getElementById(id), submissionHandler);
                                         });
-                                    })
-                                }
+                                    });
+                                };
 
-                                switch (obj.name) {
-                                    case 'delete':
-                                        removeNode(node, route);
-                                        break;
-                                    default:
-                                        executeRequest (node, route);
-                                        break;
-                                }
-                            }
+                                (obj.name === 'delete') ? removeNode(node, route): executeRequest (node, route);
+                            };
 
                             var route = obj.route;
                             var compiledRoute = route.replace('_instanceId', node.data.instanceId);
                             compiledRoute = compiledRoute.replace('_resourceId', node.data.resourceId);
                             obj.async ? executeAsync(obj, node, compiledRoute): window.location = compiledRoute;
-                        }
+                        };
 
                         var findMenuObject = function(items, node, menuItem)
                         {
                             for (var property in items.items) {
-                                if (property == menuItem) {
+                                if (property === menuItem) {
                                     executeMenuActions(items.items[property], node);
                                 } else {
                                     if (items.items[property].hasOwnProperty('items')) {
@@ -334,8 +326,8 @@ $(function(){
                                     }
                                 }
                             }
-                        }
-                    }
+                        };
+                    };
 
                     var children = initChildren();
 
@@ -345,7 +337,7 @@ $(function(){
                         title: 'myTree',
                         children: children,
                         onPostInit: function(isReloading, isError){
-                            if(params.displayMode == 'linker'){
+                            if(params.displayMode === 'linker'){
                                 initLinker(treeId);
                             }
                         },
@@ -364,7 +356,7 @@ $(function(){
                                     }
                                 },
                                 error: function (node, XMLHttpRequest, textStatus, errorThrown) {
-                                    if (XMLHttpRequest.status == 403) {
+                                    if (XMLHttpRequest.status === 403) {
                                         ClaroUtils.ajaxAuthenticationErrorHandler(function () {
                                             window.location.reload();
                                         });
@@ -376,14 +368,14 @@ $(function(){
                         },
                         onCreate: function (node, span) {
                             if (node.data.hasOwnProperty('type')) {
-                                if(undefined != jsonmenu[node.data.type]){
+                                if(undefined !== jsonmenu[node.data.type]){
                                     bindContextMenuTree(node);
                                 }
                             }
                         },
                         onDblClick: function (node) {
-                            if (params.mode == 'picker' && node.data.type != 'resourceType'){
-                                (node.shareType == 0) ? alert("you can't share this resource"): params.resourcePickedHandler(node.data.resourceId);
+                            if (params.mode === 'picker' && node.data.type !== 'resourceType'){
+                                (node.shareType === 0) ? alert("you can't share this resource"): params.resourcePickedHandler(node.data.resourceId);
                             } else {
                                 node.expand();
                                 node.activate();
@@ -396,7 +388,7 @@ $(function(){
                         },
                         dnd: {
                             onDragStart: function (node) {
-                                if(params.mode == 'picker' || params.displayMode == 'linker'){
+                                if(params.mode === 'picker' || params.displayMode === 'linker'){
                                     return false;
                                 }
                             },
@@ -429,8 +421,8 @@ $(function(){
                     });
                 }
                 return(this);
-            })
+            });
         }
 
-    })
+    });
 });
