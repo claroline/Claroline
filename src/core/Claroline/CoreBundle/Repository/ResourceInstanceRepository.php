aed83fb535a3b6a48e246ccbbb0b530bce6787c2
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-08-09 16:54:23 +0200

    use sql instead of dql for the linker resource list and the childrenAction method

--- src/core/Claroline/CoreBundle/Repository/ResourceInstanceRepository.php
+++ src/core/Claroline/CoreBundle/Repository/ResourceInstanceRepository.php
@@ -91,17 +91,97 @@ class ResourceInstanceRepository extends NestedTreeRepository
 
     public function getChildrenInstanceList(ResourceInstance $resourceInstance, ResourceType $resourceType)
     {
-        $dql = "
-            SELECT ri FROM Claroline\CoreBundle\Entity\Resource\ResourceInstance ri
-            JOIN ri.abstractResource res
-            JOIN res.resourceType rt
-            WHERE rt.type = '{$resourceType->getType()}'
-            AND ri.lft > {$resourceInstance->getLft()}
+        $sql = "
+            SELECT
+            ri.id as id,
+            ri.name as name,
+            ri.created as created,
+            ri.updated as updated,
+            ri.lft as lft,
+            ri.lvl as lvl,
+            ri.rgt as rgt,
+            ri.root as root,
+            ri.parent_id as parent_id,
+            ri.workspace_id as workspace_id,
+            ri.resource_id as resource_id,
+            uri.id as instance_creator_id,
+            uri.username as instance_creator_username,
+            ures.id as resource_creator_id,
+            ures.username as resource_creator_username,
+            rt.id as resource_type_id,
+            rt.type as type,
+            rt.is_navigable as is_navigable,
+            rt.icon as icon
+            FROM claro_resource_instance ri
+            INNER JOIN  claro_user uri
+            ON uri.id = ri.user_id
+            INNER JOIN claro_resource res
+            ON res.id = ri.resource_id
+            INNER JOIN claro_resource_type rt
+            ON res.resource_type_id = rt.id
+            INNER JOIN claro_user ures
+            ON res.user_id = ures.id
+            WHERE ri.lft > {$resourceInstance->getLft()}
             AND ri.rgt < {$resourceInstance->getRgt()}
+            AND rt.type = '{$resourceType->getType()}'
             ";
 
-        $query = $this->_em->createQuery($dql);
+        $result = $this->_em
+            ->getConnection()
+            ->query($sql);
 
-        return $query->getResult();
+        $instances = array();
+
+        while ($row = $result->fetch()) {
+            $instances[$row['id']] = $row;
+        }
+
+        return $instances;
+    }
+
+    public function getChildrenNodes($parent, $resourceTypeId = 0, $isListable = true)
+    {
+        $sql = "
+            SELECT
+            ri.id as id,
+            ri.name as name,
+            ri.created as created,
+            ri.updated as updated,
+            ri.lft as lft,
+            ri.lvl as lvl,
+            ri.rgt as rgt,
+            ri.root as root,
+            ri.parent_id as parent_id,
+            ri.workspace_id as workspace_id,
+            ri.resource_id as resource_id,
+            uri.id as instance_creator_id,
+            uri.username as instance_creator_username,
+            ures.id as resource_creator_id,
+            ures.username as resource_creator_username,
+            rt.id as resource_type_id,
+            rt.type as type,
+            rt.is_navigable as is_navigable,
+            rt.icon as icon
+            FROM claro_resource_instance ri
+            INNER JOIN  claro_user uri
+            ON uri.id = ri.user_id
+            INNER JOIN claro_resource res
+            ON res.id = ri.resource_id
+            INNER JOIN claro_resource_type rt
+            ON res.resource_type_id = rt.id
+            INNER JOIN claro_user ures
+            ON res.user_id = ures.id
+            WHERE
+            ri.parent_id = {$parent->getId()}
+            AND rt.is_listable = {$isListable}
+            ";
+            if ($resourceTypeId != 0) {
+                $sql .= "AND rt.id = {$resourceTypeId}";
+            }
+
+        return $this->_em
+                ->getConnection()
+                ->query($sql)
+                ->fetchAll(\PDO::FETCH_ASSOC);
     }
 }
