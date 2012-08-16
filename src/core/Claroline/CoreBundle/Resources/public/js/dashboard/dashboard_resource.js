e8d208a1676630e4b8ab94511c978e95c54ccc2c
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-08-16 11:41:31 +0200

    menu actions added (except new)

--- src/core/Claroline/CoreBundle/Resources/public/js/dashboard/dashboard_resource.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/dashboard/dashboard_resource.js
@@ -16,5 +16,5 @@
         function(){resetCallBack(param)}
     );
 
-    ClaroResourceManager.init($('#dr_resources_content'), 'cr', $('#dr_resources_back'));
+    ClaroResourceManager.init($('#dr_resources_content'), 'cr', $('#dr_resources_back'), $('#dr_div_form'));
 })();
