5563ee01b8d9655e22bce6e6781d6c61b67c5b7b
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-08-22 09:10:36 +0200

    refactor filters names

--- src/core/Claroline/CoreBundle/Resources/public/js/resource/data_filter.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/resource/data_filter.js
@@ -12,52 +12,52 @@
     }
 
     function initOnChange() {
-        var divFiltersString = document.getElementById(buildPrefix+'_active_filters');
-        $('#'+buildPrefix+'_select_root').on('change', function(){
+        var divFiltersString = document.getElementById(buildPrefix+'-active-filters');
+        $('#'+buildPrefix+'-select-root').on('change', function(){
             divFiltersString.innerHTML = getActiveFilterString();
         })
-        $('#'+buildPrefix+'_select_type').on('change', function(){
+        $('#'+buildPrefix+'-select-type').on('change', function(){
             divFiltersString.innerHTML = getActiveFilterString();
         })
-        $('#'+buildPrefix+'_date_from').on('change', function(){
+        $('#'+buildPrefix+'-date-from').on('change', function(){
             divFiltersString.innerHTML = getActiveFilterString();
         })
-        $('#'+buildPrefix+'_date_to').on('change', function(){
+        $('#'+buildPrefix+'-date-to').on('change', function(){
             divFiltersString.innerHTML = getActiveFilterString();
         })
     }
 
     function getActiveFilterString() {
-        return $('#'+buildPrefix+'_select_root').val()+','
-            +$('#'+buildPrefix+'_select_type').val()+','
-            +$('#'+buildPrefix+'_date_from').val()+','
-            + $('#'+buildPrefix+'_date_to').val();
+        return $('#'+buildPrefix+'-select-root').val()+','
+            +$('#'+buildPrefix+'-select-type').val()+','
+            +$('#'+buildPrefix+'-date-from').val()+','
+            + $('#'+buildPrefix+'-date-to').val();
     }
 
     function createFilterRoute() {
         var parameters = {};
         var i = 0;
 
-        var values = $('#'+buildPrefix+'_select_type').val();
+        var values = $('#'+buildPrefix+'-select-type').val();
         if (values != undefined){
             for(i=0; i< values.length; i++) {
                 parameters['types'+i] = values[i];
             }
         }
 
-        values = $('#'+buildPrefix+'_select_root').val();
+        values = $('#'+buildPrefix+'-select-root').val();
         if (values != undefined){
             for(i=0; i< values.length; i++) {
                 parameters['roots'+i] = values[i];
             }
         }
 
-        if ($('#'+buildPrefix+'_date_from').val()!= '') {
-            parameters['dateFrom'] = $('#'+buildPrefix+'_date_from').val();
+        if ($('#'+buildPrefix+'-date-from').val()!= '') {
+            parameters['dateFrom'] = $('#'+buildPrefix+'-date-from').val();
         }
 
-        if($('#'+buildPrefix+'_date_to').val()!= '') {
-            parameters['dateTo'] = $('#'+buildPrefix+'_date_to').val();
+        if($('#'+buildPrefix+'-date-to').val()!= '') {
+            parameters['dateTo'] = $('#'+buildPrefix+'-date-to').val();
         }
 
         return Routing.generate('claro_resource_filter', parameters);
@@ -67,7 +67,7 @@
         buildPrefix = prefix;
         buildFilter(div);
 
-        $('#'+buildPrefix+'_filter_button').live('click', function(){
+        $('#'+buildPrefix+'-filter-button').live('click', function(){
 
             var route = createFilterRoute();
             ClaroUtils.sendRequest(route, function(data){
@@ -75,17 +75,17 @@
             })
         //            callBackFilter(filter.getActiveFilters());
         })
-        $('#'+buildPrefix+'_reset_button').live('click', function(){
+        $('#'+buildPrefix+'-reset-button').live('click', function(){
             callBackReset();
         })
     }
 
     filter.getActiveFilters = function(){
         var activeFilters = {};
-        activeFilters.root = $('#select_root').val();
-        activeFilters.type = $('#select_type').val();
-        activeFilters.dateSubmissionTo = $('#rf_date_from').val();
-        activeFilters.dateSubmissionFrom = $('#rf_date_to').val();
+        activeFilters.root = $('#select-root').val();
+        activeFilters.type = $('#select-type').val();
+        activeFilters.dateSubmissionTo = $('#rf-date-from').val();
+        activeFilters.dateSubmissionFrom = $('#rf-date-to').val();
 
         return activeFilters;
     }
