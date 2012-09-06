4c7dd1de70cd8ce8a7d9c7b3bfb4fd0db6939dfa
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-09-06 13:44:10 +0200

    responsive pager

--- src/core/Claroline/CoreBundle/Resources/public/js/administration/user_list.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/administration/user_list.js
@@ -1,28 +1,48 @@
 (function () {
     var activePagerItem = 1;
+    var pager = null;
 
-    ClaroUtils.renderPager(document.getElementById('twig-attributes').getAttribute('data-pages'), 1, 'user', $('#user-table'));
+    renderUsers(1);
 
-    $('.user-paginator-item').on('click', function(e){
-        activePagerItem = e.target.innerHTML;
-        renderUsers();
-    });
-
-    $('.user-paginator-next-item').on('click', function(e){
-        activePagerItem++;
-        renderUsers();
-    })
-
-    $('.user-paginator-prev-item').on('click', function(e){
-        activePagerItem--;
-        renderUsers();
-    })
-
-    function renderUsers() {
-        var route = Routing.generate('claro_admin_paginated_user_list', {'page' : activePagerItem, 'format': 'html'});
+    function renderUsers(activePagerItem) {
+        var route = Routing.generate('claro_admin_paginated_user_list', {
+            'page' : activePagerItem,
+            'format': 'html'
+        });
         ClaroUtils.sendRequest(route, function(users){
             $('#user-table').remove();
             $('#user-list-block').after(users);
+            if(pager != null){
+                pager.remove();
+            }
+            pager = ClaroUtils.renderPager(document.getElementById('twig-attributes').getAttribute('data-pages'), activePagerItem, 'user', $('#user-table'));
+            setPagerActions();
+
         })
+    }
+
+    function setPagerActions() {
+        $('.user-paginator-item').on('click', function(e){
+            activePagerItem = e.target.innerHTML;
+            renderUsers(activePagerItem);
+        });
+
+        $('.user-paginator-next-item').on('click', function(e){
+            activePagerItem++;
+            renderUsers(activePagerItem);
+        })
+
+        $('.user-paginator-prev-item').on('click', function(e){
+            activePagerItem--;
+            renderUsers(activePagerItem);
+        })
+    }
+
+    window.onresize = function(e) {
+        if(pager != null) {
+            pager.remove();
+        }
+        pager = ClaroUtils.renderPager(document.getElementById('twig-attributes').getAttribute('data-pages'), activePagerItem, 'user', $('#user-table'), 20);
+        setPagerActions();
     }
 })();
