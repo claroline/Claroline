7ed3673922fb1f26b7aa15fa0e27805e5b90d3b8
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-10-17 17:31:44 +0200

    refactoring dev fixtures dev command (workspace management not refactored yet)

---
+++ src/core/Claroline/CoreBundle/Tests/DataFixtures/LoadResourceRootsData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Claroline\CoreBundle\Tests\DataFixtures;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Claroline\CoreBundle\DataFixtures\LoggableFixture;
+use Claroline\CoreBundle\Library\Workspace\Configuration;
+
+class LoadResourceRootsData extends LoggableFixture implements ContainerAwareInterface
+{
+    /** @var ContainerInterface $container */
+    private $container;
+    private $rootCount;
+    private $username;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function __construct($username, $rootCount)
+    {
+        $this->rootCount = $rootCount;
+        $this->username = $username;
+
+        $this->workspaceNames = array(
+            'biology',
+            'chemistry',
+            'mathematic',
+            'physic',
+            'geography',
+            'sociology',
+            'informatic'
+        );
+
+        $this->workspaceNamesOffset = count($this->workspaceNames);
+        $this->workspaceNamesOffset--;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $em->getRepository('Claroline\CoreBundle\Entity\User')->findOneBy(array('username' => $this->username));
+
+        for ($i = 0; $i < $this->rootCount; $i++) {
+            $config = new Configuration();
+            $config->setWorkspaceType(Configuration::TYPE_SIMPLE);
+            $config->setWorkspaceName($this->workspaceNames[rand(0, $this->workspaceNamesOffset)]);
+            $config->setWorkspaceCode('CODE');
+            $wsCreator = $this->getContainer()->get('claroline.workspace.creator');
+            $wsCreator->createWorkspace($config, $user);
+        }
+    }
+}
--- src/plugin/Claroline/ForumBundle/Tests/DataFixtures/LoadForumData.php
+++ src/plugin/Claroline/ForumBundle/Tests/DataFixtures/LoadForumData.php
@@ -2,47 +2,173 @@
 
 namespace Claroline\ForumBundle\Tests\DataFixtures;
 
-use Claroline\ForumBundle\Entity\Forum;
-use Claroline\ForumBundle\Entity\Subject;
-use Claroline\ForumBundle\Entity\Message;
-use Claroline\CoreBundle\Entity\Resource\ResourceInstance;
-use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Claroline\ForumBundle\Entity\Forum;
+use Claroline\ForumBundle\Entity\Message;
+use Claroline\ForumBundle\Entity\Subject;
 
 class LoadForumData extends AbstractFixture implements ContainerAwareInterface
 {
     /** @var ContainerInterface $container */
     private $container;
 
+    private $username;
+    private $forumName;
+    private $nbMessages;
+    private $nbSubjects;
+
+    private $logger;
+
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
     }
 
-     /**
-     * @param ObjectManager $manager
-     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function __construct($forumName, $username, $nbMessages, $nbSubjects){
+        $this->forumName = $forumName;
+        $this->username = $username;
+        $this->nbMessages = $nbMessages;
+        $this->nbSubjects = $nbSubjects;
+    }
+
     public function load(ObjectManager $manager)
     {
-        $user = $this->getReference('user/user');
-        var_dump($user->getId());
-        $root = $this
-            ->container
-            ->get('doctrine.orm.entity_manager')
-            ->getRepository('Claroline\CoreBundle\Entity\Resource\ResourceInstance')
-            ->getRootForWorkspace($this->getReference('user/user')->getPersonalWorkspace());
-        $creator = $this->container->get('claroline.resource.manager');
-
+        $creator = $this->getContainer()->get('claroline.resource.manager');
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $em->getRepository('ClarolineCoreBundle:User')->findOneBy(array('username' => $this->username));
+        $root = $em->getRepository('Claroline\CoreBundle\Entity\Resource\ResourceInstance')
+            ->findOneBy(array('parent' => null, 'workspace' => $user->getPersonalWorkspace()->getId()));
+        $collaborators = $user->getPersonalWorkspace()->getCollaboratorRole()->getUsers();
+        $maxOffset = count($collaborators);
+        $maxOffset--;
         $forum = new Forum();
-        $forum->setName('testing forum');
-        $forum->setCreator($user);
+        $forum->setName($this->forumName);
         $manager->persist($forum);
         $manager->flush();
-//        $manager->persist($user->getPersonalWorkspace());
-//        $forumInstance = $creator->create($forum, $root->getId(), 'Forum', true, null, $user);
-//        $this->addReference('forum/forumInstance', $forumInstance);
+        $forumInstance = $creator->create($forum, $root->getId(), 'Forum', true, null, $user);
+        $this->log("forum {$forumInstance->getName()} created");
 
+        for ($i = 0; $i < $this->nbSubjects; $i++) {
+            $title = $this->generateLipsum(5);
+            $user = $collaborators[rand(0, $maxOffset)];
+            $subject = new Subject();
+            $subject->setName($title);
+            $subject->setTitle($title);
+            $subject->setCreator($user);
+            $subject->setForum($forumInstance->getResource());
+            $manager->persist($subject);
+            $this->log("subject $title created");
+            $subjectInstance = $creator->create($subject, $forumInstance->getId(), 'Subject', true, null, $user);
+
+            $entityToBeDetached = array();
+            for ($j=0; $j<$this->nbMessages ; $j++){
+
+                $sender = $collaborators[rand(0, $maxOffset)];
+                $message = new Message();
+                $message->setName('tmp-'.microtime());
+                $message->setCreator($sender);
+                $message->setContent($this->generateLipsum(150, true));
+                $message->setSubject($subject);
+                $inst = $creator->create($message, $subjectInstance->getId(), 'Message', true, null, $sender);
+                $entityToBeDetached[] = $message;
+                $entityToBeDetached[] = $inst;
+            }
+            $manager->flush();
+            foreach ($entityToBeDetached as $msg) {
+                $manager->detach($msg);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    private function getArrayLipsum()
+    {
+        $lipsum = array('lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero', 'eleifend',
+            'blandit', 'nunc', 'ornare', 'odio', 'ut', 'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia', 'a',
+            'pretium', 'quis', 'congue', 'praesent', 'sagittis', 'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros', 'dictum', 'proin', 'accumsan',
+            'sapien', 'nec', 'massa', 'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus', 'quisque', 'porttitor', 'ligula',
+            'dui', 'mollis', 'tempus', 'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam', 'tincidunt', 'id', 'condimentum', 'enim',
+            'sodales', 'in', 'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque', 'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis', 'tortor',
+            'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada', 'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam', 'suspendisse',
+            'potenti', 'vivamus', 'luctus', 'fringilla', 'erat', 'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante',
+            'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae', 'etiam', 'cursus', 'aliquam', 'quam', 'dapibus',
+            'nisl', 'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu', 'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra',
+            'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae', 'urna', 'iaculis', 'lobortis', 'nisi', 'viverra',
+            'arcu', 'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis', 'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean',
+            'convallis', 'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam', 'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas',
+            'mi', 'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus', 'netus', 'fames', 'quisque', 'euismod',
+            'curabitur', 'lectus', 'elementum', 'tempor', 'risus', 'cras');
+
+        return $lipsum;
+    }
+
+    /**
+     * if nbwords = 0, then it's somewhat random (from 5 to 500)
+     *
+     * @param integer $nbWords
+     * @param boolean $isFullText
+     *
+     * @return string
+     */
+    private function generateLipsum($nbWords = 0, $isFullText = false)
+    {
+        $words = $this->getArrayLipsum();
+        $content = '';
+        $endPhrase = array('?', '!', '.', '...');
+        $loopBeforeEnd = 0;
+
+        if ($nbWords == 0) {
+            $nbWords = rand(5, 500);
+        }
+
+        for ($i = 0; $i < $nbWords; $i++) {
+
+            if ($loopBeforeEnd == 0) {
+                $loopBeforeEnd = rand(3, 15);
+            }
+
+            $loopBeforeEnd--;
+
+            if ($isFullText && $loopBeforeEnd == 0) {
+                $content.="{$endPhrase[array_rand($endPhrase)]} " . ucfirst($words[array_rand($words)]);
+            } else {
+
+                if ($content != '') {
+                    $content .= ' ';
+                }
+
+                $content.= "{$words[array_rand($words)]}";
+            }
+
+            $i++;
+        }
+
+        if ($isFullText) {
+            $content = ucfirst($content) . '.';
+        }
+
+        return $content;
+    }
+
+    public function setLogger(\Closure $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    private function log($message)
+    {
+        if (null !== $this->logger)
+        {
+            call_user_func_array($this->logger, array($message));
+        }
     }
 }
