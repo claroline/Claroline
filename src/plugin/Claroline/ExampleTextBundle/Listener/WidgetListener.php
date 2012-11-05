66cea7f69941af7fc6ba7e5e057f4267234277cd
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-11-05 09:45:12 +0100

    widget onConfigure event

--- src/plugin/Claroline/ExampleTextBundle/Listener/WidgetListener.php
+++ src/plugin/Claroline/ExampleTextBundle/Listener/WidgetListener.php
@@ -4,12 +4,18 @@ namespace Claroline\ExampleTextBundle\Listener;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Claroline\CoreBundle\Library\Plugin\Event\DisplayWidgetEvent;
-
+use Claroline\CoreBundle\Library\Widget\Event\ConfigureWidgetEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 class WidgetListener extends ContainerAware
 {
     function onDisplay(DisplayWidgetEvent $event)
     {
         $event->setContent('someContent');
+    }
+
+    function onConfigure(ConfigureWidgetEvent $event)
+    {
+        $event->setResponse(new Response('some content'));
     }
 }
