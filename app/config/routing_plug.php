0a2804b308da471bc58b950f3800c55caf06dc70
Stéphane Klein <stephaneklein221@gmail.com>
2012-11-22 15:01:19 +0100

    Removed plugin routing php loader

--- app/config/routing_plug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Symfony\Component\Routing\RouteCollection;
-
-if (file_exists(__DIR__ . '/local/plugin/routing.yml')) {
-    return $loader->import('local/plugin/routing.yml');
-}
-
-return new RouteCollection();
