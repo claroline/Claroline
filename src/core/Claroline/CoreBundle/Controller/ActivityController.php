0aeff9585699cf94f973b3ca8a3f1c361ab3833b
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2012-11-30 15:45:49 +0100

    Basic resource sequence for activities

--- src/core/Claroline/CoreBundle/Controller/ActivityController.php
+++ src/core/Claroline/CoreBundle/Controller/ActivityController.php
@@ -2,6 +2,7 @@
 
 namespace Claroline\CoreBundle\Controller;
 
+use Claroline\CoreBundle\Entity\Resource\ResourceActivity;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,11 +14,15 @@ class ActivityController extends Controller
     public function addResourceAction($resourceId, $activityId)
     {
         $em = $this->get('doctrine.orm.entity_manager');
-        $repo = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource');
-        $resource = $repo->find($resourceId);
-        $activity = $repo->find($activityId);
-        $activity->addResource($resource);
-        $em->persist($activity);
+        $repoResource = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource');
+        $resource = $repoResource->find($resourceId);
+        $activity = $repoResource->find($activityId);
+        $repoResActivity = $em->getRepository('ClarolineCoreBundle:Resource\ResourceActivity');
+        $link = new ResourceActivity();
+        $link->setActivity($activity);
+        $link->setResource($resource);
+        $link->setSequenceOrder(1);
+        $em->persist($link);
         $em->flush();
         $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
@@ -29,13 +34,42 @@ class ActivityController extends Controller
     public function removeResourceAction($resourceId, $activityId)
     {
         $em = $this->get('doctrine.orm.entity_manager');
-        $repo = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource');
-        $resource = $repo->find($resourceId);
-        $activity = $repo->find($activityId);
-        $activity->removeResource($resource);
-        $em->persist($activity);
+        $repo = $em->getRepository('ClarolineCoreBundle:Resource\ResourceActivity');
+        $resourceActivity = $repo->findOneBy(array('resource' => $resourceId, 'activity' => $activityId));
+        $em->remove($resourceActivity);
         $em->flush();
 
         return new Response('success', 204);
+    }
+
+    public function parametersAction($activityId)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $repo = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource');
+        $activity = $repo->find($activityId);
+        $resourceActivities = $this->container->get('doctrine.orm.entity_manager')->getRepository('ClarolineCoreBundle:Resource\ResourceActivity')->getResourcesActivityForActivity($activity);
+
+        return $this->render('ClarolineCoreBundle:Activity:parameters.html.twig', array('workspace' => $activity->getWorkspace(), 'activity' => $activity, 'resourceActivities' => $resourceActivities));
+    }
+
+    //dql optimization must be done later to get resource activities
+    public function setSequenceOrderAction($activityId)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $resourceActivities = $em->getRepository('ClarolineCoreBundle:Resource\ResourceActivity')->findBy(array('activity' => $activityId));
+        $params = $this->get('request')->query->all();
+        
+        foreach($resourceActivities as $resourceActivity){
+            foreach($params['ids'] as $key => $id){
+                if ($id == $resourceActivity->getResource()->getId()) {
+                    $resourceActivity->setSequenceOrder($key);
+                    $em->persist($resourceActivity);
+                }
+            }
+        }
+
+        $em->flush();
+
+        return new Response('success');
     }
 }
---
+++ src/core/Claroline/CoreBundle/Entity/Resource/ResourceActivity.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Claroline\CoreBundle\Entity\Resource;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Claroline\CoreBundle\Repository\ResourceActivityRepository")
+ * @ORM\Table(name="claro_resource_activity")
+ */
+class ResourceActivity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+
+    protected $id;
+    /**
+     * @ORM\ManyToOne(targetEntity="Claroline\CoreBundle\Entity\Resource\Activity", inversedBy="resourceActivity")
+     */
+    private $activity;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Claroline\CoreBundle\Entity\Resource\AbstractResource")
+     */
+    private $resource;
+
+    /**
+     * @ORM\Column(type="string", nullable=true, name="sequence_order")
+     */
+    protected $sequenceOrder;
+
+    public function setActivity(Activity $activity)
+    {
+        $this->activity = $activity;
+    }
+
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    public function setResource(AbstractResource $resource)
+    {
+        $this->resource = $resource;
+    }
+
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    public function setSequenceOrder($order)
+    {
+        $this->sequenceOrder = $order;
+    }
+
+    public function getSequenceOrder()
+    {
+        return $this->sequenceOrder;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
