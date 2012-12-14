da8770b156d8d028bc45d1789ee50e4b91738a20
Esdras <esdras005@hotmail.com>
2012-12-14 15:11:15 +0100

    Create parameters.yml if not exists

--- web/app_install.php
+++ web/app_install.php
@@ -1,10 +1,13 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__ . '/../app/bootstrap.php.cache';
+require_once __DIR__ . '/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
+if (!file_exists($file = __DIR__ . '/../app/config/local/parameters.yml')) {
+    touch($file);
+}
 $kernel = new AppKernel('install', true); // put second parameter to false when development is done
 $kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
