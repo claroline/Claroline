886ca2486c37cd93496ed8f7e1187261a9bbe5a7
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2013-02-08 11:42:28 +0100

    rennaming ResourceActivityRepository method

--- src/core/Claroline/CoreBundle/Repository/ResourceActivityRepository.php
+++ src/core/Claroline/CoreBundle/Repository/ResourceActivityRepository.php
@@ -14,7 +14,7 @@ class ResourceActivityRepository extends EntityRepository
      *
      * @return type
      */
-    public function getResourcesActivityForActivity(Activity $activity)
+    public function findActivities(Activity $activity)
     {
         $dql = "SELECT ra, r FROM Claroline\CoreBundle\Entity\Resource\ResourceActivity ra
             LEFT JOIN ra.resource r
