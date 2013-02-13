736c018b034404853465ac97e98d3d79099c3f1d
Jorge Solis <jorgeluissolis@gmail.com>
2013-02-13 11:02:14 +0100

    Upadate views and tool list icons

--- src/core/Claroline/CoreBundle/DataFixtures/LoadToolsData.php
+++ src/core/Claroline/CoreBundle/DataFixtures/LoadToolsData.php
@@ -22,21 +22,21 @@ class LoadToolsData extends AbstractFixture implements ContainerAwareInterface, 
     public function load(ObjectManager $manager)
     {
         $ds = DIRECTORY_SEPARATOR;
-        $basePath = "bundles{$ds}clarolinecore{$ds}images{$ds}workspace{$ds}tools{$ds}";
+        //$basePath = "bundles{$ds}clarolinecore{$ds}images{$ds}workspace{$ds}tools{$ds}";
 
         $tools = array(
-            array('home', 'home_small.png', false, false, true, true),
-            array('parameters', 'process_small.png', false, false, true, true),
-            array('resource_manager', 'resource_small.png', false, false, true, true),
-            array('calendar', 'calendar_small.png', false, false, true, true),
-            array('user_management', 'user_small.png', false, false, true, false),
-            array('group_management', 'users_small.png', false,  false, true, false)
+            array('home', 'icon-home', false, false, true, true),
+            array('parameters', 'icon-cog', false, false, true, true),
+            array('resource_manager', 'icon-folder-open', false, false, true, true),
+            array('calendar', 'icon-calendar', false, false, true, true),
+            array('user_management', 'icon-user', false, false, true, false),
+            array('group_management', 'icon-th-list', false,  false, true, false)
         );
 
         foreach ($tools as $tool) {
             $entity = new Tool();
             $entity->setName($tool[0]);
-            $entity->setIcon($basePath.$tool[1]);
+            $entity->setClass($tool[1]);
             $entity->setIsWorkspaceRequired($tool[2]);
             $entity->setIsDesktopRequired($tool[3]);
             $entity->setDisplayableInDesktop($tool[5]);
