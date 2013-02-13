cc60fa9fff1e84f7e32f0f71462da1734862714c
Stéphane Klein <stephaneklein221@gmail.com>
2013-02-13 17:46:43 +0100

    Fixed broken tests

--- src/plugin/Claroline/ForumBundle/Tests/Controller/ForumControllerTest.php
+++ src/plugin/Claroline/ForumBundle/Tests/Controller/ForumControllerTest.php
@@ -6,7 +6,6 @@ use Claroline\CoreBundle\Library\Testing\FunctionalTestCase;
 use Claroline\ForumBundle\Tests\DataFixtures\LoadForumData;
 use Claroline\ForumBundle\DataFixtures\LoadOptionsData;
 use Claroline\CoreBundle\Tests\DataFixtures\Special\LoadEntitiesInWorkspace;
-use Claroline\CoreBundle\Tests\DataFixtures\LoadUsersData;
 
 class ForumControllerTest extends FunctionalTestCase
 {
@@ -24,10 +23,10 @@ class ForumControllerTest extends FunctionalTestCase
         $this->logUser($this->getFixtureReference('user/user'));
         $crawler = $this->client
             ->request('GET', "/forum/form/subject/{$this->getFixtureReference('forum_instance/forum')->getId()}");
-        $this->assertEquals(1, count($crawler->filter('#subject_forum_form')));
+        $this->assertEquals(1, count($crawler->filter('#forum_subject_form')));
         $form = $crawler->filter('button[type=submit]')->form();
-        $form['subject_forum_form[title]'] = 'title';
-        $form['subject_forum_form[message][content]'] = 'content';
+        $form['forum_subject_form[title]'] = 'title';
+        $form['forum_subject_form[message][content]'] = 'content';
         $this->client->submit($form);
         $crawler = $this->client
             ->request('GET', "/forum/{$this->getFixtureReference('forum_instance/forum')->getId()}/offset/0");
@@ -48,9 +47,7 @@ class ForumControllerTest extends FunctionalTestCase
         $link = $crawler->filter('.link-subject')->first()->link();
         $crawler = $this->client->click($link);
         $this->assertEquals(1, count($crawler->filter('#messages_table')));
-        $subjects = $this
-            ->client
-            ->getContainer()
+        $subjects = $this->client->getContainer()
             ->get('doctrine.orm.entity_manager')
             ->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
             ->find($this->getFixtureReference('forum_instance/forum')->getId())
@@ -59,12 +56,11 @@ class ForumControllerTest extends FunctionalTestCase
         $crawler = $this->client->request('GET', "/forum/subject/{$subjects[0]->getId()}/offset/0");
         $this->assertEquals(2, count($crawler->filter('.row-message')));
         $crawler = $this->client->request('GET', "/forum/add/message/{$subjects[0]->getId()}");
-        $this->assertEquals(1, count($crawler->filter('#message_forum_form')));
+        $this->assertEquals(1, count($crawler->filter('#forum_message_form')));
         $form = $crawler->filter('input[type=submit]')->form();
-        $form['message_forum_form[content]'] = 'content';
+        $form['forum_message_form[content]'] = 'content';
         $this->client->submit($form);
         $crawler = $this->client->request('GET', "/forum/subject/{$subjects[0]->getId()}/offset/0");
         $this->assertEquals(3, count($crawler->filter('.row-message')));
-
     }
 }
