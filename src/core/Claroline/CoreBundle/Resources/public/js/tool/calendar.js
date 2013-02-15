f12e3c51ce440c8bfc825b11b821c7177b0838da
easy-es <esdras005@hotmail.com>
2013-02-15 12:00:36 +0100

    calendar

--- src/core/Claroline/CoreBundle/Resources/public/js/tool/calendar.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/tool/calendar.js
@@ -4,20 +4,25 @@
 
     calendar.initialize = function (context) {
         context = context || 'desktop';
-        
+        var clickedDate = null;
+        var id = null;
+        var  url = null;
         var dayClickWorkspace = function (date, allDay, jsEvent, view) {
+            clickedDate = date;
             $('#deleteBtn').hide();
             $('#save').show();
             $('#updateBtn').hide();
             $('#calendar_form').find('input:text, input:password, input:file, select, textarea').val('');
             $('#calendar_form').find('input:radio, input:checkbox')
-            .removeAttr('checked').removeAttr('selected');
+                .removeAttr('checked')
+                .removeAttr('selected');
+
             if (allDay) {  
                 var  currentDate = new Date();
                 var pickedDate = new Date(date); 
                 $('#divTitle').html('Start date '+pickedDate.getDate()+'/'+(pickedDate.getMonth()+1)+'/'+pickedDate.getFullYear());
                 
-                if(pickedDate > currentDate ) {
+                if (pickedDate > currentDate ) {
                     $('#calendar_form_end_day').val(pickedDate.getDate());
                     $('#calendar_form_end_month').val(pickedDate.getMonth()+1);
                     $('#calendar_form_end_year').val(pickedDate.getFullYear());         
@@ -27,57 +32,6 @@
                     $('#calendar_form_end_year').val(currentDate.getFullYear());     
                 }
 
-                $("#save").click(function() {
-                    if($('#form_title').val() !== '') {
-                        $("#save").attr("disabled", "disabled");
-                        data = new FormData($('#myForm')[0]);  
-                        day = new Date(date);
-                        data.append("date",day);
-                        var url = $("#myForm").attr("action");
-                         $.ajax({
-                            'url': url,
-                            'type': 'POST',
-                            'data': data ,
-                            'processData': false,
-                            'contentType': false,
-                            'success': function (data,textStatus, xhr) {
-                                if (xhr.status == 200 ) {           
-                                    $('#myModal').modal('hide') ;
-                                    $('#save').removeAttr("disabled"); 
-                                    $('#calendar').fullCalendar('renderEvent',
-                                    {
-                                        title: data.title,
-                                        start: data.start,
-                                        end: data.end,
-                                        allDay: data.allDay,
-                                        color : data.color
-                                    },
-                                    true // make the event "stick"
-                                    );
-                                    $('#calendar').fullCalendar('unselect');
-                                }
-                            },
-                            'error': function (xhr, textStatus) {
-                                if(xhr.status == 400){//bad request
-                                    alert(" Start date is bigger thand end date");
-                                    $('#save').removeAttr("disabled"); 
-                                    $('#output').html(textStatus);
-                                } else{
-                                    //if we got to this point we know that the controller
-                                    //did not return a json_encoded array. We can assume that           
-                                    //an unexpected PHP error occured
-                                    alert("An unexpeded error occured.");
-                                    $('#save').removeAttr("disabled"); 
-                                    //if you want to print the error:
-                                    $('#output').html(data);
-                                }
-                            }
-                        });
-                    } else {
-                        alert('title can not be empty');
-                    }
-                });
-
                 $( "#myModal" ).modal(); 
             }
         };
@@ -85,8 +39,98 @@
         var dayClickDesktop = function () {
             alert('Not implemented yet');
         };
+        
         var dayClickFunction = context === 'desktop' ? dayClickDesktop : dayClickWorkspace;
-            
+        
+        $("#save").click(function() {
+            if ($('#form_title').val() !== '') {
+                $("#save").attr("disabled", "disabled");
+                data = new FormData($('#myForm')[0]); 
+                data.append("date", new Date(clickedDate));
+                var url = $("#myForm").attr("action");
+                $.ajax({
+                    'url': url,
+                    'type': 'POST',
+                    'data': data ,
+                    'processData': false,
+                    'contentType': false,
+                    'success': function (data, textStatus, xhr) {
+                        if (xhr.status == 200 ) {           
+                            $('#myModal').modal('hide') ;
+                            $('#save').removeAttr("disabled"); 
+                            $('#calendar').fullCalendar('renderEvent',
+                            {
+                                title: data.title,
+                                start: data.start,
+                                end: data.end,
+                                allDay: data.allDay,
+                                color: data.color
+                            },
+                            true // make the event "stick"
+                            );
+                            $('#calendar').fullCalendar('unselect');
+                        }
+                    },
+                    'error': function (xhr, textStatus) {
+                        if(xhr.status == 400){//bad request
+                            alert(" Start date is bigger thand end date");
+                            $('#save').removeAttr("disabled"); 
+                            $('#output').html(textStatus);
+                        } else{
+                            //if we got to this point we know that the controller
+                            //did not return a json_encoded array. We can assume that           
+                            //an unexpected PHP error occured
+                            alert("An unexpeded error occured.");
+                            $('#save').removeAttr("disabled"); 
+                            //if you want to print the error:
+                            $('#output').html(data);
+                        }
+                    }
+                });
+            } else {
+                alert('title can not be empty');
+            }
+        });
+        
+        $("#updateBtn").click(function() {
+         $("#updateBtn").attr("disabled", "disabled");
+         var data = new FormData($('#myForm')[0]);
+         data.append('id',id);
+         url = $('a#update').attr('href');
+         $.ajax({
+             'url':url,
+             'type':'POST',
+             'data':data,
+             'processData': false,
+             'contentType': false,
+             'success': function(data,textStatus,xhr) {
+                 if(xhr.status == 200)  { 
+                     $('#myModal').modal('hide');
+                     $('#updateBtn').removeAttr("disabled"); 
+                     $('#calendar').fullCalendar( 'rerenderEvents' );
+                   
+                 }
+             }
+         });
+     });
+     
+    $("#deleteBtn").click(function() {
+    $("#deleteBtn").attr("disabled", "disabled");
+    url = $('a#delete').attr('href');
+    $.ajax({
+        'url':url,
+        'data':{
+            'id': id 
+        },
+        'success': function(data,textStatus, xhr){
+            if(xhr.status == 200) {
+                $('#deleteBtn').removeAttr("disabled"); 
+                $('#calendar').fullCalendar( 'removeEvents',id );
+                $('#myModal').modal('hide') ;
+                }
+            }                 
+        });
+    });
         $('#calendar').fullCalendar({
             header: {
                 left: 'prev,next today',
@@ -139,7 +183,7 @@
             },
             dayClick: dayClickFunction,
             eventClick: function(calEvent, jsEvent, view) {
-                var  url = null;
+               id = calEvent.id;
                 $('#deleteBtn').show();
                 $('#updateBtn').show();
                 $('#save').hide();
@@ -169,44 +213,6 @@
                             alert("An error occured"+textStatus);
                         }
                     }
-                });
-
-                $("#updateBtn").click(function() {
-                    $("#updateBtn").attr("disabled", "disabled");
-                    data = new FormData($('#myForm')[0]);
-                    data.append('id',calEvent.id);
-                    url = $('a#update').attr('href');
-                    $.ajax({
-                        'url':url,
-                        'type':'POST',
-                        'data':data,
-                        'processData': false,
-                        'contentType': false,
-                        'success': function(data,textStatus,xhr) {
-                            if(xhr.status == 200)  { 
-                                $('#updateBtn').removeAttr("disabled"); 
-                                $('#calendar').fullCalendar( 'rerenderEvents' );
-                                $('#myModal').modal('hide');
-                            }
-                        }
-                    });
-                });
-                $("#deleteBtn").click(function() {
-                    $("#deleteBtn").attr("disabled", "disabled");
-                    url = $('a#delete').attr('href');
-                    $.ajax({
-                        'url':url,
-                        'data':{
-                            'id':  calEvent.id 
-                        },
-                        'success': function(data,textStatus, xhr){
-                            if(xhr.status == 200) {
-                                $('#deleteBtn').removeAttr("disabled"); 
-                                $('#calendar').fullCalendar( 'removeEvents',calEvent.id );
-                                $('#myModal').modal('hide') ;
-                            }
-                        }                 
-                    });
                 });
                 $("#myModal").modal();
             }
