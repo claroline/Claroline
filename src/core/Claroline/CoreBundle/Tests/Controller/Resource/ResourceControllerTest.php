6e3dc6d0f63f3965c961b9b272f1a9c9b8be0603
Stéphane Klein <stephaneklein221@gmail.com>
2013-02-15 16:06:41 +0100

    Fixed resource creation rights bug

--- src/core/Claroline/CoreBundle/Tests/Controller/Resource/ResourceControllerTest.php
+++ src/core/Claroline/CoreBundle/Tests/Controller/Resource/ResourceControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Claroline\CoreBundle\Controller\ResourceController;
+namespace Claroline\CoreBundle\Controller;
 
 use Claroline\CoreBundle\Library\Testing\FunctionalTestCase;
 use Claroline\CoreBundle\Entity\Resource\Directory;
