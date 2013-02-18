cb232a8acfefd3b20e7af84122592221998b5c2f
Stéphane Klein <stephaneklein221@gmail.com>
2013-02-18 13:27:26 +0100

    [PdfPlayerBundle] Initial commit

---
+++ ClarolinePdfPlayerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Claroline\PdfPlayerBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+class ClarolinePdfPlayerBundle extends PluginBundle
+{
+}
---
+++ Listener/PdfPlayerListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Claroline\PdfPlayerBundle\Listener;
+
+use Claroline\CoreBundle\Library\Resource\Event\PlayFileEvent;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+
+class PdfPlayerListener extends ContainerAware
+{
+    public function onOpenPdf (PlayFileEvent $event)
+    {
+        $path = $this->container->getParameter('claroline.files.directory')
+            . DIRECTORY_SEPARATOR
+            . $event->getResource()->getHashName();
+        $content = $this->container->get('templating')->render(
+            'ClarolinePdfPlayerBundle::pdf.html.twig',
+            array(
+                'workspace' => $event->getResource()->getWorkspace(),
+                'path' => $path,
+                'pdf' => $event->getResource()
+            )
+        );
+        $response = new Response($content);
+        $event->setResponse($response);
+        $event->stopPropagation();
+    }
+}
