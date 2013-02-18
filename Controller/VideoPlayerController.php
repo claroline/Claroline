df50674f12128ce064e2c20bb8b9f245495defb2
Stéphane Klein <stephaneklein221@gmail.com>
2013-02-18 13:36:57 +0100

    [VideoPlayerBundle] Initial commit

---
+++ Controller/VideoPlayerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Claroline\VideoPlayerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+//todo use sf2.2 BinaryFileResponse
+class VideoPlayerController extends Controller
+{
+    public function streamAction($videoId)
+    {
+        $video = $this
+            ->get('doctrine.orm.entity_manager')
+            ->getRepository('ClarolineCoreBundle:Resource\File')
+            ->find($videoId);
+
+        $response = new StreamedResponse();
+        $path = $this->container->getParameter('claroline.files.directory')
+            . DIRECTORY_SEPARATOR
+            . $video->getHashName();
+        $response->setCallBack(
+            function () use ($path) {
+                readfile($path);
+            }
+        );
+        $response->headers->set('Content-Type', $video->getMimeType());
+
+        return $response;
+    }
+}
---
+++ Listener/VideoPlayerListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Claroline\VideoPlayerBundle\Listener;
+
+use Claroline\CoreBundle\Library\Resource\Event\PlayFileEvent;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+
+class VideoPlayerListener extends ContainerAware
+{
+    public function onOpenVideo(PlayFileEvent $event)
+    {
+        $path = $this->container->getParameter('claroline.files.directory')
+            . DIRECTORY_SEPARATOR
+            . $event->getResource()->getHashName();
+        $content = $this->container->get('templating')->render(
+            'ClarolineVideoPlayerBundle::video.html.twig',
+            array(
+                'workspace' => $event->getResource()->getWorkspace(),
+                'path' => $path,
+                'video' => $event->getResource()
+            )
+        );
+        $response = new Response($content);
+        $event->setResponse($response);
+        $event->stopPropagation();
+    }
+}
