000cdae58861068152a0793716dd1460fa2e93ba
ngodfraind <nicolas.godfraind@gmail.com>
2013-02-28 10:52:30 +0100

    [VideoPlayerBundle] Updating Events namespace.

--- Listener/VideoPlayerListener.php
+++ Listener/VideoPlayerListener.php
@@ -2,7 +2,7 @@
 
 namespace Claroline\VideoPlayerBundle\Listener;
 
-use Claroline\CoreBundle\Library\Resource\Event\PlayFileEvent;
+use Claroline\CoreBundle\Library\Event\PlayFileEvent;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Response;
 
