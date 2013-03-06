9b3356d1f4ff285767738180b48e6e9e4d2fe726
ngodfraind <nicolas.godfraind@gmail.com>
2013-03-06 12:53:01 +0100

    prepull commit (js checkstyle)

--- src/core/Claroline/CoreBundle/Resources/public/js/message/list_received.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/message/list_received.js
@@ -1,4 +1,6 @@
 (function () {
+    'use strict';
+
     $('.loading').hide();
     var stackedRequests = 0;
     $.ajaxSetup({
@@ -19,32 +21,60 @@
     var mode = 0; //0 = standard || 1 = search
     $('html, body').animate({scrollTop: 0}, 0);
     $('.delete-msg').attr('disabled', 'disabled');
-    $('.chk-delete').live('change', function(){
-        if ($('.chk-delete:checked').length){
-           $('.delete-msg').removeAttr('disabled');
+    $('.chk-delete').live('change', function() {
+        if ($('.chk-delete:checked').length) {
+            $('.delete-msg').removeAttr('disabled');
         } else {
-           $('.delete-msg').attr('disabled', 'disabled');
+            $('.delete-msg').attr('disabled', 'disabled');
         }
     });
 
-    var standardRoute = function(){
+    var standardRoute = function() {
         return Routing.generate('claro_message_list_received', {
             'offset' : $('.row-user-message').length
         });
     };
 
-    var searchRoute = function(){
+    var searchRoute = function() {
         return Routing.generate('claro_message_list_received_search', {
             'offset' : $('.row-user-message').length,
             'search': document.getElementById('search-msg-txt').value
         });
     };
 
+    function lazyloadUserMessage(route) {
+        loading = true;
+        $('#loading').show();
+        Claroline.Utilities.ajax({
+            type: 'GET',
+            url: route(),
+            success: function(messages) {
+                $('#message-table-body').append(messages);
+                loading = false;
+                $('#loading').hide();
+                if (messages.length === 0) {
+                    stop = true;
+                }
+                stackedRequests--;
+                if (stackedRequests === 0) {
+                    $('.please-wait').hide();
+                }
+
+            },
+            complete: function(){
+                if($(window).height() >= $(document).height() && stop === false) {
+                    lazyloadUserMessage(route);
+                }
+            }
+        });
+    }
+
     lazyloadUserMessage(standardRoute);
 
-    $(window).scroll(function(){
-        if  (($(window).scrollTop()+100 >= $(document).height() - $(window).height()) && loading === false && stop === false){
-            if(mode === 0){
+    $(window).scroll(function() {
+        if (($(window).scrollTop()+100 >= $(document).height() - $(window).height()) &&
+            loading === false && stop === false) {
+            if (mode === 0) {
                 lazyloadUserMessage(standardRoute);
             } else {
                 lazyloadUserMessage(searchRoute);
@@ -52,10 +82,10 @@
         }
     });
 
-   $('#search-msg').click(function(){
+    $('#search-msg').click(function() {
         $('#message-table-body').empty();
         stop = false;
-        if (document.getElementById('search-msg-txt').value !== ''){
+        if (document.getElementById('search-msg-txt').value !== '') {
             mode = 1;
             lazyloadUserMessage(searchRoute);
         } else {
@@ -69,14 +99,14 @@
         $('#validation-box-body').html('delete');
     });
 
-    $('.mark-as-read').live('click', function(e){
+    $('.mark-as-read').live('click', function(e) {
         e.preventDefault();
         Claroline.Utilities.ajax({
             type: 'GET',
             url: $(e.currentTarget).attr('href'),
-            success: function(){
-               $(e.target).css('color', 'green');
-               $(e.target).attr('class', 'icon-ok-sign');
+            success: function() {
+                $(e.target).css('color', 'green');
+                $(e.target).attr('class', 'icon-ok-sign');
             }
         });
     });
@@ -98,8 +128,8 @@
         Claroline.Utilities.ajax({
             url: route,
             success: function(){
-                $('.chk-delete:checked').each(function(index, element){
-                     $(element).parent().parent().remove();
+                $('.chk-delete:checked').each(function(index, element) {
+                    $(element).parent().parent().remove();
                 });
                 $('#validation-box').modal('hide');
                 $('#validation-box-body').empty();
@@ -115,40 +145,14 @@
         $('#validation-box-body').empty();
     });
 
-    function lazyloadUserMessage(route){
-        loading = true;
-        $('#loading').show();
-        Claroline.Utilities.ajax({
-            type: 'GET',
-            url: route(),
-            success: function(messages){
-                $('#message-table-body').append(messages);
-                loading = false;
-                $('#loading').hide();
-                if (messages.length === 0) {
-                    stop = true;
-                }
-                stackedRequests--;
-                if (stackedRequests === 0) {
-                    $('.please-wait').hide();
-                }
-
-            },
-            complete: function(){
-                if($(window).height() >= $(document).height() && stop === false){
-                    lazyloadUserMessage(route);
-                }
-            }
-        });
-    }
     $('#allChecked').click(function(){
-        if($('#allChecked').is(':checked')){
-             $(" INPUT[@class=" + 'chk-delete' + "][type='checkbox']").attr('checked', true);
-             $('.delete-msg').removeAttr('disabled');
-         }
-         else {
-            $(" INPUT[@class=" + 'chk-delete' + "][type='checkbox']").attr('checked', false);
+        if ($('#allChecked').is(':checked')){
+            $(' INPUT[@class=' + 'chk-delete' + '][type="checkbox"]').attr('checked', true);
+            $('.delete-msg').removeAttr('disabled');
+        }
+        else {
+            $(' INPUT[@class=' + 'chk-delete' + '][type="checkbox"]').attr('checked', false);
             $('.delete-msg').attr('disabled', 'disabled');
-         }
+        }
     });
 })();
