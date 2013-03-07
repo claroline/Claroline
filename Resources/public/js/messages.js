4d2899a718af504c692260f19c70becb8f9e9f57
ngodfraind <nicolas.godfraind@gmail.com>
2013-03-07 14:53:35 +0100

    [ForumBundle] javascript checkstyle

--- Resources/public/js/messages.js
+++ Resources/public/js/messages.js
@@ -1,23 +1,19 @@
-(function(){
-    var nbPages = document.getElementById('twig-attributes').getAttribute('data-nbPages');
-    var limit = document.getElementById('twig-attributes').getAttribute('data-limit');
-    var subjectId = document.getElementById('twig-attributes').getAttribute('data-subjectId');
-    var activePage = 1;
-    var pager = activePageMessages(activePage);
+(function () {
+    'use strict';
 
-    function activePageMessages(page){
+    function activePageMessages(page) {
 
-        if (pager !== undefined){
+        if (pager !== undefined) {
             pager.remove();
         }
 
         pager = Claroline.Utilities.renderPager(nbPages, page, 'message', $('#messages_table'));
         setPagerActions();
-        var offset = (page-1)*limit;
+        var offset = (page - 1) * limit;
         var route = Routing.generate('claro_forum_messages', {'subjectId': subjectId, 'offset': offset});
         Claroline.Utilities.ajax({
             url: route,
-            success: function(data){
+            success: function (data) {
                 $('#table-message-body').empty();
                 $('#table-message-body').append(data);
             },
@@ -27,20 +23,26 @@
         return pager;
     }
 
-    function setPagerActions(){
-        $('.message-paginator-item').on('click', function(e){
+    function setPagerActions() {
+        $('.message-paginator-item').on('click', function (e) {
             activePage = e.target.innerHTML;
             activePageMessages(activePage);
         });
 
-        $('.message-paginator-next-item').on('click', function(e){
+        $('.message-paginator-next-item').on('click', function () {
             activePage++;
             activePageMessages(activePage);
-        })
+        });
 
-        $('.message-paginator-prev-item').on('click', function(e){
+        $('.message-paginator-prev-item').on('click', function () {
             activePage--;
             activePageMessages(activePage);
-        })
+        });
     }
+
+    var nbPages = document.getElementById('twig-attributes').getAttribute('data-nbPages');
+    var limit = document.getElementById('twig-attributes').getAttribute('data-limit');
+    var subjectId = document.getElementById('twig-attributes').getAttribute('data-subjectId');
+    var activePage = 1;
+    var pager = activePageMessages(activePage);
 })();
--- Resources/public/js/subjects.js
+++ Resources/public/js/subjects.js
@@ -1,47 +1,49 @@
-(function(){
+(function () {
+    'use strict';
+
+    function activePageSubjects(page) {
+
+        if (pager !== undefined) {
+            pager.remove();
+        }
+
+        pager = Claroline.Utilities.renderPager(nbPages, page, 'subject', $('#subjects_table'));
+        setPagerActions();
+        var offset = (page - 1) * limit;
+        var route = Routing.generate('claro_forum_subjects', {'forumId': forumId, 'offset': offset});
+        Claroline.Utilities.ajax({
+            url: route,
+            success: function (data) {
+                $('#table-subjects-body').empty();
+                $('#table-subjects-body').append(data);
+            },
+            type: 'GET'
+        });
+
+        return pager;
+    }
+
+    function setPagerActions() {
+        $('.subject-paginator-item').on('click', function (e) {
+            activePage = e.target.innerHTML;
+            activePageSubjects(activePage);
+        });
+
+        $('.subject-paginator-next-item').on('click', function () {
+            activePage++;
+            activePageSubjects(activePage);
+        });
+
+        $('.subject-paginator-prev-item').on('click', function () {
+            activePage--;
+            activePageSubjects(activePage);
+        });
+    }
+
     var nbPages = document.getElementById('twig-attributes').getAttribute('data-nbPages');
     var limit = document.getElementById('twig-attributes').getAttribute('data-limit');
     var forumId = document.getElementById('twig-attributes').getAttribute('data-forumId');
     var activePage = 1;
     var pager = activePageSubjects(activePage);
-
-    function activePageSubjects(page){
-
-        if (pager !== undefined){
-            pager.remove();
-        }
-
-        pager = Claroline.Utilities.renderPager(nbPages, page, 'subject', $('#subjects_table'))
-        setPagerActions();
-        var offset = (page-1)*limit;
-        var route = Routing.generate('claro_forum_subjects', {'forumId': forumId, 'offset': offset});
-        Claroline.Utilities.ajax({
-            url: route,
-            success: function(data){
-                $('#table-subjects-body').empty();
-                $('#table-subjects-body').append(data);
-            },
-            type: 'GET'
-        })
-
-        return pager;
-    }
-
-    function setPagerActions(){
-        $('.subject-paginator-item').on('click', function(e){
-            activePage = e.target.innerHTML;
-            activePageSubjects(activePage);
-        });
-
-        $('.subject-paginator-next-item').on('click', function(e){
-            activePage++;
-            activePageSubjects(activePage);
-        })
-
-        $('.subject-paginator-prev-item').on('click', function(e){
-            activePage--;
-            activePageSubjects(activePage);
-        })
-    }
 
 })();
