c2f02a217c98864a487c6d053ab4c0dda1fe23b2
Anh Thao PHAM <anhthao_pham@hotmail.com>
2013-03-07 14:43:07 +0100

    workspace_edit new template

--- src/core/Claroline/CoreBundle/Form/WorkspaceEditType.php
+++ src/core/Claroline/CoreBundle/Form/WorkspaceEditType.php
@@ -2,18 +2,26 @@
 
 namespace Claroline\CoreBundle\Form;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class WorkspaceEditType extends WorkspaceType
+class WorkspaceEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
-        $builder->remove('type');
+        $builder->add('name', 'text', array('required' => true));
+        $builder->add('code', 'text', array('required' => true));
     }
-    
+
     public function getName()
     {
         return 'workspace_edit_form';
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'translation_domain' => 'platform'
+        );
     }
 }
