811cd8b42cefb687d742aff72435be215aee1f81
Stéphane Klein <stephaneklein221@gmail.com>
2013-03-12 15:06:53 +0100

    [ExoBundle] Use resource creation route

--- Listener/ExerciseListener.php
+++ Listener/ExerciseListener.php
@@ -11,21 +11,21 @@ class ExerciseListener extends ContainerAware
 {
     public function onCreateForm(CreateFormResourceEvent $event)
     {
-        $entity = new Exercise();
-        $entity->setNbQuestion(0);
-        $entity->setDuration(0);
-        $entity->setMaxAttempts(0);
-        $entity->setStartDate(new \Datetime());
-        $entity->setEndDate(new \Datetime());
-        $entity->setDateCorrection(new \Datetime());
+        $exercise = new Exercise();
+        $exercise->setNbQuestion(0);
+        $exercise->setDuration(0);
+        $exercise->setMaxAttempts(0);
+        $exercise->setStartDate(new \Datetime());
+        $exercise->setEndDate(new \Datetime());
+        $exercise->setDateCorrection(new \Datetime());
         $form = $this->container->get('form.factory')
-            ->create(new ExerciseType(), $entity);
+            ->create(new ExerciseType(), $exercise);
         $twig = $this->container->get('templating');
         $content = $twig->render(
             'UJMExoBundle:Exercise:new.html.twig',
             array(
-                'entity' => $entity,
-                'form'   => $form->createView()
+                'form'  => $form->createView(),
+                'resourceType' => 'ujm_exercise'
             )
         );
         $event->setResponseContent($content);
