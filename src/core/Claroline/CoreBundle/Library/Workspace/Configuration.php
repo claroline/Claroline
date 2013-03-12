8b934cc7ed63b1a7d5ae2ec7e62e26f827616a5a
ngodfraind <nicolas.godfraind@gmail.com>
2013-03-12 16:56:52 +0100

    Testing workspace export listners.

--- src/core/Claroline/CoreBundle/Library/Workspace/Configuration.php
+++ src/core/Claroline/CoreBundle/Library/Workspace/Configuration.php
@@ -154,6 +154,27 @@ class Configuration
 
     private function validate($parsedFile)
     {
-        return true;
+        $errors = array();
+
+        $expectedKeys = array(
+            'tools',
+            'roles',
+            'creator_role',
+            'tools_permissions',
+            'name'
+        );
+
+        foreach ($expectedKeys as $key) {
+            if (!isset($parsedFile[$key])) {
+                $errors[] = "The entry '{$key}' is missing";
+            }
+        }
+
+        if (count($errors) === 0) {
+            return true;
+        } else {
+            return $errors;
+        }
+
     }
 }
