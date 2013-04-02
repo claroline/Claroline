06e956c553dba7c37d763fe527ee61a069bedfb9
easy-es <esdras005@hotmail.com>
2013-04-02 15:35:15 +0200

    add unicity on ressource name and tools

---
+++ src/plugin/Hello/HelloBundle/Controller/HelloController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hello\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class HelloController extends Controller {
+    
+}
