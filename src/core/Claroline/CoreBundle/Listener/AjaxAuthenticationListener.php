49c978a15aa5ca12952167fa87b60787626b1a46
Stéphane Klein <stephaneklein221@gmail.com>
2013-04-04 11:44:45 +0200

    Use annotations for core listeners

--- src/core/Claroline/CoreBundle/Listener/AjaxAuthenticationListener.php
+++ src/core/Claroline/CoreBundle/Listener/AjaxAuthenticationListener.php
@@ -3,16 +3,36 @@
 namespace Claroline\CoreBundle\Listener;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use JMS\DiExtraBundle\Annotation as DI;
 
-class AjaxAuthenticationListener extends ContainerAware
+/**
+ * @DI\Service
+ */
+class AjaxAuthenticationListener
 {
+    private $securityContext;
+
     /**
-     * @param GetResponseForExceptionEvent $event An GetResponseForExceptionEvent instance
+     * @DI\InjectParams({
+     *     "context" = @DI\Inject("security.context")
+     * })
+     *
+     * @param SecurityContextInterface $context
+     */
+    public function __construct(SecurityContextInterface $context)
+    {
+        $this->securityContext = $context;
+    }
+
+    /**
+     * @DI\Observe("kernel.exception", priority = 1)
+     *
+     * @param GetResponseForExceptionEvent $event
      */
     public function onCoreException(GetResponseForExceptionEvent $event)
     {
@@ -22,7 +42,7 @@ class AjaxAuthenticationListener extends ContainerAware
         // (see http://stackoverflow.com/questions/8607212/symfony2-ajax-login)
         if ($event->getRequest()->isXmlHttpRequest()
             && ($exception instanceof AccessDeniedHttpException || $exception instanceof AuthenticationException)) {
-            $msg = $this->container->get('security.context')->getToken() instanceof AnonymousToken ?
+            $msg = $this->securityContext->getToken() instanceof AnonymousToken ?
                 'Anonymous users are not allowed to perform this action (you may have lost your session) '
                 . ' : refresh the page to proceed to authentication' :
                 'Not allowed';
