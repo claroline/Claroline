54e62bde0bff3ba83f96951072fd095087898a54
david <david@david-laptop>
2013-04-12 14:54:04 +0200

    [ExoBundle] Add Fixtures

---
+++ DataFIxtures/LoadOptionsData.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * ExoOnLine
+ * Copyright or © or Copr. Université Jean Monnet (France), 2012
+ * dsi.dev@univ-st-etienne.fr
+ *
+ * This software is a computer program whose purpose is to [describe
+ * functionalities and technical features of your software].
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+*/
+
+namespace UJM\ExoBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use UJM\ExoBundle\Entity\TypeQCM;
+
+
+class LoadOptionsData extends AbstractFixture
+{
+    private $manager;
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+
+        $val_tqcm = array('Multiple response', 'Unique response');
+        foreach ($val_tqcm as $val) {
+            $this->newTQCM($val);
+        }
+
+        $val_role = array('ROLE_ADMIN', 'ROLE_USER');
+        foreach ($val_role as $val) {
+            $this->newRole($val);
+        }
+
+        $this->manager->flush();
+    }
+
+    private function newTQCM($val)
+    {
+        $tqcm = new TypeQCM();
+        $tqcm->setValue($val);
+
+        $this->manager->persist($tqcm);
+    }
+    
+}
