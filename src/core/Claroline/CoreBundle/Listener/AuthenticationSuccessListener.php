7023f1c9a2222894c57a9500f4758cf61c003b05
Vaince <vincent.saisset@gmail.com>
2013-04-18 14:38:38 +0200

    Repare log login user. Using DiExtraBundle for catch security.interactive_login

--- src/core/Claroline/CoreBundle/Listener/AuthenticationSuccessListener.php
+++ src/core/Claroline/CoreBundle/Listener/AuthenticationSuccessListener.php
@@ -2,15 +2,41 @@
 
 namespace Claroline\CoreBundle\Listener;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
 use Claroline\CoreBundle\Library\Event\LogUserLoginEvent;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
-class AuthenticationSuccessListener extends ContainerAware
+/**
+ * @DI\Service
+ */
+class AuthenticationSuccessListener
 {
+    private $securityContext;
+    private $eventDispatcher;
+
+    /**
+     * @DI\InjectParams({
+     *     "context"    = @DI\Inject("security.context"),
+     *     "ed"         = @DI\Inject("event_dispatcher")
+     * })
+     *
+     * @param \Doctrine\ORM\EntityManager $em
+     */
+    public function __construct(SecurityContextInterface $context, $ed)
+    {
+        $this->securityContext = $context;
+        $this->eventDispatcher = $ed;
+    }
+
+    /**
+     * @DI\Observe("security.interactive_login")
+     *
+     * @param WorkspaceLogEvent $event
+     */
     public function onAuthenticationSuccess()
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->securityContext->getToken()->getUser();
         $log = new LogUserLoginEvent($user);
-        $this->container->get('event_dispatcher')->dispatch('log', $log);
+        $this->eventDispatcher->dispatch('log', $log);
     }
 }
