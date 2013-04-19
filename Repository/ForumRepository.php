f3fe5cea246d1e1c263483b98b7d3588f72f19be
kitan1982 <anhthao.pham@claroline.net>
2013-04-19 13:02:49 +0200

    [ForumBundle] postgres compatibility

--- Repository/ForumRepository.php
+++ Repository/ForumRepository.php
@@ -19,50 +19,18 @@ class ForumRepository extends EntityRepository
     public function findSubjects(Forum $forum, $offset = null, $limit = null)
     {
         $dql = "
-        SELECT
-        s.id as id,
-        COUNT(m_count.id) AS count_messages,
-        MAX(m.creationDate) AS last_message_created,
-        s.id as subjectId,
-        s.creationDate as subject_created,
-        s.title as title,
-        subjectCreator.lastName as subject_creator_lastname,
-        subjectCreator.firstName as subject_creator_firstname,
-        lastUser.lastName as last_message_creator_lastname,
-        lastUser.firstName as last_message_creator_firstname
-
-        FROM  Claroline\ForumBundle\Entity\Subject s
-
-        JOIN s.messages m
-        JOIN s.messages m_count
-        JOIN s.creator subjectCreator
-        JOIN s.forum forum
-        JOIN m.creator lastUser WITH lastUser.id =
-            (
-                SELECT lcu.id FROM Claroline\ForumBundle\Entity\Message m2
-                JOIN m2.subject s2
-                JOIN m2.creator lcu
-                JOIN s2.forum f2
-                WHERE NOT EXISTS
-                (
-                    SELECT m3 FROM Claroline\ForumBundle\Entity\Message m3
-                    JOIN m3.subject s3
-                    WHERE s2.id = s3.id
-                    AND m2.id < m3.id
-                )
-                and f2.id = :forumId
-                and m2.id = m.id
-            )
-        WHERE   forum.id = :forumId
-        GROUP BY s.id
-        ORDER BY count_messages DESC";
+            SELECT s, m FROM Claroline\ForumBundle\Entity\Subject s
+            JOIN s.forum f
+            JOIN s.messages m
+            WHERE f.id = :forumId
+        ";
 
         $query = $this->_em->createQuery($dql);
         $query->setParameter('forumId', $forum->getId());
         $query->setMaxResults($limit);
         $query->setFirstResult($offset);
 
-        return $query->getArrayResult();
+        return $query->getResult();
     }
 
     public function countMessagesForSubject(Subject $subject)
