9fecd37fe88ca81c3e25e62f9117ea2e288f020f
kitan1982 <anhthao.pham@claroline.net>
2013-04-22 16:08:38 +0200

    Used roles instead of user in many dql requests

--- src/core/Claroline/CoreBundle/Listener/ResourceLogWidgetListener.php
+++ src/core/Claroline/CoreBundle/Listener/ResourceLogWidgetListener.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManager;
 use JMS\DiExtraBundle\Annotation as DI;
 use Claroline\CoreBundle\Library\Event\DisplayWidgetEvent;
 use Claroline\CoreBundle\Entity\Workspace\AbstractWorkspace;
+use Claroline\CoreBundle\Library\Security\Utilities;
 
 /**
  * @DI\Service
@@ -17,23 +18,31 @@ class ResourceLogWidgetListener
     private $em;
     private $securityContext;
     private $twig;
+    private $utils;
 
     /**
      * @DI\InjectParams({
      *     "em"         = @DI\Inject("doctrine.orm.entity_manager"),
      *     "context"    = @DI\Inject("security.context"),
-     *     "twig"       = @DI\Inject("templating")
+     *     "twig"       = @DI\Inject("templating"),
+     *     "utils"      = @DI\Inject("claroline.security.utilities")
      * })
      *
      * @param EntityManager             $em
      * @param SecurityContextInterface  $context
      * @param TwigEngine                $twig
      */
-    public function __construct(EntityManager $em, SecurityContextInterface $context, TwigEngine $twig)
+    public function __construct(
+        EntityManager $em,
+        SecurityContextInterface $context,
+        TwigEngine $twig,
+        Utilities $utils
+    )
     {
         $this->em = $em;
         $this->securityContext = $context;
         $this->twig = $twig;
+        $this->utils = $utils;
     }
 
     /**
@@ -60,8 +69,11 @@ class ResourceLogWidgetListener
 
     private function renderLogs(AbstractWorkspace $workspace = null)
     {
-         $logs = $this->em->getRepository('ClarolineCoreBundle:Logger\ResourceLog')
-            ->findLastLogs($this->securityContext->getToken()->getUser(), $workspace);
+        $token = $this->securityContext->getToken();
+        $roles = $this->utils->getRoles($token);
+
+        $logs = $this->em->getRepository('ClarolineCoreBundle:Logger\ResourceLog')
+            ->findLastLogs($roles, $workspace);
 
         return $this->twig->render(
             'ClarolineCoreBundle:Widget:resource_events.html.twig',
