254916be6a621dd8b374b2a9ae67c0ed056ef15a
kitan1982 <anhthao.pham@claroline.net>
2013-04-23 15:03:38 +0200

    Reduced nb of sql queries for WS tag

---
+++ src/core/Claroline/CoreBundle/Repository/WorkspaceTagRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Claroline\CoreBundle\Repository;
+
+use Claroline\CoreBundle\Entity\User;
+use Doctrine\ORM\EntityRepository;
+
+class WorkspaceTagRepository extends EntityRepository
+{
+    public function findNonEmptyTagsByUser(User $user)
+    {
+        $dql = "
+            SELECT DISTINCT t
+            FROM Claroline\CoreBundle\Entity\Workspace\RelWorkspaceTag rwt
+            INNER JOIN Claroline\CoreBundle\Entity\Workspace\WorkspaceTag t WITH t = rwt.tag
+            WHERE t.user = :user
+        ";
+        $query = $this->_em->createQuery($dql);
+        $query->setParameter('user', $user);
+
+        return $query->getResult();
+    }
+}
