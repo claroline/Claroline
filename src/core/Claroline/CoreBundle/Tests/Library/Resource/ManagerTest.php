e48924002d89e1e11a0799c2052b1f80701b11d1
ngodfraind <nicolas.godfraind@gmail.com>
2013-05-23 17:18:13 +0200

    uncommenting tests

--- src/core/Claroline/CoreBundle/Tests/Library/Resource/ManagerTest.php
+++ src/core/Claroline/CoreBundle/Tests/Library/Resource/ManagerTest.php
@@ -14,111 +14,111 @@ class ManagerTest extends FixtureTestCase
         $this->manager = $this->client->getContainer()->get('claroline.resource.manager');
     }
 
-//    public function testCreationSetResourceAsLast()
-//    {
-//        $this->loadFileData('user', 'user', array('file1.txt'));
-//        $first = $this->getFile('file1.txt');
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->assertEquals(null, $first->getNext());
-//        $this->loadFileData('user', 'user', array('file2.txt'));
-//        $second = $this->getFile('file2.txt');
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->assertEquals($second, $first->getNext());
-//        $this->assertEquals($first, $second->getPrevious());
-//        $this->loadFileData('user', 'user', array('file3.txt'));
-//        $third = $this->getFile('file3.txt');
-//        $this->assertEquals($third, $second->getNext());
-//        $this->assertEquals($first, $second->getPrevious());
-//    }
-//
-//    public function testCreateSetShortcutAsLast()
-//    {
-//        $this->loadFileData('admin', 'admin', array('target.txt'));
-//        $this->loadShortcutData(
-//            $this->getFile('target.txt'),
-//            'user',
-//            'user',
-//            'first'
-//        );
-//
-//        $first = $this->getShortcut('first');
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->assertEquals(null, $first->getNext());
-//        $this->loadShortcutData(
-//            $this->getFile('target.txt'),
-//            'user',
-//            'user',
-//            'second'
-//        );
-//
-//        $second = $this->getShortcut('second');
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->assertEquals($second, $first->getNext());
-//        $this->assertEquals($first, $second->getPrevious());
-//
-//        $this->loadShortcutData(
-//            $this->getFile('target.txt'),
-//            'user',
-//            'user',
-//            'third'
-//        );
-//
-//        $third = $this->getShortcut('third');
-//        $this->assertEquals($third, $second->getNext());
-//        $this->assertEquals($first, $second->getPrevious());
-//    }
-//
-//    public function testDeleteSetPosition()
-//    {
-//        $this->loadFileData('user', 'user', array('file1.txt'));
-//        $this->loadFileData('user', 'user', array('file2.txt'));
-//        $this->loadFileData('user', 'user', array('file3.txt'));
-//        $this->loadFileData('user', 'user', array('file4.txt'));
-//        $first = $this->getFile('file1.txt');
-//        $second = $this->getFile('file2.txt');
-//        $third = $this->getFile('file3.txt');
-//        $fourth = $this->getFile('file4.txt');
-//        $this->manager->delete($second);
-//        $this->assertEquals($first, $third->getPrevious());
-//        $this->assertEquals($third, $first->getNext());
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->assertEquals($fourth, $third->getNext());
-//        $this->manager->delete($fourth);
-//        $this->assertEquals(null, $third->getNext());
-//        $this->assertEquals($first, $third->getPrevious());
-//        $this->assertEquals($third, $first->getNext());
-//        $this->assertEquals(null, $first->getPrevious());
-//        $this->manager->delete($first);
-//        $this->assertEquals(null, $third->getPrevious());
-//        $this->assertEquals(null, $third->getNext());
-//    }
-//
-//    public function testMoveSetPosition()
-//    {
-//        $this->loadFileData('user', 'user', array('file1.txt'));
-//        $this->loadFileData('user', 'user', array('file2.txt'));
-//        $this->loadDirectoryData('user', array('user/newParent'));
-//        $this->loadFileData('user', 'newParent', array('file3.txt'));
-//        $parentDir = $this->getDirectory('newParent');
-//        $first = $this->getFile('file1.txt');
-//        $second = $this->getFile('file2.txt');
-//        $third = $this->getFile('file3.txt');
-//        $this->manager->move($first, $this->getDirectory('newParent'));
-//        $this->assertEquals($parentDir, $second->getNext());
-//        $this->assertEquals(null, $second->getPrevious());
-//        $this->assertEquals(null, $first->getNext());
-//        $this->assertEquals($third->getName(), $first->getPrevious()->getName());
-//    }
-//
-//    public function testCopySetPosition()
-//    {
-//        $this->loadFileData('user', 'user', array('file1.txt'));
-//        $first = $this->getFile('file1.txt');
-//        $dir = $this->getDirectory('user');
-//        $copy = $this->manager->copy($first, $dir, $this->getUser('user'));
-//        $this->assertEquals($first, $copy->getPrevious());
-//        $this->assertEquals(null, $copy->getNext());
-//    }
+    public function testCreationSetResourceAsLast()
+    {
+        $this->loadFileData('user', 'user', array('file1.txt'));
+        $first = $this->getFile('file1.txt');
+        $this->assertEquals(null, $first->getPrevious());
+        $this->assertEquals(null, $first->getNext());
+        $this->loadFileData('user', 'user', array('file2.txt'));
+        $second = $this->getFile('file2.txt');
+        $this->assertEquals(null, $first->getPrevious());
+        $this->assertEquals($second, $first->getNext());
+        $this->assertEquals($first, $second->getPrevious());
+        $this->loadFileData('user', 'user', array('file3.txt'));
+        $third = $this->getFile('file3.txt');
+        $this->assertEquals($third, $second->getNext());
+        $this->assertEquals($first, $second->getPrevious());
+    }
+
+    public function testCreateSetShortcutAsLast()
+    {
+        $this->loadFileData('admin', 'admin', array('target.txt'));
+        $this->loadShortcutData(
+            $this->getFile('target.txt'),
+            'user',
+            'user',
+            'first'
+        );
+
+        $first = $this->getShortcut('first');
+        $this->assertEquals(null, $first->getPrevious());
+        $this->assertEquals(null, $first->getNext());
+        $this->loadShortcutData(
+            $this->getFile('target.txt'),
+            'user',
+            'user',
+            'second'
+        );
+
+        $second = $this->getShortcut('second');
+        $this->assertEquals(null, $first->getPrevious());
+        $this->assertEquals($second, $first->getNext());
+        $this->assertEquals($first, $second->getPrevious());
+
+        $this->loadShortcutData(
+            $this->getFile('target.txt'),
+            'user',
+            'user',
+            'third'
+        );
+
+        $third = $this->getShortcut('third');
+        $this->assertEquals($third, $second->getNext());
+        $this->assertEquals($first, $second->getPrevious());
+    }
+
+    public function testDeleteSetPosition()
+    {
+        $this->loadFileData('user', 'user', array('file1.txt'));
+        $this->loadFileData('user', 'user', array('file2.txt'));
+        $this->loadFileData('user', 'user', array('file3.txt'));
+        $this->loadFileData('user', 'user', array('file4.txt'));
+        $first = $this->getFile('file1.txt');
+        $second = $this->getFile('file2.txt');
+        $third = $this->getFile('file3.txt');
+        $fourth = $this->getFile('file4.txt');
+        $this->manager->delete($second);
+        $this->assertEquals($first, $third->getPrevious());
+        $this->assertEquals($third, $first->getNext());
+        $this->assertEquals(null, $first->getPrevious());
+        $this->assertEquals($fourth, $third->getNext());
+        $this->manager->delete($fourth);
+        $this->assertEquals(null, $third->getNext());
+        $this->assertEquals($first, $third->getPrevious());
+        $this->assertEquals($third, $first->getNext());
+        $this->assertEquals(null, $first->getPrevious());
+        $this->manager->delete($first);
+        $this->assertEquals(null, $third->getPrevious());
+        $this->assertEquals(null, $third->getNext());
+    }
+
+    public function testMoveSetPosition()
+    {
+        $this->loadFileData('user', 'user', array('file1.txt'));
+        $this->loadFileData('user', 'user', array('file2.txt'));
+        $this->loadDirectoryData('user', array('user/newParent'));
+        $this->loadFileData('user', 'newParent', array('file3.txt'));
+        $parentDir = $this->getDirectory('newParent');
+        $first = $this->getFile('file1.txt');
+        $second = $this->getFile('file2.txt');
+        $third = $this->getFile('file3.txt');
+        $this->manager->move($first, $this->getDirectory('newParent'));
+        $this->assertEquals($parentDir, $second->getNext());
+        $this->assertEquals(null, $second->getPrevious());
+        $this->assertEquals(null, $first->getNext());
+        $this->assertEquals($third->getName(), $first->getPrevious()->getName());
+    }
+
+    public function testCopySetPosition()
+    {
+        $this->loadFileData('user', 'user', array('file1.txt'));
+        $first = $this->getFile('file1.txt');
+        $dir = $this->getDirectory('user');
+        $copy = $this->manager->copy($first, $dir, $this->getUser('user'));
+        $this->assertEquals($first, $copy->getPrevious());
+        $this->assertEquals(null, $copy->getNext());
+    }
 
     public function testInsertBefore()
     {
