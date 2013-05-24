158784f7dda59c35a50fe6e3a19d234a6840699d
ngodfraind <nicolas.godfraind@gmail.com>
2013-05-24 15:42:10 +0200

    Fixing some tests.

---
+++ src/core/Claroline/CoreBundle/Tests/DataFixtures/Required/LoadTemplateData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Claroline\CoreBundle\Tests\DataFixtures\Required;
+
+use Claroline\CoreBundle\DataFixtures\LoadTemplateData as BaseLoadTemplateData;
+
+class LoadTemplateData extends BaseLoadTemplateData
+{
+}
+
+
