1959c3a553906ebd7abd95198ba0327f296a2745
Jorge Solis <jorgeluissolis@gmail.com>
2013-05-29 11:51:45 +0200

    New entity, data fixture, repository and twig extention for ThemeContoller

---
+++ src/core/Claroline/CoreBundle/Entity/Theme/Theme.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Claroline\CoreBundle\Entity\Theme;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Type
+ *
+ * @ORM\Table(name="claro_theme")
+ * @ORM\Entity(repositoryClass="Claroline\CoreBundle\Repository\ThemeRepository")
+ */
+class Theme
+{
+    public function __construct($name = null, $path = null)
+    {
+        $this->setName($name);
+        $this->setPath($path);
+    }
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255)
+     */
+    private $path;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Theme
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Theme
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}
