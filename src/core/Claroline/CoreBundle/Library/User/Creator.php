4f6e80be2ad12e87ba47d48f33186cea4919f498
ngodfraind <nicolas.godfraind@gmail.com>
2013-06-11 14:45:37 +0200

    Users list can be uploaded with csv.

--- src/core/Claroline/CoreBundle/Library/User/Creator.php
+++ src/core/Claroline/CoreBundle/Library/User/Creator.php
@@ -16,6 +16,8 @@ use JMS\DiExtraBundle\Annotation as DI;
  */
 class Creator
 {
+    const BATCH_SIZE = 5;
+
     private $em;
     private $trans;
     private $ch;
@@ -63,7 +65,6 @@ class Creator
         $user->addRole($this->em->getRepository('ClarolineCoreBundle:Role')->findOneByName('ROLE_USER'));
         $this->em->persist($user);
         $config = Configuration::fromTemplate($this->personalWsTemplateFile);
-        //uncomment this line when the templating system is working
         $config->setWorkspaceType(Configuration::TYPE_SIMPLE);
         $locale = $this->ch->getParameter('locale_language');
         $this->trans->setLocale($locale);
@@ -73,13 +74,11 @@ class Creator
         $workspace = $this->wsCreator->createWorkspace($config, $user, false);
         $user->setPersonalWorkspace($workspace);
         $this->em->persist($workspace);
-
         $repo = $this->em->getRepository('ClarolineCoreBundle:Tool\Tool');
         $requiredTools[] = $repo->findOneBy(array('name' => 'home'));
         $requiredTools[] = $repo->findOneBy(array('name' => 'resource_manager'));
         $requiredTools[] = $repo->findOneBy(array('name' => 'parameters'));
         $requiredTools[] = $repo->findOneBy(array('name' => 'logs'));
-
         $i = 1;
 
         foreach ($requiredTools as $requiredTool) {
@@ -95,10 +94,58 @@ class Creator
             $this->em->flush();
         }
 
-        // Log user creation
         $log = new LogUserCreateEvent($user);
         $this->ed->dispatch('log', $log);
 
         return $user;
+    }
+
+    /**
+     * Expects an array of users. Each user must be defined this way:
+     * array(
+     *     [0] => 'firstname',
+     *     [1] => 'lastname',
+     *     [2] => 'username',
+     *     [3] => 'password',
+     *     [4] => 'mail' (optionnal)
+     * )
+     * @param array $users
+     */
+    public function import($users)
+    {
+        $role = $this->em->getRepository('ClarolineCoreBundle:Role')->findOneBy(array('name' => 'ROLE_USER'));
+        $i = $j = 0;
+        var_dump($users);
+
+        foreach ($users as $user) {
+            $userEntity = new User();
+            $userEntity->setFirstName($user[0]);
+            $userEntity->setLastName($user[1]);
+            $userEntity->setUsername($user[2]);
+            $userEntity->setPassword($user[3]);
+
+            if (isset($user[4])) {
+                $userEntity->setMail($user[4]);
+            }
+
+            $userEntity->addRole($role);
+            $user = $this->create($userEntity, false);
+            $this->em->persist($user);
+
+            //echo ("UOW[{$this->em->getUnitOfWork()->size()}]".PHP_EOL);
+
+            if (($i % self::BATCH_SIZE) === 0) {
+                $j++;
+                $this->em->flush();
+                $this->em->clear();
+                //echo ("batch [{$j}] | users [{$i}] | UOW  [{$this->em->getUnitOfWork()->size()}]".PHP_EOL);
+                $role = $this->em->getRepository('ClarolineCoreBundle:Role')->findOneBy(array('name' => 'ROLE_USER'));
+            }
+
+            $i++;
+        }
+
+        $this->em->flush();
+        $this->em->clear();
     }
 }
--- src/core/Claroline/CoreBundle/Tests/DataFixtures/BatchInsert/LoadUsersData.php
+++ src/core/Claroline/CoreBundle/Tests/DataFixtures/BatchInsert/LoadUsersData.php
@@ -36,33 +36,21 @@ class LoadUsersData extends LoggableFixture implements ContainerAwareInterface
     public function load(ObjectManager $manager)
     {
         $userCreator = $this->container->get('claroline.user.creator');
-        $role = $manager->getRepository('ClarolineCoreBundle:Role')->findOneBy(array('name' => 'ROLE_USER'));
-        $count = $manager->getRepository('ClarolineCoreBundle:User')->count();
-        $totalUsers = $count + 1;
-
         $start = time();
+        $countUser = $manager->getRepository('ClarolineCoreBundle:User')->count();
 
-        for ($j = 0, $i = 0; $i < $this->numberUsers; $i++, $totalUsers++) {
-            $user = new User();
+        for ($i = 0; $i < $this->numberUsers; $i++) {
+            $totalUsers = $countUser + $i;
             $mandatoryFieldValue = "user_{$totalUsers}";
-            $user->setFirstName($mandatoryFieldValue);
-            $user->setLastName($mandatoryFieldValue);
-            $user->setUsername($mandatoryFieldValue);
-            $user->setPlainPassword($mandatoryFieldValue);
-            $user->addRole($role);
-            $userCreator->create($user, false);
-
-            $this->log("UOW[{$manager->getUnitOfWork()->size()}]");
-            if (($i % self::BATCH_SIZE) === 0) {
-                $j++;
-                $manager->flush();
-                $manager->clear();
-                $role = $manager->getRepository('ClarolineCoreBundle:Role')->findOneBy(array('name' => 'ROLE_USER'));
-                $totalInserts = $i + 1;
-                $this->log("batch [{$j}] | users [{$totalInserts}] | UOW  [{$manager->getUnitOfWork()->size()}]");
-            }
+            $users[] = array(
+                $mandatoryFieldValue,
+                $mandatoryFieldValue,
+                $mandatoryFieldValue,
+                $mandatoryFieldValue,
+            );
         }
 
+        $userCreator->import($users);
         $end = time();
         $duration = $this->container->get('claroline.utilities.misc')->timeElapsed($end - $start);
         $this->log("Time elapsed for the user creation: " . $duration);
