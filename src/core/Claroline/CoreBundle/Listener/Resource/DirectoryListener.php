dd745d9b06d2ca2e260ef92f8b6d8d9ddfe3a6a0
Stéphane Klein <stephaneklein221@gmail.com>
2013-06-14 10:37:27 +0200

    Temporarily fixed resource import

--- src/core/Claroline/CoreBundle/Listener/Resource/DirectoryListener.php
+++ src/core/Claroline/CoreBundle/Listener/Resource/DirectoryListener.php
@@ -170,7 +170,7 @@ class DirectoryListener implements ContainerAwareInterface
             $config['perms']
         );
         $ed = $this->container->get('event_dispatcher');
-        $createdResources[$config['id']] = $directory;
+        $createdResources[$config['id']] = $directory->getId();
 
         foreach ($config['children'] as $child) {
             $newEvent = new ImportResourceTemplateEvent(
