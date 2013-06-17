6255eb1f9e893d01ea1b687688f05b3a9c0c388b
ngodfraind <nicolas.godfraind@gmail.com>
2013-06-17 16:24:53 +0200

    ResourceRightsRepositoryTest done.

--- src/core/Claroline/CoreBundle/Tests/Repository/ResourceRightsRepositoryTest.php
+++ src/core/Claroline/CoreBundle/Tests/Repository/ResourceRightsRepositoryTest.php
@@ -2,33 +2,65 @@
 
 namespace Claroline\CoreBundle\Repository;
 
-use Claroline\CoreBundle\Library\Testing\FixtureTestCase;
+use Claroline\CoreBundle\Library\Testing\RepositoryTestCase;
 
-class ResourceRightsRepositoryTest extends FixtureTestCase
+class ResourceRightsRepositoryTest extends RepositoryTestCase
 {
-    public function testFindNonAdminRightsReturnsRightsForAnonymousAndWorkspaceRoles()
+    public static $repo;
+    public static $collaboratorRoleName;
+    public static $managerRoleName;
+
+    public static function setUpBeforeClass()
     {
-        $this->loadPlatformRoleData();
-        $this->loadUserData(array('john' => 'user'));
-        $this->loadDirectoryData('john', array('john/dir1'));
+        parent::setUpBeforeClass();
+        self::loadPlatformRoleData();
+        self::$repo = self::$em->getRepository('ClarolineCoreBundle:Resource\ResourceRights');
+        self::loadUserData(array('john' => 'user'));
+        self::loadDirectoryData('john', array('john/dir1'));
+        self::$collaboratorRoleName = 'ROLE_WS_COLLABORATOR_'.self::getWorkspace('john')->getId();
+        self::$managerRoleName = 'ROLE_WS_MANAGER_'.self::getWorkspace('john')->getId();
+    }
 
-        // this method doesnt return admin/anonymous roles
-        $workspaceRoles = $this->em->getRepository('ClarolineCoreBundle:Role')
-            ->findByWorkspace($this->getWorkspace('john'));
-        // this method shouldn't return admin roles
-        $dirRights = $this->em->getRepository('ClarolineCoreBundle:Resource\ResourceRights')
-            ->findNonAdminRights($this->getDirectory('dir1'));
-        $this->assertEquals(count($dirRights) - 1, count($workspaceRoles));
-        $rightRoles = array();
+    public function testFindMaximumRights()
+    {
+        $rights = self::$repo->findMaximumRights(
+            array(self::$collaboratorRoleName, self::$managerRoleName), self::getDirectory('john')
+        );
+        $this->assertEquals(1, $rights['canEdit']);
+        $rights = self::$repo->findMaximumRights(array(self::$collaboratorRoleName), self::getDirectory('john'));
+        $this->assertEquals(0, $rights['canEdit']);
+    }
 
-        foreach ($dirRights as $right) {
-            $rightRoles[] = $right->getRole()->getName();
-        }
+    public function testFindCreationRights()
+    {
+        $creationRights = self::$repo->findCreationRights(array(self::$collaboratorRoleName), self::getDirectory('john'));
+        $this->assertEquals(0, count($creationRights));
+        $creationRights = self::$repo->findCreationRights(array(self::$managerRoleName), self::getDirectory('john'));
+        $this->assertEquals(5, count($creationRights));
+        $creationRights = self::$repo->findCreationRights(
+            array(self::$managerRoleName, self::$collaboratorRoleName), self::getDirectory('john')
+        );
+        $this->assertEquals(5, count($creationRights));
+    }
 
-        $this->assertContains('ROLE_ANONYMOUS', $rightRoles);
+    public function testFindNonAdminRights()
+    {
+        $rights = self::$repo->findNonAdminRights(self::getDirectory('john'));
+        $this->assertEquals(4, count($rights));
+    }
 
-        foreach ($workspaceRoles as $role) {
-            $this->assertContains($role->getName(), $rightRoles);
-        }
+    public function testFindRecursiveByResource()
+    {
+        $rights =  self::$repo->findRecursiveByResource(self::getDirectory('john'));
+        $this->assertEquals(10, count($rights));
+    }
+
+    public function testFindRecursiveByResourceAndRole()
+    {
+        $rights =  self::$repo->findRecursiveByResourceAndRole(
+            self::getDirectory('john'),
+            self::$em->getRepository('ClarolineCoreBundle:Role')->findOneByName(self::$collaboratorRoleName)
+        );
+        $this->assertEquals(2, count($rights));
     }
 }
