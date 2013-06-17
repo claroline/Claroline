a77834edc582ff5763fdff4263a66acd0a9c5fbd
kitan1982 <anhthao.pham@claroline.net>
2013-06-17 13:00:15 +0200

    [ActivityToolBundle] Refactored open tool action

--- Listener/ToolListener.php
+++ Listener/ToolListener.php
@@ -3,6 +3,7 @@
 namespace Claroline\ActivityToolBundle\Listener;
 
 use Claroline\CoreBundle\Library\Event\DisplayToolEvent;
+use Claroline\CoreBundle\Entity\Workspace\AbstractWorkspace;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -34,62 +35,14 @@ class ToolListener extends ContainerAware
      */
     public function onDesktopOpen(DisplayToolEvent $event)
     {
-        $token = $this->container->get('security.context')->getToken();
-        $userRoles = $this->container->get('claroline.security.utilities')->getRoles($token);
-        $em = $this->container->get('doctrine.orm.entity_manager');
+        $datas = $this->fetchActivitiesDatas(true);
 
-        $criteria = array();
-        $criteria['roots'] = array();
-        $criteria['types'] = array('activity');
-        $resources = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
-            ->findByCriteria($criteria, $userRoles, true);
-
-        $resourceInfos = array();
-        $activitiesId = array();
-        $workspaceInfos = array();
-        $activityInfos = array();
-
-        foreach ($resources as $resource) {
-            $resourceId = $resource['id'];
-            $activitiesId[] = $resourceId;
-            $resourceInfos[$resourceId] = $resource;
-        }
-
-        if (count($activitiesId) > 0) {
-            $resourcesWorkspaces = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
-                ->findWorkspaceInfoByIds($activitiesId);
-
-            foreach ($resourcesWorkspaces as $resWs) {
-                $code = $resWs['code'];
-
-                if (!isset($workspaceInfos[$code])) {
-                    $workspaceInfos[$code] = array();
-                    $workspaceInfos[$code]['code'] = $code;
-                    $workspaceInfos[$code]['name'] = $resWs['name'];
-                    $workspaceInfos[$code]['resources'] = array();
-                }
-                $workspaceInfos[$code]['resources'][] = $resWs['id'];
-            }
-
-            $activities = $em->getRepository('ClarolineCoreBundle:Resource\Activity')
-                ->findActivitiesByIds($activitiesId);
-
-            foreach ($activities as $activity) {
-                $actId = $activity['id'];
-                $activityInfos[$actId] = array();
-                $activityInfos[$actId]['instructions'] = $activity['instructions'];
-                $activityInfos[$actId]['startDate'] = ($activity['startDate'] instanceof \DateTime) ?
-                    $activity['startDate']->format('Y-m-d H:i:s') : '-';
-                $activityInfos[$actId]['endDate'] = ($activity['endDate'] instanceof \DateTime) ?
-                    $activity['endDate']->format('Y-m-d H:i:s') : '-';
-            }
-        }
         $content = $this->container->get('templating')->render(
             'ClarolineActivityToolBundle::desktop_activity_list.html.twig',
             array(
-                'resourceInfos' => $resourceInfos,
-                'activityInfos' => $activityInfos,
-                'workspaceInfos' => $workspaceInfos
+                'resourceInfos' => $datas['resourceInfos'],
+                'activityInfos' => $datas['activityInfos'],
+                'workspaceInfos' => $datas['workspaceInfos']
             )
         );
         $event->setContent($content);
@@ -103,21 +56,47 @@ class ToolListener extends ContainerAware
      */
     public function onWorkspaceOpen(DisplayToolEvent $event)
     {
+        $workspace = $event->getWorkspace();
+        $datas = $this->fetchActivitiesDatas(false, $workspace);
+
+        $content = $this->container->get('templating')->render(
+            'ClarolineActivityToolBundle::workspace_activity_list.html.twig',
+            array(
+                'workspace' => $workspace,
+                'resourceInfos' => $datas['resourceInfos'],
+                'activityInfos' => $datas['activityInfos']
+            )
+        );
+        $event->setContent($content);
+        $event->stopPropagation();
+    }
+
+    private function fetchActivitiesDatas($isDesktopTool, AbstractWorkspace $workspace = null)
+    {
         $token = $this->container->get('security.context')->getToken();
         $userRoles = $this->container->get('claroline.security.utilities')->getRoles($token);
         $em = $this->container->get('doctrine.orm.entity_manager');
-        $workspace = $event->getWorkspace();
-        $root = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
-            ->findWorkspaceRoot($workspace);
+
         $criteria = array();
-        $criteria['roots'] = array($root->getPath());
+        $criteria['roots'] = array();
+
+        if (!$isDesktopTool) {
+            $root = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
+                ->findWorkspaceRoot($workspace);
+            $criteria['roots'][] = $root->getPath();
+        }
         $criteria['types'] = array('activity');
         $resources = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
             ->findByCriteria($criteria, $userRoles, true);
 
+        $activitiesDatas = array();
         $resourceInfos = array();
         $activitiesId = array();
         $activityInfos = array();
+
+        if ($isDesktopTool) {
+            $workspaceInfos = array();
+        }
 
         foreach ($resources as $resource) {
             $resourceId = $resource['id'];
@@ -126,6 +105,23 @@ class ToolListener extends ContainerAware
         }
 
         if (count($activitiesId) > 0) {
+            if ($isDesktopTool) {
+                $resourcesWorkspaces = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')
+                    ->findWorkspaceInfoByIds($activitiesId);
+
+                foreach ($resourcesWorkspaces as $resWs) {
+                    $code = $resWs['code'];
+
+                    if (!isset($workspaceInfos[$code])) {
+                        $workspaceInfos[$code] = array();
+                        $workspaceInfos[$code]['code'] = $code;
+                        $workspaceInfos[$code]['name'] = $resWs['name'];
+                        $workspaceInfos[$code]['resources'] = array();
+                    }
+                    $workspaceInfos[$code]['resources'][] = $resWs['id'];
+                }
+            }
+
             $activities = $em->getRepository('ClarolineCoreBundle:Resource\Activity')
                 ->findActivitiesByIds($activitiesId);
 
@@ -140,15 +136,13 @@ class ToolListener extends ContainerAware
             }
         }
 
-        $content = $this->container->get('templating')->render(
-            'ClarolineActivityToolBundle::workspace_activity_list.html.twig',
-            array(
-                'workspace' => $workspace,
-                'resourceInfos' => $resourceInfos,
-                'activityInfos' => $activityInfos
-            )
-        );
-        $event->setContent($content);
-        $event->stopPropagation();
+        $activitiesDatas['resourceInfos'] = $resourceInfos;
+        $activitiesDatas['activityInfos'] = $activityInfos;
+
+        if ($isDesktopTool) {
+            $activitiesDatas['workspaceInfos'] = $workspaceInfos;
+        }
+
+        return $activitiesDatas;
     }
 }
