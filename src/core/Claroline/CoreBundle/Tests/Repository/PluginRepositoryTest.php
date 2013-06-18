4184196d9bf0f462a16e4cc58a441c33fb0e625e
ngodfraind <nicolas.godfraind@gmail.com>
2013-06-18 16:52:06 +0200

    PluginRepositoryTest done.

---
+++ src/core/Claroline/CoreBundle/Tests/Repository/PluginRepositoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Claroline\CoreBundle\Repository;
+
+use Claroline\CoreBundle\Library\Testing\RepositoryTestCase;
+use Claroline\CoreBundle\Entity\Plugin;
+
+class PluginRepositoryTest extends RepositoryTestCase
+{
+    /** @var \Claroline\CoreBundle\Repository\PluginRepository */
+    public static $repo;
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+        self::loadPlatformRoleData();
+        self::$repo = self::$em->getRepository('ClarolineCoreBundle:Plugin');
+        $plugin = new Plugin();
+        $plugin->setVendorName('Vendor');
+        $plugin->setBundleName('Bundle');
+        $plugin->setHasOptions(false);
+        $plugin->setIcon('default');
+        self::$em->persist($plugin);
+        self::$em->flush();
+    }
+
+    public function testFindOneByBundleFQCN()
+    {
+        $plugin = self::$repo->findOneByBundleFQCN('Vendor\Bundle');
+        $this->assertEquals('Vendor', $plugin->getVendorNam());
+    }
+
+}
