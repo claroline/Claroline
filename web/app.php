1112e41fd4950f7873a7be959f0a4e2ffd0aaa90
ngodfraind <nicolas.godfraind@gmail.com>
2013-06-19 13:18:16 +0200

    Fixing resouces sort for anonymous and simple click for opening resources.

--- web/app.php
+++ web/app.php
@@ -8,6 +8,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 //$kernel = new AppCache(new AppKernel('prod', false));
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
