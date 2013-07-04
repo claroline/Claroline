71527ab07c1cbd5a471e93a5658f7700035e98bb
ngodfraind <nicolas.godfraind@gmail.com>
2013-07-04 15:57:48 +0200

    [ImagePlayerBundle] Updating events name.

--- Listener/ImagePlayerListener.php
+++ Listener/ImagePlayerListener.php
@@ -2,7 +2,7 @@
 
 namespace Claroline\ImagePlayerBundle\Listener;
 
-use Claroline\CoreBundle\Library\Event\PlayFileEvent;
+use Claroline\CoreBundle\Event\Event\PlayFileEvent;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Response;
 
