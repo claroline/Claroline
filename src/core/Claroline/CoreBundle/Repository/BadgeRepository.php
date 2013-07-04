a49ae8dcb432bb4511824e616de22d24ce4753e9
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-07-04 13:58:20 +0200

    Edit page work with translation

--- src/core/Claroline/CoreBundle/Repository/BadgeRepository.php
+++ src/core/Claroline/CoreBundle/Repository/BadgeRepository.php
@@ -24,4 +24,24 @@ class BadgeRepository extends EntityRepository
             ->setParameter('locale', $locale)
             ->getResult();
     }
+
+    /**
+     * @param string      $slug
+     *
+     * @return array
+     */
+    public function findBySlug($slug)
+    {
+        return $this->getEntityManager()
+            ->createQuery('
+                SELECT b, t
+                FROM ClarolineCoreBundle:Badge\Badge b
+                JOIN b.translations t
+                WHERE t.slug = :slug
+                ORDER BY t.name ASC'
+            )
+            ->setParameter('slug', $slug)
+            ->getSingleResult()
+        ;
+    }
 }
