1436b9f9e7b5090331593bdeb587a729bb1f1d23
ngodfraind <nicolas.godfraind@gmail.com>
2013-07-08 18:11:35 +0200

    ResourceRightsController done. Now fixing ResourceRightsManager.

---
+++ src/core/Claroline/CoreBundle/Manager/AbstractManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Claroline\CoreBundle\Manager;
+
+abstract class AbstractManager
+{
+    public function getEntity($class)
+    {
+        $class = '\Claroline\CoreBundle\Entity\\' . $class;
+        return new $class();
+    }
+}
