e78abc0e01aaa3a4e10187e03d426e0811e44c7c
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-07-09 11:40:27 +0200

    Adding pagination for displaying awareded badge users

--- src/core/Claroline/CoreBundle/Repository/BadgeRepository.php
+++ src/core/Claroline/CoreBundle/Repository/BadgeRepository.php
@@ -2,10 +2,35 @@
 
 namespace Claroline\CoreBundle\Repository;
 
+use Claroline\CoreBundle\Entity\Badge\Badge;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class BadgeRepository extends EntityRepository
 {
+    /**
+     * @param Badge $badge
+     *
+     * @param bool  $getQuery
+     *
+     * @return Query|array
+     */
+    public function findUsers(Badge $badge, $getQuery = false)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery('
+                SELECT ub, u
+                FROM ClarolineCoreBundle:User u
+                JOIN u.userBadges ub
+                WHERE ub.badge = :badgeId
+                ORDER BY u.lastName ASC'
+            )
+            ->setParameter('badgeId', $badge->getId())
+        ;
+
+        return ($getQuery) ? $query: $query->getResult();
+    }
+
     /**
      * @param null|string $locale
      *
