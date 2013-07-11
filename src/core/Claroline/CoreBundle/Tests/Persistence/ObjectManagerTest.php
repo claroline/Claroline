06bb975f757946a2fe5860bf7df5c3e1c2609460
Stéphane Klein <stephaneklein221@gmail.com>
2013-07-11 10:54:12 +0200

    Used variable interpolation instead of query parameter (fix entity namespace alias)

--- src/core/Claroline/CoreBundle/Tests/Persistence/ObjectManagerTest.php
+++ src/core/Claroline/CoreBundle/Tests/Persistence/ObjectManagerTest.php
@@ -116,9 +116,8 @@ class ObjectManagerTest extends MockeryTestCase
         $query = m::mock(new Query($oom));
         $oom->shouldReceive('createQuery')
             ->once()
-            ->with('SELECT object FROM :class object WHERE object.id IN (:ids)')
+            ->with('SELECT object FROM Foo\Bar object WHERE object.id IN (:ids)')
             ->andReturn($query);
-        $query->shouldReceive('setParameter')->with('class', 'Foo\Bar')->once();
         $query->shouldReceive('setParameter')->with('ids', array(1, 2))->once();
         $query->shouldReceive('getResult')->once()->andReturn(array('object 1', 'object 2'));
         $om = new ObjectManager($oom);
@@ -131,9 +130,8 @@ class ObjectManagerTest extends MockeryTestCase
         $query = m::mock(new Query($oom));
         $oom->shouldReceive('createQuery')
             ->once()
-            ->with('SELECT COUNT(object) FROM :class object')
+            ->with('SELECT COUNT(object) FROM Foo\Bar object')
             ->andReturn($query);
-        $query->shouldReceive('setParameter')->with('class', 'Foo\Bar')->once();
         $query->shouldReceive('getSingleScalarResult')->once()->andReturn(5);
         $om = new ObjectManager($oom);
         $this->assertEquals(5, $om->count('Foo\Bar'));
