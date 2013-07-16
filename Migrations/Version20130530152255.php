2be67b92f61274a7c6b6999ca805fa4de5a10cd7
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-07-16 17:23:27 +0200

    [BlogBundle] Removing associated_tag data when uninstalling plugin (doesn't work and don't know why, dropping work but not deleteing \o/)

--- Migrations/Version20130530152255.php
+++ Migrations/Version20130530152255.php
@@ -27,13 +27,15 @@ class Version20130530152255 extends BundleMigration
      */
     public function down(Schema $schema)
     {
+        //Marche pas et aucune idée de pourquoi, merci le migrationBundle
+        $this->addSql("DELETE FROM `icap__associated_tag` WHERE `taggableClass` = 'ICAP\\BlogBundle\\Entity\\Post'");
+
         $schema
             ->dropTable('icap__blog_comment')
             ->dropTable('icap__blog_post')
             ->dropTable('icap__blog_options')
             ->dropTable('icap__blog')
         ;
-         //@TODO Remove associatedTag with posts
     }
 
     /**
