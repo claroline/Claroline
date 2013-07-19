aef2cab7ccd921236d1b52ab0c408a2aeb891694
kitan1982 <anhthao.pham@claroline.net>
2013-07-19 15:06:19 +0200

    Added self-registration in workspace parameters

--- src/core/Claroline/CoreBundle/Migrations/Version20120119000000.php
+++ src/core/Claroline/CoreBundle/Migrations/Version20120119000000.php
@@ -206,7 +206,7 @@ class Version20120119000000 extends BundleMigration
         $table->addColumn('user_id', 'integer', array('notnull' => false));
         $table->addColumn('guid', 'string', array('length' => 255));
         $table->addColumn('self_registration', 'boolean', array('notnull' => false));
-        $table->addColumn('self_unsubscribe', 'boolean', array('notnull' => false));
+        $table->addColumn('self_unregistration', 'boolean', array('notnull' => false));
         $table->addUniqueIndex(array('code'));
         $table->addUniqueIndex(array('guid'));
 
