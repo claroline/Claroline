e933a11c74ca7d5d6821755f4da4a06ea68d5271
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-07-24 10:00:42 +0200

    [BlogBundle] Edition of post is possible

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -59,6 +59,47 @@ class PostController extends Controller
     }
 
     /**
+     * @Route("/{blogId}/post/edit/{postSlug}", name="icap_blog_post_edit", requirements={"blogId" = "\d+"})
+     *
+     * @ParamConverter("blog", class="ICAPBlogBundle:Blog", options={"id" = "blogId"})
+     * @ParamConverter("post", class="ICAPBlogBundle:Post", options={"slug" = "postSlug"})
+     * @Template()
+     */
+    public function editAction(Request $request, Blog $blog, Post $post)
+    {
+        $this->checkAccess("EDIT", $blog);
+
+        $form = $this->createForm(new PostType(), $post);
+
+        if("POST" === $request->getMethod()) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $translator = $this->get('translator');
+                $flashBag = $this->get('session')->getFlashBag();
+                $entityManager = $this->getDoctrine()->getManager();
+
+                try {
+                    $entityManager->persist($post);
+                    $entityManager->flush();
+
+                    $flashBag->add('success', $translator->trans('icap_blog_post_edit_success', array(), 'icap_blog'));
+                }
+                catch(\Exception $exception)
+                {
+                    $flashBag->add('error', $translator->trans('icap_blog_post_edit_error', array(), 'icap_blog'));
+                }
+                return $this->redirect($this->generateUrl('icap_blog_view', array('blogId' => $blog->getId())));
+            }
+        }
+
+        return array(
+            '_resource' => $blog,
+            'post'      => $post,
+            'form'      => $form->createView()
+        );
+    }
+
+    /**
      * @Route("/{blogId}/post/delete/{postSlug}", name="icap_blog_post_delete", requirements={"blogId" = "\d+"})
      *
      * @ParamConverter("blog", class="ICAPBlogBundle:Blog", options={"id" = "blogId"})
