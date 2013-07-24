4310e45f5e97023ce710a1306846c679c454727e
easy-es <esdras005@hotmail.com>
2013-07-24 16:39:46 +0200

    change mock implementation

--- src/core/Claroline/CoreBundle/Tests/Controller/AuthenticationControllerTest.php
+++ src/core/Claroline/CoreBundle/Tests/Controller/AuthenticationControllerTest.php
@@ -17,13 +17,13 @@ class AuthenticationControllerTest extends MockeryTestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->request = m::mock('Symfony\Component\HttpFoundation\Request');
-        $this->router = m::mock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
-        $this->userManager = m::mock('Claroline\CoreBundle\Manager\UserManager');
-        $this->encoderFactory = m::mock('Symfony\Component\Security\Core\Encoder\EncoderFactory');
-        $this->om = m::mock('Claroline\CoreBundle\Persistence\ObjectManager');
-        $this->mailer = m::mock('Swift_Mailer');
-        $this->translator = m::mock('Symfony\Component\Translation\Translator');
+        $this->request = $this->mock('Symfony\Component\HttpFoundation\Request');
+        $this->router = $this->mock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
+        $this->userManager = $this->mock('Claroline\CoreBundle\Manager\UserManager');
+        $this->encoderFactory = $this->mock('Symfony\Component\Security\Core\Encoder\EncoderFactory');
+        $this->om = $this->mock('Claroline\CoreBundle\Persistence\ObjectManager');
+        $this->mailer = $this->mock('Swift_Mailer');
+        $this->translator = $this->mock('Symfony\Component\Translation\Translator');
         $this->controller = new AuthenticationController(
             $this->request,
             $this->userManager,
@@ -36,7 +36,7 @@ class AuthenticationControllerTest extends MockeryTestCase
     }
     public function testSendEmailAction()
     {
-        $user = m::mock('Claroline\CoreBundle\Entity\User');
+        $user = $this->mock('Claroline\CoreBundle\Entity\User');
         $this->request->shouldReceive('get')->once()->with('email')->andReturn('toto@claroline.com');
         $this->userManager->shouldReceive('getUserByEmail')
             ->once()
@@ -47,7 +47,7 @@ class AuthenticationControllerTest extends MockeryTestCase
         $user->shouldReceive('setTime')->once()->with(intValue());
         $user->shouldReceive('setResetPassword')->once()->with(stringValue());
         $user->shouldReceive('getResetPassword')->once()->andReturn('123');
-        $parameterBag = m::mock('Symfony\Component\HttpFoundation\ServerBag');
+        $parameterBag = $this->mock('Symfony\Component\HttpFoundation\ServerBag');
         $parameterBag->shouldReceive('get')
             ->once()
             ->with('HTTP_ORIGIN')
