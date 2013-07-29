6bf68801bdc22d7304eb40cda7d53fb336e6086f
Stéphane Klein <stephaneklein221@gmail.com>
2013-07-29 09:22:49 +0200

    [MigrationBundle] Added writer test

--- Tests/Library/WriterTest.php
+++ Tests/Library/WriterTest.php
@@ -3,6 +3,7 @@
 namespace Claroline\MigrationBundle\Library;
 
 use Mockery as m;
+use org\bovigo\vfs\vfsStream;
 use Claroline\MigrationBundle\Tests\MockeryTestCase;
 use Claroline\MigrationBundle\Twig\SqlFormatterExtension;
 
@@ -34,12 +35,58 @@ class WriterTest extends MockeryTestCase
 
     public function testWriteMigrationClasses()
     {
-        $this->markTestSkipped('vfsstream should be used here');
-
-        $bundle = m::mock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        vfsStream::setup(
+            'root',
+            null,
+            array(
+                'bundle' => array(
+                    'path' => array(
+                        'Migrations' => array(
+                            'some_driver' => array(
+                                'Versionsome_version.php' => ''
+                            )
+                        )
+                    )
+                )
+            )
+        );
         $this->twigEnvironment->shouldReceive('addExtension');
+        $bundle = m::mock('Symfony\Component\HttpKernel\Bundle\Bundle');
+        $bundle->shouldReceive('getPath')->once()->andReturn(vfsStream::url('root') . '/bundle/path');
+        $bundle->shouldReceive('getNamespace')->once()->andReturn('Bundle\Namespace');
+        $this->fileSystem->shouldReceive('exists')
+            ->once()
+            ->with(vfsStream::url('root') . '/bundle/path/Migrations/some_driver')
+            ->andReturn(false);
+        $this->fileSystem->shouldReceive('mkdir')
+            ->once()
+            ->with(vfsStream::url('root') . '/bundle/path/Migrations/some_driver');
+        $this->twigEngine->shouldReceive('render')
+            ->once()
+            ->with(
+                'ClarolineMigrationBundle::migration_class.html.twig',
+                array(
+                    'namespace' => 'Bundle\Namespace\Migrations\some_driver',
+                    'class' => 'Versionsome_version',
+                    'upQueries' => 'queries up',
+                    'downQueries' => 'queries down'
+                )
+            )
+            ->andReturn('migration class content');
+        $this->fileSystem->shouldReceive('touch')
+            ->once()
+            ->with(vfsStream::url('root') . '/bundle/path/Migrations/some_driver/Versionsome_version.php');
 
         $writer = new Writer($this->fileSystem, $this->twigEnvironment, $this->twigEngine);
-        $writer->writeMigrationClass($bundle, 'some_driver', 'some_version', array());
+        $writer->writeMigrationClass(
+            $bundle,
+            'some_driver',
+            'some_version',
+            array(Generator::QUERIES_UP => 'queries up', Generator::QUERIES_DOWN => 'queries down')
+        );
+        $this->assertEquals(
+            'migration class content',
+            file_get_contents(vfsStream::url('root') . '/bundle/path/Migrations/some_driver/Versionsome_version.php')
+        );
     }
 }
