397ceec8d254cb80d0bd2b144ad5c134df0e74c2
easy-es <esdras005@hotmail.com>
2013-08-05 11:58:36 +0200

    upgrade tinymce bundle

--- src/core/Claroline/CoreBundle/Resources/public/js/tinymce/config.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/tinymce/config.js
@@ -1,17 +1,6 @@
-// Create a new plugin class
-tinymce.create('tinymce.plugins.ExamplePlugin', {
-    init : function (ed, url) {
-        // Register an example button
-        ed.addButton('ressourceLinker', {
-            title : 'ressourceLinker',
-            image : 'http://stfalcon.com/favicon.ico',
-            onclick : function () {
-                Claroline.ResourceManager.picker('open');
-            },
-            'class' : 'bold' // Use the bold icon from the theme
-        });
-    }
-});
 
-// Register plugin with a short name
-tinymce.PluginManager.add('ressourceLinker', tinymce.plugins.ExamplePlugin);
+function tinymce_button_ressourceLinker (ed) {
+    ed.focus();
+    Claroline.ResourceManager.picker('open');
+}
+
