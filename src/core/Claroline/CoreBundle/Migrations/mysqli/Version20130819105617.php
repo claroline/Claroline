8c54662a2a226676a976582f7396cc69b5a721db
nicolas <nicolas.godfraind@gmail.com>
2013-08-19 14:45:41 +0200

    Updating submodules + migrations.

---
+++ src/core/Claroline/CoreBundle/Migrations/mysqli/Version20130819105617.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Claroline\CoreBundle\Migrations\mysqli;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2013/08/19 10:56:18
+ */
+class Version20130819105617 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_activity 
+            DROP FOREIGN KEY FK_E4A67CACB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_activity 
+            ADD CONSTRAINT FK_E4A67CACB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_file 
+            DROP FOREIGN KEY FK_EA81C80BB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_file 
+            ADD CONSTRAINT FK_EA81C80BB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_link 
+            DROP FOREIGN KEY FK_50B267EAB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_link 
+            ADD CONSTRAINT FK_50B267EAB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_directory 
+            DROP FOREIGN KEY FK_12EEC186B87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_directory 
+            ADD CONSTRAINT FK_12EEC186B87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_resource_shortcut 
+            DROP FOREIGN KEY FK_5E7F4AB8B87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_resource_shortcut 
+            ADD CONSTRAINT FK_5E7F4AB8B87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_text 
+            DROP FOREIGN KEY FK_5D9559DCB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_text 
+            ADD CONSTRAINT FK_5D9559DCB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_activity 
+            DROP FOREIGN KEY FK_E4A67CACB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_activity 
+            ADD CONSTRAINT FK_E4A67CACB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            ALTER TABLE claro_directory 
+            DROP FOREIGN KEY FK_12EEC186B87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_directory 
+            ADD CONSTRAINT FK_12EEC186B87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            ALTER TABLE claro_file 
+            DROP FOREIGN KEY FK_EA81C80BB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_file 
+            ADD CONSTRAINT FK_EA81C80BB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            ALTER TABLE claro_link 
+            DROP FOREIGN KEY FK_50B267EAB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_link 
+            ADD CONSTRAINT FK_50B267EAB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            ALTER TABLE claro_resource_shortcut 
+            DROP FOREIGN KEY FK_5E7F4AB8B87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_resource_shortcut 
+            ADD CONSTRAINT FK_5E7F4AB8B87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+        $this->addSql("
+            ALTER TABLE claro_text 
+            DROP FOREIGN KEY FK_5D9559DCB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_text 
+            ADD CONSTRAINT FK_5D9559DCB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+    }
+}
