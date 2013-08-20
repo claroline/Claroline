b5abb81df85400cfbdaa4025b3b310d8aef0b247
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-08-20 11:24:17 +0200

    [BlogBundle] Delete blog.js file

--- Resources/public/js/blog.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $(function() {
-        $('#calendar').fullCalendar({
-            // put your options and callbacks here
-        })
-
-        $('.delete').confirmModal();
-    });
-})(jQuery);
