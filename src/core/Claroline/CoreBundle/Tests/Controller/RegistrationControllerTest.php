43d939bb56597dcdfb289d22b7b85e4a7ae8d9d8
nicolas <nicolas.godfraind@gmail.com>
2013-08-21 17:34:57 +0200

    Post authentication and registration.

---
+++ src/core/Claroline/CoreBundle/Tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Claroline\CoreBundle\Controller;
+
+use \Mockery as m;
+use Claroline\CoreBundle\Library\Testing\MockeryTestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Claroline\CoreBundle\Entity\User;
+
+/**
+ * Controller for user self-registration. Access to this functionality requires
+ * that the user is anonymous and the self-registration is allowed by the
+ * platform configuration.
+ */
+class RegistrationControllerTest extends MockeryTestCase
+{
+    private $request;
+    private $userManager;
+    private $configHandler;
+    private $validator;
+    private $controller;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->request = $this->mock('Symfony\Component\HttpFoundation\Request');
+        $this->userManager = $this->mock('Claroline\CoreBundle\Manager\UserManager');
+        $this->configHandler = $this->mock('Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler');
+        $this->validator = $this->mock('Symfony\Component\Validator\ValidatorInterface');
+        $this->controller = new RegistrationController(
+            $this->request,
+            $this->userManager,
+            $this->configHandler,
+            $this->validator
+        );
+    }
+
+    public function testUnauthorizedPostUserRegistrationAction()
+    {
+        $this->configHandler->shouldReceive('getParameter')
+            ->with('allow_self_registration')->once()->andReturn(false);
+
+        $response = new JsonResponse(array(), 403);
+        $this->assertEquals($response, $this->controller->postUserRegistrationAction());
+    }
+
+    public function testSuccessfullPostUserRegistrationAction()
+    {
+        $this->configHandler->shouldReceive('getParameter')
+            ->with('allow_self_registration')->once()->andReturn(true);
+
+        $bag = $this->getUserParameterBag();
+        $this->request->request = $bag;
+        $this->validator->shouldReceive('validate')->once()->with(
+            m::on(
+                function (User $user) {
+                    return $user->getPlainPassword() === 'password'
+                        && $user->getUsername() === 'username'
+                        && $user->getFirstName() === 'firstname'
+                        && $user->getLastName() === 'lastname'
+                        && $user->getMail() === 'mail@mail.com';
+                }
+            )
+        )->andReturn(array());
+        $this->userManager->shouldReceive('createUser')->once()->with(
+            m::on(
+                function (User $user) {
+                    return $user->getPlainPassword() === 'password'
+                        && $user->getUsername() === 'username'
+                        && $user->getFirstName() === 'firstname'
+                        && $user->getLastName() === 'lastname'
+                        && $user->getMail() === 'mail@mail.com';
+                }
+            )
+        );
+
+        $response = new JsonResponse(array(), 200);
+        $this->assertEquals($response, $this->controller->postUserRegistrationAction());
+    }
+
+    public function testFailedPostUserRegistrationAction()
+    {
+        $this->configHandler->shouldReceive('getParameter')
+            ->with('allow_self_registration')->once()->andReturn(true);
+
+        $bag = $this->getUserParameterBag();
+        $this->request->request = $bag;
+        $error = $this->mock('Symfony\Component\Validator\ConstraintViolation');
+        $error->shouldReceive('getPropertyPath')->once()->andReturn('username');
+        $error->shouldReceive('getMessage')->once()->andReturn('message');
+        $errorList = array($error);
+
+        $this->validator->shouldReceive('validate')->once()->with(
+            m::on(
+                function (User $user) {
+                    return $user->getPlainPassword() === 'password'
+                        && $user->getUsername() === 'username'
+                        && $user->getFirstName() === 'firstname'
+                        && $user->getLastName() === 'lastname'
+                        && $user->getMail() === 'mail@mail.com';
+                }
+            )
+        )->andReturn($errorList);
+
+        $response = new JsonResponse(array(array('property' => 'username', 'message' => 'message')), 422);
+        $this->assertEquals($response, $this->controller->postUserRegistrationAction());
+    }
+
+    private function getUserParameterBag()
+    {
+        $parameterBag = $this->mock('Symfony\Component\HttpFoundation\ServerBag');
+        $parameterBag->shouldReceive('get')->with('username')->once()->andReturn('username');
+        $parameterBag->shouldReceive('get')->with('password')->once()->andReturn('password');
+        $parameterBag->shouldReceive('get')->with('firstname')->once()->andReturn('firstname');
+        $parameterBag->shouldReceive('get')->with('lastname')->once()->andReturn('lastname');
+        $parameterBag->shouldReceive('get')->with('mail')->once()->andReturn('mail@mail.com');
+
+        return $parameterBag;
+    }
+}
