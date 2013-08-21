8afbab959178721abfc3b30ee1eb45e4e263ba1a
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-08-21 16:43:03 +0200

    Add new migration files for badgebundle

---
+++ src/core/Claroline/BadgeBundle/Migrations/pdo_pgsql/Version20130821154602.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Claroline\BadgeBundle\Migrations\pdo_pgsql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2013/08/21 03:46:04
+ */
+class Version20130821154602 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            CREATE TABLE claro_user_badge (
+                id SERIAL NOT NULL, 
+                user_id INT NOT NULL, 
+                badge_id INT NOT NULL, 
+                issuer_id INT DEFAULT NULL, 
+                issued_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_7EBB381FA76ED395 ON claro_user_badge (user_id)
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_7EBB381FF7A2C2FC ON claro_user_badge (badge_id)
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_7EBB381FBB9D6FEE ON claro_user_badge (issuer_id)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX user_badge_unique ON claro_user_badge (user_id, badge_id)
+        ");
+        $this->addSql("
+            CREATE TABLE claro_badge_claim (
+                id SERIAL NOT NULL, 
+                user_id INT NOT NULL, 
+                badge_id INT NOT NULL, 
+                claimed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_487A496AA76ED395 ON claro_badge_claim (user_id)
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_487A496AF7A2C2FC ON claro_badge_claim (badge_id)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX badge_claim_unique ON claro_badge_claim (user_id, badge_id)
+        ");
+        $this->addSql("
+            CREATE TABLE claro_badge_translation (
+                id SERIAL NOT NULL, 
+                badge_id INT DEFAULT NULL, 
+                locale VARCHAR(8) NOT NULL, 
+                name VARCHAR(128) NOT NULL, 
+                description VARCHAR(128) NOT NULL, 
+                slug VARCHAR(128) NOT NULL, 
+                criteria TEXT NOT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_849BC8315E237E06 ON claro_badge_translation (name)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_849BC831989D9B62 ON claro_badge_translation (slug)
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_849BC831F7A2C2FC ON claro_badge_translation (badge_id)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX badge_translation_unique_idx ON claro_badge_translation (locale, badge_id)
+        ");
+        $this->addSql("
+            CREATE TABLE claro_badge (
+                id SERIAL NOT NULL, 
+                version SMALLINT NOT NULL, 
+                image VARCHAR(255) NOT NULL, 
+                expired_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            ALTER TABLE claro_user_badge 
+            ADD CONSTRAINT FK_7EBB381FA76ED395 FOREIGN KEY (user_id) 
+            REFERENCES claro_user (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_user_badge 
+            ADD CONSTRAINT FK_7EBB381FF7A2C2FC FOREIGN KEY (badge_id) 
+            REFERENCES claro_badge (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_user_badge 
+            ADD CONSTRAINT FK_7EBB381FBB9D6FEE FOREIGN KEY (issuer_id) 
+            REFERENCES claro_user (id) 
+            ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_claim 
+            ADD CONSTRAINT FK_487A496AA76ED395 FOREIGN KEY (user_id) 
+            REFERENCES claro_user (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_claim 
+            ADD CONSTRAINT FK_487A496AF7A2C2FC FOREIGN KEY (badge_id) 
+            REFERENCES claro_badge (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_translation 
+            ADD CONSTRAINT FK_849BC831F7A2C2FC FOREIGN KEY (badge_id) 
+            REFERENCES claro_badge (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_user_badge 
+            DROP CONSTRAINT FK_7EBB381FF7A2C2FC
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_claim 
+            DROP CONSTRAINT FK_487A496AF7A2C2FC
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_translation 
+            DROP CONSTRAINT FK_849BC831F7A2C2FC
+        ");
+        $this->addSql("
+            DROP TABLE claro_user_badge
+        ");
+        $this->addSql("
+            DROP TABLE claro_badge_claim
+        ");
+        $this->addSql("
+            DROP TABLE claro_badge_translation
+        ");
+        $this->addSql("
+            DROP TABLE claro_badge
+        ");
+    }
+}
