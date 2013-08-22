2333faf59b630f05ed0a4f30785829055ed6b1e1
nicolas <nicolas.godfraind@gmail.com>
2013-08-22 14:10:51 +0200

    [ForumBundle] Upgrading migrations.

---
+++ Migrations/pdo_mysql/Version20130822140941.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Claroline\ForumBundle\Migrations\pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2013/08/22 02:09:42
+ */
+class Version20130822140941 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_forum 
+            DROP FOREIGN KEY FK_F2869DFB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_forum 
+            ADD CONSTRAINT FK_F2869DFB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_forum 
+            DROP FOREIGN KEY FK_F2869DFB87FAB32
+        ");
+        $this->addSql("
+            ALTER TABLE claro_forum 
+            ADD CONSTRAINT FK_F2869DFB87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id)
+        ");
+    }
+}
