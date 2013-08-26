59045b2b8bcdf396bf30d3d8101bdbdc641ec0ad
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-08-26 10:32:33 +0200

    Update award form with boostrap3

--- src/core/Claroline/BadgeBundle/Resources/public/js/edit.js
+++ src/core/Claroline/BadgeBundle/Resources/public/js/edit.js
@@ -21,7 +21,6 @@ $(function(){
     });
 
     var awardFormContainer  = $("#award_form_container");
-    awardFormContainer.css('display', 'none');
     var awardUsersContainer = $("#award_users_container");
 
     $('#awardUser').click(function(event) {
