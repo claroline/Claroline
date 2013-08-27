a069f84b7cea320e6fbc059eae9217ab11e25e3d
nicolas <nicolas.godfraind@gmail.com>
2013-08-27 13:57:38 +0200

    new params for tool configuration: is_configurable_in_workspace & is_configurable_in_desktop (will replace hasOptions)

--- src/core/Claroline/CoreBundle/DataFixtures/Required/LoadToolsData.php
+++ src/core/Claroline/CoreBundle/DataFixtures/Required/LoadToolsData.php
@@ -22,12 +22,12 @@ class LoadToolsData extends AbstractFixture implements ContainerAwareInterface, 
     public function load(ObjectManager $manager)
     {
         $tools = array(
-            array('home', 'icon-home', false, false, true, true, true, true),
-            array('parameters', 'icon-cog', false, false, true, true, false, false),
-            array('resource_manager', 'icon-folder-open', false, false, true, true, true, true),
-            array('agenda', 'icon-calendar', false, false, true, true, false, false),
-            array('logs', 'icon-list', false, false, true, false, false, false),
-            array('roles', 'icon-user', true, false, true, false, false, false)
+            array('home', 'icon-home', false, false, true, true, true, true, true),
+            array('parameters', 'icon-cog', false, false, true, true, false, false, false),
+            array('resource_manager', 'icon-folder-open', false, false, true, true, true, true, false),
+            array('agenda', 'icon-calendar', false, false, true, true, false, false, false),
+            array('logs', 'icon-list', false, false, true, false, false, false, false),
+            array('roles', 'icon-user', true, false, true, false, false, false, false)
         );
 
         foreach ($tools as $tool) {
@@ -39,7 +39,8 @@ class LoadToolsData extends AbstractFixture implements ContainerAwareInterface, 
             $entity->setDisplayableInWorkspace($tool[4]);
             $entity->setDisplayableInDesktop($tool[5]);
             $entity->setExportable($tool[6]);
-            $entity->setHasOptions($tool[7]);
+            $entity->setIsConfigurableInWorkspace($tool[7]);
+            $entity->setIsConfigurableInDesktop($tool[8]);
 
             $manager->persist($entity);
         }
