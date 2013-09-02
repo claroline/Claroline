14fa6a0438ff299d2f71acc904b0ce3982e97f6e
nicolas <nicolas.godfraind@gmail.com>
2013-09-02 16:00:24 +0200

    Various bugfixes (closes #237, closes #236, closes #233)

--- src/core/Claroline/CoreBundle/Form/ActivityType.php
+++ src/core/Claroline/CoreBundle/Form/ActivityType.php
@@ -5,12 +5,13 @@ namespace Claroline\CoreBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ActivityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text');
+        $builder->add('name', 'text', array('constraints' => new NotBlank()));
         $builder->add('instructions', 'textarea');
     }
 
