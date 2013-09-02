9e74861a96de38003db144d45ce776cb5470546e
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-02 17:12:02 +0200

    [MigrationBundle] Prepared kernel bundle integration

--- ClarolineMigrationBundle.php
+++ ClarolineMigrationBundle.php
@@ -3,7 +3,18 @@
 namespace Claroline\MigrationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class ClarolineMigrationBundle extends Bundle
+class ClarolineMigrationBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environement)
+    {
+        return true;
+    }
+
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
