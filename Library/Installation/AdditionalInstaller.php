27aba98814dc124f8ce93c87a2e59fe411627607
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-05 16:59:58 +0200

    [CoreBundle] Handled database creation

--- Library/Installation/AdditionalInstaller.php
+++ Library/Installation/AdditionalInstaller.php
@@ -3,11 +3,34 @@
 namespace Claroline\CoreBundle\Library\Installation;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
 use Claroline\CoreBundle\Library\Workspace\TemplateBuilder;
 
 class AdditionalInstaller extends ContainerAware
 {
     public function preInstall()
+    {
+        $this->createDatabase();
+        $this->buildDefautTemplate();
+    }
+
+    private function createDatabase()
+    {
+        $command = new CreateDatabaseDoctrineCommand();
+        $command->setContainer($this->container);
+        $code = $command->run(new ArrayInput(array(), new NullOutput()));
+
+        if ($code !== 0) {
+            throw new \Exception(
+                'Database cannot be created : either it already exists either '
+                . 'the connection parameters or rights are incorrect'
+            );
+        }
+    }
+
+    private function buildDefautTemplate()
     {
         $defaultTemplatePath = $this->container->getParameter('kernel.root_dir') . '/../templates/default.zip';
         $translator = $this->container->get('translator'); // useless
