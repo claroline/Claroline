a7e81dc12ed1b2e70ef965c1cfff94b2e728ea37
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-09-05 15:24:08 +0200

    Moving class for configuring log in admin and workspace in log namespace + update label translation key

--- src/core/Claroline/CoreBundle/Form/Log/WorkspaceLogFilterType.php
+++ src/core/Claroline/CoreBundle/Form/Log/WorkspaceLogFilterType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Claroline\CoreBundle\Form;
+namespace Claroline\CoreBundle\Form\Log;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,29 +18,28 @@ class WorkspaceLogFilterType extends AbstractType
                     'attr' => array('class' => 'input-medium'),
                     'choices' => array(
                         'all' => 'all',
-                        'ws_tool_read' => 'log_workspace_read',
+                        'workspace-tool-read' => 'log_workspace_read',
                         'resource' => array(
-                            'resource_all' => 'all',
-                            'resource_create' => 'log_create',
-                            'resource_read' => 'log_read',
-                            'resource_export' => 'log_export',
-                            'resource_delete' => 'log_delete',
-                            'resource_update' => 'log_update',
-                            'resource_move' => 'log_move',
-                            'resource_copy' => 'log_copy',
-                            'resource_shortcut' => 'log_shortcut',
-                            'resource_child_update' => 'log_child_update'
+                            'resource-all' => 'all',
+                            'resource-create' => 'log_create',
+                            'resource-read' => 'log_read',
+                            'resource-export' => 'log_export',
+                            'resource-delete' => 'log_delete',
+                            'resource-update' => 'log_update',
+                            'resource-move' => 'log_move',
+                            'resource-copy' => 'log_copy',
+                            'resource-shortcut' => 'log_shortcut'
                         ),
                         'role' => array(
-                            'ws_role_all' => 'all',
-                            'ws_role_create' => 'log_create',
-                            'ws_role_delete' => 'log_delete',
-                            'ws_role_update' => 'log_update',
-                            'ws_role_change_right' => 'log_change_right',
-                            'ws_role_subscribe_user' => 'log_subscribe_user',
-                            'ws_role_unsubscribe_user' => 'log_unsubscribe_user',
-                            'ws_role_subscribe_group' => 'log_subscribe_group',
-                            'ws_role_unsubscribe_group' => 'log_unsubscribe_group'
+                            'workspace-role-all' => 'all',
+                            'workspace-role-create' => 'log_create',
+                            'workspace-role-delete' => 'log_delete',
+                            'workspace-role-update' => 'log_update',
+                            'workspace-role-change_right' => 'log_change_right',
+                            'workspace-role-subscribe_user' => 'log_subscribe_user',
+                            'workspace-role-unsubscribe_user' => 'log_unsubscribe_user',
+                            'workspace-role-subscribe_group' => 'log_subscribe_group',
+                            'workspace-role-unsubscribe_group' => 'log_unsubscribe_group'
                         )
                     )
                 )
