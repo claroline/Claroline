77142426171eb62b96ddd6b0f19149c115497c0f
nicolas <nicolas.godfraind@gmail.com>
2013-09-06 08:58:59 +0200

    Displaying ajax error (1)

--- src/core/Claroline/CoreBundle/Resources/public/js/startup.js
+++ src/core/Claroline/CoreBundle/Resources/public/js/startup.js
@@ -8,7 +8,7 @@
             var w = window.open();
             $(w.document.body).html(html);
         } else {
-            alert('An error occured. Please contact the administrator');
+            alert(html);
         }
     };
     var ajaxAuthenticationErrorHandler = function (form) {
@@ -53,9 +53,10 @@
     });
 
     $(document).ajaxError(function (event, jqXHR) {
-        if (jqXHR.status === 403) {
+        console.debug(jqXHR.getResponseHeader('XXX-Claroline'));
+        if (jqXHR.status === 403 && jqXHR.getResponseHeader('XXX-Claroline') !== 'insufficient-permissions') {
             ajaxAuthenticationErrorHandler(jqXHR.responseText);
-        } else if (jqXHR.status === 500) {
+        } else if (jqXHR.status === 500 || jqXHR.status === 422 || jqXHR.status === 403) {
             ajaxServerErrorHandler(jqXHR.responseText);
         }
     });
