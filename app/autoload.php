737eaf31e6fa789a14839b9aaad2d00df593da9c
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-07 14:41:50 +0200

    Modified autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -2,25 +2,10 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+if (file_exists($loaderFile = __DIR__.'/../vendor/autoload.php')) {
+    $loader = require $loaderFile;
 
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+    return $loader;
 }
-
-if (file_exists(__DIR__ . '/config/local/plugin/namespaces')) {
-    $namespaces = file(__DIR__ . '/config/local/plugin/namespaces', FILE_IGNORE_NEW_LINES);
-
-    foreach ($namespaces as $namespace) {
-        $loader->add($namespace, __DIR__ . '/../src/plugin');
-    }
-}
-
-$loader->add('Claroline', array(__DIR__.'/../src/core', __DIR__.'/../src/plugin'));
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-return $loader;
