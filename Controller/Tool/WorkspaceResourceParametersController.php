3cca03a23ca457af4e05fd68ea7f0550676d5e27
easy-es <esdras005@hotmail.com>
2013-09-13 14:38:28 +0200

    [CoreBundle] undefined variable fixed

--- Controller/Tool/WorkspaceResourceParametersController.php
+++ Controller/Tool/WorkspaceResourceParametersController.php
@@ -4,9 +4,22 @@ namespace Claroline\CoreBundle\Controller\Tool;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Claroline\CoreBundle\Controller\Tool\AbstractParametersController;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use JMS\DiExtraBundle\Annotation as DI;
 
 class WorkspaceResourceParametersController extends AbstractParametersController
 {
+    private $security;
+
+    /**
+     * @DI\InjectParams({
+     *     "security"           = @DI\Inject("security.context")
+     * })
+     */
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
     /**
      * @Template("ClarolineCoreBundle:Resource:configResourcesManager.html.twig")
      */
