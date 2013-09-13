d0ecfddf4d5849d393179f03f17d817cb8ff70ea
kitan1982 <anhthao.pham@claroline.net>
2013-09-13 14:25:35 +0200

    [AnnouncementBundle] Refactored with new version of PluginBundle

--- ClarolineAnnouncementBundle.php
+++ ClarolineAnnouncementBundle.php
@@ -3,10 +3,22 @@
 namespace Claroline\AnnouncementBundle;
 
 use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
 /**
  * Bundle class.
  */
 class ClarolineAnnouncementBundle extends PluginBundle
 {
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'announcement');
+    }
+
+    public function getRequiredFixturesDirectory($environment)
+    {
+        return 'DataFixtures';
+    }
 }
