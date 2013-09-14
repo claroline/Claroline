3e84227cb0c8a9c280a1707e06a6858e458e745a
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-14 16:54:31 +0200

    [VideoPlayerBundle] Adapted bundle to new installation system

--- ClarolineVideoPlayerBundle.php
+++ ClarolineVideoPlayerBundle.php
@@ -6,5 +6,16 @@ use Claroline\CoreBundle\Library\PluginBundle;
 
 class ClarolineVideoPlayerBundle extends PluginBundle
 {
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
 
+        return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'video-player');
+    }
+
+    public function hasMigrations()
+    {
+        return false;
+    }
 }
+
