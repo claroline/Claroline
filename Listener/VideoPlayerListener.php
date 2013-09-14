74dfb509231c9b95b2c9d5f07a3e1b54d64b264e
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-14 16:55:12 +0200

    [VideoPlayerBundle] Fixed event name

--- Listener/VideoPlayerListener.php
+++ Listener/VideoPlayerListener.php
@@ -2,7 +2,7 @@
 
 namespace Claroline\VideoPlayerBundle\Listener;
 
-use Claroline\CoreBundle\Event\Event\PlayFileEvent;
+use Claroline\CoreBundle\Event\PlayFileEvent;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Response;
 
