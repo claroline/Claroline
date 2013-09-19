9446f97e225fd3fbb6f8c412d6ba92e09d987245
Jorge Solis <jorgeluissolis@gmail.com>
2013-09-19 15:36:45 +0200

    [PdfPlayerBundle] StremedResponse for a PDF

--- Controller/PdfPlayerController.php
+++ Controller/PdfPlayerController.php
@@ -3,7 +3,7 @@
 namespace Claroline\PdfPlayerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Claroline\CoreBundle\Entity\Resource\ResourceNode;
 
@@ -19,10 +19,17 @@ class PdfPlayerController extends Controller
     {
         $pdf = $this->get('claroline.manager.resource_manager')->getResourceFromNode($node);
 
+        $response = new StreamedResponse();
         $path = $this->container->getParameter(
             'claroline.param.files_directory'
         ).DIRECTORY_SEPARATOR.$pdf->getHashName();
+        $response->setCallBack(
+            function () use ($path) {
+                readfile($path);
+            }
+        );
+        $response->headers->set('Content-Type', $node->getMimeType());
 
-        return new Response(file_get_contents($path));
+        return $response;
     }
 }
