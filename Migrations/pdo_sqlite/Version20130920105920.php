fd20c6fe5e2d7c07b1365a73078e49ec845bef19
Panos <ptsavdar@gmail.com>
2013-09-20 11:00:56 +0200

    [WikiBundle] Added new migration files

---
+++ Migrations/pdo_sqlite/Version20130920105920.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Icap\WikiBundle\Migrations\pdo_sqlite;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2013/09/20 10:59:21
+ */
+class Version20130920105920 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            CREATE TABLE icap__wikibundle_section (
+                id INTEGER NOT NULL, 
+                wiki_id INTEGER NOT NULL, 
+                parent_id INTEGER DEFAULT NULL, 
+                name VARCHAR(255) NOT NULL, 
+                visible BOOLEAN DEFAULT '0' NOT NULL, 
+                text CLOB DEFAULT NULL, 
+                created DATETIME NOT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_F79A2D04AA948DBE ON icap__wikibundle_section (wiki_id)
+        ");
+        $this->addSql("
+            CREATE INDEX IDX_F79A2D04727ACA70 ON icap__wikibundle_section (parent_id)
+        ");
+        $this->addSql("
+            CREATE TABLE icap__wikibundle_wiki (
+                id INTEGER NOT NULL, 
+                text CLOB DEFAULT NULL, 
+                resourceNode_id INTEGER DEFAULT NULL, 
+                PRIMARY KEY(id)
+            )
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_31A29422B87FAB32 ON icap__wikibundle_wiki (resourceNode_id)
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            DROP TABLE icap__wikibundle_section
+        ");
+        $this->addSql("
+            DROP TABLE icap__wikibundle_wiki
+        ");
+    }
+}
