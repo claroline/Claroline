a9d142f04d3e41722fed4aa751db06e09d0acf68
Eric VINCENT <ericvincenterv@gmail.com>
2013-09-24 14:43:15 +0200

    [PathBundle] OK

--- Resources/public/js/script.js
+++ Resources/public/js/script.js
@@ -1,4 +1,4 @@
- $(document).ready(function() {
+$(document).ready(function() {
 	
 	$(document).delegate(".abstractWorkspace","click",function(e){
 		$("#abstract-resources-list").css("display","block");
