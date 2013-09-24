a14a16a3c102a30211f3d3cad23b305b4ac7b99f
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-09-24 16:50:03 +0200

    [CoreBundle] Interface modification for add/editing badge rules + correct compareRoute macro warning + add config for zenstruckform bundle

--- Resources/public/js/administration/badge/rules.js
+++ Resources/public/js/administration/badge/rules.js
@@ -2,6 +2,14 @@ $(function(){
     var ruleTabs = $("#ruleTabs");
     $('li:first-child a', ruleTabs).tab('show');
 
+    var noRuleBlock = $('#no_rule');
+
+    if (0 == $("a[data-toggle='tab']", ruleTabs).length) {
+        noRuleBlock.show();
+    } else {
+        noRuleBlock.hide();
+    }
+
     var tabPrototype        = ruleTabs.attr('data-tab-prototype');
     var tabContentPrototype = $(".badges_rules").attr('data-prototype');
 
@@ -10,26 +18,36 @@ $(function(){
 
     addTagLink.click(function(event){
         addRule();
-        $('#no_rule').hide();
+        noRuleBlock.hide();
         event.preventDefault();
     });
 
+    var ruleDeleteSelector = ".delete_rule";
+
     $(ruleTabs).on({
         mouseenter: function () {
-            $(".delete", $(this)).show();
+            $(ruleDeleteSelector, $(this)).show();
         },
         mouseleave: function () {
-            $(".delete", $(this)).hide();
+            $(ruleDeleteSelector, $(this)).hide();
         }
     }, "li");
 
-    var ruleDeleteSelector = "li .delete";
+    var ruleDeleteNoConfirmSelector = "li .delete_rule_no_confirm";
+    var ruleDeleteConfirmSelector   = "li .delete_rule_confirm";
     $(ruleDeleteSelector, ruleTabs).hide();
     $(ruleTabs).on({
         click: function () {
             deleteRule($(this).attr('data-id-tab'));
         }
-    }, ruleDeleteSelector);
+    }, ruleDeleteNoConfirmSelector);
+
+    $(ruleDeleteConfirmSelector, ruleTabs).confirmModal({'confirmCallback': confirmDeleteRule});
+
+    function confirmDeleteRule(element)
+    {
+        deleteRule(element.attr('data-id-tab'));
+    }
 
     function addRule()
     {
@@ -61,7 +79,7 @@ $(function(){
         $('li:first-child a', ruleTabs).tab('show');
 
         if (0 == $("a[data-toggle='tab']", ruleTabs).length) {
-            $('#no_rule').show();
+            noRuleBlock.show();
         }
     }
 });
