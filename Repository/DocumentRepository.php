e513b316ed8dcf9a741ef6d14c02dca542e029d9
Vaince <vincent.saisset@gmail.com>
2013-09-26 14:04:42 +0200

    [CollecticielBundle] Make my copy + UI edit improve

---
+++ Repository/DocumentRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by : Vincent SAISSET
+ * Date: 05/09/13
+ * Time: 14:56
+ */
+
+namespace Icap\DropzoneBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class DocumentRepository extends EntityRepository {
+
+}
