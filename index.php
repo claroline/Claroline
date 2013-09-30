80af8768a25bd94d0ad47d291c1c04714b9879b2
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-30 09:20:30 +0200

    [WebInstaller] Used composer for autoload setup

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 
 require_once __DIR__ . '/../../autoload.php';
 
-// tmp -> composer
-spl_autoload_register(function ($class) {
-    if (false !== strpos($class, 'Claroline\WebInstaller')) {
-        if (file_exists($file = __DIR__ . '/lib/' . str_replace('\\', '//', $class) . '.php')) {
-            require_once $file;
-        }
-    }
-});
-
 session_start();
 $container = new Claroline\WebInstaller\Container(__DIR__, $_SERVER['SCRIPT_NAME']);
 $router = new Claroline\WebInstaller\Router($container);
