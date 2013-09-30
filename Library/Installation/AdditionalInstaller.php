68b0c152c38b6c45bda13bb0a319e7971c1988f3
ngodfraind <nicolas.godfraind@gmail.com>
2013-09-30 13:55:46 +0200

    [CoreBundle] update platform command

--- Library/Installation/AdditionalInstaller.php
+++ Library/Installation/AdditionalInstaller.php
@@ -2,17 +2,12 @@
 
 namespace Claroline\CoreBundle\Library\Installation;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
-use Doctrine\Common\Persistence\Mapping\MappingException;
-use Claroline\CoreBundle\Entity\Home\HomeTab;
-use Claroline\CoreBundle\Entity\Widget\Widget;
 use Claroline\CoreBundle\Library\Workspace\TemplateBuilder;
 use Claroline\InstallationBundle\Additional\AdditionalInstaller as BaseInstaller;
 use Claroline\InstallationBundle\Bundle\BundleVersion;
-use Claroline\CoreBundle\Entity\Widget\WidgetInstance;
 
 class AdditionalInstaller extends BaseInstaller
 {
@@ -20,16 +15,18 @@ class AdditionalInstaller extends BaseInstaller
     {
         $this->createDatabaseIfNotExists();
         $this->buildDefaultTemplate();
-        $this->saveTextConfigs();
     }
 
-     public function postUpdate(BundleVersion $current, BundleVersion $target)
+    public function preUpdate(BundleVersion $current, BundleVersion $target)
     {
-//        $this->createWorkspacesListWidget();
-
+    }
+    
+    public function postUpdate(BundleVersion $current, BundleVersion $target)
+    {
+        $updater002000005 = new Updater\Updater002000005($this->container);
+                
         if (version_compare($current->getVersion(), '1.4', '<')) {
-            $this->updateWidgetsDatas();
-//            $this->updateAdminWorkspaceHomeTabDatas();
+            $updater002000005->postUpdate();
         }
     }
 
@@ -62,131 +59,5 @@ class AdditionalInstaller extends BaseInstaller
         $this->log('Creating default workspace template...');
         $defaultTemplatePath = $this->container->getParameter('kernel.root_dir') . '/../templates/default.zip';
         TemplateBuilder::buildDefault($defaultTemplatePath);
-    }
-
-    private function createWorkspacesListWidget()
-    {
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        try {
-            $workspaceWidget = $em->getRepository('ClarolineCoreBundle:Widget\Widget')
-                ->findOneByName('my_workspaces');
-
-            if (is_null($workspaceWidget)) {
-                $this->log('Creating workspaces list widget...');
-                $widget = new Widget();
-                $widget->setName('my_workspaces');
-                $widget->setConfigurable(false);
-                $widget->setIcon('fake/icon/path');
-                $widget->setPlugin(null);
-                $widget->setExportable(false);
-                $em->persist($widget);
-                $em->flush();
-
-                $widgetConfig = new WidgetInstance();
-                $widgetConfig->setWidget($widget);
-                $widgetConfig->setLock(false);
-                $widgetConfig->setVisible(true);
-                $widgetConfig->setParent(null);
-                $widgetConfig->setDesktop(true);
-
-                $em->persist($widgetConfig);
-                $em->flush();
-            }
-        }
-        catch (MappingException $e) {
-            $this->log('A MappingException has been thrown while trying to get Widget repository');
-        }
-    }
-
-    private function updateAdminWorkspaceHomeTabDatas()
-    {
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        try {
-            $homeTabConfigRepo = $em->getRepository('ClarolineCoreBundle:Home\HomeTabConfig');
-            $widgetHTCRepo = $em->getRepository('ClarolineCoreBundle:Widget\WidgetHomeTabConfig');
-
-            $homeTabConfigs = $homeTabConfigRepo->findWorkspaceHomeTabConfigsByAdmin();
-
-            foreach ($homeTabConfigs as $homeTabConfig) {
-                $homeTab = $homeTabConfig->getHomeTab();
-                $workspace = $homeTabConfig->getWorspace();
-
-                $newHomeTab = new HomeTab();
-                $newHomeTab->setType('workspace');
-                $newHomeTab->setWorkspace($workspace);
-                $newHomeTab->setName($homeTab->getName());
-                $em->persist($newHomeTab);
-                $em->flush();
-
-                $homeTabConfig->setType('workspace');
-                $homeTabConfig->setHomeTab($newHomeTab);
-
-                $widgetHomeTabConfigs = $widgetHTCRepo
-                    ->findWidgetConfigsByWorkspace($homeTab, $workspace);
-
-                foreach ($widgetHomeTabConfigs as $widgetHomeTabConfig) {
-                    $widgetHomeTabConfig->setHomeTab($newHomeTab);
-                }
-                $em->flush();
-            }
-        }
-        catch (MappingException $e) {
-            $this->log('A MappingException has been thrown while trying to get HomeTabConfig or WidgetHomeTabConfig repository');
-        }
-    }
-    
-    private function saveTextConfigs()
-    {
-        $cn = $this->container->get('doctrine.dbal.default_connection');
-        //create new table
-        $create = "
-            CREATE TABLE save_simple_text_dekstop_widget_config (
-                id INT AUTO_INCREMENT NOT NULL, 
-                user_id INT DEFAULT NULL, 
-                workspace_id INT DEFAULT NULL, 
-                is_default TINYINT(1) NOT NULL, 
-                content LONGTEXT NOT NULL, 
-                INDEX IDX_BAB9695A76ED395 (user_id), 
-                PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
-        ";
-        $cn->query($create);
-        
-        $dconfigs = "SELECT * FROM simple_text_dekstop_widget_config";
-        
-        foreach ($dconfigs as $config) {
-           $query = "INSERT INTO save_simple_text_dekstop_widget_config (workspace_id, user_id, is_default, content)
-               VALUES (null, {$config['user_id']}, {$config['is_default']}, {$config['content']})";
-           $cn->query($query);
-        }
-        
-        $wconfigs = "SELECT * FROM simple_text_dekstop_widget_config";
-        
-        foreach ($wconfigs as $config) {
-           $query = "INSERT INTO save_simple_text_dekstop_widget_config (workspace_id, user_id, is_default, content)
-               VALUES ({$config['workspace_id']}, null, {$config['is_default']}, {$config['content']})";
-           $cn->query($query);
-        }
-        
-        $cn->query('TRUNCATE simple_text_dekstop_widget_config');
-        $cn->query('TRUNCATE simple_text_workspace_widget_config');
-    }
-    
-    private function updateWidgetsDatas()
-    {  
-        $cn = $this->container->get('doctrine.dbal.default_connection');
-        $select = "SELECT * FROM claro_widget_display";
-        $datas =  $cn->query($select);
-        //
-        foreach ($datas as $row) {
-           $isAdmin = $row['parent_id'] == NULL ? true: false;
-           $wsId = $row['workspace_id'] ? $row['workspace_id']: 'null';
-           $userId = $row['user_id'] ? $row['user_id']: 'null';
-           $query = "INSERT INTO claro_widget_instance (workspace_id, user_id, widget_id, is_admin, is_desktop, name)
-           VALUES ({$wsId}, {$userId}, {$row['widget_id']}, {$isAdmin}, {$row['is_desktop']}, 'change me !')";
-           $cn->query($query);
-        }
     }
 }
