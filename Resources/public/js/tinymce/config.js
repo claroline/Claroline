3b93624843378d398264af65aa96452e9a4a13c8
easy-es <esdras005@hotmail.com>
2013-10-02 14:20:50 +0200

    [CoreBundle] toggle fullscreen mode for tinymce

--- Resources/public/js/tinymce/config.js
+++ Resources/public/js/tinymce/config.js
@@ -1,5 +1,35 @@
+
+function callback_tinymce_init() {
+    $('.mce-toolbar').each(function (a, b) {
+        if (a > 0) 
+            b.setAttribute('style', 'display: none'); 
+    });
+}
 function tinymce_button_ressourceLinker (ed) {
     ed.focus();
     Claroline.ResourceManager.picker('open');
+
+}
+function tinymce_button_fullscreenToggle (ed) {
+    ed.focus();
+    tinyMCE.execCommand('mceFullScreen');
+    if( $('.mce-toolbar').last().css('display') === 'none')
+    {
+        $('.mce-toolbar').each(function (a, b) {
+            if (a > 0)
+            {
+                b.setAttribute('style', 'display: block') 
+            } 
+        });
+    }
+    else
+    {
+        $('.mce-toolbar').each(function (a, b) {
+            if (a > 0)
+            {
+                b.setAttribute('style', 'display: none') 
+            } 
+        });   
+    }
 }
 
