094278e3296572ba0c3b4be7aa05a2530a448a66
arnaudbey <arnaud.bey@gmail.com>
2013-10-09 15:03:27 +0200

    [PathBundle] WIP resourceSidebar

--- Resources/public/js/Player/Controller/MainCtrl.js
+++ Resources/public/js/Player/Controller/MainCtrl.js
@@ -3,27 +3,7 @@
 /**
  * Main Controller
  */
-var mainCtrl = [
-    '$scope',
-    '$routeParams',
-    '$location',
-    '$http',
-    '$modal',
-    function($scope, $routeParams, $location, $http, $modal, HistoryFactory, PathFactory, StepFactory) {        
-        // Store symfony base partials route
-        $scope.templateRoute = PlayerApp.templateRoute;
-        
-        /**
-         * Open Help modal
-         * @returns void
-         */
-        $scope.openHelp = function() {
-            var modalInstance = $modal.open({
-                templateUrl: playerApp.templateRoute + 'Help/help-modal.html',
-                controller: 'HelpModalCtrl'
-            });
-
-        };
-        
-    }
-];
+function MainCtrl($scope) {        
+    // Store symfony base partials route
+    $scope.templateRoute = playerApp.templateRoute;
+}
