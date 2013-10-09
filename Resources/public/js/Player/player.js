2d0d55a07dc96ec467fb045537bf1ada02ce3094
arnaudbey <arnaud.bey@gmail.com>
2013-10-09 11:04:40 +0200

    [PathBundle] step-map into hidable div

---
+++ Resources/public/js/Player/player.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+	$("#step-map-body").hide();
+			 
+	$( "#step-map-open").click(function() {
+		if($("#step-map-body").attr("data-status") == "closed"){
+			$("#step-map-body").attr("data-status","opened");
+			$("#step-map").addClass("well");
+			$("#step-map-body").show();
+
+			$("#step-map-open a").removeClass("icon-chevron-down");
+			$("#step-map-open a").addClass("icon-chevron-up");
+		}
+		else{
+			$("#step-map-body").attr("data-status","closed");
+			$("#step-map-body").hide();
+			$("#step-map").removeClass("well");
+			$("#step-map-open a").removeClass("icon-chevron-up");
+			$("#step-map-open a").addClass("icon-chevron-down");
+		}
+
+	});
+
+});
