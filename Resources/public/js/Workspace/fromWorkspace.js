5b4950c05b80600f5d5fb017645238ded45adf3b
arnaudbey <arnaud.bey@gmail.com>
2013-10-11 10:30:56 +0200

    [PathBundle] debug

--- Resources/public/js/Workspace/fromWorkspace.js
+++ Resources/public/js/Workspace/fromWorkspace.js
@@ -2,16 +2,16 @@ $( document ).ready(function() {
 	$('button, a').tooltip({placement:'top'});
 
 	$('.path-delete').confirmModal({
-	    confirmTitle : 'Confirmation',
-	    confirmMessage : 'Pouvez-vous confirmer l\'action de suppression ?',
-	    confirmOk : 'OK',
-	    confirmCancel : 'Annuler',
-	    confirmDirection : 'ltr',
-	    confirmStyle : 'primary',
-	    confirmCallback : submitForm
-    });
+		confirmTitle : 'Confirmation',
+		confirmMessage : 'Pouvez-vous confirmer l\'action de suppression ?',
+		confirmOk : 'OK',
+		confirmCancel : 'Annuler',
+		confirmDirection : 'ltr',
+		confirmStyle : 'primary',
+		confirmCallback : submitForm
+	});
 
-    function submitForm(target) {
-    	$(target).parent('form').submit();
-    }
+	function submitForm(target) {
+		$(target).parent('form').submit();
+	}
 });
