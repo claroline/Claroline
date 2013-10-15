70e853d69a7b731c581e96da92d0fd27412cd5db
Elorfin <penin.axel@gmail.com>
2013-10-15 14:09:50 +0200

    [PathBundle] fix bug if text to truncate is null

--- Resources/public/js/TruncateText/Filter/TruncateTextFilter.js
+++ Resources/public/js/TruncateText/Filter/TruncateTextFilter.js
@@ -1,18 +1,22 @@
 function TruncateTextFilter() {
-	return function (text, length, end) {
-		// Default length for truncate : 20 caracters.
-	    if (isNaN(length))
-	        length = 20;
+    return function (text, length, end) {
+        var truncated = '';
+        if (undefined !== text && null !== text && 0 !== text.length) {
+            // Default length for truncate : 20 caracters.
+            if (isNaN(length))
+                length = 20;
 
-	    if (end === undefined)
-	        end = "...";
+            if (end === undefined)
+                end = "...";
 
-	    if (text.length <= length || text.length - end.length <= length) {
-	        return text;
-	    }
-	    else {
-	        return String(text).substring(0, length-end.length) + end;
-	    }
-
-	};
+            if (text.length <= length || text.length - end.length <= length) {
+                truncated = text;
+            }
+            else {
+                truncated = String(text).substring(0, length-end.length) + end;
+            }
+        }
+        
+        return truncated;
+    };
 }
