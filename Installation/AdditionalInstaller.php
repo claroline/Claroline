c3c93cab9ba8957faa9d2067ccfe4f6ecf59966c
nicolasjacquemet <nicolas.jacquemet@recherche.univ-lyon1.fr>
2013-11-08 09:40:40 +0100

    [LessonBundle] test

--- Installation/AdditionalInstaller.php
+++ Installation/AdditionalInstaller.php
@@ -28,7 +28,9 @@ class AdditionalInstaller extends BaseInstaller
 
     public function postUpdate($currentVersion, $targetVersion)
     {
+        $this->displayLog("postUpdate reached");
         if (version_compare($currentVersion, '1.3', '<') && version_compare($currentVersion, '1.2', '>=') && version_compare($targetVersion, '1.3', '>=') ) {
+            $this->displayLog("test passed");
             $updater13 = new Updater13($this->container);
             $updater13->setLogger($this);
             $updater13->postUpdate();
