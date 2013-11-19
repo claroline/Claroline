7eb2efcf7d7b91e5cb2dad01f515a7906fcc735d
Stéphane Klein <stephaneklein221@gmail.com>
2013-11-19 15:33:47 +0100

    [KernelBundle] Added license info

--- Bundle/AutoConfigurableInterface.php
+++ Bundle/AutoConfigurableInterface.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace Claroline\KernelBundle\Bundle;
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+namespace Claroline\KernelBundle\Bundle;
 
 interface AutoConfigurableInterface
 {
