a7ce328a513f53c4745e9288e49d39f57dc4a99e
Stéphane Klein <stephaneklein221@gmail.com>
2013-11-19 12:10:43 +0100

    [CoreBundle] Added license info (2)

--- Resources/public/js/home/height.js
+++ Resources/public/js/home/height.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * The height of a content in home page must be resized when:
  *
