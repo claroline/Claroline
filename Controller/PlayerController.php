967c3e8dee1691c7c7fb560966d510f161fa8406
Elorfin <penin.axel@gmail.com>
2013-11-20 11:38:05 +0100

    [PathBundle] initialize structure for new player

--- Controller/PlayerController.php
+++ Controller/PlayerController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Innova\PathBundle\Entity\Path;
 use Innova\PathBundle\Entity\Step;
 use Claroline\CoreBundle\Entity\Workspace\AbstractWorkspace;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
 /**
  * Player controller
@@ -22,70 +23,76 @@ use Claroline\CoreBundle\Entity\Workspace\AbstractWorkspace;
  *      service="innova.controller.path_player"
  * )
  */
-class PlayerController
+class PlayerController extends ContainerAware
 {
-    /**
-     * Current request
-     * @var \Symfony\Component\HttpFoundation\Request
-     */
-    protected $request;
-    
-    /**
-     * Inject current request into service
-     * @param Request $request
-     * @return \Innova\PathBundle\Controller\StepController
-     */
-    public function setRequest(Request $request = null)
-    {
-        $this->request = $request;
-    
-        return $this;
-    }
-    
     /**
      * Display path player
      * @param  Path $path
      * @return array
      * 
      * @Route(
-     *      "workspace/{workspaceId}/path/player/{pathId}",
+     *      "workspace/{workspaceId}/path/{pathId}",
      *      name="innova_path_player_index",
      *      options={"expose" = true}
      * )
-     * @ParamConverter("workspace", class="ClarolineCoreBundle:AbstractWorkspace", options={"mapping": {"workspaceId": "id"}})
-     * @ParamConverter("workspace", class="InnovaPathBundle:Path", options={"mapping": {"pathId": "id"}})
+     * @ParamConverter("workspace", class="ClarolineCoreBundle:Workspace\AbstractWorkspace", options={"mapping": {"workspaceId": "id"}})
+     * @ParamConverter("path", class="InnovaPathBundle:Path", options={"mapping": {"pathId": "id"}})
      * @Method("GET")
      * @Template("InnovaPathBundle:Player:main.html.twig")
      */
-    public function displayAction(AbstractWorkspace $workspace, Path $path)
+    public function displayAction(AbstractWorkspace $workspace, Path $path, Step $currentStep = null)
     {
+        // Get root step of path if no step requested
+        if (empty($currentStep)) {
+            $steps = $path->getSteps();
+            if (!empty($steps) && !empty($steps[0])) {
+                // Root step exists => grab it
+                $currentStep = $steps[0];
+            }
+        }
+        
         return array (
             'workspace' => $workspace,
             'path' => $path,
+            'currentStep' => $currentStep
         );
     }
     
     /**
-     * 
+     * @Method("GET")
+     * @Template("InnovaPathBundle:Player:components/breadcrumbs.html.twig")
      */
-    public function displayBreadcrumbAction()
+    public function displayBreadcrumbsAction(AbstractWorkspace $workspace, Path $path, Step $currentStep = null)
     {
         return array ();
     }
     
     /**
-     * 
+     * @Method("GET")
+     * @Template("InnovaPathBundle:Player:components/resources.html.twig")
      */
-    public function displayResourcesAction(Path $path, Step $step)
+    public function displayResourcesAction()
     {
         return array ();
     }
     
     /**
-     * 
+     * @Method("GET")
+     * @Template("InnovaPathBundle:Player:components/squares-browser.html.twig")
      */
-    public function displaySquaresBrowser()
+    public function displaySquaresBrowserAction()
     {
-        
+        return array ();
+    }
+    
+    /**
+     * @Method("GET")
+     * @Template("InnovaPathBundle:Player:components/current-step.html.twig")
+     */
+    public function displayCurrentStepAction(Step $currentStep)
+    {
+        return array (
+            'currentStep' => $currentStep,
+        );
     }
 }
