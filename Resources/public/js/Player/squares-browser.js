a12c802cf11da92eef905e983dbfde553b219127
beya <beya@DA2013178>
2013-11-22 09:56:33 +0100

    [PathBundle] WIP

--- Resources/public/js/Player/squares-browser.js
+++ Resources/public/js/Player/squares-browser.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
 	$('.step').tooltip({placement:'top'});
+
+	$( ".show-sibling" ).click(function() {
+		var lvl = $( this ).attr("data-lvl");
+		$(".hidden.lvl-"+lvl).removeClass("hidden");
+	});
+
+	$( ".hide-sibling" ).click(function() {
+		var lvl = $( this ).attr("data-lvl");
+		$(".hidden.lvl-"+lvl).removeClass("hidden");
+	});
+
 });
