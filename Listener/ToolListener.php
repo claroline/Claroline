7156d0807bfe25dd9fd9fe9e4789f9d33b8587b3
arnaudbey <arnaud.bey@gmail.com>
2013-12-12 11:19:11 +0100

    [PathBundle] path are openable through the resources page

--- Listener/ToolListener.php
+++ Listener/ToolListener.php
@@ -4,7 +4,8 @@ namespace Innova\PathBundle\Listener;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Claroline\CoreBundle\Event\OpenResourceEvent;
 use Claroline\CoreBundle\Event\DisplayToolEvent;
 
 class ToolListener extends ContainerAware
@@ -15,5 +16,23 @@ class ToolListener extends ContainerAware
         $subRequest = $this->container->get('request')->duplicate(array('id' => $id), array(), array("_controller" => 'innova.controller.path:fromWorkspaceAction'));
         $response = $this->container->get('http_kernel')->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
         $event->setContent($response->getContent());
+    }
+
+   
+    public function onPathOpen(OpenResourceEvent $event)
+    {
+        $path = $event->getResource();
+	    $route = $this->container
+	        ->get('router')
+	        ->generate(
+	            'innova_path_player_index',
+	            array(
+	            	'workspaceId' => $path->getResourceNode()->getWorkspace()->getId(),
+	            	'pathId' => $path->getId(),
+	            	'stepId' => $path->getRootStep()->getId()
+	            )
+	        );
+	    $event->setResponse(new RedirectResponse($route));
+	    $event->stopPropagation();
     }
 }
