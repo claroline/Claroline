ff36c6b102e1edd314c5771ceee1e3b6d52c434e
ngodfraind <nicolas.godfraind@gmail.com>
2013-12-18 14:19:02 +0100

    [ForumBundle] Fixing the category listing.

--- Repository/ForumRepository.php
+++ Repository/ForumRepository.php
@@ -76,33 +76,65 @@ class ForumRepository extends EntityRepository
         $dql = "
             SELECT c.id as id,
             count(m) as count_messages,
-            last.creationDate AS last_message_created,
-            c.name as name,
-            lastUser.lastName as last_message_creator_lastname,
-            lastUser.firstName as last_message_creator_firstname
+            c.name as name
             FROM Claroline\ForumBundle\Entity\Category c
             LEFT JOIN c.subjects s
             LEFT JOIN s.messages m
             JOIN c.forum forum
-
-            LEFT JOIN s.messages last WITH last.id =
-            (
-                SELECT MAX(m2.id) FROM Claroline\ForumBundle\Entity\Message m2
-                JOIN m2.subject s2
-                JOIN s2.category c2
-                WHERE c2.id = c.id
-            )
-
-            LEFT JOIN last.creator lastUser
             WHERE forum.id = :forumId
             GROUP BY c
-            ORDER BY last_message_created DESC
         ";
 
         $query = $this->_em->createQuery($dql);
         $query->setParameter('forumId', $forum->getId());
 
-        return ($getQuery) ? $query: $query->getResult();
+        $categories = $query->getResult();
+
+        $dql = "
+            SELECT m.creationDate as last_message_created,
+            c.id as categoryId,
+            lastUser.lastName as last_message_creator_lastname,
+            lastUser.firstName as last_message_creator_firstname
+            FROM Claroline\ForumBundle\Entity\Message m
+            JOIN m.creator lastUser
+            JOIN m.subject s
+            JOIN s.category c
+            JOIN c.forum forum
+            WHERE forum.id = :forumId
+            AND m.id IN (
+                SELECT max(m2.id) FROM Claroline\ForumBundle\Entity\Message m2
+                JOIN m2.subject s2
+                JOIN s2.category c2
+                where c2.id = c.id
+                GROUP BY c.id
+            )
+            GROUP BY c
+        ";
+
+        $query = $this->_em->createQuery($dql);
+        $query->setParameter('forumId', $forum->getId());
+
+        $lastMessages = $query->getResult();
+
+        //merge both results
+        $merged = $categories;
+
+        foreach ($categories as $key => $category) {
+
+            $merged[$key]['last_message_created'] = null;
+            $merged[$key]['last_message_creator_lastname'] = null;
+            $merged[$key]['last_message_creator_firstname'] = null;
+
+            foreach ($lastMessages as $lastMessage) {
+                if ($category['id'] === $lastMessage['categoryId']) {
+                    $merged[$key]['last_message_created'] = $lastMessage['last_message_created'];
+                    $merged[$key]['last_message_creator_lastname'] = $lastMessage['last_message_creator_lastname'];
+                    $merged[$key]['last_message_creator_firstname'] = $lastMessage['last_message_creator_firstname'];
+                }
+            }
+        }
+
+        return $merged;
     }
 
     public function countMessagesForSubject(Subject $subject)
