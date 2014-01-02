5fb4db9533a13ae82a8bdf159af23c6300368991
ngodfraind <nicolas.godfraind@gmail.com>
2014-01-02 16:36:41 +0100

    [CoreBundle] Configuration mink/selenium.

--- ClarolineCoreBundle.php
+++ ClarolineCoreBundle.php
@@ -106,6 +106,10 @@ class ClarolineCoreBundle extends InstallableBundle implements AutoConfigurableI
             } elseif ($bundle instanceof \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle) {
                 return $config;
             }
+            
+            if ($environment === 'test' && $bundle instanceof \Behat\MinkBundle\MinkBundle) {
+                return $config->addContainerResource($this->buildPath('mink'));
+            }
         }
 
         return false;
