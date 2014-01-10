a01c8d312212490094c28ec176488b27e969ced9
Elorfin <penin.axel@gmail.com>
2014-01-10 13:30:40 +0100

    [PathBundle] fix js includes after code refactoring

--- Resources/public/angularjs/Editor/EditorApp.js
+++ Resources/public/angularjs/Editor/EditorApp.js
@@ -1,8 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-//var EditorApp = angular.module('EditorApp', ['ngRoute', 'ui.bootstrap', 'pageslide-directive', 'ui.sortable', 'tinymce-directive']);
-var EditorApp = angular.module('EditorApp', ['ngRoute', 'ui.bootstrap', 'pageslide-directive', 'ui.sortable']);
+var EditorApp = angular.module('EditorApp', ['ngRoute', 'ui.bootstrap', 'ui.pageslide', 'ui.sortable', 'ui.tinymce']);
 
 // Declare routes
 EditorApp.config(['$routeProvider', function($routeProvider) {
