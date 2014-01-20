753ce15b384a917087535d6ae7f7aae0b2e449c4
Stéphane Klein <stephaneklein221@gmail.com>
2014-01-20 13:54:52 +0100

    [CoreBundle] Changed field type

---
+++ Migrations/drizzle_pdo_mysql/Version20140120135106.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Claroline\CoreBundle\Migrations\drizzle_pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/01/20 01:51:09
+ */
+class Version20140120135106 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_widget_home_tab_config CHANGE widget_order widget_order INT NOT NULL
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_widget_home_tab_config CHANGE widget_order widget_order VARCHAR(255) NOT NULL
+        ");
+    }
+}
