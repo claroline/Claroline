f19e8559c3f1926a76dd56b58807d2e3fc79def1
arnaudbey <arnaud.bey@gmail.com>
2014-01-22 13:49:23 +0100

    [PathBundle] fix broken dev_env

--- Listener/ToolListener.php
+++ Listener/ToolListener.php
@@ -3,44 +3,18 @@
 namespace Innova\PathBundle\Listener;
 
 use Claroline\CoreBundle\Event\DisplayToolEvent;
-use Symfony\Component\Templating\EngineInterface;
-use Innova\PathBundle\Manager\PathManager;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
-class ToolListener
-{
-    /**
-     * Template engine
-     * @var \Symfony\Component\Templating\EngineInterface $templating
-     */
-    protected $templating;
-    
-    /**
-     * Path manager
-     * @var \Innova\PathBundle\Manager\PathManager $pathManager
-     */
-    protected $pathManager;
-    
-    /**
-     * Class constructor
-     * @param \Symfony\Component\Templating\EngineInterface $templating
-     * @param \Innova\PathBundle\Manager\PathManager $pathManager
-     */
-    public function __construct(
-        EngineInterface $templating, 
-        PathManager     $pathManager)
-    {
-        $this->templating = $templating;
-        $this->pathManager = $pathManager;
-    }
-    
+class ToolListener extends ContainerAware
+{    
     public function onWorkspaceOpen(DisplayToolEvent $event)
     {
         // Retrieve data
         $workspace = $event->getWorkspace();
-        $paths = $this->pathManager->findAllFromWorkspace($workspace);
+        $paths = $this->container->get('innova_path.manager.path')->findAllFromWorkspace($workspace);
         
         // Build response content
-        $content = $this->templating->render(
+        $content = $this->container->get('templating')->render(
             'InnovaPathBundle::index.html.twig',
             array(
                 'workspace' => $workspace,
