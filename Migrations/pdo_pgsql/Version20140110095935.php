08961444d6587329229f739abf5ee207a1860a68
ngodfraind <nicolas.godfraind@gmail.com>
2014-01-23 09:56:55 +0100

    [CoreBundle] Migrations.

--- Migrations/pdo_pgsql/Version20140110095935.php
+++ Migrations/pdo_pgsql/Version20140110095935.php
@@ -1,14 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jorge
- * Date: 22/01/14
- * Time: 17:34
- */
 
 namespace Claroline\CoreBundle\Migrations\pdo_pgsql;
 
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
 
-class Version20140110095935 {
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/01/10 09:59:36
+ */
+class Version20140110095935 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_user 
+            DROP CONSTRAINT FK_EB8D285282D40A1F
+        ");
+        $this->addSql("
+            ALTER TABLE claro_user 
+            ADD CONSTRAINT FK_EB8D285282D40A1F FOREIGN KEY (workspace_id) 
+            REFERENCES claro_workspace (id) 
+            ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+    }
 
-} 
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_user 
+            DROP CONSTRAINT FK_EB8D285282D40A1F
+        ");
+        $this->addSql("
+            ALTER TABLE claro_user 
+            ADD CONSTRAINT FK_EB8D285282D40A1F FOREIGN KEY (workspace_id) 
+            REFERENCES claro_workspace (id) 
+            ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+    }
+}
