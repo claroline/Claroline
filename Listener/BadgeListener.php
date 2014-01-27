7c8a064222c981b6f893f92ac1ce624a69668e2a
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-01-27 16:01:21 +0100

    [BlogBundle] Manage more event for validation link

--- Listener/BadgeListener.php
+++ Listener/BadgeListener.php
@@ -4,6 +4,9 @@ namespace Icap\BlogBundle\Listener;
 
 use Claroline\CoreBundle\Event\Badge\BadgeCreateValidationLinkEvent;
 use Icap\BlogBundle\Event\Log\LogPostCreateEvent;
+use Icap\BlogBundle\Event\Log\LogPostDeleteEvent;
+use Icap\BlogBundle\Event\Log\LogPostReadEvent;
+use Icap\BlogBundle\Event\Log\LogPostUpdateEvent;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -29,6 +32,9 @@ class BadgeListener
 
     /**
      * @DI\Observe("badge-resource-icap_blog-post_create-generate_validation_link")
+     * @DI\Observe("badge-resource-icap_blog-post_delete-generate_validation_link")
+     * @DI\Observe("badge-resource-icap_blog-post_read-generate_validation_link")
+     * @DI\Observe("badge-resource-icap_blog-post_update-generate_validation_link")
      */
     public function onBagdeCreateValidationLink(BadgeCreateValidationLinkEvent $event)
     {
@@ -38,6 +44,9 @@ class BadgeListener
         switch($log->getAction())
         {
             case LogPostCreateEvent::ACTION:
+            case LogPostDeleteEvent::ACTION:
+            case LogPostReadEvent::ACTION:
+            case LogPostUpdateEvent::ACTION:
                 $logDetails = $event->getLog()->getDetails();
                 $parameters = array(
                     'blogId'   => $logDetails['post']['blog'],
