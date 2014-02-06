f35f94d1d907cea56b419b22791bd2e925d78550
arnaudbey <arnaud.bey@gmail.com>
2014-02-06 10:05:02 +0100

    [PathBundle] WIP handle iframed resources

--- Resources/public/js/Player/player.js
+++ Resources/public/js/Player/player.js
@@ -2,15 +2,12 @@ $( document ).ready(function() {
 
 	$("*").tooltip({placement:'top'});
 
-	/* IFRAME MANAGEMENT */
-	$(".resource-frame").height($(window).height());
 
 	$('.resource-frame').on('load', function () {
-		var iframe = $(this);
-		cleanFrame(iframe);
-		iframe.show();
+        var frame = $(this);
+		frame.show();
+        frame.css("height", frame.contents().find("#wrap").height() + 20);
     });
-
 
    $('.resource-tab a').on('click', function () {
    		var iframe = $($(this).attr("href") +" iframe");
@@ -20,30 +17,3 @@ $( document ).ready(function() {
    		}
    	});
 });
-
-function cleanFrame(iframe){
-	iframe.contents().find("#top_bar").remove();
-	iframe.contents().find("#left-bar").remove();
-	iframe.contents().find("#footer").remove();
-	iframe.contents().find("#push").remove();
-	iframe.contents().find("body").css("padding", "10px").removeClass("left-bar-push");
-	iframe.contents().find("body").css("overflow-x", "hidden");
-	iframe.contents().find("#wrap>.container").removeClass("container");
-}
-
-/*
-function launchFullScreen(element) {
-	if (element.requestFullscreen){ 
-		element.requestFullscreen(); 
-	}
-	else if (element.mozRequestFullScreen){ 
-		element.mozRequestFullScreen(); 
-	}
-	else if (element.webkitRequestFullscreen){ 
-		element.webkitRequestFullscreen(); 
-	}
-	else if (element.msRequestFullscreen){ 
-		element.msRequestFullscreen(); 
-	}
-}
-*/
