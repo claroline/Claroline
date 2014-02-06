8b6be9eccaec57668f04904e96f464668dcb2aac
ngodfraind <nicolas.godfraind@gmail.com>
2014-02-06 11:55:47 +0100

    [CoreBundle] Removed duplicata.

--- DependencyInjection/Compiler/DynamicConfigPass.php
+++ DependencyInjection/Compiler/DynamicConfigPass.php
@@ -37,9 +37,6 @@ class DynamicConfigPass implements CompilerPassInterface
         $container->removeDefinition('swiftmailer.mailer.default.transport');
         $container->setDefinition('swiftmailer.mailer.default.transport', $transport);
 
-        $storage = $container->findDefinition('session.storage');
-        $storage->addMethodCall('setOptions', array(new Reference('claroline.session.storage_options')));
-
         //session storage
         $handler = new Definition();
         $handler->setClass('SessionHandlerInterface');
