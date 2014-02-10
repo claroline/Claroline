bda43f80caed1dd189fa3a223ffab5285edb84b8
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-02-10 17:18:16 +0100

    [CoreBundle] Restrict choice of badge for creating collection, only badges owned by user are available

--- Form/Badge/BadgeCollectionType.php
+++ Form/Badge/BadgeCollectionType.php
@@ -17,6 +17,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * @DI\Service("claroline.form.badge.collection")
@@ -29,22 +30,30 @@ class BadgeCollectionType extends AbstractType
     /** @var \Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler */
     private $platformConfigHandler;
 
+    /** @var SecurityContext */
+    private $securityContext;
+
     /**
      * @DI\InjectParams({
      *     "badgeRepository"       = @DI\Inject("claroline.repository.badge"),
-     *     "platformConfigHandler" = @DI\Inject("claroline.config.platform_config_handler")
+     *     "platformConfigHandler" = @DI\Inject("claroline.config.platform_config_handler"),
+     *     "securityContext"       = @DI\Inject("security.context")
      * })
      */
-    public function __construct(BadgeRepository $badgeRepository, PlatformConfigurationHandler $platformConfigHandler)
+    public function __construct(BadgeRepository $badgeRepository, PlatformConfigurationHandler $platformConfigHandler, SecurityContext $securityContext)
     {
         $this->badgeRepository       = $badgeRepository;
         $this->platformConfigHandler = $platformConfigHandler;
+        $this->securityContext       = $securityContext;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var \Claroline\CoreBundle\Entity\User $user */
+        $user = $this->securityContext->getToken()->getUser();
+
         /** @var \Claroline\CoreBundle\Entity\Badge\Badge[] $badgeChoices */
-        $badgeChoices  = $this->badgeRepository->findOrderedByName($this->platformConfigHandler->getParameter('locale_language'));
+        $badgeChoices  = $this->badgeRepository->findByUser($user);
 
         foreach ($badgeChoices as $badgeChoice) {
             $badgeChoice->setLocale($this->platformConfigHandler->getParameter('locale_language'));
