e25f9b144f6b1faa6912a4f08f81206530e34ae8
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-03-11 16:41:51 +0100

    [CoreBundle] Creation of the doctrine entity resolver to be able to use doctrine entity listener easily on every entities

---
+++ DependencyInjection/Compiler/DoctrineEntityListenerPass.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Claroline\CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DoctrineEntityListenerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('claroline.doctrine.entity_listener_resolver');
+        $services   = $container->findTaggedServiceIds('doctrine.entity_listener');
+
+        foreach ($services as $service => $attributes) {
+            $definition->addMethodCall(
+                'addMapping',
+                array($container->getDefinition($service)->getClass(), $service)
+            );
+        }
+    }
+}
---
+++ Entity/Badge/Listener/BadgeListener.php
@@ -0,0 +1,59 @@
+<?php
+namespace Claroline\CoreBundle\Entity\Badge\Listener;
+
+use Claroline\CoreBundle\Entity\Badge\Badge;
+use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\SecurityContext;
+
+/**
+ * @DI\Service("claroline.entity_listener.badge")
+ * @DI\Tag("doctrine.entity_listener")
+ */
+class BadgeListener
+{
+    /** @var \Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler */
+    private $platformConfigHandler;
+
+    /** @var \Symfony\Component\Security\Core\SecurityContext */
+    private $securityContext;
+
+    /**
+     * @DI\InjectParams({
+     *     "platformConfigHandler" = @DI\Inject("claroline.config.platform_config_handler"),
+     *     "securityContext"       = @DI\Inject("security.context")
+     * })
+     */
+    public function __construct(PlatformConfigurationHandler $platformConfigHandler, SecurityContext $securityContext)
+    {
+        $this->platformConfigHandler = $platformConfigHandler;
+        $this->securityContext       = $securityContext;
+    }
+
+    /**
+     * @param Badge              $badge
+     * @param LifecycleEventArgs $event
+     */
+    public function postLoad(Badge $badge, LifecycleEventArgs $event)
+    {
+        // Set the locale on tha badge
+        $platformLocale = $this->platformConfigHandler->getParameter('locale_language');
+
+        /** @var \Claroline\CoreBundle\Entity\User $user */
+        $user           = $this->securityContext->getToken()->getUser();
+        $userLocale     = null;
+
+        if ('anon.' !== $user) {
+            $userLocale = $user->getLocale();
+        }
+
+        $locale = $platformLocale;
+
+        if (null !== $userLocale) {
+            $locale = $userLocale;
+        }
+
+        $badge->setLocale($locale);
+    }
+}
