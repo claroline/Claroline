a62cc8ab07ee7b1a9ea1491d8076ed122742426f
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-03-12 10:57:01 +0100

    [CoreBundle] Don't use dev-master branch of DoctrineBundle but still use entity listener features

--- DependencyInjection/Compiler/DoctrineEntityListenerPass.php
+++ DependencyInjection/Compiler/DoctrineEntityListenerPass.php
@@ -4,11 +4,15 @@ namespace Claroline\CoreBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class DoctrineEntityListenerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
+        $config = $container->getDefinition('doctrine.orm.default_configuration');
+        $config->addMethodCall('setEntityListenerResolver', array(new Reference('claroline.doctrine.entity_listener_resolver')));
+
         $definition = $container->getDefinition('claroline.doctrine.entity_listener_resolver');
         $services   = $container->findTaggedServiceIds('doctrine.entity_listener');
 
