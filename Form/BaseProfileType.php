787775fe2f2b8bb988468dbad28b84c3a0f32d07
ngodfraind <nicolas.godfraind@gmail.com>
2014-03-20 11:33:50 +0100

    [CoreBundle] Adding captcha to the creation form.

--- Form/BaseProfileType.php
+++ Form/BaseProfileType.php
@@ -52,6 +52,8 @@ class BaseProfileType extends AbstractType
             )
             ->add('accepted_terms', 'checkbox', array('label' => 'I accept the terms of service'));
         }
+
+        $builder->add('captcha', 'captcha', array('label' => 'captcha'));
     }
 
     public function getName()
