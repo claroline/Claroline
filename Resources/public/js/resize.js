d13cce643641dc71ea4d6dac2e13de880d4363d3
david <david@DavidB>
2014-03-21 16:31:01 +0100

    [ExoBundle] Refactoring view

--- Resources/public/js/resize.js
+++ Resources/public/js/resize.js
@@ -5,14 +5,18 @@ $(document).ready(function() {
     $(document).find('table').find('tr').find('td').find('iframe').each(function() {
        resize($(this));
     });
+    $(document).find('table').find('tr').find('td').find('video').each(function() {
+        resize($(this));
+    });
     $(document).find('table').find('tr').find('td').find('object').each(function() {
+        resize($(this));
         $(this).find('embed').each(function() {
             resize($(this));
         });
     });
     function resize(element) {
-        var maxWidth = 20;
-        var maxHeight = 20;
+        var maxWidth = 100;
+        var maxHeight = 100;
         var ratio = 0;
         var width = element.width();
         var height = element.height();
