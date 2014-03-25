79a1594270355402a61aab19cebb6ef1aa74bfd1
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-03-25 15:23:43 +0100

    [CoreBundle] Changing old badge tool display name + creating new my badges tool

--- Library/Installation/AdditionalInstaller.php
+++ Library/Installation/AdditionalInstaller.php
@@ -121,6 +121,13 @@ class AdditionalInstaller extends BaseInstaller
             $updater021000->setLogger($this->logger);
             $updater021000->postUpdate();
         }
+
+        if (version_compare($currentVersion, '2.11.1', '<')) {
+            $this->buildDefaultTemplate();
+            $updater021000 = new Updater\Updater021101($this->container);
+            $updater021000->setLogger($this->logger);
+            $updater021000->postUpdate();
+        }
     }
 
     private function setLocale()
