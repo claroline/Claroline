a00dfec4c95b60445b513299a16a223c9034cf63
arnaudbey <arnaud.bey@gmail.com>
2014-04-03 16:16:33 +0200

    [PathBundle] fix required fixtures

--- DataFixtures/Required/LoadStepWhereData.php
+++ DataFixtures/Required/LoadStepWhereData.php
@@ -20,18 +20,22 @@ class LoadStepWhereData extends AbstractFixture
 
         $stepWhere1 = new stepWhere();
         $stepWhere1->setName("at_home");
+        $stepWhere1->setDefault(0);
         $manager->persist($stepWhere1);
 
         $stepWhere2 = new stepWhere();
         $stepWhere2->setName("classroom");
+        $stepWhere2->setDefault(0);
         $manager->persist($stepWhere2);
 
         $stepWhere3 = new stepWhere();
         $stepWhere3->setName("library");
+        $stepWhere3->setDefault(0);
         $manager->persist($stepWhere3);
 
         $stepWhere4 = new stepWhere();
         $stepWhere4->setName("anywhere");
+        $stepWhere4->setDefault(1);
         $manager->persist($stepWhere4);
 
         $manager->flush();
--- DataFixtures/Required/LoadStepWhoData.php
+++ DataFixtures/Required/LoadStepWhoData.php
@@ -19,14 +19,17 @@ class LoadStepWhoData extends AbstractFixture
     {
         $stepWho1 = new stepWho();
         $stepWho1->setName("student");
+        $stepWho1->setDefault(1);
         $manager->persist($stepWho1);
 
         $stepWho2 = new stepWho();
         $stepWho2->setName("group");
+        $stepWho2->setDefault(0);
         $manager->persist($stepWho2);
 
         $stepWho3 = new stepWho();
         $stepWho3->setName("class");
+        $stepWho3->setDefault(0);
         $manager->persist($stepWho3);
 
         $manager->flush();
