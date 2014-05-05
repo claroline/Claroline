84a6e78891e2070f7555d9479afd02702b1042af
Jorge Solis Pauwels <jorgeluissolis@gmail.com>
2014-05-05 13:43:33 +0200

    [CoreBundle] Translated demo fixtures

--- DataFixtures/Demo/LoadRegionData.php
+++ DataFixtures/Demo/LoadRegionData.php
@@ -25,32 +25,55 @@ class LoadRegionData extends AbstractFixture
     public function load(ObjectManager $manager)
     {
         $header = array(
-            "Contenu d'exemple" => array("url" => "/type/opengraph", "icon" => ""),
-            "Accueil" => array("url" => "/", "icon" => "")
+            'Sample content' => array(
+                'url' => '/type/opengraph',
+                'icon' => '',
+                'translation' => array('fr' => 'Contenu d\'exemple', 'es' => 'Contenido de ejemplo')
+            ),
+            'Home' => array(
+                'url' => '/',
+                'icon' => '',
+                'translation' => array('fr' => 'Accueil', 'es' => 'Inicio')
+            )
         );
 
         $footer = array(
-            "Services" => array("url" => "http://www.claroline.net/communaute-2/", "icon" => ""),
-            "Communauté" => array("url" => "http://www.claroline.net/worldwide/", "icon" => ""),
-            "Consortium" => array("url" => "http://www.claroline.net/consortium/", "icon" => ""),
-            "À propos nous" => array("url" => "http://www.claroline.net/breve-presentation/", "icon" => ""),
-            "Accueil" => array("url" => "/", "icon" => "")
+            'Contact' => array(
+                'url' => 'http://www.claroline.net/type/contact',
+                'icon' => '',
+                'translation' => array('fr' => 'Contactez-nous', 'es' => 'Contacto')
+            ),
+            'Consortium' => array(
+                'url' => 'http://www.claroline.net/type/consortium',
+                'icon' => '',
+                'translation' => array('fr' => 'Consortium', 'es' => 'Consorcio')
+            ),
+            'About Us' => array(
+                'url' => 'http://www.claroline.net/type/claroline',
+                'icon' => '',
+                'translation' => array('fr' => '&Agrave; propos de nous', 'es' => '¿Quiénes somos?')
+            ),
+            'Home' => array(
+                'url' => '/',
+                'icon' => '',
+                'translation' => array('fr' => 'Accueil', 'es' => 'Inicio')
+            )
         );
 
-        $this->menuToRegion($manager, $this->menu($manager, "Entête", $header), 'header');
-        $this->menuToRegion($manager, $this->menu($manager, "Pied de page", $footer), 'footer');
+        $this->menuToRegion($manager, $this->menu($manager, 'Header', $header), 'header');
+        $this->menuToRegion($manager, $this->menu($manager, 'footer', $footer), 'footer');
     }
 
-    private function menu($manager, $name = "Menu", $links = null)
+    private function menu($manager, $name = 'Menu', $links = null)
     {
         $content = new Content($manager);
         $content->setTitle($name);
 
         $manager->persist($content);
 
-        $type = $manager->getRepository("ClarolineCoreBundle:Home\Type")->findOneBy(array('name' => 'menu'));
+        $type = $manager->getRepository('ClarolineCoreBundle:Home\Type')->findOneBy(array('name' => 'menu'));
 
-        $first = $manager->getRepository("ClarolineCoreBundle:Home\Content2Type")->findOneBy(
+        $first = $manager->getRepository('ClarolineCoreBundle:Home\Content2Type')->findOneBy(
             array('back' => null, 'type' => $type)
         );
 
@@ -68,8 +91,16 @@ class LoadRegionData extends AbstractFixture
             $linkContent->setContent($link['url']);
 
             $manager->persist($linkContent);
+            $manager->flush();
 
-            $first = $manager->getRepository("ClarolineCoreBundle:Home\SubContent")->findOneBy(
+            foreach ($link['translation'] as $locale => $translation) {
+                $linkContent->setTitle($translation);
+                $linkContent->setTranslatableLocale($locale);
+                $manager->persist($linkContent);
+                $manager->flush();
+            }
+
+            $first = $manager->getRepository('ClarolineCoreBundle:Home\SubContent')->findOneBy(
                 array('back' => null, 'father' => $content)
             );
 
@@ -87,9 +118,9 @@ class LoadRegionData extends AbstractFixture
 
     public function menuToRegion($manager, $content, $regionName)
     {
-        $region = $manager->getRepository("ClarolineCoreBundle:Home\Region")->findOneBy(array('name' => $regionName));
+        $region = $manager->getRepository('ClarolineCoreBundle:Home\Region')->findOneBy(array('name' => $regionName));
 
-        $first = $manager->getRepository("ClarolineCoreBundle:Home\Content2Region")->findOneBy(
+        $first = $manager->getRepository('ClarolineCoreBundle:Home\Content2Region')->findOneBy(
             array('back' => null, 'region' => $region)
         );
 
