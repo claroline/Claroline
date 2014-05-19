7d1105d6bb57afec31235c25c9621b212e8f3032
bradInside <brad.inside@gmail.com>
2014-05-19 15:55:07 +0200

    [DropzoneBundle] creating BDD field for unlockedDrops.

---
+++ Migrations/pdo_sqlsrv/Version20140519135327.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Icap\DropzoneBundle\Migrations\pdo_sqlsrv;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/05/19 01:53:30
+ */
+class Version20140519135327 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_drop 
+            ADD unlocked_drop BIT NOT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_drop 
+            ADD CONSTRAINT DF_3AD19BA6_5A78C0A7 DEFAULT '0' FOR unlocked_drop
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_drop 
+            DROP COLUMN unlocked_drop
+        ");
+    }
+}
