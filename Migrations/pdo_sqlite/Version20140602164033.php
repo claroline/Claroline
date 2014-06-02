10c3af974db6d9047527ab4ca8d246d11faee855
Anh Thao PHAM <anhthao.pham@claroline.net>
2014-06-02 16:43:29 +0200

    [CoreBundle] Fixes incorrect migrations

--- Migrations/pdo_sqlite/Version20140602164033.php
+++ Migrations/pdo_sqlite/Version20140602164033.php
@@ -8,9 +8,9 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2014/06/02 02:07:39
+ * Generation date: 2014/06/02 04:40:34
  */
-class Version20140602140737 extends AbstractMigration
+class Version20140602164033 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -21,52 +21,6 @@ class Version20140602140737 extends AbstractMigration
         $this->addSql("
             ALTER TABLE claro_resource_node 
             ADD COLUMN accessible_until DATETIME DEFAULT NULL
-        ");
-        $this->addSql("
-            DROP INDEX tag_unique_name_and_user
-        ");
-        $this->addSql("
-            DROP INDEX IDX_C8EFD7EFA76ED395
-        ");
-        $this->addSql("
-            DROP INDEX IDX_C8EFD7EF82D40A1F
-        ");
-        $this->addSql("
-            CREATE TEMPORARY TABLE __temp__claro_workspace_tag AS 
-            SELECT id, 
-            user_id, 
-            name 
-            FROM claro_workspace_tag
-        ");
-        $this->addSql("
-            DROP TABLE claro_workspace_tag
-        ");
-        $this->addSql("
-            CREATE TABLE claro_workspace_tag (
-                id INTEGER NOT NULL, 
-                user_id INTEGER DEFAULT NULL, 
-                name VARCHAR(255) NOT NULL, 
-                PRIMARY KEY(id), 
-                CONSTRAINT FK_C8EFD7EFA76ED395 FOREIGN KEY (user_id) 
-                REFERENCES claro_user (id) 
-                ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
-            )
-        ");
-        $this->addSql("
-            INSERT INTO claro_workspace_tag (id, user_id, name) 
-            SELECT id, 
-            user_id, 
-            name 
-            FROM __temp__claro_workspace_tag
-        ");
-        $this->addSql("
-            DROP TABLE __temp__claro_workspace_tag
-        ");
-        $this->addSql("
-            CREATE UNIQUE INDEX tag_unique_name_and_user ON claro_workspace_tag (user_id, name)
-        ");
-        $this->addSql("
-            CREATE INDEX IDX_C8EFD7EFA76ED395 ON claro_workspace_tag (user_id)
         ");
     }
 
@@ -216,56 +170,6 @@ class Version20140602140737 extends AbstractMigration
         ");
         $this->addSql("
             CREATE UNIQUE INDEX UNIQ_A76799FF2DE62210 ON claro_resource_node (previous_id)
-        ");
-        $this->addSql("
-            DROP INDEX IDX_C8EFD7EFA76ED395
-        ");
-        $this->addSql("
-            DROP INDEX tag_unique_name_and_user
-        ");
-        $this->addSql("
-            CREATE TEMPORARY TABLE __temp__claro_workspace_tag AS 
-            SELECT id, 
-            user_id, 
-            name 
-            FROM claro_workspace_tag
-        ");
-        $this->addSql("
-            DROP TABLE claro_workspace_tag
-        ");
-        $this->addSql("
-            CREATE TABLE claro_workspace_tag (
-                id INTEGER NOT NULL, 
-                user_id INTEGER DEFAULT NULL, 
-                workspace_id INTEGER DEFAULT NULL, 
-                name VARCHAR(255) NOT NULL, 
-                PRIMARY KEY(id), 
-                CONSTRAINT FK_C8EFD7EFA76ED395 FOREIGN KEY (user_id) 
-                REFERENCES claro_user (id) 
-                ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, 
-                CONSTRAINT FK_C8EFD7EF82D40A1F FOREIGN KEY (workspace_id) 
-                REFERENCES claro_workspace (id) 
-                ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE
-            )
-        ");
-        $this->addSql("
-            INSERT INTO claro_workspace_tag (id, user_id, name) 
-            SELECT id, 
-            user_id, 
-            name 
-            FROM __temp__claro_workspace_tag
-        ");
-        $this->addSql("
-            DROP TABLE __temp__claro_workspace_tag
-        ");
-        $this->addSql("
-            CREATE INDEX IDX_C8EFD7EFA76ED395 ON claro_workspace_tag (user_id)
-        ");
-        $this->addSql("
-            CREATE UNIQUE INDEX tag_unique_name_and_user ON claro_workspace_tag (user_id, name)
-        ");
-        $this->addSql("
-            CREATE INDEX IDX_C8EFD7EF82D40A1F ON claro_workspace_tag (workspace_id)
         ");
     }
 }
