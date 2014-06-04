36a73e451fb52ef311336e2870a32d259a27bf69
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-06-04 16:36:57 +0200

    [PortfolioBundle] Update PHPDoc

--- Repository/Widget/AbstractWidgetRepository.php
+++ Repository/Widget/AbstractWidgetRepository.php
@@ -13,7 +13,7 @@ class AbstractWidgetRepository extends EntityRepository
      * @param \Icap\PortfolioBundle\Entity\Portfolio|\Icap\PortfolioBundle\Repository\Widget\User $portfolio
      * @param bool                                                                                $executeQuery
      *
-     * @return array|\Doctrine\ORM\Query
+     * @return \Icap\PortfolioBundle\Entity\Widget\AbstractWidget|\Doctrine\ORM\Query
      */
     public function findOneByTypeAndPortfolio($type, Portfolio $portfolio, $executeQuery = true)
     {
