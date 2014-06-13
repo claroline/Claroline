737408f862d9332083e13c4c94ae7d7ac6838b23
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-06-13 14:02:29 +0200

    [PortfolioBundle] Change the way we add children in widget

--- Resources/public/js/directives/collectionFormDirective.js
+++ Resources/public/js/directives/collectionFormDirective.js
@@ -3,6 +3,7 @@
 portfolioApp
     .directive("collectionForm", function() {
         var link = function (scope, element, attrs, ngModel) {
+            console.log(attrs);
             if(!ngModel) {
                 return;// do nothing if no ng-model
             }
@@ -10,6 +11,8 @@ portfolioApp
             ngModel.$render = function() {
                 scope.collection = ngModel.$viewValue || [];
             };
+
+            scope.emptyChild = jQuery.parseJSON(attrs.collectionForm.replace(/'/g, '"'));
 
             scope.addChild = function(child) {
                 scope.collection.push(child);
@@ -21,6 +24,12 @@ portfolioApp
 
             scope.cancelDeletionOfChild = function(child) {
                 delete child.toDelete;
+            };
+
+            scope.addEmptyChild = function(index) {
+                if (index == scope.collection.length) {
+                    scope.collection.push(angular.copy(scope.emptyChild));
+                }
             };
         };
 
