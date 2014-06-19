1e894417e64fc41c34b97ca27a34d100a4a64876
Anh Thao PHAM <anhthao.pham@claroline.net>
2014-06-19 12:05:26 +0200

    [CoreBundle] Adds resultMax in Rule

--- Migrations/pdo_mysql/Version20140619115559.php
+++ Migrations/pdo_mysql/Version20140619115559.php
@@ -8,23 +8,39 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2014/06/19 09:29:33
+ * Generation date: 2014/06/19 11:56:00
  */
-class Version20140619092932 extends AbstractMigration
+class Version20140619115559 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         $this->addSql("
+            ALTER TABLE claro_badge_rule 
+            ADD resultMax VARCHAR(255) DEFAULT NULL
+        ");
+        $this->addSql("
             ALTER TABLE claro_activity_rule_action 
             DROP rule_type
+        ");
+        $this->addSql("
+            ALTER TABLE claro_activity_rule 
+            ADD resultMax VARCHAR(255) DEFAULT NULL
         ");
     }
 
     public function down(Schema $schema)
     {
         $this->addSql("
+            ALTER TABLE claro_activity_rule 
+            DROP resultMax
+        ");
+        $this->addSql("
             ALTER TABLE claro_activity_rule_action 
             ADD rule_type VARCHAR(255) DEFAULT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE claro_badge_rule 
+            DROP resultMax
         ");
     }
 }
