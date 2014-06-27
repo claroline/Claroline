19736a2e3eb595d2a637bcccfb0f5b1d865897a5
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-06-27 10:54:40 +0200

    [PortfolioBundle] Don't display change column button if there is only one column available

--- Resources/public/js/controllers/designController.js
+++ Resources/public/js/controllers/designController.js
@@ -1,48 +1,49 @@
 'use strict';
 
 portfolioApp
-    .controller("designController", ["$scope", "$filter", "portfolioManager", "widgetsManager", function($scope, $filter, portfolioManager, widgetsManager) {
-        $scope.changing = false;
+    .controller("designController", ["$scope", "$filter", "portfolioManager", "widgetsManager",
+        function($scope, $filter, portfolioManager, widgetsManager) {
+            $scope.changing = false;
 
-        $scope.changeDisposition = function (disposition) {
-            if (!$scope.changing && disposition !== $scope.portfolio.disposition) {
-                $scope.changing = true;
-                $scope.portfolio.disposition = disposition;
-                portfolioManager.save($scope.portfolio).then(function (data) {
-                    $scope.changing = false;
-                });
-            }
-        }
-
-        $scope.changeColumn = function($event, widget, column) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            widget.column = column;
-            widgetsManager.save(widget);
-        };
-
-        $scope.$watch('portfolio.disposition', function(newValue, oldValue) {
-            if (newValue !== undefined) {
-                switch(newValue) {
-                    case 1:
-                        $scope.cols = [1, 2];
-                        var widgetsToUpdate = $filter('filter')($scope.widgets, {type: '!title', column: 3});
-                        angular.forEach(widgetsToUpdate, function(widget, key) {
-                            widget.column = 2;
-                        });
-                        break;
-                    case 2:
-                        $scope.cols = [1, 2, 3];
-                        break;
-                    default:
-                        $scope.cols = [];
-                        var widgetsToUpdate = $filter('filter')($scope.widgets, {type: '!title', column: '!3'});
-                        angular.forEach(widgetsToUpdate, function(widget, key) {
-                            widget.column = 1;
-                        });
-                        break;
+            $scope.changeDisposition = function (disposition) {
+                if (!$scope.changing && disposition !== $scope.portfolio.disposition) {
+                    $scope.changing = true;
+                    $scope.portfolio.disposition = disposition;
+                    portfolioManager.save($scope.portfolio).then(function (data) {
+                        $scope.changing = false;
+                    });
                 }
             }
-        });
-    }]);
+
+            $scope.changeColumn = function($event, widget, column) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                widget.column = column;
+                widgetsManager.save(widget);
+            };
+
+            $scope.$watch('portfolio.disposition', function(newValue, oldValue) {
+                if (newValue !== undefined) {
+                    switch(newValue) {
+                        case 1:
+                            $scope.cols = [1, 2];
+                            var widgetsToUpdate = $filter('filter')($scope.widgets, {type: '!title', column: 3});
+                            angular.forEach(widgetsToUpdate, function(widget, key) {
+                                widget.column = 2;
+                            });
+                            break;
+                        case 2:
+                            $scope.cols = [1, 2, 3];
+                            break;
+                        default:
+                            $scope.cols = [];
+                            var widgetsToUpdate = $filter('filter')($scope.widgets, {type: '!title', column: '!3'});
+                            angular.forEach(widgetsToUpdate, function(widget, key) {
+                                widget.column = 1;
+                            });
+                            break;
+                    }
+                }
+            });
+        }]);
