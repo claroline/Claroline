7667630157a9637c4ed0af199378f2b2201adf5e
Panos <ptsavdar@gmail.com>
2014-08-26 15:56:18 +0200

    [WebsiteBundle] added libs and styles

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -9,7 +9,91 @@
 namespace Icap\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use Icap\WebsiteBundle\Entity\Website;
+
+use Claroline\CoreBundle\Event\Log\LogResourceReadEvent;
+use Claroline\CoreBundle\Library\Resource\ResourceCollection;
+
 
 class Controller extends BaseController{
+    /**
+     * @param string $permission
+     *
+     * @param Website $website
+     *
+     * @throws AccessDeniedException
+     */
+    protected function checkAccess($permission, Website $website)
+    {
+        $collection = new ResourceCollection(array($website->getResourceNode()));
+        if (!$this->get('security.context')->isGranted($permission, $collection)) {
+            throw new AccessDeniedException($collection->getErrorsForDisplay());
+        }
+        $logEvent = new LogResourceReadEvent($website->getResourceNode());
+        $this->get('event_dispatcher')->dispatch('log', $logEvent);
+    }
 
-} 
+    /**
+     * @param string $permission
+     *
+     * @param Website $website
+     *
+     * @return bool
+     */
+    protected function isUserGranted($permission, Website $website, $collection = null)
+    {
+        if ($collection === null) {
+            $collection = new ResourceCollection(array($website->getResourceNode()));
+        }
+        $checkPermission = false;
+        if ($this->get('security.context')->isGranted($permission, $collection)) {
+            $checkPermission = true;
+        }
+        return $checkPermission;
+    }
+
+    /**
+     * @param $event
+     *
+     * @return Controller
+     */
+    protected function dispatch($event)
+    {
+        $this->get('event_dispatcher')->dispatch('log', $event);
+        return $this;
+    }
+
+    /**
+     * @param Website $website
+     * @param string $childType
+     * @param string $action
+     * @param array $details
+     *
+     * @return Controller
+     */
+    protected function dispatchChildEvent(Website $website, $childType, $action, $details = array())
+    {
+        $event = new LogResourceChildUpdateEvent(
+            $website->getResourceNode(),
+            $childType,
+            $action,
+            $details
+        );
+        return $this->dispatch($event);
+    }
+
+    /**
+     * Retrieve logged user. If anonymous return null.
+     * @return user
+     */
+    protected function getLoggedUser()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        if (is_string($user)) {
+            $user = null;
+        }
+        return $user;
+    }
+}
--- Listener/WebsiteListener.php
+++ Listener/WebsiteListener.php
@@ -8,7 +8,105 @@
 
 namespace Icap\WebsiteBundle\Listener;
 
+use Icap\WebsiteBundle\Entity\Website;
+use Icap\WebsiteBundle\Form\WebsiteType;
 
-class WebsiteListener {
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
+use Claroline\CoreBundle\Event\CreateFormResourceEvent;
+use Claroline\CoreBundle\Event\CreateResourceEvent;
+use Claroline\CoreBundle\Event\DeleteResourceEvent;
+use Claroline\CoreBundle\Event\OpenResourceEvent;
+use Claroline\CoreBundle\Event\CopyResourceEvent;
+use Claroline\CoreBundle\Event\LogCreateDelegateViewEvent;
+
+class WebsiteListener extends ContainerAware{
+    public function onCreateForm(CreateFormResourceEvent $event)
+    {
+        $form = $this->container->get('form.factory')->create(new WebsiteType(), new Website());
+        $content = $this->container->get('templating')->render(
+            'ClarolineCoreBundle:Resource:createForm.html.twig',
+            array(
+                'form' => $form->createView(),
+                'resourceType' => 'icap_website'
+            )
+        );
+        $event->setResponseContent($content);
+        $event->stopPropagation();
+    }
+    public function onCreate(CreateResourceEvent $event)
+    {
+        $request = $this->container->get('request');
+        $form = $this->container->get('form.factory')->create(new WebsiteType(), new Website());
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $website = $form->getData();
+            $event->setResources(array($website));
+        } else {
+            $content = $this->container->get('templating')->render(
+                'ClarolineCoreBundle:Resource:createForm.html.twig',
+                array(
+                    'form' => $form->createView(),
+                    'resourceType' => 'icap_website'
+                )
+            );
+            $event->setErrorFormContent($content);
+        }
+        $event->stopPropagation();
+    }
+    public function onOpen(OpenResourceEvent $event)
+    {
+        $route = $this->container
+            ->get('router')
+            ->generate(
+                'icap_website_view',
+                array('websiteId' => $event->getResource()->getId())
+            );
+        $event->setResponse(new RedirectResponse($route));
+        $event->stopPropagation();
+    }
+    public function onDelete(DeleteResourceEvent $event)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $em->remove($event->getResource());
+        $em->flush();
+        $event->stopPropagation();
+    }
+    public function onCopy(CopyResourceEvent $event)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        /*$wiki = $event->getResource();
+        $oldRoot = $wiki->getRoot();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $sectionRepository = $em->getRepository('IcapWikiBundle:Section');
+        $sections = $sectionRepository->children($oldRoot);
+        $newSectionsMap = array();
+        $newWiki = new Wiki();
+        $newWiki->setWikiCreator($user);
+        $em->persist($newWiki);
+        $em->flush($newWiki);
+        $newRoot = $newWiki->getRoot();
+        $newRoot->getActiveContribution()->setText($oldRoot->getActiveContribution()->getText());
+        $newSectionsMap[$oldRoot->getId()] = $newRoot;
+        foreach ($sections as $section) {
+            $newSection = new Section();
+            $newSection->setWiki($newWiki);
+            $newSection->setVisible($section->getVisible());
+            $newSectionParent = $newSectionsMap[$section->getParent()->getId()];
+            $newSection->setParent($newSectionParent);
+            $newSection->setAuthor($user);
+            $activeContribution = new Contribution();
+            $activeContribution->setTitle($section->getActiveContribution()->getTitle());
+            $activeContribution->setText($section->getActiveContribution()->getText());
+            $activeContribution->setSection($newSection);
+            $activeContribution->setContributor($user);
+            $newSection->setActiveContribution($activeContribution);
+            $newSectionsMap[$section->getId()] = $newSection;
+            $sectionRepository->persistAsLastChildOf($newSection, $newSectionParent);
+        }
+        $event->setCopy($newWiki);*/
+        $event->stopPropagation();
+    }
 } 
