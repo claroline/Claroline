2cbff34e378cc2cfa79627c0469f1255a17d09db
Elorfin <penin.axel@gmail.com>
2014-08-26 12:03:06 +0200

    [CoreBundle] change Activity description field to type TEXT. VARCHAR(255) is too short for HTML content

---
+++ Migrations/drizzle_pdo_mysql/Version20140826104917.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Claroline\CoreBundle\Migrations\drizzle_pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/08/26 10:49:18
+ */
+class Version20140826104917 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_activity CHANGE description description TEXT NOT NULL
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_activity CHANGE description description VARCHAR(255) NOT NULL
+        ");
+    }
+}
