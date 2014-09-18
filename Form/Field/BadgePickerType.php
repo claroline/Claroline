9b2fde42918829f154b07881816500120aa39990
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-09-18 16:06:16 +0200

    [CoreBundle] Add parent to badge picker field

--- Form/Field/BadgePickerType.php
+++ Form/Field/BadgePickerType.php
@@ -71,6 +71,11 @@ class BadgePickerType extends AbstractType
         );
     }
 
+    public function getParent()
+    {
+        return 'text';
+    }
+
     public function getName()
     {
         return 'badgepicker';
