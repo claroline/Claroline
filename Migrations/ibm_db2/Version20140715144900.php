5187721e483ce3a134d6beefe2fb83ebe873bde2
bradInside <brad.inside@gmail.com>
2014-09-25 15:39:50 +0200

    [DropzoneBundle] Re adding migration 900 to 8.1 #111

---
+++ Migrations/ibm_db2/Version20140715144900.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Icap\DropzoneBundle\Migrations\ibm_db2;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/07/15 02:49:06
+ */
+class Version20140715144900 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            ADD COLUMN force_comment_in_correction SMALLINT NOT NULL
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            DROP COLUMN force_comment_in_correction
+        ");
+    }
+}
