6f3ed1005406b5de128abbe5a8cdaf85c6a72565
bradInside <brad.inside@gmail.com>
2014-09-26 15:23:58 +0200

    [DropzoneBundle] Correct Migrations files from 900
    #111

---
+++ Migrations/pdo_pgsql/Version20140926131620.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Icap\DropzoneBundle\Migrations\pdo_pgsql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2014/09/26 01:16:24
+ */
+class Version20140926131620 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            ADD event_agenda_drop INT DEFAULT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            ADD event_agenda_correction INT DEFAULT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            ADD CONSTRAINT FK_6782FC23E6B974D2 FOREIGN KEY (event_agenda_drop) 
+            REFERENCES claro_event (id) 
+            ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            ADD CONSTRAINT FK_6782FC238D9E1321 FOREIGN KEY (event_agenda_correction) 
+            REFERENCES claro_event (id) 
+            ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_6782FC23E6B974D2 ON icap__dropzonebundle_dropzone (event_agenda_drop)
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_6782FC238D9E1321 ON icap__dropzonebundle_dropzone (event_agenda_correction)
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            DROP CONSTRAINT FK_6782FC23E6B974D2
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            DROP CONSTRAINT FK_6782FC238D9E1321
+        ");
+        $this->addSql("
+            DROP INDEX UNIQ_6782FC23E6B974D2
+        ");
+        $this->addSql("
+            DROP INDEX UNIQ_6782FC238D9E1321
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            DROP event_agenda_drop
+        ");
+        $this->addSql("
+            ALTER TABLE icap__dropzonebundle_dropzone 
+            DROP event_agenda_correction
+        ");
+    }
+}
