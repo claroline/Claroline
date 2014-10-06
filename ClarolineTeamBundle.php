ea371e9b76ca4430f464d30dd0899ae1ee5a7ab1
Anh Thao PHAM <anhthao.pham@claroline.net>
2014-10-06 16:03:20 +0200

    [TeamBundle] Adds required files

---
+++ ClarolineTeamBundle.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\TeamBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+
+class ClarolineTeamBundle extends PluginBundle
+{
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(
+            __DIR__ . '/Resources/config/routing.yml',
+            null,
+            'team'
+        );
+    }
+
+    public function hasMigrations()
+    {
+        return true;
+    }
+}
