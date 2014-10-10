22ce9ffd743873d88a2ac2a739423e478b82b6f2
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-10-10 11:03:26 +0200

    [PortfolioBundle] Scroll to the bottom of the comment list when adding a comment

--- Resources/public/js/controllers/portfolioController.js
+++ Resources/public/js/controllers/portfolioController.js
@@ -1,7 +1,8 @@
 'use strict';
 
 portfolioApp
-    .controller("portfolioController", ["$scope", "$filter", "portfolioManager", "widgetsManager", "$attrs", "widgetsConfig", "assetPath", function($scope, $filter, portfolioManager, widgetsManager, $attrs, widgetsConfig, assetPath) {
+    .controller("portfolioController", ["$scope", "$filter", "portfolioManager", "widgetsManager", "$attrs", "widgetsConfig", "assetPath", "$timeout",
+                                function($scope, $filter, portfolioManager, widgetsManager, $attrs, widgetsConfig, assetPath, $timeout) {
         $scope.portfolio = portfolioManager.getPortfolio($attrs['portfolioContainer']);
         $scope.portfolio.$promise.then(function () {
             $scope.widgets = widgetsManager.widgets;
@@ -20,9 +21,17 @@ portfolioApp
                 var comment = {
                     'message' : this.comment
                 };
+                var wrapper = $(".communication_panel .media-list")[0];
+
                 this.portfolio.comments.push(comment);
                 portfolioManager.save(this.portfolio);
                 this.comment = '';
+
+
+                // will fail if you scroll immediately because we scroll before the view is updated;
+                $timeout(function(){
+                  wrapper.scrollTop = wrapper.scrollHeight;
+                },0);
             }
         };
     }]);
