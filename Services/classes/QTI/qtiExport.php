a1388471ff9d478b416a58a27f4df466d7d761bf
david <david@DavidB>
2014-10-10 11:05:34 +0200

    [ExoBundle] Refactoring 17 QTI

--- Services/classes/QTI/qtiExport.php
+++ Services/classes/QTI/qtiExport.php
@@ -1,9 +1,8 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * To export a question in QTI
+ *
  */
 
 namespace UJM\ExoBundle\Services\classes\QTI;
