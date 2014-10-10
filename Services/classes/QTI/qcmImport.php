de77917de834d93899fccb91fe738e3df85d0cef
david <david@DavidB>
2014-10-10 15:34:22 +0200

    [ExoBundle] Refactoring 21 QTI

---
+++ Services/classes/QTI/qcmImport.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * To import a QCM question in QTI
+ *
+ */
+
+namespace UJM\ExoBundle\Services\classes\QTI;
+
+class qcmImport extends qtiImport
+{
+    
+}
---
+++ Services/classes/QTI/qtiImport.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * To import a question in QTI
+ *
+ */
+
+namespace UJM\ExoBundle\Services\classes\QTI;
+
+class qtiImport
+{
+    
+}
