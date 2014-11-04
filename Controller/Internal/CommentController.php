46787d41eecd51eb050afcd19351aa2340d8c972
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-11-04 11:42:33 +0100

    [PortfolioBundle] Let only guide to comment on portfolio

--- Controller/Internal/CommentController.php
+++ Controller/Internal/CommentController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @Route("/internal/portfolio/{id}")
@@ -26,6 +27,12 @@ class CommentController extends BaseController
     public function postAction(Request $request, User $loggedUser, Portfolio $portfolio)
     {
         $this->checkPortfolioToolAccess();
+
+        $portfolioGuide = $this->get("icap_portfolio.manager.portfolio_guide")->getByPortfolioAndGuide($portfolio, $loggedUser);
+
+        if (null === $portfolioGuide) {
+            throw new NotFoundHttpException();
+        }
 
         $commentManager = $this->getCommentsManager();
 
