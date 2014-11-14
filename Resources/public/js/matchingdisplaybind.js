09e0a160a8dfc997805d412e64025c757d5d0944
Marion Crayssac <marion.crayssac@univ-st-etienne.fr>
2014-11-14 12:10:43 +0100

    [ExoBundle] Interface passing question of bind matching questions and begining correction

--- Resources/public/js/matchingdisplaybind.js
+++ Resources/public/js/matchingdisplaybind.js
@@ -1,44 +1,61 @@
+var responses = [];
 $(function() {
-    
-    var conn;
 
     jsPlumb.ready(function() {
         jsPlumb.setContainer($("body"));
-
-        $(".origin").each(function() {
-            $(this).draggable({
-                cursor: 'move',
-                revert: 'invalid',
-                start: function(){
-                    $(this).show();             
-                },
-            });
-        });
-
+        
         jsPlumb.makeSource($(".origin"), {
-            anchor:"Right",
+            isSource:true,
+            anchor: "Right",
         });
 
         jsPlumb.makeTarget($(".droppable"), {
+            isTarget:true,
             anchor: "Left",
         });
-
-        $(".droppable").each(function() {
-            
-            $(this).droppable({
-                tolerance: "pointer",
-                drop: function(event, ui) {
-                    var drag = ui.draggable.attr("id");
-                    var drop = $(this).attr("id");
-                    conn = jsPlumb.connect({
-                        source: drag,
-                        target: drop,
-                    });
-                    jsPlumb.bind("click", function(conn, originalEvent) {
-                        jsPlumb.detach(conn);
-                    });
+        
+        jsPlumb.importDefaults({
+            ConnectionsDetachable:false,
+            Connector : "Straight",
+            Endpoint : "Dot",
+            EndpointStyle : {fillStyle:"#777", radius: 5},
+            PaintStyle : { strokeStyle:"#777", lineWidth: 4},
+            HoverPaintStyle:{strokeStyle:"red"},
+            LogEnabled:false,
+            DropOptions:{tolerance:"touch"},
+            onMaxConnections: 1
+        });
+                    
+        jsPlumb.bind("click", function(connections){
+            jsPlumb.detach(connections);
+        });
+        
+        $("#droppable_48").click(function() {
+            $(".origin").each(function() {
+                var idProposal = $(this).attr("id");
+                var proposal = idProposal.replace('origin_', '');
+                responses[proposal] = 'NULL';
+                var connections = jsPlumb.getConnections({source:idProposal});
+                responses[proposal] = proposals = [];
+                for(i=0; i<connections.length; i++) {
+                    var idLabel = connections[i].targetId;
+                    var label = idLabel.replace('droppable_', '');
+                    responses[proposal][i] = label;
                 }
+                dragStop();
             });
         });
     });
+    
+    function dragStop() {
+        var resp = '';
+        $.each(responses, function(key, value) {
+            if (value) {
+                resp = resp + key + '-' + value + ';';
+            }
+        });
+        $('#jsonResponse').val(resp);
+    }
+    
+    
 });
