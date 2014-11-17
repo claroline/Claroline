4b56316910f314f5b6164df2b4b0a3976ba3c8c4
Godfraind <nicolas.godfraind@gmail.com>
2014-11-17 14:21:41 +0100

    [BundleRecorder] bundle.ini instanciate bundle in the correct order

--- Recorder.php
+++ Recorder.php
@@ -33,6 +33,7 @@ class Recorder
     private $vendorDir;
     private $removableBundles = array();
     private $logger;
+    private $fromRepo;
 
     public function __construct(
         Detector $detector,
@@ -45,6 +46,8 @@ class Recorder
         $this->bundleHandler = $bundleHandler;
         $this->operationHandler = $operationHandler;
         $this->vendorDir = $vendorDir;
+        $installedFile = new JsonFile($this->vendorDir . '/composer/installed.json');
+        $this->fromRepo = new InstalledFilesystemRepository($installedFile);
     }
 
     public function setLogger(\Closure $logger)
@@ -102,26 +105,12 @@ class Recorder
 
     public function buildBundleFile()
     {
-        $installedFile = new JsonFile($this->vendorDir . '/composer/installed.json');
-        $fromRepo = new InstalledFilesystemRepository($installedFile);
-        $toRepo = new ArrayRepository();
-
-        $pool = new Pool();
-        $pool->addRepository($fromRepo);
-        $pool->addRepository(new PlatformRepository());
-        $request = new Request($pool);
-
-        foreach ($fromRepo->getPackages() as $package) {
-            $request->install($package->getName());
-        }
-
-        $solver = new Solver(new DefaultPolicy(), $pool, $toRepo);
-        $operations = $solver->solve($request);
         $orderedBundles = array();
 
-        foreach ($operations as $operation) {
-            $bundles = $this->detector->detectBundles($operation->getPackage()->getPrettyName());
-            $autoload = $operation->getPackage()->getAutoload();
+        foreach ($this->fromRepo->getCanonicalPackages() as $package) {
+            $prettyName = $package->getPrettyName();
+            $bundles = $this->detector->detectBundles($prettyName);
+            $autoload = $package->getAutoload();
 
             if (count($bundles) > 0) {
                 if (isset($autoload['psr-4'])) {
@@ -136,12 +125,27 @@ class Recorder
             }
         }
 
+        $orderedBundles = $this->orderClaroBundlesForInstall($orderedBundles);
         $this->bundleHandler->writeBundleFile(array_unique($orderedBundles));
     }
 
-    private function isClarolinePackage(PackageInterface $package)
+    /**
+     * Returns if the $element is a claroline package.
+     *
+     * @param PackageInterface|string $element
+     * @return boolean
+     */
+    private function isClarolinePackage($element)
     {
-        return $package->getType() === 'claroline-core' || $package->getType() === 'claroline-plugin';
+        if ($element instanceof PackageInterface) {
+            return $element->getType() === 'claroline-core' || $element->getType() === 'claroline-plugin';
+        }
+
+        foreach ($this->fromRepo->getCanonicalPackages() as $package) {
+            if ($element === $package->getPrettyName()) {
+                return $this->isClarolinePackage($package);
+            }
+        }
     }
 
     private function getOperationBundleType(PackageInterface $package)
@@ -149,5 +153,103 @@ class Recorder
         return $package->getType() === 'claroline-core' ?
             Operation::BUNDLE_CORE :
             Operation::BUNDLE_PLUGIN;
+    }
+
+    /**
+     * This is a simplistic sort but it's enough for now 
+     * (it won't handle recursive/multiple dependencies but it's enough for now)
+     */
+    public function orderClaroBundlesForInstall(array $bundles)
+    {
+        //maybe use $this->fromRepo instead
+        $installedBundles = json_decode(file_get_contents($this->vendorDir . '/composer/installed.json'));
+        $claroBundles = array();
+
+        foreach ($installedBundles as $installedBundle) {
+            if ($installedBundle->type === 'claroline-core'
+                || $installedBundle->type === 'claroline-plugin') {
+                $claroBundles[$this->getNameSpace($installedBundle->name)] =
+                    $this->getDependencies($installedBundle->require);
+            }
+        }
+
+        $bundleCopies = $bundles;
+
+        foreach ($claroBundles as $claroBundle => $dependencies) {
+            foreach ($dependencies as $dependency) {
+                if (!$this->isDependencySetBefore($bundles, $claroBundle, $dependency)) {
+                    //we have to place the bundle properly
+                    $key = array_search($claroBundle, $bundleCopies);
+                    $newKey = $this->arrayRightShift($dependency, $bundleCopies);
+                    $bundleCopies[$newKey] = $claroBundle;
+                    unset($bundleCopies[$key]);
+                    $bundleCopies = array_values($bundleCopies);
+                }
+            }
+        }
+
+        return $bundleCopies;
+    }
+
+    /**
+     * Shift an array to the right.
+     * Example: $array = array([0] => 'a', [1] => 'b', [2] => 'c');
+     * $return = arrayRightShift('a', $array);
+     * $array = array([0] => 'a', [1] => 'b', [2] => 'b', [3] => c)
+     */
+    private function arrayRightShift($search, &$array) {
+        $key = array_search($search, $array);
+
+        for ($i = count($array); $i > $key; $i--) {
+            $array[$i] = $array[$i - 1];
+        }
+
+        return $key + 1;
+    }
+
+    private function isDependencySetBefore(array $bundles, $bundle, $dependency) {
+        $foundDep = false;
+
+        foreach ($bundles as $initBundle) {
+            if ($bundle === $dependency) $foundDep = true;
+            if ($bundle === $initBundle) {
+                return $foundDep;
+            }
+        }
+    }
+
+    /**
+     * Returns the dependencies of a Claroline package.
+     *
+     * @param The require class wich was parsed from the installed.json with json_decode
+     * @return array
+     */
+    private function getDependencies(\StdClass $require)
+    {
+        $dependencies = [];
+
+        foreach($require as $name => $el) {
+            if ($this->isClarolinePackage($name)) {
+                $dependencies[] = $this->getNameSpace($name);
+            }
+        }
+
+        return $dependencies;
+    }
+
+    private function getNameSpace($prettyName)
+    {
+        foreach ($this->fromRepo->getCanonicalPackages() as $package) {
+            if ($prettyName === $package->getPrettyName()) {
+                $autoload = $package->getAutoload();
+                if (isset($autoload['psr-0'])) {
+                    $prefixe = array_shift(array_keys($autoload['psr-0']));
+
+                    return $prefixe . '\\' . str_replace('\\', '', $prefixe);
+                } else {
+                    return $prettyName;
+                }
+            }
+        }
     }
 }
