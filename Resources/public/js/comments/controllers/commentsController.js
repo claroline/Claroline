7ad8cd2a56eda8527f654d4c190f4ad39e5c6e67
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-11-19 17:12:10 +0100

    [PortfolioBundle] Add tinymce configuration

--- Resources/public/js/comments/controllers/commentsController.js
+++ Resources/public/js/comments/controllers/commentsController.js
@@ -3,6 +3,11 @@
 commentsApp
     .controller("commentsController", ["$scope", "$timeout", "commentsManager", function($scope, $timeout, commentsManager) {
         $scope.comments = commentsManager.comments;
+        $scope.tinyMceConfig = {
+            forced_root_block : "",
+            force_br_newlines : true,
+            force_p_newlines : false
+        };
 
         $scope.addComment = function(portfolioId, message) {
             commentsManager.addComment(portfolioId, message);
