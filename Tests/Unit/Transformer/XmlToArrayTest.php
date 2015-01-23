5f686770c44f1e4a04210054304f92bc84d437a2
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-01-23 14:38:43 +0100

    [PortfolioBundle] Add a test for xml to array transformer

--- Tests/Unit/Transformer/XmlToArrayTest.php
+++ Tests/Unit/Transformer/XmlToArrayTest.php
@@ -154,4 +154,80 @@ CONTENT;
 
         $this->assertEquals($expected, $transformer->transform($content));
     }
+
+    public function testTransformWithEntryAndRdfTag()
+    {
+        $transformer = new XmlToArray();
+
+        $content = <<<CONTENT
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:leap2="http://terms.leapspecs.org/"
+      xmlns:categories="http://www.leapspecs.org/2A/categories">
+    <leap2:version>http://www.leapspecs.org/2010-07/2A/</leap2:version>
+    <id>54be827257316</id>
+    <title>54be8272572d8</title>
+    <author>
+        <name>54be827257361 54be82725739a</name>
+    </author>
+    <updated>2015-01-20T17:29:38+01:00</updated>
+
+    <entry>
+        <title>Mes super badges</title>
+        <id>portfolio:badges1</id>
+        <updated>2010-10-07T22:30:23+02:00</updated>
+        <content></content>
+
+        <rdf:type rdf:resource="leap2:selection"/>
+        <category term="Grouping" scheme="categories:selection_type#"/>
+    </entry>
+</feed>
+CONTENT;
+
+        $expected = array (
+            'version' => array(
+                'value' => 'http://www.leapspecs.org/2010-07/2A/'
+            ),
+            'id' =>  array(
+                'value' => '54be827257316'
+            ),
+            'title' =>  array(
+                'value' => '54be8272572d8'
+            ),
+            'author' => array(
+                'name' => array(
+                    'value' => '54be827257361 54be82725739a'
+                ),
+            ),
+            'updated' =>  array(
+                'value' => '2015-01-20T17:29:38+01:00'
+            ),
+            'entry' => array(
+                'title'    => array(
+                    'value' => 'Mes super badges'
+                ),
+                'id'       => array(
+                    'value' => 'portfolio:badges1'
+                ),
+                'updated'  => array(
+                    'value' => '2010-10-07T22:30:23+02:00'
+                ),
+                'content'  => array(),
+                'type'     => array(
+                    'attributes' => array(
+                        'resource' => 'leap2:selection'
+                    )
+                ),
+                'category' => array(
+                    'attributes' => array(
+                        'term' => 'Grouping',
+                        'scheme' => 'categories:selection_type#'
+                    )
+                )
+            )
+        );
+
+        $this->assertEquals($expected, $transformer->transform($content));
+    }
 }
