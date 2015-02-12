d2dba7d9487896f25de7dc6d95c19dc7cf7f2dec
ngodfraind <nicolas.godfraind@gmail.com>
2015-02-12 17:05:51 +0100

    [CoreBundle] Removing foreign key constraints.

--- Migrations/pdo_mysql/Version20150211170325.php
+++ Migrations/pdo_mysql/Version20150211170325.php
@@ -15,24 +15,25 @@ class Version20150211170325 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql("
+            SET foreign_key_checks = 0;
             CREATE TABLE claro_panel_facet (
-                id INT AUTO_INCREMENT NOT NULL, 
-                facet_id INT NOT NULL, 
-                name VARCHAR(255) NOT NULL, 
-                position INT NOT NULL, 
-                isDefaultCollapsed TINYINT(1) NOT NULL, 
-                INDEX IDX_DA3985FFC889F24 (facet_id), 
+                id INT AUTO_INCREMENT NOT NULL,
+                facet_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                position INT NOT NULL,
+                isDefaultCollapsed TINYINT(1) NOT NULL,
+                INDEX IDX_DA3985FFC889F24 (facet_id),
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
         ");
         $this->addSql("
-            ALTER TABLE claro_panel_facet 
-            ADD CONSTRAINT FK_DA3985FFC889F24 FOREIGN KEY (facet_id) 
-            REFERENCES claro_facet (id) 
+            ALTER TABLE claro_panel_facet
+            ADD CONSTRAINT FK_DA3985FFC889F24 FOREIGN KEY (facet_id)
+            REFERENCES claro_facet (id)
             ON DELETE CASCADE
         ");
         $this->addSql("
-            ALTER TABLE claro_field_facet 
+            ALTER TABLE claro_field_facet
             DROP FOREIGN KEY FK_F6C21DB2FC889F24
         ");
         $this->addSql("
@@ -42,10 +43,11 @@ class Version20150211170325 extends AbstractMigration
             ALTER TABLE claro_field_facet CHANGE facet_id panelFacet_id INT NOT NULL
         ");
         $this->addSql("
-            ALTER TABLE claro_field_facet 
-            ADD CONSTRAINT FK_F6C21DB2E99038C0 FOREIGN KEY (panelFacet_id) 
-            REFERENCES claro_panel_facet (id) 
+            ALTER TABLE claro_field_facet
+            ADD CONSTRAINT FK_F6C21DB2E99038C0 FOREIGN KEY (panelFacet_id)
+            REFERENCES claro_panel_facet (id)
             ON DELETE CASCADE
+            SET foreign_key_checks = 1;
         ");
         $this->addSql("
             CREATE INDEX IDX_F6C21DB2E99038C0 ON claro_field_facet (panelFacet_id)
@@ -55,7 +57,7 @@ class Version20150211170325 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->addSql("
-            ALTER TABLE claro_field_facet 
+            ALTER TABLE claro_field_facet
             DROP FOREIGN KEY FK_F6C21DB2E99038C0
         ");
         $this->addSql("
@@ -68,9 +70,9 @@ class Version20150211170325 extends AbstractMigration
             ALTER TABLE claro_field_facet CHANGE panelfacet_id facet_id INT NOT NULL
         ");
         $this->addSql("
-            ALTER TABLE claro_field_facet 
-            ADD CONSTRAINT FK_F6C21DB2FC889F24 FOREIGN KEY (facet_id) 
-            REFERENCES claro_facet (id) 
+            ALTER TABLE claro_field_facet
+            ADD CONSTRAINT FK_F6C21DB2FC889F24 FOREIGN KEY (facet_id)
+            REFERENCES claro_facet (id)
             ON DELETE CASCADE
         ");
         $this->addSql("
