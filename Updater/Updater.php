aa38776055ee5ef1549452be91d3e5f74c952a70
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-03-04 15:13:17 +0100

    [InstallationBundle] Using LoggableTrait

--- Updater/Updater.php
+++ Updater/Updater.php
@@ -2,33 +2,9 @@
 
 namespace Claroline\InstallationBundle\Updater;
 
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
+use Claroline\InstallationBundle\Log\LoggableTrait;
 
 abstract class Updater
 {
-    /**
-     * @var LoggerInterface
-     */
-    protected $logger;
-
-    /**
-     * @param LoggerInterface $logger
-     *
-     * @return $this
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-
-        return $this;
-    }
-
-    /**
-     * @param $message
-     */
-    public function log($message)
-    {
-        $this->logger->log(LogLevel::INFO, $message);
-    }
+    use LoggableTrait;
 }
