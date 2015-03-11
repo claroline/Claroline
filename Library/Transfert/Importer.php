30aa410822992f81b132218e2218d888530fc841
ngodfraind <nicolas.godfraind@gmail.com>
2015-03-11 14:33:07 +0100

    [CoreBundle] version + removing method from importer abstract class

--- Library/Transfert/Importer.php
+++ Library/Transfert/Importer.php
@@ -131,14 +131,9 @@ abstract class Importer
     abstract function validate(array $data);
 
     /**
-     * @param array $data
-     */
-     abstract function import(array $data);
-
-    /**
      * @param Workspace $workspace
      * @param array $files
      * @param mixed $object
      */
      abstract function export(Workspace $workspace, array &$files, $object);
-} 
+}
