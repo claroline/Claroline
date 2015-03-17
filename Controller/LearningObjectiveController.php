76fb419b621eded8f6607e5fec85e9284de2bde5
Stéphane Klein <stephaneklein221@gmail.com>
2015-03-17 12:47:39 +0100

    [CompetencyBundle] Set learning objectives admin layout

--- Controller/LearningObjectiveController.php
+++ Controller/LearningObjectiveController.php
@@ -9,6 +9,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
 /**
  * @DI\Tag("security.secure_service")
  * @SEC\PreAuthorize("canOpenAdminTool('learning-objectives')")
+ * @EXT\Route("/objectives", requirements={"id"="\d+"}, options={"expose"=true})
+ * @EXT\Method("GET")
  */
 class LearningObjectiveController
 {
@@ -16,12 +18,40 @@ class LearningObjectiveController
      * Displays the index of the learning objectives tool, i.e.
      * the list of learning objectives.
      *
-     * @EXT\Route("/objectives", name="hevinci_objectives_index")
+     * @EXT\Route("/", name="hevinci_objectives_index")
      * @EXT\Template
      *
      * @return array
      */
     public function objectivesAction()
+    {
+        return [];
+    }
+
+    /**
+     * Displays the index of the learning objectives tool, i.e.
+     * the list of learning objectives.
+     *
+     * @EXT\Route("/users", name="hevinci_objectives_users")
+     * @EXT\Template
+     *
+     * @return array
+     */
+    public function usersAction()
+    {
+        return [];
+    }
+
+    /**
+     * Displays the index of the learning objectives tool, i.e.
+     * the list of learning objectives.
+     *
+     * @EXT\Route("/groups", name="hevinci_objectives_groups")
+     * @EXT\Template
+     *
+     * @return array
+     */
+    public function groupsAction()
     {
         return [];
     }
