e2e3b2b3eb531ba39358253742d469cf3e4c4701
gogolecuisto <linard.gauthier@gmail.com>
2015-03-24 11:19:34 +0100

    [UrlBundle] Initial Bundle Structure

---
+++ HeVinciUrlBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HeVinci\UrlBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+class HeVinciUrlBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+}
