17c26abe519a6503c57b098ce45a30f9f816b8a8
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-04-01 14:55:39 +0200

    [PortfolioBundle] Deal with external uri configuration for portfolio

--- Listener/ConfigureTopRightMenuListener.php
+++ Listener/ConfigureTopRightMenuListener.php
@@ -2,6 +2,7 @@
 
 namespace Icap\PortfolioBundle\Listener;
 
+use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use Claroline\CoreBundle\Menu\ConfigureMenuEvent;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\Translation\Translator;
@@ -11,16 +12,26 @@ use Symfony\Component\Translation\Translator;
  */
 class ConfigureTopRightMenuListener
 {
+    /**
+     * @var Translator
+     */
     private $translator;
+
+    /**
+     * @var PlatformConfigurationHandler
+     */
+    private $platformConfigHandler;
 
     /**
      * @DI\InjectParams({
      *     "translator" = @DI\Inject("translator"),
+     *     "platformConfigHandler" = @DI\Inject("claroline.config.platform_config_handler")
      * })
      */
-    public function __construct(Translator $translator)
+    public function __construct(Translator $translator, PlatformConfigurationHandler $platformConfigHandler)
     {
         $this->translator = $translator;
+        $this->platformConfigHandler = $platformConfigHandler;
     }
 
     /**
@@ -30,12 +41,19 @@ class ConfigureTopRightMenuListener
      */
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
+        $menuItemConfig = ['route' => 'icap_portfolio_list'];
+
+        if ($this->platformConfigHandler->getParameter('portfolio_url')) {
+            $menuItemConfig = ['uri' => $this->platformConfigHandler->getParameter('portfolio_url')];
+        }
+
         $menu = $event->getMenu();
 
-        $menu->addChild(
-            $this->translator->trans('my_portfolios', array(), 'platform'),
-            array('route' => 'icap_portfolio_list')
-        )
+        $menu
+            ->addChild(
+                $this->translator->trans('my_portfolios', array(), 'icap_portfolio'),
+                $menuItemConfig
+            )
             ->setAttribute('class', 'dropdown')
             ->setAttribute('role', 'presentation')
             ->setExtra('icon', 'fa fa-list-alt');
