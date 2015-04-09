5e01c06219c03ba83102b0fa11b3e766586dfaed
david <david@DavidB>
2015-04-09 14:52:38 +0200

    [ExoBundle] Initialization the form for the open question with one word

--- Resources/public/js/interactionOpen.js
+++ Resources/public/js/interactionOpen.js
@@ -1,4 +1,6 @@
 var typeOpen;
+var container;
+var tablewr;
 
 function insertStyle(tOpen) {
 
@@ -10,7 +12,7 @@ function insertStyle(tOpen) {
          if (typeOpen[$(this).val()] == 2) {
              $(this).prop('selected', true);
          } else {
-             $(this).attr('disabled', 'disabled');
+             //$(this).attr('disabled', 'disabled');
          }
      });
 }
@@ -20,3 +22,53 @@ function CheckForm() {
         $("*[id$='_penalty']").val($("*[id$='_penalty']").val().replace(/[-]/, ''));
     }*/
 }
+
+function formWordResponse() {
+    
+    container = $('#ujm_exobundle_interactionopentype_wordResponses');
+    tablewr = $('#tablewr');
+    $('.form-collection-add').remove();
+    
+    $('#add_wr').click(function (e) {
+        $('#tablewr').find('tbody').append('<tr></tr>');
+        deleteWr = 'Meuh !'
+        addWr(container, deleteWr);
+        e.preventDefault(); // prevent add # in the url
+        return false;
+    });
+}
+
+// Add a choice
+function addWr(container, deleteWr) {
+    var uniqChoiceID = false;
+
+    var index = $('#tablewr').find('tr:not(:first)').length;
+
+    while (uniqChoiceID == false) {
+        if ($('#ujm_exobundle_interactionopentype_wordResponses_' + index + '_label').length) {
+            index++;
+        } else {
+            uniqChoiceID = true;
+        }
+    }
+
+    // change the "name" by the index and delete the symfony delete form button
+    var contain = $(container.attr('data-prototype').replace(/__name__label__/g, 'wr n°' + (index))
+        .replace(/__name__/g, index)
+        .replace('<a class="btn btn-danger remove" href="#">Delete</a>', '')
+    );
+
+    // Remove the useless fileds form
+    container.remove();
+}
+
+$('#ujm_exobundle_interactionopentype_typeopenquestion').change( function () {
+    if (typeOpen[$(this).val()] == 4) {
+         $('#qOpenOneWord').css('display', 'block');
+         $('#qOpenScoreMaxLongResp').css('display', 'none');
+         formWordResponse();
+     } else if (typeOpen[$(this).val()] == 2) {
+         $('#qOpenScoreMaxLongResp').css('display', 'block');
+         $('#qOpenOneWord').css('display', 'none');
+     }
+});
