d029300dc71be54380592f9de82d2b738f0f3cba
david <david@DavidB>
2015-04-09 16:28:24 +0200

    [ExoBundle] Finalization the form for the open question with one word

--- Resources/public/js/interactionOpen.js
+++ Resources/public/js/interactionOpen.js
@@ -25,7 +25,7 @@ function CheckForm() {
 
 function formWordResponse() {
     
-    container = $('#ujm_exobundle_interactionopentype_wordResponses');
+    container = $('div#ujm_exobundle_interactionopentype_wordResponses');
     tablewr = $('#tablewr');
     $('.form-collection-add').remove();
     
@@ -57,9 +57,48 @@ function addWr(container, deleteWr) {
         .replace(/__name__/g, index)
         .replace('<a class="btn btn-danger remove" href="#">Delete</a>', '')
     );
+    
+    // Add the button to delete a choice
+    addDeleteMeuh(contain, deleteWr);
+    
+    // Add the modified dataprototype to the page
+    container.append(contain);
+    
+    container.find('.row').each(function () {
+        addRowToTablewr($(this));
+    });
+
+    // Add the delete button
+    $('#tablewr').find('tr:last').append('<td class="classic"></td>');
+    $('#tablewr').find('td:last').append(contain.find('a.btn-danger'));
 
     // Remove the useless fileds form
     container.remove();
+}
+
+// A supprimer avec refonte des interface, cette fonction dupliquée dans plusieurs JS sera dans allQuestionsType
+function addDeleteMeuh(tr, deleteTrans) {
+    alert('Ceci est un message de la fonction "addDeleteMeuh" : Ne pas oubliez de me supprimer avec le merge des vues !!!!!!!!!');
+    // Create the button to delete
+    var delLink = $('<a title="'+deleteTrans+'" href="#" class="btn btn-danger"><i class="fa fa-close"></i></a>');
+    // Add the button to the row
+    tr.append(delLink);
+    // When click, delete the matching row in the table
+    delLink.click(function(e) {
+    $(this).parent('td').parent('tr').remove();
+        e.preventDefault();
+        return false;
+    });
+    alert('PS : n\'oubliez pas de renommer mon appel et virer les alerts ...');
+}
+
+function addRowToTablewr(row) {
+    if (row.find('input').length) {
+        if (row.find('input').attr('id').indexOf('ordre') == -1) {
+            $('#tablewr').find('tr:last').append('<td class="classic"></td>');
+            $('#tablewr').find('td:last').append(row.find('input'));
+        }
+    }
 }
 
 $('#ujm_exobundle_interactionopentype_typeopenquestion').change( function () {
