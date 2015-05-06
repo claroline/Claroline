2e2714a62c6fd72ad6e2e84e3ae260b7cad1d8c4
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-05-06 11:07:04 +0200

    [CoreBundle] Update admin tool unique constraint

--- Migrations/pdo_mysql/Version20150506095645.php
+++ Migrations/pdo_mysql/Version20150506095645.php
@@ -8,12 +8,18 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2015/05/04 04:10:21
+ * Generation date: 2015/05/06 09:56:47
  */
-class Version20150504161020 extends AbstractMigration
+class Version20150506095645 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
+        $this->addSql("
+            DROP INDEX UNIQ_C10C14EC5E237E06 ON claro_admin_tools
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX admin_tool_plugin_unique ON claro_admin_tools (name, plugin_id)
+        ");
         $this->addSql("
             DROP INDEX UNIQ_60F909655E237E06 ON claro_tools
         ");
@@ -30,6 +36,12 @@ class Version20150504161020 extends AbstractMigration
 
     public function down(Schema $schema)
     {
+        $this->addSql("
+            DROP INDEX admin_tool_plugin_unique ON claro_admin_tools
+        ");
+        $this->addSql("
+            CREATE UNIQUE INDEX UNIQ_C10C14EC5E237E06 ON claro_admin_tools (name)
+        ");
         $this->addSql("
             DROP INDEX tool_plugin_unique ON claro_tools
         ");
