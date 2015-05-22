4dd4f2ff17d176556b32c060519e63d4adda5778
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-05-22 16:55:21 +0200

    [WebsiteBundle] Fix usage of innovaAngularjsBundle

--- IcapWebsiteBundle.php
+++ IcapWebsiteBundle.php
@@ -4,8 +4,10 @@ namespace Icap\WebsiteBundle;
 
 use Claroline\CoreBundle\Library\PluginBundle;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class IcapWebsiteBundle extends PluginBundle
+class IcapWebsiteBundle extends PluginBundle implements ConfigurationProviderInterface
 {
     public function getConfiguration($environment)
     {
