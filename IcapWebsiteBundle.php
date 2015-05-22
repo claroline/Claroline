6fd41cbb64cf3cbcde9f70a2992a5f10f0ae84fa
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-05-22 10:50:28 +0200

    [WebsiteBundle] Add angularjs from the innova angularjs bundle

--- IcapWebsiteBundle.php
+++ IcapWebsiteBundle.php
@@ -13,4 +13,17 @@ class IcapWebsiteBundle extends PluginBundle
 
         return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'icap_website');
     }
+
+    public function suggestConfigurationFor(Bundle $bundle, $environment)
+    {
+        $bundleClass = get_class($bundle);
+        $config = new ConfigurationBuilder();
+        $emptyConfigs = array(
+            'Innova\AngularJSBundle\InnovaAngularJSBundle',
+        );
+        if (in_array($bundleClass, $emptyConfigs)) {
+            return $config;
+        }
+        return false;
+    }
 }
