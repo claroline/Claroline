49fa29ca32be5bae9b1fce413c4a94b0fcd0095d
Elorfin <penin.axel@gmail.com>
2015-05-27 10:22:53 +0200

    [PathBundle] manage resources

--- Resources/public/js/ResourcePrimary/Controller/ResourcesPrimaryShowCtrl.js
+++ Resources/public/js/ResourcePrimary/Controller/ResourcesPrimaryShowCtrl.js
@@ -3,11 +3,24 @@
  * @returns {ResourcesPrimaryShowCtrl}
  * @constructor
  */
-var ResourcesPrimaryShowCtrl = function ResourcesPrimaryShowCtrl() {
+var ResourcesPrimaryShowCtrl = function ResourcesPrimaryShowCtrl($sce) {
     // Call parent constructor
     ResourcesPrimaryBaseCtrl.apply(this, arguments);
 
     // Get resource URL to populate IFrame
+    if (angular.isObject(this.resources) && angular.isObject(this.resources[0])) {
+        console.log('calculate resource URL');
+        var url = Routing.generate('claro_resource_open', {
+            node         : this.resources[0].id,
+            resourceType : this.resources[0].type
+        });
+
+        if (url) {
+            this.resourceUrl = $sce.trustAsResourceUrl('http://localhost' + url);
+
+            console.log(this.resourceUrl);
+        }
+    }
 
     return this;
 };
@@ -15,3 +28,5 @@ var ResourcesPrimaryShowCtrl = function ResourcesPrimaryShowCtrl() {
 // Extends the base controller
 ResourcesPrimaryShowCtrl.prototype = ResourcesPrimaryBaseCtrl.prototype;
 ResourcesPrimaryShowCtrl.prototype.constructor = ResourcesPrimaryShowCtrl;
+
+ResourcesPrimaryShowCtrl.prototype.resourceUrl = null;
--- Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
+++ Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
@@ -20,3 +20,41 @@
         }
     ]);
 })();
+
+
+/*$(document).ready(function() {
+ // Enable tooltip
+ $('*').tooltip({ placement: 'top' });
+
+ var $frame = $('iframe#{{ currentStep.activity.primaryResource.id }}');
+
+ // Resize IFrame on load
+ $frame.load(function () {
+ resizeIframe($(this));
+
+ // Observe DOM modifications to resize IFrame to fit content
+ var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+ var observer = new MutationObserver(function (mutations, observer) {
+ resizeIframe($frame);
+ });
+
+ observer.observe($frame.get(0).contentDocument.body, {
+ subtree: true,
+ childList: true
+ });
+
+ $frame.on('resize', function () {
+ resizeIframe($frame);
+ });
+ }).attr('src', '{{ path('claro_resource_open', {'node': currentStep.activity.primaryResource.id, 'resourceType': currentStep.activity.primaryResource.resourceType.name }) }}');
+
+ // Resize IFrame on window resize
+ $(window).on('resize', function () {
+ resizeIframe($frame);
+ });
+ });
+
+ function resizeIframe(frame) {
+ var height = frame.contents().height();
+ frame.animate({ height: height }, 100, function() {});
+ }*/
