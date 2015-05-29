3dcd292db872c6b836d8673c3e27da363aa041cb
david <david@DavidB>
2015-05-29 15:25:43 +0200

    [ExoBundle] Refactoring for test insight

--- Repository/ExerciseRepository.php
+++ Repository/ExerciseRepository.php
@@ -30,7 +30,7 @@ class ExerciseRepository extends EntityRepository
         }
         $dql = 'SELECT sum(r.mark) as noteExo, p.id as paper
             FROM UJM\ExoBundle\Entity\Response r JOIN r.paper p JOIN p.exercise e
-            WHERE e.id= ?1 AND p.interupt=0 group by p.id ?2';
+            WHERE e.id= ?1 AND p.interupt=0 group by p.id ?3';
 
         $query = $this->_em->createQuery($dql)
                       ->setParameters(array(1 => $exoId, 2 => $order, 3 => $orderBy));
