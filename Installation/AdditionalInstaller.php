9fae5111bb417745f4dc0e16d598cb9012e3ae6b
Elorfin <penin.axel@gmail.com>
2015-06-01 15:08:11 +0200

    [PathBundle] clean installers

--- Installation/AdditionalInstaller.php
+++ Installation/AdditionalInstaller.php
@@ -20,18 +20,6 @@ class AdditionalInstaller extends BaseInstaller
     }
     
     /**
-     * Action to perform after Bundle installation
-     * Load default allowed types for the non digital resources
-     * @return \Innova\PathBundle\Installation\AdditionalInstaller
-     */
-    public function postInstall()
-    {
-        $this->widgetInstaller();
-
-        return $this;
-    }
-    
-    /**
      * Action to perform after Bundle update
      * Load default allowed types for the non digital resources if the previous bundle version is less than 1.1
      * @param string $currentVersion - The current version of the bundle
@@ -41,28 +29,11 @@ class AdditionalInstaller extends BaseInstaller
     public function postUpdate($currentVersion, $targetVersion)
     {
         if ( version_compare($currentVersion, '1.2.9', '<') && version_compare($targetVersion, '1.2.9', '>=') ) {
-            // Update entity class name
-            $em = $this->container->get('doctrine.orm.entity_manager');
-            $query = $em->createQuery(
-                "UPDATE Claroline\CoreBundle\Entity\Resource\ResourceNode AS rn
-                 SET rn.class='Innova\\PathBundle\\Entity\\Path\\Path' 
-                 WHERE rn.class='Innova\\PathBundle\\Entity\\Path' "
-            );
-            $query->getResult();
+            $updater010209 = new Updater\Updater010209($this->container);
+            $updater010209->setLogger($this->logger);
+            $updater010209->postUpdate();
         }
-
-        $this->widgetInstaller();
         
         return $this;
-    }
-
-    /**
-     * Insert widget
-     */
-    public function widgetInstaller()
-    {
-        $widgetInstaller = new WidgetInstaller($this->container);
-        $widgetInstaller->setLogger($this->logger);
-        $widgetInstaller->postUpdate();
     }
 }
---
+++ Installation/Updater/Updater010209.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Innova\PathBundle\Installation\Updater;
+
+use Claroline\InstallationBundle\Updater\Updater;
+
+/**
+ * Update the entity namespace in BD
+ */
+class Updater010209 extends Updater
+{
+    private $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function postUpdate()
+    {
+        // Update entity class name
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $query = $em->createQuery(
+            "UPDATE Claroline\CoreBundle\Entity\Resource\ResourceNode AS rn
+             SET rn.class='Innova\\PathBundle\\Entity\\Path\\Path'
+             WHERE rn.class='Innova\\PathBundle\\Entity\\Path' "
+        );
+
+        $query->getResult();
+    }
+}
