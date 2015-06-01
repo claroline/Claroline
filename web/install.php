162ba3a156fb339ca2cff60a5a9d36b18b7ccf38
Godfraind <nicolas.godfraind@gmail.com>
2015-06-01 10:36:59 +0200

    Update install.php
    Removing deprecated warnings

--- web/install.php
+++ web/install.php
@@ -3,9 +3,11 @@
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Claroline\WebInstaller\Kernel;
+use Symfony\Component\Debug\Debug;
 
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 $request = Request::createFromGlobals();
+Debug::enable(E_ALL ^ ~E_DEPRECATED, false);
 
 if (!file_exists($file = __DIR__ . '/../app/config/is_installed.php') || false === require_once $file) {
     AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
