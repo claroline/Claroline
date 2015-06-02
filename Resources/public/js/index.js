cb5a4b484708d334b418f54a50ebfb1fcfb23d43
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-06-02 10:51:22 +0200

    [PortfolioBundle] Little refactorization of the code to clean it

--- Resources/public/js/index.js
+++ Resources/public/js/index.js
@@ -5,37 +5,6 @@
             $("#portfolio_space_tabs a[href='#" + locationhash.substr(2) + "']").tab("show");
         }
 
-        function initEvents() {
-            $('.exchange_link').click(function(event) {
-                var scope = angular.element($("#exchange_space_container")).scope();
-                scope.$apply(function(){
-                    var portfolioId = $(event.delegateTarget).val();
-                    if (portfolioId !== scope.selectedPortfolioId) {
-
-                        scope.clickOnPortolio(portfolioId);
-                    }
-                    $('#portfolio_space_tabs a[href="#exchange_space"]').tab('show');
-                });
-            });
-            $('.delete').confirmModal({
-                confirmCallback: deleteConfirmCallback,
-                confirmDismiss: false
-            });
-        }
-        initEvents();
-
-        function deleteConfirmCallback(target, modal) {
-            $.ajax({
-                url: $(target).attr('href'),
-                success: function(data, textStatus, jqXHR) {
-                    $("#list_content").html(data);
-                    initEvents();
-                    toastr.success(Translator.trans('portfolio_deleted_ajax_notification', {}, 'icap_portfolio'));
-                    modal.modal('hide');
-                }
-            });
-        }
-
         toastr.options = {
             "closeButton": true,
             "positionClass": "toast-top-center",
@@ -51,39 +20,90 @@
 
         var modal = window.Claroline.Modal;
 
-        $("a.modal_action, #no_portfolio a").click(function (event) {
-            event.preventDefault();
-            modal.fromUrl(
-                event.delegateTarget.href,
-                function(modalElement) {
-                    var modalForm = $("form", modalElement);
-
-                    modalElement.on('click', 'button[type="submit"]', function (event) {
-                        event.preventDefault();
-                        submitForm(modalElement, modalForm);
-                    });
-
-                    modalElement.on('keypress', function (event) {
-                        if (event.keyCode === 13 && e.target.nodeName !== 'TEXTAREA') {
-                            event.preventDefault();
-                            submitForm(modalElement, modalForm);
-                        }
-                    });
-                }
-            );
+        var elementsToModalized = [
+            {
+                target: "a.modal_action",
+                message: Translator.trans('portfolio_added_ajax_notification', {}, 'icap_portfolio')
+            },
+            {
+                target: "#no_portfolio a",
+                message: Translator.trans('portfolio_added_ajax_notification', {}, 'icap_portfolio')
+            },
+            {
+                target: "a.rename_link",
+                message: Translator.trans('portfolio_renamed_ajax_notification', {}, 'icap_portfolio')
+            }];
+        elementsToModalized.forEach(function (element, index) {
+            modalized(element);
         });
 
-        function submitForm(modalElement, form) {
+        $('#list_content').on('click', '.exchange_link', function(event) {
+            var scope = angular.element($("#exchange_space_container")).scope();
+            scope.$apply(function(){
+                var portfolioId = $(event.delegateTarget).val();
+                if (portfolioId !== scope.selectedPortfolioId) {
+
+                    scope.clickOnPortolio(portfolioId);
+                }
+                $('#portfolio_space_tabs a[href="#exchange_space"]').tab('show');
+            });
+        });
+
+        $('#list_content').on('click', '.delete', function(event) {
+            event.preventDefault();
+            var deleteLink = $(event.target);
+            deleteLink.confirmModal({
+                confirmCallback: deleteConfirmCallback,
+                confirmDismiss: false,
+                confirmAutoOpen: true
+            });
+        });
+
+        function deleteConfirmCallback(target, modal) {
+            $.ajax({
+                url: $(target).attr('href'),
+                success: function(data, textStatus, jqXHR) {
+                    $("#list_content").html(data);
+                    toastr.success(Translator.trans('portfolio_deleted_ajax_notification', {}, 'icap_portfolio'));
+                    modal.modal('hide');
+                }
+            });
+        }
+
+        function submitForm(modalElement, form, message) {
             $.ajax({
                 url: form.attr('action'),
                 data: form.serializeArray(),
                 type: 'POST',
                 success: function(data, textStatus, jqXHR) {
                     $("#list_content").html(data);
-                    initEvents();
                     modalElement.modal('hide');
-                    toastr.success(Translator.trans('portfolio_added_ajax_notification', {}, 'icap_portfolio'));
+                    toastr.success(message);
                 }
+            });
+        }
+
+        function modalized(element) {
+            $('#portfolio_list').on('click', element.target, function (event) {
+                event.preventDefault();
+                modal.fromUrl(
+                    event.target.href,
+                    function(modalElement) {
+                        var modalForm = $("form", modalElement);
+
+                        modalElement.on('click', 'button[type="submit"]', function (event) {
+                            event.preventDefault();
+                            submitForm(modalElement, modalForm, element.message);
+                        });
+
+                        modalElement.on('keypress', function (event) {
+                            if (event.keyCode === 13 && event.target.nodeName !== 'TEXTAREA') {
+                                event.preventDefault();
+                                submitForm(modalElement, modalForm);
+                            }
+                        });
+                    }
+                );
             });
         }
     });
