5e99170e66503985bea59c4433dc206fbeac4352
Elorfin <penin.axel@gmail.com>
2015-06-11 14:16:28 +0200

    [PathBundle] do not open new tab when switching between Player/Editor + try to redirect to current step if possible

--- Resources/public/js/Path/Controller/PathBaseCtrl.js
+++ Resources/public/js/Path/Controller/PathBaseCtrl.js
@@ -4,12 +4,15 @@
  * @returns {PathBaseCtrl}
  * @constructor
  */
-var PathBaseCtrl = function PathBaseCtrl($route, PathService) {
+var PathBaseCtrl = function PathBaseCtrl($window, $route, $routeParams, PathService) {
+    this.window = $window;
     this.pathService = PathService;
 
     // Store path to make it available by all UI components
     this.pathService.setId(this.id);
     this.pathService.setPath(this.path);
+
+    this.currentStep = $routeParams;
 
     // Force reload of the route (as ng-view is deeper in the directive tree, route resolution is deferred and it causes issues)
     $route.reload();
@@ -28,3 +31,8 @@ PathBaseCtrl.prototype.id = null;
  * @type {object}
  */
 PathBaseCtrl.prototype.path = {};
+
+/**
+ * Current step ID (used to generate edit and preview routes)
+ */
+PathBaseCtrl.prototype.currentStep = {};
