f666a5dcc9334792a846a072c242b3b980ffad88
Thomas Verniers <thomas@MacBook-Air-de-Thomas.local>
2015-07-06 11:27:55 +0200

    [PresenceBundle] Basics files

---
+++ FormaLibrePresenceBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FormaLibre\PresenceBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+class FormaLibrePresenceBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+    
+}
