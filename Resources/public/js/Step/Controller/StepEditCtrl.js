654ff4db0e66f52372350e3c56ac415e3a6a8483
Elorfin <penin.axel@gmail.com>
2015-08-18 15:07:48 +0200

    [PathBundle] use Claroline TinyMCE to have access to custom plugins (like ResourcePicker)

--- Resources/public/js/Step/Controller/StepEditCtrl.js
+++ Resources/public/js/Step/Controller/StepEditCtrl.js
@@ -9,8 +9,34 @@ var StepEditCtrl = function StepEditCtrl(step, inheritedResources, PathService, 
     this.scope       = $scope;
     this.stepService = StepService;
 
-    // Set TinyMCE language (need to remap english language)
-    this.tinymceOptions.language = 'en_EN' == AngularApp.locale ? 'en' : AngularApp.locale;
+    // Initialize TinyMCE
+    var tinymce = window.tinymce;
+    tinymce.claroline.init    = tinymce.claroline.init || {};
+    tinymce.claroline.plugins = tinymce.claroline.plugins || {};
+
+    var plugins = [
+        'autoresize advlist autolink lists link image charmap print preview hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars fullscreen',
+        'insertdatetime media nonbreaking table directionality',
+        'template paste textcolor emoticons code'
+    ];
+    var toolbar = 'undo redo | styleselect | bold italic underline | forecolor | alignleft aligncenter alignright | preview fullscreen';
+
+    $.each(tinymce.claroline.plugins, function(key, value) {
+        if ('autosave' != key &&  value === true) {
+            plugins.push(key);
+            toolbar += ' ' + key;
+        }
+    });
+
+    for (var prop in tinymce.claroline.configuration) {
+        if (tinymce.claroline.configuration.hasOwnProperty(prop)) {
+            this.tinymceOptions[prop] = tinymce.claroline.configuration[prop];
+        }
+    }
+
+    this.tinymceOptions.plugins = plugins;
+    this.tinymceOptions.toolbar1 = toolbar;
 
     /**
      * Activity resource picker config
@@ -57,62 +83,11 @@ StepEditCtrl.prototype.collapsedPanels = {
     properties        : true
 };
 
-//we need the claroline tinymce object for additionnal initialization parameters
-var tinymce = window.tinymce;
-tinymce.claroline.init = tinymce.claroline.init || {};
-tinymce.claroline.plugins = tinymce.claroline.plugins || {};
-
-/**
- * Tinymce setup
- */ 
- 
-var tinymceSetup = function(editor) {
-    $.each(tinymce.claroline.init, function(key, func) {
-        func(editor);
-    });
-}
-
-var plugins = [
-    'autoresize advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars fullscreen',
-    'insertdatetime media nonbreaking save table directionality',
-    'template paste textcolor emoticons code'
-];
-var toolbar = 'undo redo | styleselect | bold italic underline | forecolor | alignleft aligncenter alignright | preview fullscreen';
-
-$.each(tinymce.claroline.plugins, function(key, value) {
-    if (value === true) {
-        plugins.push(key);
-        toolbar += ' ' + key;
-    }
-});
-
 /**
  * Tiny MCE options
  * @type {object}
  */
-StepEditCtrl.prototype.tinymceOptions = {
-    relative_urls: false,
-    theme: 'modern',
-    browser_spellcheck : true,
-    entity_encoding : "numeric",
-    autoresize_min_height: 150,
-    autoresize_max_height: 500,
-    plugins: plugins, //j'ai repris la liste des plugins de ce fichier, mais sinon on peut aussi utiliser celle du tinymce du core bundle
-    toolbar1: toolbar, //même chose pour la toolbar
-    //setup: tinymceSetup, //pour une raison que j'ignore, la détection auto du changement de description casse avec cette ligne
-    paste_preprocess: function (plugin, args) { //on peut aussi utiliser le paste du CoreBundle (mais il est différent donc je n'en suis pas sûr, et puis le comportement doit peut-être être différent de toute façon).
-        var link = $('<div>' + args.content + '</div>').text().trim(); //inside div because a bug of jquery
-        var url = link.match(/^(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})$/);
-
-        if (url) {
-            args.content = '<a href="' + link + '">' + link + '</a>';
-            window.Claroline.Home.generatedContent(link, function (data) {
-                insertContent(data);
-            }, false);
-        }
-    }
-};
+StepEditCtrl.prototype.tinymceOptions = {};
 
 /**
  * Display activity linked to the Step
