c9a77becfc2b052093205525fb71eb67018a6c31
Eric VINCENT <ericvincenterv@gmail.com>
2015-08-25 16:00:05 +0200

    [CollecticielBundle] Début travail sur point 57 de la liste de JJQ (saisie URL)

--- Form/DocumentType.php
+++ Form/DocumentType.php
@@ -33,7 +33,11 @@ class DocumentType extends AbstractType
             );
         } else {
             $this->setName('innova_collecticiel_document_file_form_url');
-            $builder->add('document', 'url', array('required' => true, 'label' => 'url document'));
+            $builder->add('document', 'url', array(
+                'required' => true,
+                'label' => 'url document',
+                'default_protocol' => 'http'),
+            'protocols', array('http', 'https', 'www'));
         }
     }
 
