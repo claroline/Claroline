cee5d3552d69d431925248533fe5ea1220840d2e
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-09-02 16:56:48 +0200

    [PortfolioBundle] Add method to retrieve data (fixed data for now)

--- Resources/public/js/statistics/controllers/statisticsViewController.js
+++ Resources/public/js/statistics/controllers/statisticsViewController.js
@@ -11,45 +11,13 @@ statisticsApp
             $scope.cosPoints.push([i, Math.cos(i)]);
         }
 
-        $scope.sinPoints = [];
-        for (var i=0; i<2*Math.PI; i+=0.4){
-           $scope.sinPoints.push([i, 2*Math.sin(i-.8)]);
-        }
-
-        $scope.powPoints1 = [];
-        for (var i=0; i<2*Math.PI; i+=0.4) {
-            $scope.powPoints1.push([i, 2.5 + Math.pow(i/4, 2)]);
-        }
-
-        $scope.powPoints2 = [];
-        for (var i=0; i<2*Math.PI; i+=0.4) {
-            $scope.powPoints2.push([i, -2.5 - Math.pow(i/4, 2)]);
-        }
-
-        $scope.chartData = [$scope.cosPoints, $scope.sinPoints, $scope.powPoints1, $scope.powPoints2];
+        $scope.chartData = [];
 
         $scope.chartOptions = {
-            series:[
+            series: [
                 {
-                    // Change our line width and use a diamond shaped marker.
-                    lineWidth:2,
-                    markerOptions: { style:'diamond' }
-                },
-                {
-                    // Don't show a line, just show markers.
-                    // Make the markers 7 pixels with an 'x' style
-                    showLine:false,
-                    markerOptions: { size: 7, style:"x" }
-                },
-                {
-                    // Use (open) circlular markers.
-                    markerOptions: { style:"circle" }
-                },
-                {
-                    // Use a thicker, 5 pixel line and 10 pixel
-                    // filled square markers.
-                    lineWidth:5,
-                    markerOptions: { style:"filledSquare", size:10 }
+                    lineWidth: 2,
+                    markerOptions: { style:"filledSquare", size: 5 }
                 }
             ]
         };
@@ -57,7 +25,12 @@ statisticsApp
         $scope.clickOnPortolio = function() {
             $('#chart').replaceWith('<div id="chart"></div>');
             if ($scope.selectedPortfolio !== null) {
-                $.jqplot('chart', $scope.chartData, $scope.chartOptions);
+                $scope.fetchData();
             }
         };
+
+        $scope.fetchData = function() {
+            $scope.chartData = [$scope.cosPoints];
+            $.jqplot('chart', $scope.chartData, $scope.chartOptions);
+        }
     }]);
