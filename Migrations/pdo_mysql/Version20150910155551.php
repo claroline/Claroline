6dabdece33308453dfc172d4f673dee74e152855
kitan1982 <anhthao.pham@claroline.net>
2015-09-10 15:52:49 +0200

    [CoreBundle] Fix migrations name

---
+++ Migrations/pdo_mysql/Version20150910155551.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Claroline\CoreBundle\Migrations\pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2015/09/03 03:55:53
+ */
+class Version20150910155551 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_log 
+            ADD other_element_id INT DEFAULT NULL
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_log 
+            DROP other_element_id
+        ");
+    }
+}
