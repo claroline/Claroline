cd1d5bd6f5b8eac708372aeb4245d6802c03dde1
pitrackster <pitrackster@gmail.com>
2015-09-18 15:56:57 +0200

    [ExoBundle] student note is computed

--- Resources/public/js/sequence/Correction/Controllers/CorrectionCtrl.js
+++ Resources/public/js/sequence/Correction/Controllers/CorrectionCtrl.js
@@ -18,19 +18,70 @@
                 console.log(this.answers);
                 console.log('sequence');
                 console.log(this.sequence);
+                this.note = this.setNote();
             };
-            
+
             /**
-             * 
-             * @returns string
+             * Set the student note /20
+             * 1 - Get the total points on the entire exercise
+             * 2 - For each student answer get the points and subtract penalties
+             * 3 - Calculate the final note ( studentPoints * 20 / totalPoints )
+             * @returns number
              */
-            this.setNote = function (){
+            this.setNote = function () {
                 var nbAnswers = this.answers.length;
-                for(var i = 0; i < nbAnswers; i++){
-                    var currentAnswer = this.answers[i];
-                    var currentQuestion = currentAnswer.question;
-                    
+                var score = 0;
+                var totalPoints = 0;
+                var studentPoints = 0;
+                for (var i = 0; i < nbAnswers; i++) {
+                    // anwser is an array [questionId] => true/false if question is multiple or a string if not
+                    var answer = this.answers[i].answer;
+                    var penalty = this.answers[i].penalty;
+                    var question = this.answers[i].question;
+                    //var solutions = question.solutions;
+                    totalPoints += this.getSolutionsPoints(question.solutions);
+                    studentPoints += this.getStudentQuestionScore(question, answer, penalty);
                 }
+                score = studentPoints * 20 / totalPoints;
+                return score > 0 ? score : 0;
+            };
+
+            this.getStudentQuestionScore = function (question, answer, penalty) {
+                var score = 0;
+                // usefull on choice questions but for the others ???
+                var isMultiple = question.multiple;
+                if (isMultiple) {
+                    for (var i = 0; i < question.solutions.length; i++) {
+                        var questionId = parseInt(question.solutions[i].id);
+                        if(answer[questionId] === true){
+                            score += question.solutions[i].score;
+                        }
+                        
+                    }
+                }
+                else{
+                    var questionId = parseInt(question.solutions[0].id);
+                    if(questionId === parseInt(answer)){
+                        score += question.solutions[0].score;
+                    }
+                }
+                score -= penalty;
+                console.log('crac ' + score.toString());
+                return score;
+
+            };
+
+            /**
+             * get the points available in a question solutions
+             * @param solutions
+             * @returns number
+             */
+            this.getSolutionsPoints = function (solutions) {
+                var points = 0;
+                for (var i = 0; i < solutions.length; i++) {
+                    points += solutions[i].score;
+                }
+                return points;
             };
 
             this.setCurrentQuestion = function (question) {
@@ -64,8 +115,8 @@
                     return id === choice.id;
                 }
             };
-            
-            this.generateUrl = function (witch){                
+
+            this.generateUrl = function (witch) {
                 return CommonService.generateUrl(witch);
             };
         }
