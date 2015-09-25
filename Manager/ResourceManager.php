d7a3a393160b9268729f751adb6dc6d39061ba62
ngodfraind <nicolas.godfraind@gmail.com>
2015-09-25 13:46:27 +0200

    [CoreBundle] Less strict export.

--- Manager/ResourceManager.php
+++ Manager/ResourceManager.php
@@ -1024,38 +1024,40 @@ class ResourceManager
             if ($this->container->get('security.context')->isGranted('EXPORT', $node)) {
                 $resource = $this->getResourceFromNode($node);
 
-                if (get_class($resource) === 'Claroline\CoreBundle\Entity\Resource\ResourceShortcut') {
-                    $node = $resource->getTarget();
-                }
-
-                $filename = $this->getRelativePath($currentDir, $node) . $node->getName();
-                $resource = $this->getResourceFromNode($node);
-
-                //if it's a file, we may have to add the extension back in case someone removed it from the name
-                if ($node->getResourceType()->getName() === 'file') {
-                    $extension = '.' . pathinfo($resource->getHashName(), PATHINFO_EXTENSION);
-                    if (!preg_match("#$extension#", $filename)) $filename .= $extension;
-                }
-
-                if ($node->getResourceType()->getName() !== 'directory') {
-                    $event = $this->dispatcher->dispatch(
-                        "download_{$node->getResourceType()->getName()}",
-                        'DownloadResource',
-                        array($resource)
-                    );
-
-                    $obj = $event->getItem();
-
-                    if ($obj !== null) {
-                        $archive->addFile($obj, iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename));
-                    } else {
-                         $archive->addFromString(iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename), '');
+                if ($resource) {
+                    if (get_class($resource) === 'Claroline\CoreBundle\Entity\Resource\ResourceShortcut') {
+                        $node = $resource->getTarget();
                     }
-                } else {
-                    $archive->addEmptyDir(iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename));
-                }
 
-                $this->dispatcher->dispatch('log', 'Log\LogResourceExport', array($node));
+                    $filename = $this->getRelativePath($currentDir, $node) . $node->getName();
+                    $resource = $this->getResourceFromNode($node);
+
+                    //if it's a file, we may have to add the extension back in case someone removed it from the name
+                    if ($node->getResourceType()->getName() === 'file') {
+                        $extension = '.' . pathinfo($resource->getHashName(), PATHINFO_EXTENSION);
+                        if (!preg_match("#$extension#", $filename)) $filename .= $extension;
+                    }
+
+                    if ($node->getResourceType()->getName() !== 'directory') {
+                        $event = $this->dispatcher->dispatch(
+                            "download_{$node->getResourceType()->getName()}",
+                            'DownloadResource',
+                            array($resource)
+                        );
+
+                        $obj = $event->getItem();
+
+                        if ($obj !== null) {
+                            $archive->addFile($obj, iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename));
+                        } else {
+                             $archive->addFromString(iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename), '');
+                        }
+                    } else {
+                        $archive->addEmptyDir(iconv(mb_detect_encoding($filename), $this->getEncoding(), $filename));
+                    }
+
+                    $this->dispatcher->dispatch('log', 'Log\LogResourceExport', array($node));
+                }
             }
         }
 
