20d9859ee5fcea38802a0dc232ecdd86df8ca22d
Elorfin <penin.axel@gmail.com>
2015-10-01 15:38:39 +0200

    [PathBundle] little fixes

--- Resources/public/js/ResourcePrimary/Controller/ResourcesPrimaryBaseCtrl.js
+++ Resources/public/js/ResourcePrimary/Controller/ResourcesPrimaryBaseCtrl.js
@@ -1,22 +1,23 @@
 /**
  * Resources primary base controller
- * @returns {ResourcesSecondaryBaseCtrl}
+ * @returns {ResourcesPrimaryBaseCtrl}
  * @constructor
  */
 var ResourcesPrimaryBaseCtrl = function ResourcesPrimaryBaseCtrl() {
+    // Call parent constructor
+    ResourcesCtrl.apply(this, arguments);
+
     this.resourceIcons  = AngularApp.resourceIcons;
 
     return this;
 };
+
+// Extends the base controller
+ResourcesPrimaryBaseCtrl.prototype = Object.create(ResourcesCtrl.prototype);
+ResourcesPrimaryBaseCtrl.prototype.constructor = ResourcesCtrl;
 
 /**
  * Icons of the Resources
  * @type {object}
  */
 ResourcesPrimaryBaseCtrl.prototype.resourceIcons = {};
-
-/**
- * Resources owned by the Step
- * @type {Array}
- */
-ResourcesPrimaryBaseCtrl.prototype.resources = [];
