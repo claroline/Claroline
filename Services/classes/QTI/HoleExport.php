e345b3f90f7b9644f1ba599c27ae590195311151
Pourquier <charlotte.abbot@univ-st-etienne.fr>
2015-10-21 14:43:36 +0200

    [ExoBundle] QTI for feeback in line

--- Services/classes/QTI/HoleExport.php
+++ Services/classes/QTI/HoleExport.php
@@ -98,7 +98,15 @@ class HoleExport extends QtiExport
             $mapEntry->setAttribute('mappedValue', $resp->getScore());
             $mapEntry->setAttribute('caseSensitive', $resp->getCaseSensitive());
             $mapping->appendChild($mapEntry);
-
+            if(($resp->getFeedback()!=Null) && ($resp->getFeedback()!="")){
+                $feedbackInline = $this->document->CreateElement('feedbackInline');
+                $feedbackInline->setAttribute("outcomeIdentifier", "FEEDBACK");
+                $feedbackInline->setAttribute("identifier","choice_".$resp->getId());
+                $feedbackInline->setAttribute("showHide","show");
+                $feedbackInlinetxt = $this->document->CreateTextNode($resp->getFeedback());
+                $feedbackInline->appendChild($feedbackInlinetxt);
+                $mapEntry->appendChild($feedbackInline);
+            }
             ++$i;
         }
         $Tagvalue = $this->document->CreateElement('value');
--- Services/classes/QTI/OpenShortExport.php
+++ Services/classes/QTI/OpenShortExport.php
@@ -66,6 +66,16 @@ class OpenShortExport  extends OpenExport
             $mapEntry->setAttribute('mappedValue', $resp->getScore());
             $mapEntry->setAttribute('caseSensitive', $resp->getCaseSensitive());
             $mapping->appendChild($mapEntry);
+            
+            if (($resp->getFeedback() != Null) && ($resp->getFeedback() != "")) {
+                $feedbackInline = $this->document->CreateElement('feedbackInline');
+                $feedbackInline->setAttribute("outcomeIdentifier", "FEEDBACK");
+                $feedbackInline->setAttribute("identifier", "Choice" . $resp->getId());
+                $feedbackInline->setAttribute("showHide", "show");
+                $feedbackInlinetxt = $this->document->CreateTextNode($resp->getFeedback());
+                $feedbackInline->appendChild($feedbackInlinetxt);
+                $mapEntry->appendChild($feedbackInline);
+            }
 
             ++$i;
         }
