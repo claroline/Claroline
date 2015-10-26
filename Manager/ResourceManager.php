182c5a3f64f74854fa4e2a7896c81d89c3356327
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2015-10-26 12:03:04 +0100

    [CoreBundle] Suppression always remove $node even if something went wrong before.

--- Manager/ResourceManager.php
+++ Manager/ResourceManager.php
@@ -964,10 +964,12 @@ class ResourceManager
                      * not work for directory containing children.
                      */
                     $this->om->remove($resource);
-                    $this->om->remove($node);
                 }
             }
+
+            $this->om->remove($node);
         }
+
         $this->om->endFlushSuite();
 
         if (!$softDelete) {
