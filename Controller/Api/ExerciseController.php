935c05944c9c37fce048ba6a1db9fc89814d8113
Stéphane Klein <stephaneklein221@gmail.com>
2015-11-06 15:59:52 +0100

    [ExoBundle] Answer management (draft)

--- Controller/Api/ExerciseController.php
+++ Controller/Api/ExerciseController.php
@@ -2,10 +2,14 @@
 
 namespace UJM\ExoBundle\Controller\Api;
 
+use Claroline\CoreBundle\Entity\User;
 use JMS\DiExtraBundle\Annotation as DI;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use UJM\ExoBundle\Entity\Exercise;
+use UJM\ExoBundle\Entity\Paper;
+use UJM\ExoBundle\Entity\Question;
 use UJM\ExoBundle\Manager\ApiManager;
 
 /**
@@ -29,14 +33,69 @@ class ExerciseController
     }
 
     /**
+     * @todo right management
+     * @todo full/partial parameter (w/o solution)
+     *
      * @EXT\Route("/exercises/{id}")
      *
      * @param Exercise $exercise
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @return JsonResponse
      */
     public function exportAction(Exercise $exercise)
     {
         return new JsonResponse($this->manager->exportExercise($exercise));
+    }
+
+    /**
+     * @todo right management
+     * @todo max attempt check
+     *
+     * @EXT\Route("/exercises/{id}/attempts")
+     * @EXT\Method("POST")
+     * @EXT\ParamConverter("currentUser", options={"authenticatedUser"=true})
+     *
+     * @param User      $currentUser
+     * @param Exercise  $exercise
+     * @return JsonResponse
+     */
+    public function attemptAction(User $currentUser, Exercise $exercise)
+    {
+        return new JsonResponse($this->manager->openExercise($exercise, $currentUser, false));
+    }
+
+    /**
+     * @todo right management
+     * @todo handle data validation errors
+     *
+     * @EXT\Route("/papers/{paperId}/questions/{questionId}")
+     * @EXT\Method("POST")
+     * @EXT\ParamConverter("currentUser", options={"authenticatedUser"=true})
+     * @EXT\ParamConverter("paper", class="UJMExoBundle:Paper", options={"mapping": {"paperId": "id"}})
+     * @EXT\ParamConverter("question", class="UJMExoBundle:Question", options={"mapping": {"questionId": "id"}})
+     *
+     * @param Request   $request
+     * @param User      $currentUser
+     * @param Paper     $paper
+     * @param Question  $question
+     * @return JsonResponse
+     */
+    public function submitAnswerAction(
+        Request $request,
+        User $currentUser,
+        Paper $paper,
+        Question $question
+    )
+    {
+        $data = $request->request->get('data', null);
+        $ip = $request->getClientIp();
+        $errors = $this->manager->validateAnswerFormat($question, $data);
+
+        if (!count($errors) !== 0) {
+            return new JsonResponse($errors, 422);
+        }
+
+        $this->manager->recordAnswer($paper, $question, $data, $ip);
+
+        return new JsonResponse();
     }
 }
--- Entity/ExerciseQuestion.php
+++ Entity/ExerciseQuestion.php
@@ -33,13 +33,13 @@ class ExerciseQuestion
      */
     private $ordre;
 
-    public function __construct(\UJM\ExoBundle\Entity\Exercise $exercise, \UJM\ExoBundle\Entity\Question $question)
+    public function __construct(Exercise $exercise, Question $question)
     {
         $this->exercise = $exercise;
         $this->question = $question;
     }
 
-    public function setExercise(\UJM\ExoBundle\Entity\Exercise $exercise)
+    public function setExercise(Exercise $exercise)
     {
         $this->exercise = $exercise;
     }
@@ -49,7 +49,7 @@ class ExerciseQuestion
         return $this->exercise;
     }
 
-    public function setQuestion(\UJM\ExoBundle\Entity\Question $question)
+    public function setQuestion(Question $question)
     {
         $this->question = $question;
     }
