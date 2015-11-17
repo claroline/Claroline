4079575ee5569f4fa68759e545c2fa346000924a
Panos <ptsavdar@gmail.com>
2015-11-17 10:02:49 +0100

    [FormulaPluginBundle] Fixed some of first commit issues

---
+++ Resources/public/tinymce/plugins/formula/langs/en.js
@@ -0,0 +1,5 @@
+tinymce.addI18n('en',{
+    'Formula': 'Formula editor',
+    'Cancel': 'Cancel',
+    'Insert Formula': 'Insert formula'
+});
---
+++ Resources/public/tinymce/plugins/formula/langs/fr_FR.js
@@ -0,0 +1,5 @@
+tinymce.addI18n('fr_FR',{
+    'Formula': 'Éditeur d\'équations',
+    'Cancel': 'Annuler',
+    'Insert Formula': 'Insérer l\'équation'
+});
