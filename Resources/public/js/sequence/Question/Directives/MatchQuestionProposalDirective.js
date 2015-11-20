6079302fc6a6e0e4374702ba937ec85d9bfc2f96
Eldoniel <nicofourdu@hotmail.fr>
2015-11-20 14:54:37 +0100

    [ExoBundle] Match question directive fixed

--- Resources/public/js/sequence/Question/Directives/MatchQuestionProposalDirective.js
+++ Resources/public/js/sequence/Question/Directives/MatchQuestionProposalDirective.js
@@ -3,7 +3,8 @@
     'use strict';
 
     angular.module('Question').directive('matchQuestionProposal', [
-        function () {
+        '$timeout',
+        function ($timeout) {
             return {
                 restrict: 'E',
                 replace: true,
@@ -22,24 +23,26 @@
                     console.log(element);
                     console.log(element.attr);
                     
-                    if (element.attr("id").substr(0,9) === "draggable") {
-                        jsPlumb.addEndpoint(element, {
-                            anchor: 'RightMiddle',
-                            cssClass: "endPoints",
-                            isSource: true,
-                            maxConnections: -1
-                        });
-                    }
-                    else {
-                        jsPlumb.addEndpoint(element, {
-                            anchor: 'LeftMiddle',
-                            cssClass: "endPoints",
-                            isTarget: true,
-                            maxConnections: -1
-                        });
-                    }
+                    $timeout(function init() {
+                        if (element.attr("id").substr(0,9) === "draggable") {
+                            jsPlumb.addEndpoint(element, {
+                                anchor: 'RightMiddle',
+                                cssClass: "endPoints",
+                                isSource: true,
+                                maxConnections: -1
+                            });
+                        }
+                        else {
+                            jsPlumb.addEndpoint(element, {
+                                anchor: 'LeftMiddle',
+                                cssClass: "endPoints",
+                                isTarget: true,
+                                maxConnections: -1
+                            });
+                        }
                     
-                    jsPlumb.detachEveryConnection();
+                        jsPlumb.detachEveryConnection();
+                    });
                 }
             };
         }
