efe4ff2a0fa3befcd14c0549d06b47c0b5981e6e
Eldoniel <nicofourdu@hotmail.fr>
2015-11-26 10:02:26 +0100

    [ExoBundle] Move MatchQuestionProposal

---
+++ Resources/public/js/sequence/Player/Question/Directives/MatchQuestionProposalDirective.js
@@ -0,0 +1,52 @@
+
+(function () {
+    'use strict';
+
+    angular.module('Question').directive('matchQuestionProposal', [
+        '$timeout',
+        function ($timeout) {
+            return {
+                restrict: 'E',
+                replace: true,
+                controller: 'MatchQuestionProposalCtrl',
+                controllerAs: 'matchQuestionProposalCtrl',
+                templateUrl: AngularApp.webDir + 'bundles/ujmexo/js/sequence/Question/Partials/proposal.html',
+                scope: {
+                    step: '=',
+                    question: '=',
+                    selfRemove: "&"
+                },
+                link: function (scope, element, attr, matchQuestionProposalCtrl) {
+                    matchQuestionProposalCtrl.setProposal(attr.proposal);
+                    matchQuestionProposalCtrl.init(attr.proposal);
+                    
+                    console.log(element);
+                    console.log(element.attr);
+                    
+                    $timeout(function init() {
+                        if (element.attr("id").substr(0,9) === "draggable") {
+                            jsPlumb.addEndpoint(element, {
+                                anchor: 'RightMiddle',
+                                cssClass: "endPoints",
+                                isSource: true,
+                                maxConnections: -1
+                            });
+                        }
+                        else {
+                            jsPlumb.addEndpoint(element, {
+                                anchor: 'LeftMiddle',
+                                cssClass: "endPoints",
+                                isTarget: true,
+                                maxConnections: -1
+                            });
+                        }
+                    
+                        jsPlumb.detachEveryConnection();
+                    });
+                }
+            };
+        }
+    ]);
+})();
+
+
