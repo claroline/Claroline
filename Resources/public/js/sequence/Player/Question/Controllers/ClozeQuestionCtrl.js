12527cf57fd9b4e856ec3da5ce2cb5e24714d0e9
Eldoniel <nicofourdu@hotmail.fr>
2015-12-04 14:36:30 +0100

    [ExoBundle] Description doesn't appear when null + begin work on hints

--- Resources/public/js/sequence/Player/Question/Controllers/ClozeQuestionCtrl.js
+++ Resources/public/js/sequence/Player/Question/Controllers/ClozeQuestionCtrl.js
@@ -9,6 +9,7 @@
             this.formatedClozeText = '';
             this.isCollapsed = false;
             this.currentQuestionPaperData = {};
+            this.usedHints = [];// contains hints texts
             
             this.init = function (question) {
                 // those data are updated by view and sent to common service as soon as they change
@@ -23,6 +24,48 @@
                         this.getHintData(this.currentQuestionPaperData.hints[i]);
                     }
                 }
+            };
+
+            /**
+             * check if a Hint has already been used (in paper)
+             * @param {type} id
+             * @returns {Boolean}
+             */
+            this.hintIsUsed = function (id) {
+                if (this.currentQuestionPaperData && this.currentQuestionPaperData.hints) {
+                    for (var i = 0; i < this.currentQuestionPaperData.hints.length; i++) {
+                        if (this.currentQuestionPaperData.hints[i] === id) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            };
+
+            /**
+             * Get hint data and update student data in common service
+             * @param {type} hintId
+             * @returns {undefined}
+             */
+            this.showHint = function (id) {
+                var penalty = QuestionService.getHintPenalty(this.question.hints, id);
+                $ngBootbox.confirm(Translator.trans('question_show_hint_confirm', {1: penalty}, 'ujm_sequence'))
+                        .then(function () {
+                            this.getHintData(id);
+                            this.currentQuestionPaperData.hints.push(id);
+                            this.updateStudentData();
+                            // hide button
+                            angular.element('#hint-' + id).hide();
+                        }.bind(this));
+            };
+
+            this.getHintData = function (id) {
+                var promise = QuestionService.getHint(id);
+                promise.then(function (result) {
+                    //console.log(result);
+                    this.usedHints.push(result);
+
+                }.bind(this));
             };
 
             this.setQuestion = function (question) {
