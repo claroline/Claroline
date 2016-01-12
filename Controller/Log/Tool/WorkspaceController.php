4268b2338243909786c5bb05223eb62d92d71ad6
Panos <ptsavdar@gmail.com>
2016-01-12 14:20:34 +0100

    [CoreBundle] Added tracking by user tab

--- Controller/Log/Tool/WorkspaceController.php
+++ Controller/Log/Tool/WorkspaceController.php
@@ -60,4 +60,44 @@ class WorkspaceController extends Controller
 
         return $this->get('claroline.log.manager')->getWorkspaceList($workspace, $page);
     }
+
+    /**
+     * @EXT\Route(
+     *     "/{workspaceId}/tool/logs/user",
+     *     name="claro_workspace_logs_by_user_show",
+     *     requirements={"workspaceId" = "\d+"},
+     *     defaults={"page" = 1}
+     * )
+     * @EXT\Route(
+     *     "/{workspaceId}/tool/logs/user{page}",
+     *     name="claro_workspace_logs_by_user_show_paginated",
+     *     requirements={"workspaceId" = "\d+", "page" = "\d+"},
+     *     defaults={"page" = 1}
+     * )
+     *
+     *
+     * @EXT\ParamConverter(
+     *      "workspace",
+     *      class="ClarolineCoreBundle:Workspace\Workspace",
+     *      options={"id" = "workspaceId", "strictId" = true}
+     * )
+     *
+     * @EXT\Template("ClarolineCoreBundle:Tool/workspace/logs:logByUser.html.twig")
+     *
+     * Displays logs list using filter parameteres and page number
+     *
+     * @param \Claroline\CoreBundle\Entity\Workspace\Workspace $workspace
+     * @param $page int The requested page number.
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @return Response
+     */
+    public function logByUserAction(Workspace $workspace, $page)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('logs', $workspace)) {
+            throw new AccessDeniedException();
+        }
+
+        return $this->get('claroline.log.manager')->countByUserWorkspaceList($workspace, $page);
+    }
 }
