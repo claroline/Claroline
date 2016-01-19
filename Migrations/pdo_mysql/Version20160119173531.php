de4c7bdb4d351a7327338a9a3364619716b89b0b
Panos <ptsavdar@gmail.com>
2016-01-19 17:39:51 +0100

    [BadgeBundle] Fixed bug with badges widgets caused from missing migration file

---
+++ Migrations/pdo_mysql/Version20160119173531.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Icap\BadgeBundle\Migrations\pdo_mysql;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution
+ *
+ * Generation date: 2016/01/19 05:35:33
+ */
+class Version20160119173531 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_widget_badge_usage_config 
+            ADD simple_view TINYINT(1) NOT NULL
+        ");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("
+            ALTER TABLE claro_widget_badge_usage_config 
+            DROP simple_view
+        ");
+    }
+}
