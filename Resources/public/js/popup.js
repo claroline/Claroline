f271f18e6eb501e77962494755b2b46d6d6d86fd
Eric VINCENT <ericvincenterv@gmail.com>
2016-01-25 13:43:37 +0100

    [CollecticielBundle] [WIP]fix #210 ajax

--- Resources/public/js/popup.js
+++ Resources/public/js/popup.js
@@ -25,16 +25,32 @@ $(document).ready(function () {
     });
 
     $('.td_action').each(function(){
-        alert("rere");
         var documentId = $(this).find('input[name="document_id"]').val();
-        var isValidate = document.getElementById('document_validate_' + documentId).value == '1' ? true:false;
+        var isValidate = document.getElementById('document_validate_' + documentId).value;
         var commentLength = document.getElementById('document_comments_length_' + documentId).value;
         var senderId = document.getElementById('document_sender_' + documentId).value;
         var docDropUserId = document.getElementById('document_drop_user_' + documentId).value;
 
+//alert(isValidate);
+//alert(commentLength);
+//alert(senderId + " " + docDropUserId);
         //
         // Afficher les tests ici qui permettront de rafraîchir les données.
         //
+        if (isValidate == false || senderId != docDropUserId) {
+//            alert("cas 1");
+            var selector = "#delete_" + documentId;
+        }
+        else if (isValidate == true && commentLength == 0 && senderId == docDropUserId) {
+//            alert("cas 2");
+            var selector = "#cancel_" + documentId;
+        }
+        else {
+//            alert("cas 3");
+            var selector = "#lock_" + documentId;
+        }
+        $(selector).css({'display': 'inline'});
+
     });
 
 
@@ -83,19 +99,31 @@ $(document).ready(function () {
     // InnovaERV
     // Ajout pour le traitement de la case Ã  cocher lors de la soumission de documents
     $('#validate-modal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget); // Button that triggered the modal
-      var documentId = button.data('document_id'); // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this);
-      modal.find('#modal_confirm').attr("data-document_id", documentId); //TODO change this to use data() instead of attr()
-      //documentId
-      //
+
+        alert("validate-modal");
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var documentId = button.data('document_id'); // Extract info from data-* attributes
+
+        var senderId = button.data('document_sender_id'); // Extract info from data-* attributes
+        var commentLength = button.data('document_comment_length');
+        var docDropUserId = button.data('document_dropuser_id'); // Extract info from data-* attributes
+
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        alert(senderId);
+        modal.find('#modal_confirm').attr("data-document_id", documentId); //TODO change this to use data() instead of attr()
+        modal.find('#modal_confirm').attr("data-document_sender_id", senderId); //TODO change this to use data() instead of attr()
+        modal.find('#modal_confirm').attr("data-document_comment_length", commentLength); //TODO change this to use data() instead of attr()
+        modal.find('#modal_confirm').attr("data-document_docDropUser_id", docDropUserId); //TODO change this to use data() instead of attr()
+        //documentId
+        //
     });
 
     // InnovaERV
     // Ajout pour le traitement du clic sur le bouton "Oui, valider"
     $('#modal_confirm').on('click', function(event) {
+        alert("modal_confirm");
         var selector = "#document_id_"+$(this).attr("data-document_id"); // Extract info from data-* attributes
         var row = "row_"+$(this).attr("data-document_id"); // Extract info from data-* attributes
         var documentId = $(this).attr("data-document_id");
@@ -108,23 +136,41 @@ $(document).ready(function () {
 
         // Récupération de l'id du document
         var docId = $(this).attr("data-document_id");
+        var senderId = $(this).attr("data-document_sender_id");
+        var commentLength = $(this).attr("data-document_comment_length");
+        var docDropUserId = $(this).attr("data-document_docDropUser_id"); // Extract info from data-* attributes
 
+alert(docId);
+alert(senderId);
+alert(commentLength);
+alert(docDropUserId);
+die();
         // Ajax : appel de la route qui va mettre Ã  jour la base de donnÃ©es
         // Ajax : route "innova_collecticiel_validate_document" dans DocumentController
         var req = "#request_id_"+$(this).attr("data-document_id"); // Extract info from data-* attributes
 
-        var isValidate = document.getElementById('document_validate').value == '1' ? true:false;
-        var commentLength = document.getElementById('document_comments_length').value;
-        var senderId = document.getElementById('document_sender').value;
-        var docDropUserId = document.getElementById('document_drop_user').value;
+//        var commentLength = document.getElementById('document_comments_length_' + documentId).value;
+//        var senderId = document.getElementById('document_sender_' + documentId).value;
+//        var docDropUserId = document.getElementById('document_drop_user_' + documentId).value;
 
+
+        var selectorCommentLength = $(this).attr("data-document_commentLength");
+        var selectorSenderId = $(this).attr("data-document_senderId");
+        var selectorDocDropUserId = $(this).attr("data-document_docDropUserId");
+
+alert(selectorCommentLength);
+alert(selectorSenderId);
+alert(selectorDocDropUserId);
 //        var linkDelete = document.getElementById("delete_" + documentId);
 //        if($(linkDelete).hasClass(''))
 
         //
         // Afficher les tests ici qui permettront de rafraîchir les données.
         //
+        var selector = "#lock_" + documentId;
+        $(selector).css({'display': 'inline'});
 
+        alert("ici5");
 
         // Ajout : vu avec Arnaud.
         // Ajout de "complete" afin de mettre Ã  jour la partie "HTML" qui va actualiser et afficher "Demande transmise"
