1223fb0e6c90a6894b47d5112eb4692e47cf75cc
David Boit <david.boit@univ-st-etienne.fr>
2016-02-05 12:51:30 +0100

    [ExoBundle] Manage migrations

--- Installation/AdditionalInstaller.php
+++ Installation/AdditionalInstaller.php
@@ -8,13 +8,13 @@ class AdditionalInstaller extends BaseInstaller
 {
     public function preUpdate($currentVersion, $targetVersion)
     {
-        if (version_compare($currentVersion, '6.0.0', '<=')) {
-            $updater = new Updater600100($this->container);
-            $updater->setLogger($this->logger);
-            $updater->preUpdate();
-        }
+//        if (version_compare($currentVersion, '6.0.0', '<=')) {
+//            $updater = new Updater600100($this->container);
+//            $updater->setLogger($this->logger);
+//            $updater->preUpdate();
+//        }
 
-        if (version_compare($currentVersion, '6.0.0', '=')) {
+        if (version_compare($currentVersion, '6.0.0', '<=')) {
             $updater = new Updater600200($this->container);
             $updater->setLogger($this->logger);
             $updater->preUpdate();
@@ -23,13 +23,13 @@ class AdditionalInstaller extends BaseInstaller
 
     public function postUpdate($currentVersion, $targetVersion)
     {
-        if (version_compare($currentVersion, '6.0.0', '<=')) {
-            $updater = new Updater600100($this->container);
-            $updater->setLogger($this->logger);
-            $updater->postUpdate();
-        }
+//        if (version_compare($currentVersion, '6.0.0', '<=')) {
+//            $updater = new Updater600100($this->container);
+//            $updater->setLogger($this->logger);
+//            $updater->postUpdate();
+//        }
 
-        if (version_compare($currentVersion, '6.0.0', '=')) {
+        if (version_compare($currentVersion, '6.0.0', '<=')) {
             $updater = new Updater600200($this->container);
             $updater->setLogger($this->logger);
             $updater->postUpdate();
