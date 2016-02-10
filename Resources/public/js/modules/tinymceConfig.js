56f44fff75ef0239cf7d664baad48239bd889ff8
Panos <ptsavdar@gmail.com>
2016-02-10 17:42:39 +0100

    [PortfolioBundle] Fixed tinymce config

--- Resources/public/js/modules/tinymceConfig.js
+++ Resources/public/js/modules/tinymceConfig.js
@@ -1,35 +1,36 @@
-'use strict';
+(function() {
+    'use strict';
 
-var appDirectives = angular.module('app.config', []);
+    var appDirectives = angular.module('app.config', []);
 
-var tinymce = window.tinymce;
-tinymce.claroline.init = tinymce.claroline.init || {};
-tinymce.claroline.plugins = tinymce.claroline.plugins || {};
+    var tinymce = window.tinymce;
+    tinymce.claroline.init = tinymce.claroline.init || {};
+    tinymce.claroline.plugins = tinymce.claroline.plugins || {};
 
-var plugins = [
-    'autoresize advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars fullscreen',
-    'insertdatetime media nonbreaking table directionality',
-    'template paste textcolor emoticons code'
-];
-var toolbar = 'undo redo | styleselect | bold italic underline | forecolor | alignleft aligncenter alignright | preview fullscreen';
+    var plugins = [
+        'autoresize advlist autolink lists link image charmap print preview hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars fullscreen',
+        'insertdatetime media nonbreaking save table directionality',
+        'template paste textcolor emoticons code -mention -accordion -codemirror'
+    ];
+    var toolbar1 = 'bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | fullscreen displayAllButtons';
+    $.each(tinymce.claroline.plugins, function(key, value) {
+        if ('autosave' != key &&  value === true) {
+            plugins.push(key);
+            toolbar1 += ' ' + key;
+        }
+    });
 
-$.each(tinymce.claroline.plugins, function(key, value) {
-    if ('autosave' != key &&  value === true) {
-        plugins.push(key);
-        toolbar += ' ' + key;
+    var tinyMceConfig = {};
+    for (var prop in tinymce.claroline.configuration) {
+        if (tinymce.claroline.configuration.hasOwnProperty(prop)) {
+            tinyMceConfig[prop] = tinymce.claroline.configuration[prop];
+        }
     }
-});
 
-var tinyMceConfig = {};
-for (var prop in tinymce.claroline.configuration) {
-    if (tinymce.claroline.configuration.hasOwnProperty(prop)) {
-        tinyMceConfig[prop] = tinymce.claroline.configuration[prop];
-    }
-}
+    tinyMceConfig.plugins = plugins;
+    tinyMceConfig.toolbar1 = toolbar1;
+    tinyMceConfig.format = 'text';
 
-tinyMceConfig.plugins = plugins;
-tinyMceConfig.toolbar1 = toolbar;
-tinyMceConfig.format = 'text';
-
-appDirectives.value('tinyMceConfig', tinyMceConfig);
+    appDirectives.value('tinyMceConfig', tinyMceConfig);
+})();
