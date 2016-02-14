8e1c5b09ec619a0dc79edcff546e226707a58bc6
Stéphane Klein <stephaneklein221@gmail.com>
2016-02-14 23:29:15 +0100

    Refactor webpack config

--- webpack-watch.js
+++
@@ -1,4 +0,0 @@
-const frontendConfig = require('./app/config/frontend.json')
-const configure = require('./vendor/claroline/core-bundle/Resources/scripts/lib/webpack.js')
-
-module.exports = configure(frontendConfig, __dirname, true)
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const colors = require('colors/safe')
+const scriptPath = './vendor/claroline/core-bundle/Resources/scripts/lib'
+const collectPackages = require(path.resolve(scriptPath, 'collect-packages'))
+const buildConfig = require(path.resolve(scriptPath, 'webpack'))
+
+const isWatchMode = process.argv.indexOf('--claro-tgt=watch') !== -1
+const config = buildConfig(__dirname, collectPackages(__dirname), isWatchMode)
+
+console.log(colors.yellow(JSON.stringify(config._debug, null, 2) + '\n'))
+
+module.exports = config
