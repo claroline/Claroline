1b7cd64238bed84d1c40f35881d1132366cbf8f2
Eldoniel <nicofourdu@hotmail.fr>
2016-02-16 09:16:33 +0100

    [ExoBundle] WIP feedback for match

--- Resources/public/js/sequence/Player/Question/Controllers/MatchQuestionCtrl.js
+++ Resources/public/js/sequence/Player/Question/Controllers/MatchQuestionCtrl.js
@@ -117,6 +117,8 @@
                     this.solutions = result.solutions;
                     this.questionFeedback = result.feedback;
                 }.bind(this));
+                
+                console.log(this.solutions);
             };
             
             /**
