3ebd7fa7fc2cc4c08ce51faeb630ec6f8635236c
Eldoniel <nicofourdu@hotmail.fr>
2016-02-29 16:50:13 +0100

    [ExoBundle] Fix the disabling on match questions

--- Resources/public/js/sequence/Player/Question/Controllers/MatchQuestionCtrl.js
+++ Resources/public/js/sequence/Player/Question/Controllers/MatchQuestionCtrl.js
@@ -283,7 +283,13 @@
              */
             $scope.$on('show-feedback', function (event, data) {
                 this.showFeedback();
+                this.unbindEvents();
             }.bind(this));
+            
+            this.unbindEvents = function () {
+                jsPlumb.unbind("beforeDrop");
+                jsPlumb.unbind("click");
+            };
 
             /**
              * Hide / show a specific panel content and handle hide / show button icon 
@@ -411,27 +417,22 @@
              * @returns {undefined}
              */
             this.handleBeforDrop = function (data) {
-                if (!this.feedbackIsVisible) {
-                    var jsPlumbConnection = jsPlumb.getConnections(data.connection);
-                    // avoid drawing the same connection multiple times
-                    if (jsPlumbConnection.length > 0 && data.sourceId === jsPlumbConnection[0].sourceId && data.targetId === jsPlumbConnection[0].targetId) {
-                        jsPlumb.detach(jsPlumbConnection);
-                        return false;
-                    } else {
-                        var sourceId = data.sourceId.replace('draggable_', '');
-                        var targetId = data.targetId.replace('droppable_', '');
-                        var connection = {
-                            source: sourceId,
-                            target: targetId
-                        };
-                        this.connections.push(connection);
-                    }
-                    this.updateStudentData();
-                    return true;
-                }
-                else {
+                var jsPlumbConnection = jsPlumb.getConnections(data.connection);
+                // avoid drawing the same connection multiple times
+                if (jsPlumbConnection.length > 0 && data.sourceId === jsPlumbConnection[0].sourceId && data.targetId === jsPlumbConnection[0].targetId) {
+                    jsPlumb.detach(jsPlumbConnection);
                     return false;
+                } else {
+                    var sourceId = data.sourceId.replace('draggable_', '');
+                    var targetId = data.targetId.replace('droppable_', '');
+                    var connection = {
+                        source: sourceId,
+                        target: targetId
+                    };
+                    this.connections.push(connection);
                 }
+                this.updateStudentData();
+                return true;
             };
 
             /**
@@ -442,20 +443,18 @@
              * @returns {undefined}
              */
             this.removeConnection = function (data) {
-                if (!this.feedbackIsVisible) {
-                    var sourceId = data.sourceId.replace('draggable_', '');
-                    var targetId = data.targetId.replace('droppable_', '');
-                    // connection is removed from dom even with this commented... 
-                    // If not commented, code stops at this methods...
-                    // jsPlumb.detach(data); 
+                var sourceId = data.sourceId.replace('draggable_', '');
+                var targetId = data.targetId.replace('droppable_', '');
+                // connection is removed from dom even with this commented... 
+                // If not commented, code stops at this methods...
+                // jsPlumb.detach(data); 
 
-                    for (var i = 0; i < this.connections.length; i++) {
-                        if (this.connections[i].source === sourceId && this.connections[i].target === targetId) {
-                            this.connections.splice(i, 1);
-                        }
+                for (var i = 0; i < this.connections.length; i++) {
+                    if (this.connections[i].source === sourceId && this.connections[i].target === targetId) {
+                        this.connections.splice(i, 1);
                     }
-                    this.updateStudentData();
                 }
+                this.updateStudentData();
             };
 
             /**
