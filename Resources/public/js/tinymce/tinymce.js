6dd5dcf75965ae218f709fcbbb340a793b4686e7
ngodfraind <nicolas.godfraind@gmail.com>
2016-03-10 23:56:59 +0100

    [CoreBundle] Removing theme css from tinymce.

--- Resources/public/js/tinymce/tinymce.js
+++ Resources/public/js/tinymce/tinymce.js
@@ -240,7 +240,7 @@
      */
 
     // Get theme to load inside tinymce in order to have no display differences
-    var themeCSS = _.find(_.pluck(document.styleSheets, 'href'), function(link){return link.indexOf("themes/") !== -1});
+    //var themeCSS = _.find(_.pluck(document.styleSheets, 'href'), function(link){return link.indexOf("themes/") !== -1});
 
     tinymce.claroline.configuration = {
         paste_data_images: true,
@@ -250,7 +250,7 @@
         'browser_spellcheck': true,
         'autoresize_min_height': 100,
         'autoresize_max_height': 500,
-        'content_css':  [themeCSS, home.asset + 'bundles/clarolinecore/css/common/tinymce.css'],
+        'content_css':  [home.asset + 'bundles/clarolinecore/css/common/tinymce.css'],
         'toolbar2': 'styleselect | undo redo | forecolor backcolor | bullist numlist | outdent indent | ' +
                     'media link charmap | print preview code',
         'extended_valid_elements': 'user[id], a[data-toggle|data-parent]',
