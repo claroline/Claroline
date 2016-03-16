b3c42ac218b6aaf9ff88c1a3e22d71989de9a12e
Cindy Tessier <cindy.tessier@univ-st-etienne.fr>
2016-03-16 14:03:19 +0100

    [ExoBundle] Timer - Validate exercise when time is over

--- Resources/public/js/sequence/Player/Exercise/Controllers/ExercisePlayerCtrl.js
+++ Resources/public/js/sequence/Player/Exercise/Controllers/ExercisePlayerCtrl.js
@@ -30,24 +30,14 @@
                 $scope.$storage.counter =  $scope.$storage.counter + 1;
                 mytimeout = $timeout($scope.onTimeout,1000);
                 if ($scope.$storage.counter == $scope.$storage.durationExo) { //$scope.counter == 7) {
-                  $scope.affiche_message();
+                  $scope.exercisePlayerCtrl.validateStep('end');
+                  $scope.$storage.$reset({
+                     counter: 0
+                  });
+                  $timeout.cancel(mytimeout);
               }
             }
             var mytimeout = $timeout($scope.onTimeout,1000);
-
-            $scope.stop = function(){
-                $timeout.cancel(mytimeout);
-            }
-
-            $scope.start = function(){
-                $scope.$storage.counter = 0;
-                mytimeout = $timeout($scope.onTimeout,1000);
-            }
-
-            $scope.affiche_message = function(){
-               alert("temps écoulé !");
-            }
-
 
             // init directive with appropriate data
             this.init = function (paper, exercise, user, currentStepIndex, duration) {
@@ -101,12 +91,12 @@
              * @param {Number} index (nullable) the step index when using direct access
              */
             this.validateStep = function (action, index) {
-                
+
                 // manualy disable tooltips...
                 $('.tooltip').each(function () {
                     $(this).hide();
                 });
-                
+
                 // get next step index
                 this.currentStepIndex = this.getNextStepIndex(this.currentStepIndex, action, index);
 
@@ -114,15 +104,15 @@
                 var studentData = PlayerDataSharing.getStudentData();
                 // save the given answer (even if empty !)
                 var submitPromise = ExerciseService.submitAnswer(this.paper.id, studentData);
-                submitPromise.then(function (result) {                    
+                submitPromise.then(function (result) {
                     // then navigate to desired step / end / terminate exercise
                     this.handleStepNavigation(action, studentData.paper);
-                    
+
                 }.bind(this));
             };
 
             /**
-             * 
+             *
              * @param {number} current current index
              * @param {string} action
              * @param {number} index the index to reach (when the drop box is used)
@@ -139,7 +129,7 @@
             };
 
             /**
-             * Navigate to desired step or end exercise and redirect to appropriate view 
+             * Navigate to desired step or end exercise and redirect to appropriate view
              * @param {string} action
              * @param {object} paper
              */
@@ -150,7 +140,7 @@
                 } else if (action && action === 'end') {
                     var endPromise = ExerciseService.endSequence(paper);
                     endPromise.then(function (result) {
-                        if (this.checkCorrectionAvailability()) {                      
+                        if (this.checkCorrectionAvailability()) {
                             // go to paper correction view
                             var url = CommonService.generateUrl('paper-list', this.exercise.id) + '#/' + this.exercise.id + '/' + paper.id;
                             $window.location = url;
