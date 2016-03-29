2366c2c2e4b99f460b276c84a0489897d78a8ea9
ngodfraind <nicolas.godfraind@gmail.com>
2016-03-29 11:23:59 +0200

    [CoreBundle] Adding Serializers.

---
+++ Library/View/ExporterViewHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Claroline\CoreBundle\Library\View;
+
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class ExporterViewHandler
+{
+    public function createResponse(ViewHandler $handler, View $view, Request $request, $format)
+    {
+        $data = $view->getData();
+        $context = $view->getSerializationContext();
+        $container = $handler->getContainer();
+        $format = $view->getFormat();
+        $file = $container->get('claroline.library.view.serializer.serializer')->serialize($data, $format);
+        $response = new StreamedResponse();
+
+        $response->setCallBack(
+            function () use ($file) {
+                readfile($file);
+            }
+        );
+
+        $response->headers->set('Content-Transfer-Encoding', 'octet-stream');
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename=file.' . $format);
+
+        switch ($format) {
+            case 'csv': $response->headers->set('Content-Type', 'text/csv'); break;
+            case 'xls': $response->headers->set('Content-Type', 'application/vnd.ms-excel'); break;
+        }
+
+        return $response;
+    }
+}
