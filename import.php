85ce8b97318aaea14e3cc94caf4c460c9b7b1645
ngodfraind <nicolas.godfraind@gmail.com>
2016-04-05 09:55:58 +0200

    fix:using bootstrap from the packages directory

--- import.php
+++ import.php
@@ -20,28 +20,6 @@ cmd("git branch -f {$bundle} {$bundle}/master");
 cmd("git checkout {$bundle}");
 cmd("git pull --no-tags {$bundle} master");
 cmd("git filter-branch -f --msg-filter 'sed \"1 s/^/[{$bundle}] /\"' HEAD");
-
-// Replace the namespace of the bundle by "Claroline" if needed
-/*
-if ($namespace !== 'Claroline') {
-  $iterator = new RecursiveDirectoryIterator(__DIR__);
-  $iteratorIterator = new RecursiveIteratorIterator($iterator);
-  $sources = new RegexIterator(
-    $iteratorIterator,
-    '/^.+\.(php|yaml|yml|xml|twig|twigjs)$/i',
-    RecursiveRegexIterator::GET_MATCH
-  );
-
-  foreach ($sources as $file) {
-    cmd("sed -i -e 's/{$namespace}/Claroline/' {$file[0]}");
-  }
-
-  cmd("git add .");
-  cmd("git commit -m '[$bundle] Switch to Claroline namespace'");
-}
-*/
-// Merge the branch into master using subtree feature
-
 cmd("git checkout import-with-history");
 cmd("git read-tree --prefix={$prefix}/ -u {$bundle}");
 cmd("git commit -m 'Import {$bundle}'");
