8442e68bb4618195f97e4a8bcf7b4eb6d2be1376
Rémy Taymans <remytaymans@gmail.com>
2016-04-06 18:16:06 +0200

    [FlashCardBundle] FlashCard is replace by Deck.

---
+++ Form/DeckType.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\FlashCardBundle\Form;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+
+/**
+ * @DI\Service("claroline_form_deck")
+ * @DI\Tag("form.type")
+ */
+class DeckType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', [
+                'label' => 'title',
+                'constraints' => new NotBlank()
+            ]);
+    }
+
+    public function getName()
+    {
+        return 'claroline_form_deck';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('translation_domain', 'platform');
+    }
+}
---
+++ Manager/DeckManager.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\FlashCardBundle\Manager;
+
+use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Entity\Workspace\Workspace;
+use Claroline\CoreBundle\Persistence\ObjectManager;
+use Claroline\FlashCardBundle\Entity\Deck;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Form\FormView;
+
+/**
+ * @DI\Service("claroline.deck.deck_manager")
+ */
+class DeckManager
+{
+    private $om;
+    private $templating;
+
+    /**
+     * @DI\InjectParams({
+     *     "om"         = @DI\Inject("claroline.persistence.object_manager"),
+     *     "templating" = @DI\Inject("templating")
+     * })
+     *
+     * @param ObjectManager     $om
+     * @param EngineInterface   $templating
+     */
+    public function __construct(ObjectManager $om, EngineInterface $templating)
+    {
+        $this->om = $om;
+        $this->templating = $templating;
+    }
+
+    /**
+     * Creates a flashcard resource.
+     *
+     * @param Deck $deck
+     * @return Deck
+     */
+    public function create(Deck $deck)
+    {
+        $this->om->persist($deck);
+        $this->om->flush();
+
+        return $deck;
+    }
+
+    /**
+     * Deletes a flashcard resource.
+     *
+     * @param Deck $deck
+     */
+    public function delete(Deck $deck)
+    {
+        $this->om->remove($deck);
+        $this->om->flush();
+    }
+
+    /**
+     * Returns the content of the result resource form.
+     * 
+     * @param FormView $view
+     * @return string
+     **/
+    public function getDeckFormContent(FormView $view)
+    {
+        return $this->templating->render(
+            'ClarolineCoreBundle:Resource:createForm.html.twig',
+            [
+                'form' => $view,
+                'resourceType' => 'claroline_flashcard'
+            ]
+        );
+    }
+}
