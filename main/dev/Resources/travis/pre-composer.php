29409146f4da65ed8603ebd37d5423375ca8989a
Godfraind <nicolas.godfraind@gmail.com>
2016-04-12 10:24:32 +0200

    Import dev tools (#132)
    * [DevBundle] Initial commit
    
    * [DevBundle] Bundle structure + first dependencies + basic tool command
    
    * [DevBundle] Fix escapes
    
    * [DevBundle] Fix escapes (2)
    
    * [DevBundle] Add cs/cs-dry doc
    
    * [DevBundle] Add scripts and resources for travis setup
    
    * [DevBundle] Fix incorrect path and ensure errors are converted to exceptions
    
    * [DevBundle] Fix yaml comment
    
    * [DevBundle] Fix config file name
    
    * [DevBundle] Relax error reporting (allow symfony deprecation messages...)
    
    * [DevBundle] Add non-test db settings (shouldn't be needed)
    
    * [DevBundle] Fix bundle autoloading
    
    * [DevBundle] Remove autoload registration (cache issue)
    
    * [DevBundle] Fix target bundle autoloading
    
    * [DevBundle] Save package dir during travis setup
    
    * [DevBundle] Adding dev commands + update composer.json
    
    * [DevBundle] Renaming commands + readme.
    
    * [DevBundle] Update readme.
    
    * [DevBundle] Change target db name
    
    * [DevBundle] Write bundle file after update as well
    
    * [DevBundle] Add travis doc + default config files
    
    * [DevBundle] Fix ordered list
    
    * [DevBundle] Fix code indent
    
    * [DevBundle] Fix markdown
    
    * [DevBundle] Try with blank lines...
    
    * [DevBundle] Fix travis autoloading
    
    * [DevBundle] Adding Translation Manager.
    
    * [DevBundle] wip
    
    * [DevBundle] Recursive traduction fill.
    
    * [DevBundle] Fixing notice
    
    * [DevBundle] More logs infos.
    
    * [DevBundle] Set prefer-stable option in generated composer.json
    
    * [DevBundle] Raise memory limit, disable xdebug in .travis.yml template
    
    * [DevBundle] Switch to psr-4 autoloading
    
    * [DevBundle] Reference distribution package + add mockery (tmp)
    
    * [DevBundle] Remove dist dependency (travis fix)
    
    * [DevBundle] Simplify travis setup
    
    * Preparing import.
    
    * Import DevBundle
    
    * Importing devbundle.
    
    * .gitattributes
    
    * Use local dev script in travis
    
    * Exclude dev bundle from prod env/release
    
    * [DevBundle] Remove obsolete config and update doc
    
    * [DevBundle] Use working directory to find composer.json

---
+++ main/dev/Resources/travis/pre-composer.php
@@ -0,0 +1,50 @@
+<?php
+
+/*******************************************************************
+ * This script updates the main composer.json file of the project
+ * with a reference to a package located in the filesystem in
+ * order to test that package in the full context of the Claroline
+ * platform.
+ *
+ * It is intended to be launched from the travis configuration
+ * file immediately before the execution of composer.
+ *
+ * Note that in order to work properly, this script MUST be
+ * executed from the root directory of the platform.
+ ******************************************************************/
+
+// convert errors to exceptions
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+if ($argc < 3) {
+  echo "Expected three arguments: package_name package_path\n";
+  exit(1);
+}
+
+$packageName = $argv[1];
+$packagePath = $argv[2];
+$composerFile = getcwd().'/composer.json';
+
+if (!file_exists($composerFile)) {
+  echo "No composer.json found (looked for {$composerFile})\n";
+  exit(1);
+}
+
+$data = json_decode(file_get_contents($composerFile));
+$data->require->{$packageName} = "*";
+
+if (!isset($data->repositories)) {
+    $data->repositories = [];
+}
+
+$repo = new stdClass();
+$repo->type = 'path';
+$repo->url = $packagePath;
+$data->repositories[] = $repo;
+
+$content = json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+file_put_contents($composerFile, $content);
+
+echo "Updated composer.json with content:\n{$content}\n";
