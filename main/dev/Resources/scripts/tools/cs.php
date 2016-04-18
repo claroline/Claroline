5d4eeaee0ff14cbfeb213edca35f3fa2974d7047
Stéphane Klein <stephaneklein221@gmail.com>
2016-04-18 15:24:12 +0200

    Remove cs scripts and tool command

--- main/dev/Resources/scripts/tools/cs.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require_once __DIR__.'/../tool-header.php';
-
-$cmd = "{$binDir}/php-cs-fixer fix {$argv[1]} --config-file {$configDir}/cs.php";
-system($cmd, $returnCode);
-exit($returnCode === 0 ? 0 : 1);
