266e7908273186c5b33fb600bb935638a4a9f7c3
pitrackster <pitrackster@gmail.com>
2016-04-26 15:40:29 +0200

    removed unused check

--- plugin/video-recorder/Installation/AdditionalInstaller.php
+++ plugin/video-recorder/Installation/AdditionalInstaller.php
@@ -14,14 +14,5 @@ class AdditionalInstaller extends BaseInstaller
       // load plugin default data
       $default = new DefaultData();
       $default->load($om);
-      // check libav-tools is installed
-      $cmd = 'avconv -version';
-      exec($cmd, $output, $return);
-      // if error
-      if(count($output) === 0 || $return !== 0){
-        echo 'libav-tools not found... disable InnovaVideoRecorderPlugin';
-        $plugin = $om->getRepository('ClarolineCoreBundle:Plugin')->findPluginByShortName('InnovaVideoRecorderBundle');
-        $this->container->get('claroline.manager.plugin_manager')->disable($plugin);
-      }
   }
 }
