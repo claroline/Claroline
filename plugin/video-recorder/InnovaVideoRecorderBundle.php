ed0d2ec4c41e007deed7585fa705f555d6bc2f38
pitrackster <pitrackster@gmail.com>
2016-04-26 14:17:49 +0200

    implements getExtraRequirements method

--- plugin/video-recorder/InnovaVideoRecorderBundle.php
+++ plugin/video-recorder/InnovaVideoRecorderBundle.php
@@ -28,4 +28,19 @@ class InnovaVideoRecorderBundle extends PluginBundle implements AutoConfigurable
 
         return $config->addRoutingResource(__DIR__.'/Resources/config/routing.yml', null, 'video_recorder');
     }
+
+    public function getExtraRequirements()
+    {
+        return array(
+            'libav-tools' => array(
+                'test' => function () {
+                    $cmd = 'avconv -h';
+                    exec($cmd, $output, $return);
+
+                    return count($output) > 0 && $return === 0;
+                },
+                'failure_msg' => 'libavtools_not_installed',
+            ),
+        );
+    }
 }
