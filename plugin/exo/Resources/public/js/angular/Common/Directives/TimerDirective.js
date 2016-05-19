c85c854b471e84987cea6cc35a9fe74de8afa781
Elorfin <penin.axel@gmail.com>
2016-05-19 16:41:32 +0200

    simplify TimerDirective and use it in Player

--- plugin/exo/Resources/public/js/angular/Common/Directives/TimerDirective.js
+++ plugin/exo/Resources/public/js/angular/Common/Directives/TimerDirective.js
@@ -1,98 +1,48 @@
 /**
  * Create a Timer
+ * @param {TimerService} TimerService
  * @constructor
  */
-var TimerDirective = function TimerDirective() {
+var TimerDirective = function TimerDirective(TimerService) {
     return {
         restrict: 'E',
         replace: true,
         templateUrl: AngularApp.webDir + 'bundles/ujmexo/js/angular/Common/Partials/timer.html',
         bindToController: true,
         controllerAs: 'timerCtrl',
-        controller: [
-            '$localStorage',
-            '$timeout',
-            function TimerCtrl($localStorage, $timeout) {
-                console.log('append directive');
-
-                this.$localStorage = $localStorage;
-                this.$timeout      = $timeout;
-
-                // Initialize var
-                this.$localStorage.$default({
-                    counter: 0,
-                    hours: 0,
-                    minutes: 0,
-                    seconds: 0
-                });
-
-                // Function to increase te timer
-                var onTimeout = function onTimeout() {
-                    // Increase the timer
-                    this.$localStorage.counter = $localStorage.counter + 1;
-
-                    // Calculate remaining duration
-                    var remaining = this.duration - this.$localStorage.counter;
-
-                    // Transform counter into hours, minutes and seconds
-                    var hoursMod = remaining % 3600; // Hours which are not plain
-                    this.$localStorage.hours = (remaining - hoursMod) / 3600;
-
-                    var minutesMod = hoursMod % 60; // Minutes which are not plain
-                    this.$localStorage.minutes = (hoursMod - minutesMod) / 60;
-
-                    this.$localStorage.seconds = minutesMod;
-
-                    // If timer reach the exercise duration
-                    if (this.$localStorage.counter == this.duration) {
-                        // Cancel timeout function
-                        this.$timeout.cancel(onTimeout);
-
-                        // Remove local storage
-                        this.$localStorage.$reset({
-                            counter: 0,
-                            hours: 0,
-                            minutes: 0,
-                            seconds: 0
-                        });
-
-                        // Execute registered callback
-                        this.onEnd();
-                    } else {
-                        // Call function to increase next
-                        this.$timeout(onTimeout.bind(this), 1000);
-                    }
-                }.bind(this);
-
-                this.destroy = function destroy() {
-                    this.$timeout.cancel(onTimeout);
-                };
-
-                // Call for the first time the function to increase timer
-                this.$timeout(onTimeout.bind(this), 1000);
-            }
-        ],
+        controller: function TimerCtrl() {},
         link: function link(scope, element, attrs, ctrl) {
-            scope.$on('$destroy', function onDestroy() {
-                ctrl.destroy();
-            });
+            if (ctrl.timerStart) {
+                TimerService.start(ctrl.timer.id);
+            }
+
+            if (ctrl.timerDestroy) {
+                scope.$on('$destroy', function onDestroy() {
+                    TimerService.destroy(ctrl.timer.id);
+                });
+            }
         },
         scope: {
             /**
-             * Duration of the Timer (in seconds)
+             * The Timer to display
              */
-            duration : '=',
+            timer: '=',
 
             /**
-             * Callback to execute when the Timer end
+             * Start the Timer when the Directive is created ?
              */
-            onEnd    : '&'
+            timerStart: '=',
+
+            /**
+             * End the Timer when the Directive is destroyed ?
+             */
+            timerDestroy: '='
         }
     };
 };
 
 // Set up dependency injection
-TimerDirective.$inject = [];
+TimerDirective.$inject = [ 'TimerService' ];
 
 // Register directive into AngularJS
 angular
