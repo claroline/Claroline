ab4d656b79c2a88eda306f7bc51c31e845d70283
Elorfin <penin.axel@gmail.com>
2016-05-22 13:07:39 +0200

    new StepManager add question method

--- plugin/exo/Manager/StepManager.php
+++ plugin/exo/Manager/StepManager.php
@@ -2,7 +2,9 @@
 
 namespace UJM\ExoBundle\Manager;
 
+use Claroline\CoreBundle\Persistence\ObjectManager;
 use JMS\DiExtraBundle\Annotation as DI;
+use UJM\ExoBundle\Entity\Question;
 use UJM\ExoBundle\Entity\Step;
 use UJM\ExoBundle\Entity\StepQuestion;
 
@@ -12,17 +14,56 @@ use UJM\ExoBundle\Entity\StepQuestion;
 class StepManager
 {
     /**
+     * @var ObjectManager
+     */
+    private $om;
+
+    /**
+     * @var QuestionManager
+     */
+    private $questionManager;
+
+    /**
      * StepManager constructor.
      *
      * @DI\InjectParams({
+     *     "om"              = @DI\Inject("claroline.persistence.object_manager"),
      *     "questionManager" = @DI\Inject("ujm.exo.question_manager")
      * })
      *
+     * @param ObjectManager $om
      * @param QuestionManager $questionManager
      */
-    public function __construct(QuestionManager $questionManager)
+    public function __construct(
+        ObjectManager $om,
+        QuestionManager $questionManager)
     {
+        $this->om = $om;
         $this->questionManager = $questionManager;
+    }
+
+    /**
+     * Append a Question to a Step
+     * @param Step $step
+     * @param Question $question
+     * @param int $order if -1 the question will be added at the end of the Step
+     */
+    public function addQuestion(Step $step, Question $question, $order = -1)
+    {
+        $stepQuestion = new StepQuestion();
+
+        $stepQuestion->setStep($step);
+        $stepQuestion->setQuestion($question);
+
+        if (-1 === $order) {
+            // Calculate current Question order
+            $order = count($step->getStepQuestions());
+        }
+
+        $stepQuestion->setOrdre($order);
+
+        $this->om->persist($stepQuestion);
+        $this->om->flush();
     }
 
     /**
