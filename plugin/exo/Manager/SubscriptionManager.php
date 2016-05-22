1bb7f57ed8a3bf558bbcf407b043922fe2ea4f50
Elorfin <penin.axel@gmail.com>
2016-05-22 12:17:17 +0200

    implement SubscriptionManager

--- plugin/exo/Manager/SubscriptionManager.php
+++ plugin/exo/Manager/SubscriptionManager.php
@@ -2,8 +2,11 @@
 
 namespace UJM\ExoBundle\Manager;
 
+use Claroline\CoreBundle\Entity\User;
 use Claroline\CoreBundle\Persistence\ObjectManager;
 use JMS\DiExtraBundle\Annotation as DI;
+use UJM\ExoBundle\Entity\Exercise;
+use UJM\ExoBundle\Entity\Subscription;
 
 /**
  * @DI\Service("ujm.exo.subscription_manager")
@@ -29,9 +32,51 @@ class SubscriptionManager
 
     /**
      * Subscribes a Use to an Exercise
+     *
+     * @param Exercise $exercise
+     * @param User $user
+     * @param bool $flush
+     *
+     * @return SubscriptionManager
      */
-    public function subscribe()
+    public function subscribe(Exercise $exercise, User $user, $flush = false)
     {
-        
+        $subscription = new Subscription();
+        $subscription->setUser($user);
+        $subscription->setExercise($exercise);
+        $subscription->setAdmin(true);
+        $subscription->setCreator(true);
+
+        $this->om->persist($subscription);
+
+        if ($flush) {
+            $this->om->flush();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Delete all Subscriptions of an Exercise
+     *
+     * @param Exercise $exercise
+     * @param bool $flush
+     *
+     * @return SubscriptionManager
+     */
+    public function deleteSubscriptions(Exercise $exercise, $flush = false)
+    {
+        $subscriptions = $this->om->getRepository('UJMExoBundle:Subscription')
+            ->findByExercise($exercise);
+
+        foreach ($subscriptions as $subscription) {
+            $this->om->remove($subscription);
+        }
+
+        if ($flush) {
+            $this->om->flush();
+        }
+
+        return $this;
     }
 }
