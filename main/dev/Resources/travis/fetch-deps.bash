61934ba71e9dec952669504aa44a4fa1984732c9
Stéphane Klein <stephaneklein221@gmail.com>
2016-05-23 12:49:03 +0200

    Relax var check for external PRs (#366)

--- main/dev/Resources/travis/fetch-deps.bash
+++ main/dev/Resources/travis/fetch-deps.bash
@@ -9,16 +9,19 @@
 #
 # 1. this script must be executed with bash, not sh
 # 2. the working directory must be the root directory of the platform
+# 3. for internal pull requests, the following environment variables are all
+#    supposed to be available:
+#      - TRAVIS_REPO_SLUG (set by travis)
+#      - REMOTE_HOST
+#      - REMOTE_USER
+#      - REMOTE_PASS
+#      - CACHE_PATH
 ################################################################################
 
 set -e
 set -o pipefail
 
 : ${TRAVIS_REPO_SLUG:?"must be set"}
-: ${REMOTE_HOST:?"must be set"}
-: ${REMOTE_USER:?"must be set"}
-: ${REMOTE_PASS:?"must be set"}
-: ${CACHE_PATH:?"must be set"}
 
 DIST=../$TRAVIS_REPO_SLUG
 
