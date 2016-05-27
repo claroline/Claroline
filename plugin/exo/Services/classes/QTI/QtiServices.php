ebbdb2e722e2a253a65ebd60c5f54b5c192b7852
charlotte <charlotte.abbot@univ-st-etienne.fr>
2016-05-27 12:03:11 +0200

    Export of exercise alone and in a ws

--- plugin/exo/Services/classes/QTI/QtiServices.php
+++ plugin/exo/Services/classes/QTI/QtiServices.php
@@ -6,6 +6,17 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class QtiServices
 {
+    private $container;
+
+    /**
+     * Constructor.
+     *
+     * @param \Symfony\Component\DependencyInjection\Container $container
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
     /**
      * For create a zip where questions while be integrate.
      *
@@ -36,6 +47,7 @@ class QtiServices
      */
     public function sortPathOfQuestions(QtiRepository $qtiRepo, $stepDir = '')
     {
+        var_dump($stepDir);
         $pathQtiDir = $qtiRepo->getUserDir().'questions';
         if ($stepDir != '') {
             $pathQtiDir .= '/'.$stepDir;
@@ -44,12 +56,40 @@ class QtiServices
         //create array with sort file
         $dirs = array();
         foreach ($questions as $question) {
+            var_dump($question != '..');
             if ($question != '.' && $question != '..' && $question->getExtension() == '') {
+                echo('la');
                 $dirs[] = $pathQtiDir.'/'.$question->getFilename();
             }
         }
+
         sort($dirs);
 
         return $dirs;
+    }
+    /**
+         * create the directory questions to export an exercise and export the qti files.
+         *
+         * @param \UJM\ExoBundle\Entity\Question[] $questions
+         * @param int                              $numStep
+         */
+    public function createQuestionsDirectory(array $questions, $numStep)
+    {
+        $qtiRepo = $this->container->get('ujm.exo_qti_repository');
+
+        @mkdir($qtiRepo->getUserDir().'questions');
+        $i = 'a';
+        @mkdir($qtiRepo->getUserDir().'questions/'.$numStep);
+        foreach ($questions as $question) {
+            $qtiRepo->export($question);
+            @mkdir($qtiRepo->getUserDir().'questions/'.$numStep.'/'.$numStep.'_question_'.$i);
+            $iterator = new \DirectoryIterator($qtiRepo->getUserDir());
+            foreach ($iterator as $element) {
+                if (!$element->isDot() && $element->isFile()) {
+                    rename($qtiRepo->getUserDir().$element->getFilename(), $qtiRepo->getUserDir().'questions/'.$numStep.'/'.$numStep.'_question_'.$i.'/'.$element->getFilename());
+                }
+            }
+            $i .= 'a';
+        }
     }
 }
