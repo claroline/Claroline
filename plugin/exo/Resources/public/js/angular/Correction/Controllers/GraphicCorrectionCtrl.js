07465653b1094604696274c173382cd62debc827
Elorfin <penin.axel@gmail.com>
2016-05-28 18:49:53 +0200

    wip new graphic implementation
    rework graphic questions

--- plugin/exo/Resources/public/js/angular/Correction/Controllers/GraphicCorrectionCtrl.js
+++ plugin/exo/Resources/public/js/angular/Correction/Controllers/GraphicCorrectionCtrl.js
@@ -2,55 +2,25 @@
  * Correction for Graphic Questions
  * @param {QuestionService}        QuestionService
  * @param {GraphicQuestionService} GraphicQuestionService
+ * @param {ImageAreaService} ImageAreaService
  * @constructor
  */
-var GraphicCorrectionCtrl = function GraphicCorrectionCtrl(QuestionService, GraphicQuestionService) {
+var GraphicCorrectionCtrl = function GraphicCorrectionCtrl(QuestionService, GraphicQuestionService, ImageAreaService) {
     AbstractCorrectionCtrl.apply(this, arguments);
 
     this.GraphicQuestionService = GraphicQuestionService;
+    this.ImageAreaService = ImageAreaService;
 };
 
 // Extends AbstractQuestionCtrl
 GraphicCorrectionCtrl.prototype = Object.create(AbstractCorrectionCtrl.prototype);
 
 // Set up dependency injection (get DI from parent too)
-GraphicCorrectionCtrl.$inject = AbstractCorrectionCtrl.$inject.concat([ 'GraphicQuestionService' ]);
+GraphicCorrectionCtrl.$inject = AbstractCorrectionCtrl.$inject.concat([ 'GraphicQuestionService', 'ImageAreaService' ]);
 
-/**
- * Get the full URL of the Image
- * @returns {string}
- */
-GraphicCorrectionCtrl.prototype.getImageUrl = function getImageUrl() {
-    return this.GraphicQuestionService.getImageUrl(this.question);
-};
-
-GraphicCorrectionCtrl.prototype.createElements = function createElements() {
-    // create correction elements (backgrounds)
-    for (var i = 0; i < this.question.solutions.length; i++) {
-        var solution = this.question.solutions[i];
-        var elem = document.createElement('div');
-        var style = '';
-        style += 'position:absolute;';
-        style += 'border:1px solid #eee;';
-        style += 'opacity:0.6;';
-        style += 'height:' + solution.size.toString() + 'px;';
-        style += 'width:' + solution.size.toString() + 'px;';
-
-        if (solution.shape === "circle") {
-            style += 'border-radius:50%;';
-        }
-        var coords = solution.value.split(',');
-        var imgPosition = $('#document-img-' + this.question.id).position();
-        var topValue = imgPosition.top + parseFloat(coords[1]);
-        var leftValue = imgPosition.left + parseFloat(coords[0]);
-
-        style += 'top:' + topValue.toString() + 'px;';
-        style += 'left:' + leftValue.toString() + 'px;';
-        style += 'background-color:' + solution.color + ';';
-        elem.setAttribute('style', style);
-        document.getElementById('document-img-container-' + this.question.id).appendChild(elem);
-    }
-};
+GraphicCorrectionCtrl.prototype.getAreaColor = function getAreaColor(area) {
+    return this.ImageAreaService.COLORS[area.color];
+}
 
 // Register controller into AngularJS
 angular
--- plugin/exo/Resources/public/js/angular/Question/Controllers/Type/GraphicQuestionCtrl.js
+++ plugin/exo/Resources/public/js/angular/Question/Controllers/Type/GraphicQuestionCtrl.js
@@ -2,293 +2,75 @@
  * Graphic Question Controller
  * @param {FeedbackService}        FeedbackService
  * @param {GraphicQuestionService} GraphicQuestionService
+ * @param {ImageAreaService}       ImageAreaService
  * @constructor
  */
-var GraphicQuestionCtrl = function GraphicQuestionCtrl(FeedbackService, GraphicQuestionService) {
+var GraphicQuestionCtrl = function GraphicQuestionCtrl(FeedbackService, GraphicQuestionService, ImageAreaService) {
     AbstractQuestionCtrl.apply(this, arguments);
 
     this.GraphicQuestionService = GraphicQuestionService;
-
-    // init coord answer array in any case
-    // id order is totally arbitrary
-    for (var i = 0; i < this.question.coords.length; i++) {
-        this.coords.push({
-            id: this.question.coords[i].id,
-            x: 'a', // to ensure ujm compatibility
-            y: 'a'
-        });
-    }
-
-    // init draggable elements with answers if any
-    if (this.answer && this.answer.length > 0) {
-        for (var i = 0; i < this.answer.length; i++) {
-            var answerArray = this.answer[i].split('-');
-            if (answerArray[0] !== 'a' || answerArray[1] !== 'a') {
-                var coord = this.coords[i];
-                coord.x = answerArray[0] !== 'a' ? parseFloat(answerArray[0]):0;
-                coord.y = answerArray[1] !== 'a' ? parseFloat(answerArray[1]):0;
-            }
-
-        }
-    }
+    this.ImageAreaService = ImageAreaService;
 };
 
 // Extends AbstractQuestionCtrl
 GraphicQuestionCtrl.prototype = Object.create(AbstractQuestionCtrl.prototype);
 
 // Set up dependency injection (get DI from parent too)
-GraphicQuestionCtrl.$inject = AbstractQuestionCtrl.$inject.concat([ 'GraphicQuestionService' ]);
+GraphicQuestionCtrl.$inject = AbstractQuestionCtrl.$inject.concat([ 'GraphicQuestionService', 'ImageAreaService' ]);
 
 /**
  * The current image of the question
- * @type {HTMLElement}
+ * @type {object}
  */
 GraphicQuestionCtrl.prototype.$image = null;
 
 /**
- * Tells wether the answers are all found, not found, or if only one misses
+ * Tells whether the answers are all found, not found, or if only one misses
  * @type {Integer}
  */
 GraphicQuestionCtrl.prototype.feedbackState = -1;
-
-/**
- * Definition of the crosshair
- * @type {{img: string, size: number}}
- */
-GraphicQuestionCtrl.prototype.crosshair = {
-    url  : AngularApp.webDir + 'bundles/ujmexo/images/graphic/answer.png',
-    size : 16
-};
-
-/**
- *
- * @type {Array}
- */
-GraphicQuestionCtrl.prototype.coords = []; // student answers
-
-/**
- *
- * @type {Array}
- */
-GraphicQuestionCtrl.prototype.notFoundZones = [];
-
-/**
- * @type {boolean}
- */
-GraphicQuestionCtrl.prototype.notFoundZonesAreSet = false;
 
 /**
  * Get the full URL of the Image
  * @returns {string}
  */
 GraphicQuestionCtrl.prototype.getImageUrl = function getImageUrl() {
-    return this.GraphicQuestionService.getImageUrl(this.question);
-};
-
-/**
- *
- */
-GraphicQuestionCtrl.prototype.initPreviousAnswers = function initPreviousAnswers() {
-    for (var i = 0; i < this.coords.length; i++) {
-        // ensure that we are not in default values
-        if (this.coords[i].x !== 'a' && this.coords[i].y !== 'a') {
-            // crosshair
-            var $crosshair = $('#crosshair_' + this.coords[i].id);
-            /*
-             * compute crosshair coords
-             * the method is a bit complex and I'm not sure it's the better one...
-             * when we record the student answer in database we have two values that are relative to the document image container
-             * The point coordinates are the top left corner so we need to center the crosshair with $crosshair[0].width / 2
-             * assuming crosshair image is a square
-             *
-             * if we user center-text class on col elements this does not work anymore...
-             */
-            var coordX = $('#document-img').offset().left + this.coords[i].x - $crosshair.offset().left - (this.crosshair.size / 2);
-            var coordY = $('#document-img').offset().top + this.coords[i].y - $crosshair.offset().top - (this.crosshair.size / 2);
-            $('#crosshair_' + this.coords[i].id).css('top', coordY);
-            $('#crosshair_' + this.coords[i].id).css('left', coordX);
-        }
+    var url = null;
+    if (this.question.image && this.question.image.url) {
+        url = AngularApp.webDir + this.question.image.url;
     }
-};
 
-/**
- *
- */
-GraphicQuestionCtrl.prototype.initDragAndDrop = function initDragAndDrop() {
-    var self = this;
-
-    // init ui draggable objects
-    $(".draggable").draggable({
-        // automatic z-index
-        stack: ".draggable",
-        // drop only in container
-        containment: '.droppable-container',
-        stop: function (event, ui) {
-            // get dragged element id
-            var draggedId = $(this).attr('id').replace('crosshair_', '');
-            // get dragged coordinates with offset instead of position
-            var coordX = $(this).offset().left - $('#document-img').offset().left;
-            var coordY = $(this).offset().top - $('#document-img').offset().top;
-
-            // update this.coords
-            for (var i = 0; i < self.coords.length; i++) {
-                if (self.coords[i].id === draggedId) {
-                    self.coords[i].x = coordX;
-                    self.coords[i].y = coordY;
-                }
-            }
-            // update student data in shared service
-            self.updateStudentData();
-        }
-    });
+    return url;
 };
 
 /**
  * Reset answer
  */
 GraphicQuestionCtrl.prototype.reset = function reset() {
-    for (var i = 0; i < this.coords.length; i++) {
-        $('#crosshair_' + this.coords[i].id).css('top', 'auto');
-        $('#crosshair_' + this.coords[i].id).css('left', 'auto');
-    }
-
-    this.coords.splice(0, this.coords.length);
-    for (var i = 0; i < this.question.coords.length; i++) {
-        this.coords.push({
-            id: this.question.coords[i].id,
-            x: 0,
-            y: 0
-        });
-    }
-
-    this.updateStudentData();
+    this.answer.splice(0, this.answer.length);
 };
 
-/**
- *
- * @returns {*|null}
- */
-GraphicQuestionCtrl.prototype.getAssetsDir = function getAssetsDir() {
-    return AngularApp.webDir;
-};
+GraphicQuestionCtrl.prototype.isPointerValid = function isPointerValid(pointer) {
+    var valid = undefined;
 
-/**
- *
- */
-GraphicQuestionCtrl.prototype.showRightAnswerZones = function showRightAnswerZones() {
-    var pointX = 0;
-    var pointY = 0;
-    var startX = 0;
-    var startY = 0;
-    var start;
-    var centerX = 0;
-    var centerY = 0;
-
-    for (var i = 0; i < this.question.solutions.length; i++) {
-        for (var j=0; j<$(".crosshair").length; j++) {
-            var firstElementId = $(".crosshair")[j].id;
-            var firstElementNumId = firstElementId.replace("crosshair_", "");
-            var topElementsHeight = $("#" + firstElementId).parent().parent().parent().prop("offsetHeight");
-            
-            var pointOffsetX = $("#" + firstElementId).offset().left;
-            var pointOffsetY = $("#" + firstElementId).offset().top;
-            var imageOffsetX = $('#document-img').offset().left;
-            var imageOffsetY = $('#document-img').offset().top;
-            pointX = pointOffsetX - imageOffsetX;
-            pointY = pointOffsetY - imageOffsetY;
-            start = this.question.solutions[i].value.split(",");
-            startX = parseFloat(start[0]);
-            startY = parseFloat(start[1]);
-            centerX = startX + this.question.solutions[i].size/2;
-            centerY = startY + this.question.solutions[i].size/2;
-            var endX = startX + this.question.solutions[i].size;
-            var endY = startY + this.question.solutions[i].size;
-            
-            var offsetFromPanelX = $("#" + firstElementId).prop("x") - pointX;
-            var offsetFromPanelY = $("#" + firstElementId).prop("y") - pointY;
-
-            var distance = Math.sqrt((centerX-pointX)*(centerX-pointX) + (centerY-pointY)*(centerY-pointY));
-            distance = Math.round(distance);
-
-            if (((this.question.solutions[i].size >= distance*2 && this.question.solutions[i].shape === "circle")
-                || (this.question.solutions[i].shape === "square" && pointX > startX && pointX < endX && pointY > startY && pointY < endY))
-                && this.notFoundZones.indexOf(this.question.solutions[i]) !== -1) {
-                var rightPointY = pointY + topElementsHeight;
-                var visualPointX = $("#" + firstElementId).prop("x");
-                var visualPointY = $("#" + firstElementId).prop("y");
-                $("#" + firstElementId).replaceWith("<i id='crosshair_valid_" + firstElementNumId + "' class='text-success feedback-info fa fa-check' data-toggle='tooltip' style='top: " + visualPointY + "px; left: " + visualPointX + "px; position: absolute; z-index: 3;' title='" + (this.question.solutions[i].feedback ? this.question.solutions[i].feedback : '') + "' ></i>");
-
-                var answerZone = this.getAnswerZoneHTML(this.question.solutions[i], startX + offsetFromPanelX, startY + offsetFromPanelY);
-                document.getElementsByClassName('droppable-container')[0].appendChild(answerZone);
-
-                this.notFoundZones.splice(this.notFoundZones.indexOf(this.question.solutions[i]), 1);
+    if (this.question.solutions && this.feedback.visible) {
+        valid = false;
+        for (var i = 0; i < this.question.solutions.length; i++) {
+            valid = this.ImageAreaService.isInArea(this.question.solutions[i], pointer);
+            if (valid) {
+                break;
             }
         }
     }
-    
-    if (this.notFoundZones.length === 0) {
-        this.feedbackState = 0;
-    } else if (this.notFoundZones.length === 1) {
-        this.feedbackState = 1;
-    } else {
-        this.feedbackState = 2;
-    }
+
+    console.log(valid);
+
+    return valid;
 };
 
-/**
- *
- */
-GraphicQuestionCtrl.prototype.getAnswerZoneHTML = function getAnswerZoneHTML(solution, x, y) {
-    var element = document.createElement('div');
-    var style = '';
-    style += 'position:absolute;';
-    style += 'opacity:0.6;';
-    style += 'height:' + solution.size + 'px;';
-    style += 'width:' + solution.size + 'px;';
-
-    if (solution.shape === "circle") {
-        style += 'border-radius:50%;';
-    }
-
-    style += 'top:' + y + 'px;';
-    style += 'left:' + x + 'px;';
-    style += 'background-color:' + solution.color + ';';
-    element.setAttribute('style', style);
-    element.setAttribute('class', 'answerField');
-
-    return element;
-};
-
-/**
- *
- */
-GraphicQuestionCtrl.prototype.setWrongFeedback = function setWrongFeedback() {
-    var elements = $(".crosshair");
-    for (var i = 0; i < elements.length; i++) {
-        var id = elements[i].id;
-        var numId = id.replace("crosshair_", "");
-        var rightPointY = $("#" + id).prop("y");
-        var pointX = $("#" + id).prop("x");
-        $("#" + id).replaceWith("<i id='crosshair_invalid_" + numId + "' class='text-danger fa fa-close feedback-info crosshair_invalid' data-toggle='tooltip' style='top: " + rightPointY + "px; left: " + pointX + "px; position: absolute; z-index: 3;' title='" + (this.question.solutions[i].feedback ? this.question.solutions[i].feedback : '') + "' ></i>");
-    }
-};
-
-/**
- *
- */
-GraphicQuestionCtrl.prototype.disableDraggable = function disableDraggable() {
-    for (var i = 0; i < this.question.coords.length; i++) {
-        $("#crosshair_" + this.question.coords[i].id).draggable('disable');
-    }
-};
-
-/**
- *
- */
-GraphicQuestionCtrl.prototype.enableDraggable = function enableDraggable() {
-    for (var i = 0; i < this.question.coords.length; i++) {
-        $("#crosshair_" + this.question.coords[i].id).draggable();
-        $("#crosshair_" + this.question.coords[i].id).draggable('enable');
+GraphicQuestionCtrl.prototype.areaHasPointer = function (area) {
+    for (var i = 0; i < this.answer.length; i++) {
+        this.ImageAreaService.isInArea(area, this.answer[i]);
     }
 };
 
@@ -296,53 +78,17 @@ GraphicQuestionCtrl.prototype.enableDraggable = function enableDraggable() {
  *
  */
 GraphicQuestionCtrl.prototype.onFeedbackShow = function onFeedbackShow() {
-    if (!this.notFoundZonesAreSet) {
-        for (var i=0; i < this.question.solutions.length; i++) {
-            this.notFoundZones.push(this.question.solutions[i]);
-        }
-        this.notFoundZonesAreSet = true;
-    }
-    
-    this.disableDraggable();
-    this.showRightAnswerZones();
-    this.setWrongFeedback();
+    this.feedbackState = this.GraphicQuestionService.answersAllFound(this.question, this.answer);
 };
 
 /**
  *
  */
 GraphicQuestionCtrl.prototype.onFeedbackHide = function onFeedbackHide() {
-    this.hideWrongFeedbacks();
-    this.enableDraggable();
-};
-
-/**
- *
- */
-GraphicQuestionCtrl.prototype.hideWrongFeedbacks = function hideWrongFeedbacks() {
-    var elements = $(".crosshair_invalid");
-    for (var i = 0; i < elements.length; i++) {
-        var former_id = elements[i].id;
-        var id = elements[i].id.replace("_invalid", "");
-        var top = $("#" + former_id).css('top');
-        var left = $("#" + former_id).css('left');
-        $("#" + former_id).replaceWith("<img id='" + id + "' class='crosshair draggable ui-draggable ui-draggable-handle' data-ng-src='" + this.getAssetsDir() + "bundles/ujmexo/images/graphic/answer.png' src='" + this.getAssetsDir() + "bundles/ujmexo/images/graphic/answer.png' style='top:" + top + "; position: relative; left: " + left + "; z-index: 1;'/>");
-    }
-};
-
-/**
- * Called on each checkbox / radiobutton click
- * We need to share those information with parent controllers
- * For that purpose we use a shared service
- */
-GraphicQuestionCtrl.prototype.updateStudentData = function updateStudentData() {
-    this.answer.splice(0, this.answer.length);
-    for (var i = 0; i < this.coords.length; i++) {
-        // Se want to store the center of the crosshair
-        var x = this.coords[i].x + (this.crosshair.size / 2);
-        var y = this.coords[i].y + (this.crosshair.size / 2);
-
-        this.answer.push((x + '-' + y));
+    // Reset validation
+    for (var i = 0; i < this.answer.length; i++) {
+        // Keep track on answer found by user (only errors are reset)
+        this.answer.$invalid = false;
     }
 };
 
