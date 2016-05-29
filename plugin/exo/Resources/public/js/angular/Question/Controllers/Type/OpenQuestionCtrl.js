0f47a5d58198de4608f04c59ceee247acdbb2619
Elorfin <penin.axel@gmail.com>
2016-05-29 20:57:30 +0200

    various fixes

--- plugin/exo/Resources/public/js/angular/Question/Controllers/Type/OpenQuestionCtrl.js
+++ plugin/exo/Resources/public/js/angular/Question/Controllers/Type/OpenQuestionCtrl.js
@@ -1,17 +1,20 @@
 /**
  * Open Question Controller
  * @param {FeedbackService} FeedbackService
+ * @param {OpenQuestionService} OpenQuestionService
  * @constructor
  */
-var OpenQuestionCtrl = function OpenQuestionCtrl(FeedbackService) {
+var OpenQuestionCtrl = function OpenQuestionCtrl(FeedbackService, OpenQuestionService) {
     AbstractQuestionCtrl.apply(this, arguments);
+
+    this.OpenQuestionService = OpenQuestionService;
 };
 
 // Extends AbstractQuestionCtrl
 OpenQuestionCtrl.prototype = Object.create(AbstractQuestionCtrl.prototype);
 
 // Set up dependency injection (get DI from parent too)
-OpenQuestionCtrl.$inject = AbstractQuestionCtrl.$inject;
+OpenQuestionCtrl.$inject = AbstractQuestionCtrl.$inject.concat([ 'OpenQuestionService' ]);
 
 /**
  * Answer of the student with highlighted keywords
@@ -30,7 +33,6 @@ OpenQuestionCtrl.prototype.feedbackState = -1;
  */
 OpenQuestionCtrl.prototype.onFeedbackShow = function onFeedbackShow() {
     if (this.question.solutions) {
-        var numAnswersFound = 0;
         this.answerWithKeywords = this.answer ? this.answer : '';
 
         // Get EOL
@@ -46,7 +48,6 @@ OpenQuestionCtrl.prototype.onFeedbackShow = function onFeedbackShow() {
                 var searchFlags      = 'g' + (solution.caseSensitive ? 'i' : '');
                 var searchExpression = new RegExp(solution.word, searchFlags);
                 if (-1 !== this.answer.search(searchExpression)) {
-                        numAnswersFound++;
                         // Keyword has been found in answer => Update formatted answer
                         var keyword = '';
                         keyword += '<b class="text-success feedback-info" data-toggle="tooltip" title="' + (solution.feedback || '') + '">';
@@ -58,14 +59,8 @@ OpenQuestionCtrl.prototype.onFeedbackShow = function onFeedbackShow() {
                 }
             }
         }
-    }
-    
-    if (this.question.solutions.length === numAnswersFound) {
-        this.feedbackState = this.FeedbackService.SOLUTION_FOUND;
-    } else if (this.question.solutions.length -1 === numAnswersFound) {
-        this.feedbackState = this.FeedbackService.ONE_ANSWER_MISSING;
-    } else {
-        this.feedbackState = this.FeedbackService.MULTIPLE_ANSWERS_MISSING;
+
+        this.OpenQuestionService.answersAllFound(this.question, this.answer);
     }
 };
 
