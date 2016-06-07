65696c00db8f4ad20ca1936a9cf2b40b349c80f2
Elorfin <penin.axel@gmail.com>
2016-06-07 15:16:18 +0200

    fix typo

--- plugin/exo/Resources/modules/common/module.js
+++ plugin/exo/Resources/modules/common/module.js
@@ -14,7 +14,7 @@ import TinyMceService from './Services/TinyMceService'
 
 angular
   .module('Common', ['ngSanitize'])
-  .filter('date_string', ['$filter', DateStringFilter)
+  .filter('date_string', ['$filter', DateStringFilter])
   .filter('path', RouterFilter)
   .filter('simple_type', ['CommonService', SimpleTypeFilter])
   .filter('unsafe', ['$sce', UnsafeFilter])
