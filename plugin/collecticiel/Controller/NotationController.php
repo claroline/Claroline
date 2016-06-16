21b191eb1a9dcccfdae107248b8f7c024f701f82
Eric VINCENT <ericvincenterv@gmail.com>
2016-06-16 14:43:37 +0200

    fix claroline/Distribution#665 wrong updating column note in notation table

--- plugin/collecticiel/Controller/NotationController.php
+++ plugin/collecticiel/Controller/NotationController.php
@@ -219,6 +219,9 @@ class NotationController extends DropzoneBaseController
 
         $notation[0]->setRecordOrTransmit(true);
 
+        $note = $this->get('request')->query->get('note');
+        $notation[0]->setNote($note);
+
         // Mise à jour de la base de données
         $em->persist($notation[0]);
         $em->flush();
