79fd28968a0bb1a12e648d667e65cc02feef5192
Axel Penin <Elorfin@users.noreply.github.com>
2016-07-07 12:07:12 +0200

    [ExoBundle] Stand-alone module for Feedbacks (#793)
    * create standalone module for Feedbacks
    
    * fix coding style
    
    fix coding style
    
    * create ES6 class for FeedbackService
    
    remove semicolon
    
    fix missing this. prefix

--- plugin/exo/Resources/modules/exercise/module.js
+++ plugin/exo/Resources/modules/exercise/module.js
@@ -1,6 +1,8 @@
 /**
- * Exercise Module
- */
+* Exercise Module
+*/
+
+import angular from 'angular/index'
 
 import 'angular-bootstrap'
 import 'angular-strap'
@@ -9,6 +11,7 @@ import 'angular-ui-translation/angular-translation'
 import '#/main/core/modal/module'
 
 import './../common/module'
+import './../feedback/module'
 import './../step/module'
 import './../paper/module'
 import './../timer/module'
@@ -20,59 +23,56 @@ import ExercisePlayerCtrl from './Controllers/ExercisePlayerCtrl'
 import ExerciseDirective from './Directives/ExerciseDirective'
 import StartButtonDirective from './Directives/StartButtonDirective'
 import ExerciseService from './Services/ExerciseService'
-import FeedbackService from './Services/FeedbackService'
 
 angular
-    .module('Exercise', [
-        'ui.translation',
-        'ui.bootstrap',
-        'ui.tinymce',
-        'ui.modal',
-        'mgcrea.ngStrap.datepicker',
-        'Common',
-        'Step',
-        'Paper',
-        'Timer'
-    ])
-    .controller('ExerciseCtrl', [
-        'ExerciseService',
-        'PaperService',
-        'UserPaperService',
-        '$route',
-        ExerciseCtrl
-    ])
-    .controller('ExerciseMetadataCtrl', [
-        '$location',
-        'ExerciseService',
-        'TinyMceService',
-        ExerciseMetadataCtrl
-    ])
-    .controller('ExerciseOverviewCtrl', [
-        'ExerciseService',
-        'UserPaperService',
-        ExerciseOverviewCtrl
-    ])
-    .controller('ExercisePlayerCtrl', [
-        '$location',
-        'step',
-        'paper',
-        'ExerciseService',
-        'FeedbackService',
-        'UserPaperService',
-        'TimerService',
-        ExercisePlayerCtrl
-    ])
-    .directive('exercise', [
-        ExerciseDirective
-    ])
-    .directive('buttonStart', [
-        StartButtonDirective
-    ])
-    .service('ExerciseService', [
-        '$http',
-        '$q',
-        ExerciseService
-    ])
-    .service('FeedbackService', [
-        FeedbackService
-    ])
+  .module('Exercise', [
+    'ui.translation',
+    'ui.bootstrap',
+    'ui.tinymce',
+    'ui.modal',
+    'mgcrea.ngStrap.datepicker',
+    'Common',
+    'Feedback',
+    'Step',
+    'Paper',
+    'Timer'
+  ])
+  .controller('ExerciseCtrl', [
+    'ExerciseService',
+    'PaperService',
+    'UserPaperService',
+    '$route',
+    ExerciseCtrl
+  ])
+  .controller('ExerciseMetadataCtrl', [
+    '$location',
+    'ExerciseService',
+    'TinyMceService',
+    ExerciseMetadataCtrl
+  ])
+  .controller('ExerciseOverviewCtrl', [
+    'ExerciseService',
+    'UserPaperService',
+    ExerciseOverviewCtrl
+  ])
+  .controller('ExercisePlayerCtrl', [
+    '$location',
+    'step',
+    'paper',
+    'ExerciseService',
+    'FeedbackService',
+    'UserPaperService',
+    'TimerService',
+    ExercisePlayerCtrl
+  ])
+  .directive('exercise', [
+    ExerciseDirective
+  ])
+  .directive('buttonStart', [
+    StartButtonDirective
+  ])
+  .service('ExerciseService', [
+    '$http',
+    '$q',
+    ExerciseService
+  ])
