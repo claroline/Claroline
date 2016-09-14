fa9e10887469de7b02fa60b9c2e9600382402dcb
Axel Penin <Elorfin@users.noreply.github.com>
2016-09-14 09:02:01 +0200

    [PathBundle] fixes save button always disabled after publish (#1149)
    remove console

--- plugin/path/Resources/modules/path/Controller/PathEditCtrl.js
+++ plugin/path/Resources/modules/path/Controller/PathEditCtrl.js
@@ -41,9 +41,7 @@ export default class PathEditCtrl extends PathBaseCtrl {
 
       if (!empty && updated) {
         // Initialization is already done, so mark path as unsaved for each modification
-        if (this.published !== true) {
-          this.unsaved = true
-        }
+        this.unsaved = true
       }
     }, true)
   }
