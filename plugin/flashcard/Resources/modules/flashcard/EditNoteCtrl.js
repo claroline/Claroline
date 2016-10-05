4f1c9fd03cca8f1c10f611b80cd4fd29a090140e
Rémy Taymans <remytaymans@gmail.com>
2016-10-05 16:28:35 +0200

    [FlashCardBundle] eslint corrections

--- plugin/flashcard/Resources/modules/flashcard/EditNoteCtrl.js
+++ plugin/flashcard/Resources/modules/flashcard/EditNoteCtrl.js
@@ -9,7 +9,7 @@
  */
 
 export default class EditNoteCtrl {
-  constructor (service, $routeParams, $location, $http) {
+  constructor (service, $routeParams, $location) {
     this.deck = service.getDeck()
     this.deckNode = service.getDeckNode()
     this.canEdit = service._canEdit
