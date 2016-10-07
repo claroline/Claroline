1d3ae2081ff45973a057a1423ab6146ff07d391d
Godfraind <nicolas.godfraind@gmail.com>
2016-10-07 11:55:54 +0200

    [CoreBundle] Model sharing working with groups. (#1344)

--- main/core/Form/WorkspaceType.php
+++ main/core/Form/WorkspaceType.php
@@ -37,47 +37,49 @@ class WorkspaceType extends AbstractType
         }
 
         if ($this->forApi) {
-            $codeConstraints = array(new NotBlank());
+            $codeConstraints = [new NotBlank()];
         } else {
-            $codeConstraints = array(new WorkspaceUniqueCode(), new NotBlank());
+            $codeConstraints = [new WorkspaceUniqueCode(), new NotBlank()];
         }
 
         $builder
             ->add(
                 'name',
                 'text',
-                array(
+                [
                     'label' => 'name',
                     'constraints' => new NotBlank(),
-                )
+                ]
             )
             ->add(
                 'code',
                 'text',
-                array(
+                [
                     'constraints' => $codeConstraints,
                     'label' => 'code',
-                )
+                ]
             )
             ->add(
                 'description',
                 isset($options['theme_options']['tinymce']) && !$options['theme_options']['tinymce'] ?
                     'textarea' :
                     'tinymce',
-                array('required' => false, 'label' => 'description')
+                ['required' => false, 'label' => 'description']
             );
 
         if (!$this->forApi) {
             $builder->add(
                     'model',
                     'entity',
-                    array(
+                    [
                         'class' => 'ClarolineCoreBundle:Model\WorkspaceModel',
                         'query_builder' => function (EntityRepository $er) use ($user) {
-
                             return $er->createQueryBuilder('wm')
-                                ->join('wm.users', 'u')
+                                ->leftJoin('wm.users', 'u')
+                                ->leftJoin('wm.groups', 'g')
+                                ->leftJoin('g.users', 'gu')
                                 ->where('u.id = :userId')
+                                ->orWhere('gu.id = :userId')
                                 ->setParameter('userId', $user->getId())
                                 ->orderBy('wm.name', 'ASC');
                         },
@@ -85,51 +87,51 @@ class WorkspaceType extends AbstractType
                         'required' => false,
                         'label' => 'model',
                         'mapped' => false,
-                    )
+                    ]
                 );
         }
         $builder
-                ->add('displayable', 'checkbox', array('required' => false, 'label' => 'displayable_in_workspace_list'))
-                ->add('selfRegistration', 'checkbox', array('required' => false, 'label' => 'public_registration'))
-                ->add('registrationValidation', 'checkbox', array('required' => false, 'label' => 'registration_validation'))
-                ->add('selfUnregistration', 'checkbox', array('required' => false, 'label' => 'public_unregistration'));
+                ->add('displayable', 'checkbox', ['required' => false, 'label' => 'displayable_in_workspace_list'])
+                ->add('selfRegistration', 'checkbox', ['required' => false, 'label' => 'public_registration'])
+                ->add('registrationValidation', 'checkbox', ['required' => false, 'label' => 'registration_validation'])
+                ->add('selfUnregistration', 'checkbox', ['required' => false, 'label' => 'public_unregistration']);
 
         if ($this->forApi) {
             $builder->add(
                 'maxStorageSize',
                 'text',
-                array(
+                [
                     'label' => 'max_storage_size',
-                    'constraints' => array(new NotBlank()),
-                )
+                    'constraints' => [new NotBlank()],
+                ]
             );
             $builder->add(
                 'maxUploadResources',
                 'text',
-                array(
+                [
                     'label' => 'max_amount_resources',
-                    'constraints' => array(new NotBlank()),
-                )
+                    'constraints' => [new NotBlank()],
+                ]
             );
             $builder->add(
                 'maxUsers',
                 'text',
-                array(
+                [
                     'label' => 'workspace_max_users',
-                    'constraints' => array(new NotBlank()),
-                )
+                    'constraints' => [new NotBlank()],
+                ]
             );
-            $params = array(
+            $params = [
                 'label' => 'expiration_date',
                 'format' => 'dd-MM-yyyy',
                 'widget' => 'single_text',
                 'input' => 'datetime',
-                'attr' => array(
+                'attr' => [
                     'class' => 'datepicker input-small',
                     'data-date-format' => 'dd-mm-yyyy',
                     'autocomplete' => 'off',
-                ),
-            );
+                ],
+            ];
 
             $builder->add('endDate', 'datepicker', $params);
         }
@@ -147,7 +149,7 @@ class WorkspaceType extends AbstractType
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $default = array('translation_domain' => 'platform');
+        $default = ['translation_domain' => 'platform'];
         if ($this->forApi) {
             $default['csrf_protection'] = false;
         }
