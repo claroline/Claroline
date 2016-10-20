d5b7507ac6228e0d8a6ccb66f211ad555eb1127e
Rémy Taymans <remytaymans@gmail.com>
2016-10-20 22:29:29 +0200

    [FlashCardBundle] Run php-cs

--- plugin/flashcard/Migrations/pdo_mysql/Version20161020110419.php
+++ plugin/flashcard/Migrations/pdo_mysql/Version20161020110419.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2016/10/20 11:04:36
  */
@@ -14,21 +14,21 @@ class Version20161020110419 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_fcbundle_card_learning CHANGE painfull painful TINYINT(1) NOT NULL
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_fcbundle_card_log CHANGE painfull painful TINYINT(1) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_fcbundle_card_learning CHANGE painful painfull TINYINT(1) NOT NULL
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_fcbundle_card_log CHANGE painful painfull TINYINT(1) NOT NULL
-        ");
+        ');
     }
 }
