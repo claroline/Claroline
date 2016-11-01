89c92c7b1ea3bd8164c5b239c8ce2e9f2c45057b
CPASimUSante <dechepy.olivier@chu-amiens.fr>
2016-11-01 10:34:26 +0100

    Fix user import script : missing conditionnal statement (#1500)

--- main/core/Manager/UserManager.php
+++ main/core/Manager/UserManager.php
@@ -507,7 +507,9 @@ class UserManager
             }
 
             if ($userEntity && $options['ignore-update']) {
-                $logger(" Skipping  {$userEntity->getUsername()}...");
+                if ($logger) {
+                    $logger(" Skipping  {$userEntity->getUsername()}...");
+                }
                 continue;
             }
 
@@ -594,8 +596,10 @@ class UserManager
         }
 
         $this->objectManager->endFlushSuite();
-        $logger($countCreated.' users created.');
-        $logger($countUpdated.' users updated.');
+        if ($logger) {
+            $logger($countCreated.' users created.');
+            $logger($countUpdated.' users updated.');
+        }
 
         return $returnValues;
     }
