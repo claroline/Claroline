ac1828e8b1068cdf3cc0c2dfc808dd2491edd0e2
Axel Penin <Elorfin@users.noreply.github.com>
2016-12-15 16:29:51 +0100

    [travis] ignore npm-shrinkwrap (#1684)
    * [travis] ignore npm-shrinkwrap
    
    * simplify code

--- main/dev/Resources/travis/fetch-deps.bash
+++ main/dev/Resources/travis/fetch-deps.bash
@@ -87,5 +87,9 @@ cp -r $DIST vendor/claroline/distribution
 echo "Building app/config/bundles.ini..."
 composer bundles
 
-fetch npm $NPM_SUM "npm install" node_modules
+# Gets npm dependencies
+# Removes the shrinkwrap to allow a PR to update dependencies
+# After installation, a new shrinkwrap is generated
+fetch npm $NPM_SUM "rm -f npm-shrinkwrap.json && npm install && npm shrinkwrap" node_modules
+
 fetch bower $BOWER_SUM "npm run bower" web/packages
