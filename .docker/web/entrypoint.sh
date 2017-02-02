b37f4f7a05313dbf77f5bd302cf3dcd55b50a727
Donovan Tengblad <purplefish32@gmail.com>
2017-02-02 18:11:50 +0100

    Docker install

--- .docker/web/entrypoint.sh
+++ .docker/web/entrypoint.sh
@@ -13,30 +13,38 @@ echo "MySQL is up"
 echo "Setting correct file permissions"
 chmod -R 777 app/cache app/config app/logs app/sessions files web/uploads
 
-echo "Executing configuration script"
-php scripts/configure.php
-
-echo "Composer install"
-composer sync-dev
-
-if [[ -v PLATFORM_NAME ]]; then
-  echo "Changing platform name to $PLATFORM_NAME";
-  sed -i "/name: claroline/c\name: $PLATFORM_NAME" app/config/platform_options.yml
-fi
-
-if [[ -v PLATFORM_SUPPORT_EMAIL ]]; then
-  echo "Changing platform support email to $PLATFORM_SUPPORT_EMAIL";
-  sed -i "/support_email: null/c\support_email: $PLATFORM_SUPPORT_EMAIL" app/config/platform_options.yml
-fi
-
-if [ -v ADMIN_FIRSTNAME ] && [ -v ADMIN_LASTNAME ] && [ -v ADMIN_USERNAME ] && [ -v ADMIN_PASSWORD ]  && [ -v ADMIN_EMAIL ]; then
-  echo '*********************************************************************************************************************'
-  echo "Creating default admin user : $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL"
-  echo '*********************************************************************************************************************'
-
-  php app/console claroline:user:create -a $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL
+if [ -f installed ]; then
+   echo "ClarolineConnect is allready installed"
 else
-  echo 'ClarolineConnect installed without an admin account'
+  echo "Executing configuration script"
+  php scripts/configure.php
+
+  echo "Composer install"
+  composer sync-dev
+
+  if [[ -v PLATFORM_NAME ]]; then
+    echo "Changing platform name to $PLATFORM_NAME";
+    sed -i "/name: claroline/c\name: $PLATFORM_NAME" app/config/platform_options.yml
+  fi
+
+  if [[ -v PLATFORM_SUPPORT_EMAIL ]]; then
+    echo "Changing platform support email to $PLATFORM_SUPPORT_EMAIL";
+    sed -i "/support_email: null/c\support_email: $PLATFORM_SUPPORT_EMAIL" app/config/platform_options.yml
+  fi
+
+  users = $(mysql $DB_NAME -u $DB_USER -p$DB_PASSWORD -h $DB_HOST -se "select count(*) from claro_user")
+
+  if [ "$users" -eq "0" ] && [ -v ADMIN_FIRSTNAME ] && [ -v ADMIN_LASTNAME ] && [ -v ADMIN_USERNAME ] && [ -v ADMIN_PASSWORD ]  && [ -v ADMIN_EMAIL ]; then
+    echo '*********************************************************************************************************************'
+    echo "Creating default admin user : $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL"
+    echo '*********************************************************************************************************************'
+
+    php app/console claroline:user:create -a $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL
+  else
+    echo 'ClarolineConnect installed without an admin account'
+  fi
+
+  touch installed
 fi
 
 exec "$@"
