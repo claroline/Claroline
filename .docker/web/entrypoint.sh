873d99a59bfa7af7b311a0664ba68deb1c756fce
Donovan Tengblad <purplefish32@gmail.com>
2017-02-02 11:42:42 +0100

    Docker Added admin user creation

--- .docker/web/entrypoint.sh
+++ .docker/web/entrypoint.sh
@@ -29,4 +29,14 @@ if [[ -v PLATFORM_SUPPORT_EMAIL ]]; then
   sed -i "/support_email: null/c\support_email: $PLATFORM_SUPPORT_EMAIL" app/config/platform_options.yml
 fi
 
+if [ -v ADMIN_FIRSTNAME ] && [ -v ADMIN_LASTNAME ] && [ -v ADMIN_USERNAME ] && [ -v ADMIN_PASSWORD ]  && [ -v ADMIN_EMAIL ]; then
+  echo '*********************************************************************************************************************'
+  echo "Creating default admin user : $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL"
+  echo '*********************************************************************************************************************'
+
+  php app/console claroline:user:create -a $ADMIN_FIRSTNAME $ADMIN_LASTNAME $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_EMAIL
+else
+  echo 'ClarolineConnect installed without an admin account'
+fi
+
 exec "$@"
