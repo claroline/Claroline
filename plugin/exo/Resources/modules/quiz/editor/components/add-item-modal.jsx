603f815a8fb88592c1701c26cdf81406f7ab7086
Guillou Patrick <pitrackster@users.noreply.github.com>
2017-02-06 15:18:54 +0100

    revamped add-item-modal (#1945)

--- plugin/exo/Resources/modules/quiz/editor/components/add-item-modal.jsx
+++ plugin/exo/Resources/modules/quiz/editor/components/add-item-modal.jsx
@@ -1,5 +1,6 @@
-import React, {PropTypes as T} from 'react'
+import React, {Component, PropTypes as T} from 'react'
 import Modal from 'react-bootstrap/lib/Modal'
+import classes from 'classnames'
 import {trans} from './../../../utils/translate'
 import {listItemMimeTypes, getDefinition} from './../../../items/item-types'
 import {Icon as ItemIcon} from './../../../items/components/icon.jsx'
@@ -7,32 +8,62 @@ import {BaseModal} from './../../../modal/components/base.jsx'
 
 export const MODAL_ADD_ITEM = 'MODAL_ADD_ITEM'
 
-export const AddItemModal = props =>
-  <BaseModal {...props} className="add-item-modal">
-    <Modal.Body>
-      <div role="listbox">
-        {listItemMimeTypes().map(type =>
-          <div
-            key={type}
-            className="modal-item-entry"
-            role="option"
-            onClick={() => props.handleSelect(type)}
-          >
-            <ItemIcon name={getDefinition(type).name} size="lg"/>
-            <div className="modal-item-desc">
-              <span className="modal-item-name">
-                {trans(getDefinition(type).name, {}, 'question_types')}
-              </span>
-              <p>
-                {trans(`${getDefinition(type).name}_desc`, {}, 'question_types')}
-              </p>
-            </div>
+class AddItemModal extends Component {
+  constructor(props) {
+    super(props)
+    const itemMimeTypes = listItemMimeTypes()
+
+    this.state = {
+      itemMimeTypes: itemMimeTypes,
+      currentType: itemMimeTypes[0],
+      currentName: trans(getDefinition(itemMimeTypes[0]).name, {}, 'question_types'),
+      currentDesc: trans(`${getDefinition(itemMimeTypes[0]).name}_desc`, {}, 'question_types')
+    }
+  }
+
+  handleItemMouseOver(type) {
+    const name = trans(getDefinition(type).name, {}, 'question_types')
+    const desc = trans(`${getDefinition(type).name}_desc`, {}, 'question_types')
+    this.setState({
+      currentType: type,
+      currentName: name,
+      currentDesc: desc
+    })
+  }
+
+  render() {
+    return (
+      <BaseModal {...this.props} className="add-item-modal">
+        <Modal.Body>
+          <div className="modal-item-list" role="listbox">
+            {this.state.itemMimeTypes.map(type =>
+              <div
+                key={type}
+                className={classes('modal-item-entry', {'selected': this.state.currentType === type})}
+                role="option"
+                onMouseOver={() => this.handleItemMouseOver(type)}
+                onClick={() => this.props.handleSelect(type)}
+              >
+                <ItemIcon name={getDefinition(type).name} size="lg"/>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </Modal.Body>
-  </BaseModal>
+          <div className="modal-item-desc">
+            <span className="modal-item-name">
+              {this.state.currentName}
+            </span>
+            <p>
+              {this.state.currentDesc}
+            </p>
+          </div>
+        </Modal.Body>
+      </BaseModal>
+    )
+  }
+}
 
 AddItemModal.propTypes = {
   handleSelect: T.func.isRequired
 }
+
+export {AddItemModal}
