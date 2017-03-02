f8faceb91b75ac03f50f9d3c420a185e70815936
kitan1982 <anhthao.pham@claroline.net>
2017-03-02 11:32:32 +0100

    Displays errors in fast-install

--- scripts/fast-install.php
+++ scripts/fast-install.php
@@ -1,3 +1,7 @@
 <?php
 
-@exec('ln -s '.__DIR__.'/../files/data '.__DIR__.'/../web');
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+exec('ln -s '.__DIR__.'/../files/data '.__DIR__.'/../web');
