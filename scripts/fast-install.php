61dab8099120a12bcc4d0bf9b596d255f59c71c6
kitan1982 <anhthao.pham@claroline.net>
2017-03-03 10:33:37 +0100

    Adds comments to fast-install script

--- scripts/fast-install.php
+++ scripts/fast-install.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This script creates a symlink to files/data directory into web directory.
+ *
+ * It is needed for the installation from a pre-built archive
+ * as this symlink cannot be generated correctly in the archive.
+ */
+
 set_error_handler(function ($severity, $message, $file, $line) {
     throw new ErrorException($message, 0, $severity, $file, $line);
 });
