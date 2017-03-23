5c7e67ab67af6311992ff4ad5b92a085b6ef4b32
Guillou Patrick <pitrackster@users.noreply.github.com>
2017-03-23 13:48:30 +0100

    [ExoBundle] exclude content items from question bank results (#2175)
    * exclude content items from question bank results
    
    * happy travis

--- plugin/exo/Serializer/Item/ItemSerializer.php
+++ plugin/exo/Serializer/Item/ItemSerializer.php
@@ -205,6 +205,15 @@ class ItemSerializer extends AbstractSerializer
             }
         }
 
+        // Sets the creator of the Item if not set
+        $creator = $question->getCreator();
+        if (empty($creator) || !($creator instanceof User)) {
+            $token = $this->tokenStorage->getToken();
+            if (!empty($token) && $token->getUser() instanceof User) {
+                $question->setCreator($token->getUser());
+            }
+        }
+
         // Force client ID if needed
         if (!in_array(Transfer::USE_SERVER_IDS, $options)) {
             $question->setUuid($data->id);
@@ -351,15 +360,6 @@ class ItemSerializer extends AbstractSerializer
     {
         if (isset($metadata->model)) {
             $question->setModel($metadata->model);
-        }
-
-        // Sets the creator of the Item if not set
-        $creator = $question->getCreator();
-        if (empty($creator) || !($creator instanceof User)) {
-            $token = $this->tokenStorage->getToken();
-            if (!empty($token) && $token->getUser() instanceof User) {
-                $question->setCreator($token->getUser());
-            }
         }
 
         if (isset($metadata->category)) {
