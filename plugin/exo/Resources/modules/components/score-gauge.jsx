1c2676e07004fe850b80ec15e2715f31ca4ca4ff
Guillou Patrick <pitrackster@users.noreply.github.com>
2017-04-25 13:13:28 +0200

    avoid to much decimals on scores (#2278)

--- plugin/exo/Resources/modules/components/score-gauge.jsx
+++ plugin/exo/Resources/modules/components/score-gauge.jsx
@@ -1,20 +1,24 @@
 import React, {PropTypes as T} from 'react'
 import classes from 'classnames'
 
-const ScoreGauge = props =>
-  <div className={classes(
-    'score-gauge',
-    'c100',
-    props.size,
-    'p'+(props.userScore ? Math.round((props.userScore / props.maxScore) * 100) : 0)
-  )}>
-    <span>{ (props.userScore || 0 === props.userScore ? props.userScore+'' : '-') + '/' + props.maxScore }</span>
+const ScoreGauge = props => {
+  const userScore = Math.round(props.userScore * 100) / 100
+  return (
+    <div className={classes(
+      'score-gauge',
+      'c100',
+      props.size,
+      'p'+(props.userScore ? Math.round((props.userScore / props.maxScore) * 100) : 0)
+    )}>
+      <span>{ (userScore || 0 === userScore ? userScore+'' : '-') + '/' + props.maxScore }</span>
 
-    <div className="slice" role="presentation">
-      <div className="bar" role="presentation"></div>
-      <div className="fill" role="presentation"></div>
+      <div className="slice" role="presentation">
+        <div className="bar" role="presentation"></div>
+        <div className="fill" role="presentation"></div>
+      </div>
     </div>
-  </div>
+  )
+}
 
 ScoreGauge.propTypes = {
   size: T.oneOf(['sm']),
