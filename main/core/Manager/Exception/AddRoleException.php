eb2686f47283626f57bbf4c10d4e34feed30b8e5
Godfraind <nicolas.godfraind@gmail.com>
2017-04-28 09:17:24 +0200

    [CoreBundle] Fixing the user creation limit check. (#2319)

--- main/core/Manager/Exception/AddRoleException.php
+++ main/core/Manager/Exception/AddRoleException.php
@@ -13,4 +13,25 @@ namespace Claroline\CoreBundle\Manager\Exception;
 
 class AddRoleException extends \Exception
 {
+    public function __construct($total, $toAdd, $max)
+    {
+        $this->total = $total;
+        $this->toAdd = $toAdd;
+        $this->max = $max;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getToAdd()
+    {
+        return $this->toAdd;
+    }
+
+    public function getMax()
+    {
+        return $this->max;
+    }
 }
