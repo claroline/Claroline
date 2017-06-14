72fd752798b79b26b86e11018481e946a3dba1a6
Godfraind <nicolas.godfraind@gmail.com>
2017-06-14 16:54:07 +0200

    [ExoBundle] Fixes for clozes for docimology (#2478)
    * hotfix from server
    
    * [ExoBundle] Fixing score computation if no answer.
    
    * [ExoBundle] More bugfix.
    
    * phpmd

--- plugin/exo/Library/Item/Definition/ClozeDefinition.php
+++ plugin/exo/Library/Item/Definition/ClozeDefinition.php
@@ -123,16 +123,18 @@ class ClozeDefinition extends AbstractDefinition
         if (!is_null($answer)) {
             foreach ($answer as $holeAnswer) {
                 $hole = $question->getHole($holeAnswer->holeId);
-                $keyword = $hole->getKeyword($holeAnswer->answerText);
-                if (!empty($keyword)) {
-                    if (0 < $keyword->getScore()) {
-                        $corrected->addExpected($keyword);
+                if ($hole) {
+                    $keyword = $hole->getKeyword($holeAnswer->answerText);
+                    if (!empty($keyword)) {
+                        if (0 < $keyword->getScore()) {
+                            $corrected->addExpected($keyword);
+                        } else {
+                            $corrected->addUnexpected($keyword);
+                        }
                     } else {
-                        $corrected->addUnexpected($keyword);
-                    }
-                } else {
-                    // Retrieve the best answer for the hole
+                        // Retrieve the best answer for the hole
                     $corrected->addMissing($this->findHoleExpectedAnswer($hole));
+                    }
                 }
             }
         } else {
@@ -190,7 +192,7 @@ class ClozeDefinition extends AbstractDefinition
                     // Increment the hole answers count
                     ++$holes[$holeAnswer->holeId]->answered;
 
-                    $keyword = $holesMap[$holeAnswer->holeId]->getKeyword($holeAnswer->answerText);
+                    $keyword = isset($holesMap[$holeAnswer->holeId]) ? $holesMap[$holeAnswer->holeId]->getKeyword($holeAnswer->answerText) : null;
                     if ($keyword) {
                         if (!isset($holes[$holeAnswer->holeId]->keywords[$keyword->getId()])) {
                             // Initialize the Hole keyword counter if it's the first time we find it
