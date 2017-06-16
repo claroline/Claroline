d3e5e5280ba65679230c3baa55f6a1308782ec8e
Godfraind <nicolas.godfraind@gmail.com>
2017-06-16 16:12:28 +0200

    Update ThemeManager.php (#2496)
    * Update ThemeManager.php
    
    Fixes administration theme error
    
    * phpmd

--- main/core/Manager/ThemeManager.php
+++ main/core/Manager/ThemeManager.php
@@ -188,4 +188,9 @@ class ThemeManager
 
         return count($themes) > 0 ? $themes[count($themes) - 1] : null;
     }
+
+    public static function listStockThemesName()
+    {
+        return self::$stockThemes;
+    }
 }
