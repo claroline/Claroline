812ceb91330388fa7fd35da18d0809aff48f039a
Godfraind <nicolas.godfraind@gmail.com>
2017-06-21 09:01:39 +0200

    [CoreBundle] Missing use and stuff for v10 upgrade (#2510)
    * Missing use and stuff
    
    * happy travis

--- main/core/Library/Installation/Updater/Updater100000.php
+++ main/core/Library/Installation/Updater/Updater100000.php
@@ -13,6 +13,7 @@
 namespace Claroline\CoreBundle\Library\Installation\Updater;
 
 use Claroline\CoreBundle\DataFixtures\PostInstall\Data\PostLoadRolesData;
+use Claroline\CoreBundle\Entity\Plugin;
 use Claroline\InstallationBundle\Updater\Updater;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -56,8 +57,8 @@ class Updater100000 extends Updater
             $plugin = new Plugin();
             $plugin->setBundleName('CoreBundle');
             $plugin->setVendorName('Claroline');
-            $this->persist($plugin);
-            $this->flush();
+            $this->om->persist($plugin);
+            $this->om->flush();
         } else {
             $this->log('CoreBundle already installed');
         }
