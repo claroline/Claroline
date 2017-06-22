2b15b9b41d6b523e975c6208be9be3d22a1e70f4
Godfraind <nicolas.godfraind@gmail.com>
2017-06-22 15:57:31 +0200

    Fixing translations module. (#2519)

--- main/core/Resources/modules/translation/module.js
+++ main/core/Resources/modules/translation/module.js
@@ -1,10 +1,10 @@
 import angular from 'angular/index'
 
-import {getTranslator} from './index'
+import {ClarolineTranslator} from './index'
 
 /**
  * Expose WillDurand JS translator as an angular service
  */
 angular
   .module('translation', [])
-  .service('Translator', () => getTranslator())
+  .service('Translator', () => ClarolineTranslator)
