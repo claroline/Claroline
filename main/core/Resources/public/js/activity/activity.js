d5ee3e926731206b1c93ba4ad10b8ece7b51909e
Godfraind <nicolas.godfraind@gmail.com>
2017-07-05 21:03:11 +0200

    [CoreBundle] Activity ressources iframe height fixed. (#2610)

--- main/core/Resources/public/js/activity/activity.js
+++ main/core/Resources/public/js/activity/activity.js
@@ -17,6 +17,18 @@
         }
     };
 
+    /**
+     * The the height of the activities
+     */
+    activity.setHeight = function() {
+      $('.claroline-app-iframe').each(function () {
+        activity.height(this);
+        $(this).on('load', function() {
+
+        })
+      })
+    }
+
     /** Events **/
 
     $('body').on('click', '.activity-tabs > li > a:not(.add-resource)', function () {
@@ -26,40 +38,15 @@
         $(this).parent().addClass('active');
         $('.activities > div').addClass('hide');
         $('#' + id).removeClass('hide');
-
-        $('#' + id + ' .activity-iframe').each(function () {
-            activity.height(this);
-        });
-    });
-
-    $('.activity-iframe').load(function () {
-        var iframe = this;
-        setTimeout(function () {
-            activity.height(iframe);
-        }, 50);
-    });
-
-    $(window).on('resize', function () {
-        clearTimeout(activity.iframeChange);
-        activity.iframeChange = setTimeout(function () {
-            $('.activity-iframe').each(function () {
-                activity.height(this);
-            });
-        }, 300);
     });
 
     $(document).ready(function () {
-        clearTimeout(activity.iframeChange);
-        activity.iframeChange = setTimeout(function () {
-            $('.activity-iframe').each(function () {
-                activity.height(this);
-            });
-        }, 300);
+      setInterval(activity.setHeight, 1000)
     });
-    
+
     $('#display-activity-evaluation-btn').on('click', function () {
         var activityParamsId = $(this).data('activity-params-id');
-        
+
         $.ajax({
             url: Routing.generate(
                 'claro_display_activity_evaluation',
