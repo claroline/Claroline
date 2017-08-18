1ca3155ca1462e182b7b325aeef24b96f2f0cf47
Anh Thao PHAM <anhthao.pham@claroline.net>
2017-08-18 17:19:34 +0200

    [10.x] Adds stats for Ordering items (#2786)
    * Adds stats for Ordering items
    
    * eslint

--- plugin/exo/Resources/modules/items/ordering/index.js
+++ plugin/exo/Resources/modules/items/ordering/index.js
@@ -21,8 +21,61 @@ function getCorrectedAnswer(item, answer = {data: []}) {
   return corrected
 }
 
-function generateStats() {
-  return {}
+function generateStats(item, papers, withAllParpers) {
+  const stats = {
+    orders: {},
+    unused: {},
+    unanswered: 0,
+    total: 0
+  }
+  Object.values(papers).forEach(p => {
+    if (withAllParpers || p.finished) {
+      let total = 0
+      let nbAnswered = 0
+      const unusedItems = {}
+      // compute the number of times the item is present in the structure of the paper
+      p.structure.steps.forEach(s => {
+        s.items.forEach(i => {
+          if (i.id === item.id) {
+            ++total
+            ++stats.total
+            i.items.forEach(choice => {
+              unusedItems[choice.id] = true
+            })
+          }
+        })
+      })
+      // compute the number of times the item has been answered
+      p.answers.forEach(a => {
+        if (a.questionId === item.id && a.data) {
+          ++nbAnswered
+          const unused = Object.assign({}, unusedItems)
+          let orderingKey = ''
+          a.data.forEach(d => {
+            orderingKey += d.itemId
+            unused[d.itemId] = false
+          })
+
+          if (!stats.orders[orderingKey]) {
+            stats.orders[orderingKey] = {
+              data: a.data,
+              count: 0
+            }
+          }
+          ++stats.orders[orderingKey].count
+
+          for (let key in unused) {
+            if (unused[key]) {
+              stats.unused[key] = stats.unused[key] ? stats.unused[key] + 1 : 1
+            }
+          }
+        }
+      })
+      stats.unanswered += total - nbAnswered
+    }
+  })
+
+  return stats
 }
 
 export default {
