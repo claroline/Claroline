5bc1e807aebaedf9f26a14be8d95a0d0c95bf32c
Godfraind <nicolas.godfraind@gmail.com>
2017-10-25 15:20:08 +0200

    [PathBundle] Fixing notification constructor. (#3164)

--- plugin/path/Listener/Notification/NotificationListener.php
+++ plugin/path/Listener/Notification/NotificationListener.php
@@ -4,13 +4,29 @@ namespace Innova\PathBundle\Listener\Notification;
 
 use Icap\NotificationBundle\Event\Notification\NotificationCreateDelegateViewEvent;
 use JMS\DiExtraBundle\Annotation as DI;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @DI\Service()
  */
-class NotificationListener extends ContainerAware
+class NotificationListener
 {
+    private $container;
+
+    /**
+     * PathListener constructor.
+     *
+     * @DI\InjectParams({
+     *     "container" = @DI\Inject("service_container")
+     * })
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     /**
      * @DI\Observe("create_notification_item_resource-innova_path-step_unlock")
      *
@@ -21,7 +37,8 @@ class NotificationListener extends ContainerAware
         $notificationView = $event->getNotificationView();
 
         $content = $this->container->get('templating')->render(
-            'InnovaPathBundle:Notification:notification_item.html.twig', [
+            'InnovaPathBundle:Notification:notification_item.html.twig',
+            [
                 'notification' => $notificationView->getNotification(),
                 'status' => $notificationView->getStatus(),
                 'systemName' => $event->getSystemName(),
@@ -44,7 +61,8 @@ class NotificationListener extends ContainerAware
         $notificationView = $event->getNotificationView();
 
         $content = $this->container->get('templating')->render(
-            'InnovaPathBundle:Notification:unlockdone.html.twig', [
+            'InnovaPathBundle:Notification:unlockdone.html.twig',
+            [
                 'notification' => $notificationView->getNotification(),
                 'status' => $notificationView->getStatus(),
                 'systemName' => $event->getSystemName(),
