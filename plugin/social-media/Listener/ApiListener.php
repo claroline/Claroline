c828c2d6a3d25956db65f8c44f291a9465286981
David Tisch <david.tischmacher@gmail.com>
2017-10-31 14:31:46 +0100

    [SocialMediaBundle ] Add likes count to serialized resource node (#3184)
    * Add likes count to serialized resource node
    
    * Display social information in appropriate location

---
+++ plugin/social-media/Listener/ApiListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Icap\SocialmediaBundle\Listener;
+
+use Claroline\CoreBundle\Event\Resource\DecorateResourceNodeEvent;
+use Claroline\CoreBundle\Persistence\ObjectManager;
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * Class ApiListener.
+ *
+ * @DI\Service
+ */
+class ApiListener
+{
+    /** @var ObjectManager */
+    private $om;
+
+    /**
+     * @DI\InjectParams({
+     *     "om" = @DI\Inject("claroline.persistence.object_manager")
+     * })
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    /**
+     * Add like count to serialized resource node when requested through API.
+     *
+     * @DI\Observe("serialize_resource_node")
+     */
+    public function onSerialize(DecorateResourceNodeEvent $event)
+    {
+        $count = $this->om->getRepository('IcapSocialmediaBundle:LikeAction')->countLikes([
+            'resource' => $event->getResourceNode()->getId(),
+        ]);
+
+        $event->add('social', [
+            'likes' => $count,
+        ]);
+    }
+}
