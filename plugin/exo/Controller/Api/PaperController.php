c0b22048e4e2b64ee0c803f80ff3909116f7a641
TSAVDARIS Panagiotis <ptsavdar@gmail.com>
2017-12-22 08:52:25 +0100

    Fixed papers csv export (#3336)

--- plugin/exo/Controller/Api/PaperController.php
+++ plugin/exo/Controller/Api/PaperController.php
@@ -229,10 +229,8 @@ class PaperController extends AbstractController
             throw new AccessDeniedException();
         }
 
-        $data = $this->exerciseManager->exportResultsToCsv($exercise);
-
-        return new StreamedResponse(function () use ($data) {
-            return $data;
+        return new StreamedResponse(function () use ($exercise) {
+            $this->exerciseManager->exportResultsToCsv($exercise);
         }, 200, [
             'Content-Type' => 'application/force-download',
             'Content-Disposition' => 'attachment; filename="export.csv"',
