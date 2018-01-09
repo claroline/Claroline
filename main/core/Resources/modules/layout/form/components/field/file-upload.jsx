b427771740fe8da1d27656f5732af86cf1765d59
Elorfin <penin.axel@gmail.com>
2018-01-09 10:57:41 +0100

    [CoreBundle] fixes file upload component

--- main/core/Resources/modules/layout/form/components/field/file-upload.jsx
+++ main/core/Resources/modules/layout/form/components/field/file-upload.jsx
@@ -1,68 +1,61 @@
 import React, {Component} from 'react'
-import {PropTypes as T} from 'prop-types'
+
+import {PropTypes as T, implementPropTypes} from '#/main/core/scaffolding/prop-types'
+import {FormField as FormFieldTypes} from '#/main/core/layout/form/prop-types'
+
 //this is not pretty
 import {connect} from 'react-redux'
-import {actions} from '#/main/core/data/form/actions.js'
+import {actions} from '#/main/core/data/form/actions'
 
-class File extends Component {
-  constructor(props) {
-    super(props)
-  }
-
+class FileInput extends Component
+{
   render() {
     return (
-      <fieldset>
-        <input
-          id={this.props.controlId}
-          type="file"
-          className="form-control"
-          accept={`${this.props.types.join(',')}`}
-          ref={input => this.input = input}
-          onChange={() => {
-            if (this.input.files[0]) {
-              const file = this.input.files[0]
-              //this.props.value = 'publicFile'
-              if (this.props.autoUpload) {
-                this.props.uploadFile(file, this.props.uploadUrl, this.props.onChange)
-              }
+      <input
+        id={this.props.id}
+        type="file"
+        className="form-control"
+        accept={`${this.props.types.join(',')}`}
+        ref={input => this.input = input}
+        onChange={() => {
+          if (this.input.files[0]) {
+            const file = this.input.files[0]
+            //this.props.value = 'publicFile'
+            if (this.props.autoUpload) {
+              this.props.uploadFile(file, this.props.uploadUrl, this.props.onChange)
             }
           }
-        }
-        />
-      </fieldset>
+        }}
+      />
     )
   }
 }
 
-File.propTypes = {
-  controlId: T.string.isRequired,
+implementPropTypes(FileInput, FormFieldTypes, {
   value: T.object,
-  disabled: T.bool.isRequired,
   types: T.arrayOf(T.string).isRequired,
   max: T.number.isRequired,
   autoUpload: T.bool.isRequired,
-  onChange: T.func.isRequired,
-  uploadUrl: T.array.isRequired,
+  uploadUrl: T.array,
   uploadFile: T.func.isRequired
-}
-
-File.defaultProps = {
-  disabled: false,
+}, {
   types: [],
   max: 1,
   autoUpload: true,
   onChange: () => {},
   uploadUrl: ['apiv2_file_upload']
-}
+})
 
 //this is not pretty
-const ConnectedFile = connect(
-  () => ({}),
-  dispatch => ({uploadFile(file, url, callback) {
-    dispatch(actions.uploadFile(file, url, callback))
-  }})
-)(File)
+const File = connect(
+  null,
+  dispatch => ({
+    uploadFile(file, url, callback) {
+      dispatch(actions.uploadFile(file, url, callback))
+    }
+  })
+)(FileInput)
 
 export {
-  ConnectedFile as File
+  File
 }
