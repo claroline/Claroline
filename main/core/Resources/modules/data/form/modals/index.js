66713c44a9d6c5481ecfd45ffa78408b0b6cfe74
Axel Penin <Elorfin@users.noreply.github.com>
2018-06-04 16:00:29 +0200

    [CoreBundle] registers data form modal (#4253)

--- main/core/Resources/modules/data/form/modals/index.js
+++ main/core/Resources/modules/data/form/modals/index.js
@@ -1,10 +1,13 @@
 import {registry} from '#/main/app/modals/registry'
 
-import {MODAL_CONFIGURE_FIELD, ConfigureFieldModal} from '#/main/core/data/form/modals/components/configure-field.jsx'
+import {MODAL_CONFIGURE_FIELD, ConfigureFieldModal} from '#/main/core/data/form/modals/components/configure-field'
+import {MODAL_DATA_FORM, DataFormModal} from '#/main/core/data/form/modals/components/data-form'
 
 // make the modal available for use
 registry.add(MODAL_CONFIGURE_FIELD, ConfigureFieldModal)
+registry.add(MODAL_DATA_FORM, DataFormModal)
 
 export {
-  MODAL_CONFIGURE_FIELD
+  MODAL_CONFIGURE_FIELD,
+  MODAL_DATA_FORM
 }
