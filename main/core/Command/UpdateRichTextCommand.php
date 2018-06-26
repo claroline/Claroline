41bd273d2855db3b79d44efacc16fccdd12cb420
Godfraind <nicolas.godfraind@gmail.com>
2018-06-26 15:41:23 +0200

    Rich Text updater (#4333)

--- main/core/Command/UpdateRichTextCommand.php
+++ main/core/Command/UpdateRichTextCommand.php
@@ -12,6 +12,7 @@
 namespace Claroline\CoreBundle\Command;
 
 use Claroline\AppBundle\Command\BaseCommandTrait;
+use Doctrine\Common\Util\ClassUtils;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -42,7 +43,53 @@ class UpdateRichTextCommand extends ContainerAwareCommand
                'f',
                InputOption::VALUE_NONE,
                'When set to true, no confirmation required'
+           )
+           ->addOption(
+               'all',
+               'a',
+               InputOption::VALUE_NONE,
+               'When set to true, all entities'
            );
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $params = [
+        'old_string' => 'The string to match',
+        'new_string' => 'The string to replace',
+    ];
+        foreach ($params as $argument => $argumentName) {
+            if (!$input->getArgument($argument)) {
+                $input->setArgument(
+                $argument, $this->askArgument($output, $argumentName)
+            );
+            }
+        }
+        $helper = $this->getHelper('question');
+        $entities = array_keys($this->getParsableEntities());
+        $question = new ChoiceQuestion('Entity to parse: (use \',\' as a separator) ', $entities);
+        $question->setMultiselect(true);
+        while (null === $entity = $input->getArgument('classes')) {
+            $entity = $helper->ask($input, $output, $question);
+            $input->setArgument('classes', $entity);
+        }
+    }
+
+    protected function askArgument(OutputInterface $output, $argumentName)
+    {
+        $argument = $this->getHelper('dialog')->askAndValidate(
+        $output,
+        "Enter the {$argumentName}: ",
+        function ($argument) {
+            if (empty($argument)) {
+                throw new \Exception('This argument is required');
+            }
+
+            return $argument;
+        }
+    );
+
+        return $argument;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -56,14 +103,16 @@ class UpdateRichTextCommand extends ContainerAwareCommand
         $escaped = addcslashes($toMatch, '%_');
 
         foreach ($classes as $class) {
-            $data = $em->getRepository($class)->createQueryBuilder('e')
-                ->where("e.{$parsable[$class]} LIKE :str")
-                ->setParameter('str', "%{$escaped}%")
-                ->getQuery()
-                ->getResult();
+            foreach ($parsable[$class] as $property) {
+                $data = $em->getRepository($class)->createQueryBuilder('e')
+                  ->where("e.{$property} LIKE :str")
+                  ->setParameter('str', "%{$escaped}%")
+                  ->getQuery()
+                  ->getResult();
 
-            if ($data) {
-                $entities = array_merge($entities, $data);
+                if ($data) {
+                    $entities = array_merge($entities, $data);
+                }
             }
         }
 
@@ -79,31 +128,35 @@ class UpdateRichTextCommand extends ContainerAwareCommand
 
         foreach ($entities as $entity) {
             $continue = false;
-            $func = 'get'.ucfirst($parsable[get_class($entity)]);
-            $text = $entity->$func();
 
-            if ($input->getOption('force')) {
-                $continue = true;
-            } else {
-                $infos = 'Class: '.get_class($entity)."\n";
-                $infos .= 'Id: '.$entity->getId()."\n";
-                $infos .= $text;
-                $output->writeln('<comment>'.$infos.'</comment>');
+            foreach ($parsable[ClassUtils::getRealClass(get_class($entity))] as $property) {
+                $func = 'get'.ucfirst($property);
+                $text = $entity->$func();
 
-                $helper = $this->getHelper('question');
-                $question = new ChoiceQuestion('Edit ?', ['yes', 'no']);
-                $answer = $helper->ask($input, $output, $question);
-                if ('yes' === $answer) {
+                if ($input->getOption('force')) {
                     $continue = true;
-                }
-            }
+                } else {
+                    $infos = 'Class: '.get_class($entity)."\n";
+                    $infos .= 'Id: '.$entity->getId()."\n";
+                    $infos .= $text;
+                    $output->writeln('<comment>'.$infos.'</comment>');
 
-            if ($continue) {
-                $text = str_replace($toMatch, $toReplace, $text);
-                $func = 'set'.ucfirst($parsable[get_class($entity)]);
-                $entity->$func($text);
-                $em->persist($entity);
-                ++$i;
+                    $helper = $this->getHelper('question');
+                    $question = new ChoiceQuestion('Edit ?', ['yes', 'no']);
+                    $answer = $helper->ask($input, $output, $question);
+
+                    if ('yes' === $answer) {
+                        $continue = true;
+                    }
+                }
+
+                if ($continue) {
+                    $text = str_replace($toMatch, $toReplace, $text);
+                    $func = 'set'.ucfirst($property);
+                    $entity->$func($text);
+                    $em->persist($entity);
+                    ++$i;
+                }
             }
         }
 
@@ -115,12 +168,16 @@ class UpdateRichTextCommand extends ContainerAwareCommand
     private function getParsableEntities()
     {
         return [
-            'Claroline\CoreBundle\Entity\Content' => 'content',
-            'Claroline\CoreBundle\Entity\Resource\Revision' => 'content',
-            'Claroline\AgendaBundle\Entity\Event' => 'description',
-            'Claroline\CoreBundle\Entity\Resource\Activity' => 'description',
-            'Innova\PathBundle\Entity\Path\Path' => 'description',
-            'Claroline\CoreBundle\Entity\Widget\SimpleTextConfig' => 'content',
+            'Claroline\CoreBundle\Entity\Content' => ['content'],
+            'Claroline\CoreBundle\Entity\Resource\Revision' => ['content'],
+            'Claroline\AgendaBundle\Entity\Event' => ['description'],
+            'Claroline\CoreBundle\Entity\Resource\Activity' => ['description'],
+            'Innova\PathBundle\Entity\Path\Path' => ['description'],
+            'Innova\PathBundle\Entity\Step' => ['description'],
+            'Claroline\CoreBundle\Entity\Widget\SimpleTextConfig' => ['content'],
+            'UJM\ExoBundle\Entity\Exercise' => ['endMessage'],
+            'UJM\ExoBundle\Entity\Item\Item' => ['content'],
+            'Claroline\ForumBundle\Entity\Message' => ['content'],
         ];
     }
 }
