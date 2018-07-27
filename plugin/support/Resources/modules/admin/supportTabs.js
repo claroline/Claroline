4309fc783b8bcb6014e863a0795a8e352838a296
Godfraind <nicolas.godfraind@gmail.com>
2018-07-27 11:44:21 +0200

    Removing SupportBundle (moved to FormaLibre repo) (#4484)

--- plugin/support/Resources/modules/admin/supportTabs.js
+++
@@ -1,8 +0,0 @@
-/*global Routing*/
-import $ from 'jquery'
-
-$('.close-ticket-tab-btn').on('click', function (e) {
-  e.preventDefault()
-  const ticketUserId = $(this).data('ticket-user-id')
-  window.location.href = Routing.generate('formalibre_admin_ticket_user_close', {ticketUser: ticketUserId})
-})
--- plugin/support/Resources/modules/support/supportTabs.js
+++
@@ -1,8 +0,0 @@
-/*global Routing*/
-import $ from 'jquery'
-
-$('.close-ticket-tab-btn').on('click', function (e) {
-  e.preventDefault()
-  const ticketId = $(this).data('ticket-id')
-  window.location.href = Routing.generate('formalibre_ticket_tab_close', {ticket: ticketId})
-})
