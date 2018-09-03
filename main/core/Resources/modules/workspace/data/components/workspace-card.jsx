29c3cc03621020e4f555e903eca83571f1d00fcb
ngodfraind <nicolas.godfraind@gmail.com>
2018-09-03 11:43:34 +0200

    Workspace thumbnail now visible

--- main/core/Resources/modules/workspace/data/components/workspace-card.jsx
+++ main/core/Resources/modules/workspace/data/components/workspace-card.jsx
@@ -11,7 +11,7 @@ const WorkspaceCard = props =>
   <DataCard
     {...props}
     id={props.data.id}
-    poster={props.data.thumbnail ? asset(props.data.thumbnail) : null}
+    poster={props.data.thumbnail ? asset(props.data.thumbnail.url) : null}
     icon="fa fa-book"
     title={props.data.name}
     subtitle={props.data.code}
