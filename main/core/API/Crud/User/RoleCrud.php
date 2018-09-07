58eb778c90c98c4e9e96e71a4633a625bb2614cc
Godfraind <nicolas.godfraind@gmail.com>
2018-09-07 14:15:54 +0200

    Create resource permissions on role creation. (#4670)

--- main/core/API/Crud/User/RoleCrud.php
+++ main/core/API/Crud/User/RoleCrud.php
@@ -6,6 +6,7 @@ use Claroline\AppBundle\Event\Crud\CreateEvent;
 use Claroline\AppBundle\Event\Crud\PatchEvent;
 use Claroline\AppBundle\Event\StrictDispatcher;
 use Claroline\CoreBundle\Entity\Role;
+use Doctrine\DBAL\Driver\Connection;
 use JMS\DiExtraBundle\Annotation as DI;
 
 /**
@@ -16,15 +17,19 @@ class RoleCrud
 {
     /**
      * @DI\InjectParams({
-     *     "dispatcher" = @DI\Inject("claroline.event.event_dispatcher")
+     *     "dispatcher" = @DI\Inject("claroline.event.event_dispatcher"),
+     *     "conn"       = @DI\Inject("doctrine.dbal.default_connection")
      * })
      *
      * @param StrictDispatcher $dispatcher
      */
-    public function __construct(StrictDispatcher $dispatcher)
-    {
+    public function __construct(
+        StrictDispatcher $dispatcher,
+        Connection $conn
+    ) {
         //too many dependencies, simplify this when we can
         $this->dispatcher = $dispatcher;
+        $this->conn = $conn;
     }
 
     /**
@@ -39,6 +44,28 @@ class RoleCrud
 
         if (!$role->getWorkspace()) {
             $role->setName(strtoupper('role_'.$role->getTranslationKey()));
+        }
+    }
+
+    /**
+     * @DI\Observe("crud_post_create_object_claroline_corebundle_entity_role")
+     *
+     * @param CreateEvent $event
+     */
+    public function postCreate(CreateEvent $event)
+    {
+        /** @var Role $role */
+        $role = $event->getObject();
+
+        if ($role->getWorkspace()) {
+            $sql = "
+              INSERT INTO claro_resource_rights (role_id, mask, resourceNode_id)
+              SELECT {$role->getId()}, 1, resource.id FROM claro_resource_node resource
+              WHERE resource.workspace_id = {$role->getWorkspace()->getId()}
+            ";
+
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
         }
     }
 
