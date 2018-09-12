30fa3f60430763c3c995a0d90918535caa5b6dcf
Godfraind <nicolas.godfraind@gmail.com>
2018-09-12 16:40:18 +0200

    Fixing file upload route (for retrocompatibility) (#4721)

--- main/core/Controller/FileController.php
+++ main/core/Controller/FileController.php
@@ -37,7 +37,7 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
- * @EXT\Route("/file", options={"expose"=true})
+ * @EXT\Route(options={"expose"=true})
  */
 class FileController extends AbstractApiController
 {
@@ -129,7 +129,7 @@ class FileController extends AbstractApiController
     }
 
     /**
-     * @EXT\Route("resource/media/{node}", name="claro_file_get_media")
+     * @EXT\Route("/resource/media/{node}", name="claro_file_get_media")
      * @EXT\Method("GET")
      *
      * @param ResourceNode $node
