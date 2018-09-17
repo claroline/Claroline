9e0ebbd65b8093bd21adfc58d769cb51e25aa820
Anh Thao PHAM <anhthao.pham@claroline.net>
2018-09-17 15:33:17 +0200

    Fixes home tabs & widgets deletion (#4765)
    * Adds voter for widget instance
    
    * Adds voter for widget container. closes #4752

---
+++ main/core/Security/Voter/Tool/Home/WidgetInstanceVoter.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\CoreBundle\Security\Voter\Tool\Home;
+
+use Claroline\CoreBundle\Entity\Tab\HomeTab;
+use Claroline\CoreBundle\Entity\Widget\WidgetInstance;
+use Claroline\CoreBundle\Security\Voter\AbstractVoter;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * @DI\Service
+ * @DI\Tag("security.voter")
+ */
+class WidgetInstanceVoter extends AbstractVoter
+{
+    public function checkPermission(TokenInterface $token, $object, array $attributes, array $options)
+    {
+        switch ($attributes[0]) {
+            case self::CREATE:
+            case self::EDIT:
+            case self::DELETE:
+                return $this->check($token, $object);
+      }
+
+        return VoterInterface::ACCESS_ABSTAIN;
+    }
+
+    public function check(TokenInterface $token, WidgetInstance $object)
+    {
+        $homeTab = $object->getContainer()->getHomeTab();
+
+        switch ($homeTab->getType()) {
+            case HomeTab::TYPE_ADMIN_DESKTOP:
+                if (!$this->isAdmin($token)) {
+                    return VoterInterface::ACCESS_DENIED;
+                }
+                break;
+            case HomeTab::TYPE_DESKTOP:
+                if ($homeTab->getUser() !== $token->getUser()) {
+                    return VoterInterface::ACCESS_DENIED;
+                }
+                break;
+            case HomeTab::TYPE_WORKSPACE:
+                return $this->isGranted(self::EDIT, $homeTab->getWorkspace()) ?
+                    VoterInterface::ACCESS_GRANTED :
+                    VoterInterface::ACCESS_DENIED;
+        }
+
+        return VoterInterface::ACCESS_ABSTAIN;
+    }
+
+    public function getClass()
+    {
+        return WidgetInstance::class;
+    }
+
+    public function getSupportedActions()
+    {
+        return[self::OPEN, self::VIEW, self::CREATE, self::EDIT, self::DELETE, self::PATCH];
+    }
+}
