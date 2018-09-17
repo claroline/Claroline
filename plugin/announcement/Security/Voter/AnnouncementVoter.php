6f717d98ead29c3e9b82260d137f539b88a48df7
Anh Thao PHAM <anhthao.pham@claroline.net>
2018-09-17 09:39:48 +0200

    Adds a voter for announcement. closes #4743 (#4758)

---
+++ plugin/announcement/Security/Voter/AnnouncementVoter.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\AnnouncementBundle\Security\Voter;
+
+use Claroline\CoreBundle\Security\Voter\AbstractVoter;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * @DI\Service
+ * @DI\Tag("security.voter")
+ */
+class AnnouncementVoter extends AbstractVoter
+{
+    public function checkPermission(TokenInterface $token, $object, array $attributes, array $options)
+    {
+        return $this->isGranted($attributes, $object->getAggregate()->getResourceNode());
+    }
+
+    public function getClass()
+    {
+        return 'Claroline\AnnouncementBundle\Entity\Announcement';
+    }
+
+    public function getSupportedActions()
+    {
+        return[self::OPEN, self::VIEW, self::CREATE, self::EDIT, self::DELETE, self::PATCH];
+    }
+}
