3497d25ecf8bb953f96261d04cab1ebb61d4e3da
Godfraind <nicolas.godfraind@gmail.com>
2018-09-25 17:30:40 +0200

    Resource types clean (#4840)
    * [CoreBundle] Remove unused resource types from database perms.
    
    * remove var_dump
    
    * Remove "NOT"
    
    * innova audio
    
    * more res in list

--- main/core/Library/Installation/AdditionalInstaller.php
+++ main/core/Library/Installation/AdditionalInstaller.php
@@ -165,6 +165,11 @@ class AdditionalInstaller extends BaseInstaller implements ContainerAwareInterfa
             $updater->setLogger($this->logger);
             $updater->postUpdate();
         }
+        if (version_compare($currentVersion, '12.0.15', '<')) {
+            $updater = new Updater\Updater120015($this->container, $this->logger);
+            $updater->setLogger($this->logger);
+            $updater->postUpdate();
+        }
 
         $termsOfServiceManager = $this->container->get('claroline.common.terms_of_service_manager');
         $termsOfServiceManager->sendDatas();
