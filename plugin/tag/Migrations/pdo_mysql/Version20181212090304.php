566f62c09767bf2d1f8b5ebff2c6bac24d8c4485
Axel Penin <Elorfin@users.noreply.github.com>
2018-12-13 14:37:52 +0100

    [TagBundle] removes duplicated tagged objects (#5288)

--- plugin/tag/Migrations/pdo_mysql/Version20181212090304.php
+++ plugin/tag/Migrations/pdo_mysql/Version20181212090304.php
@@ -17,7 +17,7 @@ class Version20181212090304 extends AbstractMigration
         $this->addSql('
             DROP INDEX `unique` ON claro_tagbundle_tagged_object
         ');
-        
+
         // fixes tags on forum subjects
         $this->addSql('
             UPDATE claro_tagbundle_tagged_object AS t 
@@ -25,7 +25,18 @@ class Version20181212090304 extends AbstractMigration
             SET t.object_id = f.uuid 
             WHERE t.object_id = "0" AND object_class = "Claroline\\ForumBundle\\Entity\\Subject"
         ');
-        
+
+        // removes duplicated tagged objects
+        $this->addSql('
+            DELETE t1 
+            FROM claro_tagbundle_tagged_object AS t1 
+            INNER JOIN claro_tagbundle_tagged_object AS t2 
+            WHERE t1.id < t2.id 
+            AND t1.object_class = t2.object_class 
+            AND t1.object_id = t2.object_id 
+            AND t1.tag_id = t2.tag_id
+        ');
+
         $this->addSql('
             CREATE UNIQUE INDEX `unique` ON claro_tagbundle_tagged_object (object_id, object_class, tag_id)
         ');
