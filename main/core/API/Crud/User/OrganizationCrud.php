3ca3b3aac2df9fdfe9b1f0e7ac40daae790311af
Godfraind <nicolas.godfraind@gmail.com>
2019-02-14 17:23:32 +0100

    [CoreBundle] Organization crypto keys crud. (#5511)

--- main/core/API/Crud/User/OrganizationCrud.php
+++ main/core/API/Crud/User/OrganizationCrud.php
@@ -7,8 +7,10 @@ use Claroline\AppBundle\Event\Crud\CreateEvent;
 use Claroline\AppBundle\Event\Crud\DeleteEvent;
 use Claroline\AppBundle\Event\Crud\PatchEvent;
 use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Cryptography\CryptographicKey;
 use Claroline\CoreBundle\Entity\Organization\Organization;
 use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Manager\CryptographyManager;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -20,16 +22,20 @@ class OrganizationCrud
 {
     /**
      * @DI\InjectParams({
-     *     "tokenStorage" = @DI\Inject("security.token_storage"),
-     *     "om"           = @DI\Inject("claroline.persistence.object_manager")
+     *     "tokenStorage"  = @DI\Inject("security.token_storage"),
+     *     "om"            = @DI\Inject("claroline.persistence.object_manager"),
+     *     "cryptoManager" = @DI\Inject("claroline.manager.cryptography_manager"),
+     *     "crud"          = @DI\Inject("claroline.api.crud")
      * })
      *
      * @param TokenStorageInterface $tokenStorage
      */
-    public function __construct(TokenStorageInterface $tokenStorage, ObjectManager $om)
+    public function __construct(TokenStorageInterface $tokenStorage, ObjectManager $om, CryptographyManager $cryptoManager, Crud $crud)
     {
         $this->tokenStorage = $tokenStorage;
         $this->om = $om;
+        $this->crud = $crud;
+        $this->cryptoManager = $cryptoManager;
     }
 
     /**
@@ -48,6 +54,20 @@ class OrganizationCrud
     }
 
     /**
+     * @DI\Observe("crud_post_create_object_claroline_corebundle_entity_organization_organization")
+     *
+     * @param CreateEvent $event
+     */
+    public function postCreate(CreateEvent $event)
+    {
+        $organization = $event->getObject();
+        $key = $this->cryptoManager->generatePair();
+        $key->setOrganization($organization);
+        $this->om->persist($key);
+        $this->om->flush();
+    }
+
+    /**
      * @DI\Observe("crud_pre_delete_object_claroline_corebundle_entity_organization_organization")
      *
      * @param DeleteEvent $event
@@ -60,6 +80,12 @@ class OrganizationCrud
             $event->block();
 
             // we can also throw an exception
+        }
+
+        $keys = $this->om->getRepository(CryptographicKey::class)->findBy(['organization' => $organization]);
+
+        foreach ($keys as $key) {
+            $this->crud->delete($key);
         }
     }
 
