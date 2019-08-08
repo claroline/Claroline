7572a4b6b81c79d1e7af0ca3a925e1df819366b9
Anh Thao PHAM <anhthao.pham@claroline.net>
2019-08-08 09:32:16 +0200

    Allows to send a message to users who have done a step (#6053)
    * Allows to send a message to users who have done a step
    
    * eslint

--- main/core/Resources/modules/tools/dashboard/path/modals/step-details/components/modal.jsx
+++ main/core/Resources/modules/tools/dashboard/path/modals/step-details/components/modal.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {PropTypes as T} from 'prop-types'
 import omit from 'lodash/omit'
+import cloneDeep from 'lodash/cloneDeep'
 
 import {trans} from '#/main/app/intl/translation'
 import {Modal} from '#/main/app/overlays/modal/components/modal'
@@ -10,40 +11,100 @@ import {
   TableRow,
   TableCell
 } from '#/main/core/layout/table/components/table'
+import {ModalButton} from '#/main/app/buttons/modal/containers/button'
 
-const StepDetailsModal = props =>
-  <Modal
-    {...omit(props, 'users')}
-    icon="fa fa-fw fa-user"
-    title={trans('users')}
-  >
-    <div className="modal-body">
-      <Table className="data-table">
-        <thead>
-          <TableRow>
-            <TableHeaderCell align={'left'}>
-              {trans('first_name')}
-            </TableHeaderCell>
-            <TableHeaderCell align={'left'}>
-              {trans('last_name')}
-            </TableHeaderCell>
-          </TableRow>
-        </thead>
-        <tbody>
-          {props.users.map((user,idx) =>
-            <TableRow key={idx}>
-              <TableCell align={'left'}>
-                {user.firstName}
-              </TableCell>
-              <TableCell align={'left'}>
-                {user.lastName}
-              </TableCell>
-            </TableRow>
-          )}
-        </tbody>
-      </Table>
-    </div>
-  </Modal>
+import {MODAL_USER_MESSAGE} from '#/main/core/user/modals/message'
+
+class StepDetailsModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selection: []
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        {...omit(this.props, 'users', 'sendMessage')}
+        icon="fa fa-fw fa-user"
+        title={trans('users')}
+      >
+        <div className="modal-body">
+          <Table className="data-table">
+            <thead>
+              <TableRow>
+                <TableHeaderCell align={'left'}>
+                  <input
+                    type="checkbox"
+                    id="checkbox-all"
+                    onChange={e => {
+                      if(e.target.checked) {
+                        this.setState({selection: this.props.users})
+                      } else {
+                        this.setState({selection: []})
+                      }
+                    }}
+                  />
+                </TableHeaderCell>
+                <TableHeaderCell align={'left'}>
+                  {trans('first_name')}
+                </TableHeaderCell>
+                <TableHeaderCell align={'left'}>
+                  {trans('last_name')}
+                </TableHeaderCell>
+              </TableRow>
+            </thead>
+            <tbody>
+              {this.props.users.map((user) =>
+                <TableRow key={`table-row-${user.id}`}>
+                  <TableCell align={'left'}>
+                    <input
+                      type="checkbox"
+                      id="checkbox-all"
+                      checked={-1 < this.state.selection.findIndex(u => u.id === user.id)}
+                      onChange={e => {
+                        const newSelection = cloneDeep(this.state.selection)
+                        const index = newSelection.findIndex(u => u.id === user.id)
+
+                        if (e.target.checked && -1 === index) {
+                          newSelection.push(user)
+                        } else if (!e.target.checked && -1 < index) {
+                          newSelection.splice(index, 1)
+                        }
+                        this.setState({selection: newSelection})
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell align={'left'}>
+                    {user.firstName}
+                  </TableCell>
+                  <TableCell align={'left'}>
+                    {user.lastName}
+                  </TableCell>
+                </TableRow>
+              )}
+            </tbody>
+          </Table>
+
+          <ModalButton
+            className="btn"
+            style={{marginTop: 10}}
+            primary={true}
+            disabled={0 === this.state.selection.length}
+            modal={[MODAL_USER_MESSAGE, {
+              to: this.state.selection
+            }]}
+          >
+            <span className="fa fa-fw fa-envelope icon-with-text-right" />
+            {trans('send_message')}
+          </ModalButton>
+        </div>
+      </Modal>
+    )
+  }
+}
 
 StepDetailsModal.propTypes = {
   users: T.arrayOf(T.shape({
