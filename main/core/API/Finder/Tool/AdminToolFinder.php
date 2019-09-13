60103439fc4fae71253b8b67fd9b926ecf6598b6
Elorfin <penin.axel@gmail.com>
2019-09-13 10:20:08 +0200

    excludes disabled from admin tools data source

--- main/core/API/Finder/Tool/AdminToolFinder.php
+++ main/core/API/Finder/Tool/AdminToolFinder.php
@@ -13,6 +13,7 @@ namespace Claroline\CoreBundle\API\Finder\Tool;
 
 use Claroline\AppBundle\API\Finder\AbstractFinder;
 use Claroline\CoreBundle\Entity\Tool\AdminTool;
+use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use Claroline\CoreBundle\Manager\PluginManager;
 use Doctrine\ORM\QueryBuilder;
 
@@ -20,15 +21,19 @@ class AdminToolFinder extends AbstractFinder
 {
     /** @var PluginManager */
     private $pluginManager;
+    /** @var PlatformConfigurationHandler */
+    private $configHandler;
 
     /**
      * AdminToolFinder constructor.
      *
-     * @param PluginManager $pluginManager
+     * @param PluginManager                $pluginManager
+     * @param PlatformConfigurationHandler $configHandler
      */
-    public function __construct(PluginManager $pluginManager)
+    public function __construct(PluginManager $pluginManager, PlatformConfigurationHandler $configHandler)
     {
         $this->pluginManager = $pluginManager;
+        $this->configHandler = $configHandler;
     }
 
     public function getClass()
@@ -44,12 +49,20 @@ class AdminToolFinder extends AbstractFinder
     ) {
         $bundles = $this->pluginManager->getEnabled(true);
 
+        // only grab tools from enabled plugins
         $qb->leftJoin('obj.plugin', 'p');
         $qb->andWhere($qb->expr()->orX(
             $qb->expr()->in('CONCAT(p.vendorName, p.bundleName)', ':bundles'),
             $qb->expr()->isNull('p')
         ));
         $qb->setParameter('bundles', $bundles);
+
+        // exclude disabled tools
+        $disabledAdmin = $this->configHandler->getParameter('security.disabled_admin_tools');
+        if (!empty($disabledAdmin)) {
+            $qb->andWhere('obj.name NOT IN (:disabledTools)');
+            $qb->setParameter('disabledTools', $disabledAdmin);
+        }
 
         foreach ($searches as $filterName => $filterValue) {
             switch ($filterName) {
