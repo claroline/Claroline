af566b121aaea6237749764843c31cecaa6478e9
Elorfin <penin.axel@gmail.com>
2019-09-20 11:13:29 +0200

    fixes community tool

--- main/core/Resources/modules/tools/community/parameters/components/tab.jsx
+++ main/core/Resources/modules/tools/community/parameters/components/tab.jsx
@@ -8,6 +8,7 @@ import {LINK_BUTTON} from '#/main/app/buttons'
 import {FormData} from '#/main/app/content/form/containers/data'
 import {ToolPage} from '#/main/core/tool/containers/page'
 
+import {route} from '#/main/core/workspace/routing'
 import {Workspace as WorkspaceTypes} from '#/main/core/workspace/prop-types'
 import {selectors} from '#/main/core/tools/community/parameters/store'
 
@@ -48,7 +49,7 @@ const ParametersTab = props => {
                 name: 'registration.url',
                 type: 'url',
                 label: trans('registration_url'),
-                calculated: () => url(['claro_workspace_subscription_url_generate', {slug: props.workspace.meta.slug}, true]),
+                calculated: (workspace) => `${url(['claro_index', {}, true])}#${route(props.workspace)}`,
                 required: true,
                 disabled: true
               }, {
