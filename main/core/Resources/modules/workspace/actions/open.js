b6f294a7d81f0fccfbdb8536bd965a6b087f662c
Elorfin <penin.axel@gmail.com>
2019-09-20 11:16:33 +0200

    removes rights check in workspace open action

--- main/core/Resources/modules/workspace/actions/open.js
+++ main/core/Resources/modules/workspace/actions/open.js
@@ -1,4 +1,3 @@
-import {hasPermission} from '#/main/app/security'
 import {trans} from '#/main/app/intl/translation'
 import {LINK_BUTTON} from '#/main/app/buttons'
 import {route} from '#/main/core/workspace/routing'
@@ -11,7 +10,6 @@ export default (workspaces) => ({
   type: LINK_BUTTON,
   label: trans('open', {}, 'actions'),
   primary: true,
-  displayed: hasPermission('open', workspaces[0]),
   icon: 'fa fa-fw fa-arrow-circle-o-right',
   target: route(workspaces[0]),
   scope: ['object'],
