8be119d02ae27d5e63302ce25ea5f2d1d3da71b1
Axel Penin <Elorfin@users.noreply.github.com>
2019-12-12 12:16:39 +0100

    fixes messages sent to deleted groups (#6491)
    * fixes messages sent to deleted groups
    
    * Update GroupRepository.php

--- main/core/Repository/GroupRepository.php
+++ main/core/Repository/GroupRepository.php
@@ -11,12 +11,12 @@
 
 namespace Claroline\CoreBundle\Repository;
 
-use Claroline\AppBundle\Persistence\MissingObjectException;
 use Claroline\CoreBundle\Entity\Group;
 use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class GroupRepository extends EntityRepository
 {
@@ -26,7 +26,7 @@ class GroupRepository extends EntityRepository
      * @param Workspace $workspace
      * @param bool      $executeQuery
      *
-     * @return array[Group]|Query
+     * @return Group[]|Query
      */
     public function findByWorkspace(Workspace $workspace, $executeQuery = true)
     {
@@ -49,10 +49,10 @@ class GroupRepository extends EntityRepository
     /**
      * Returns the groups which are member of a workspace.
      *
-     * @param array $workspace
+     * @param array $workspaces
      * @param bool  $executeQuery
      *
-     * @return array[Group]|Query
+     * @return Group[]|Query
      */
     public function findGroupsByWorkspaces(array $workspaces, $executeQuery = true)
     {
@@ -77,7 +77,7 @@ class GroupRepository extends EntityRepository
      * Returns the groups which are member of a workspace
      * and whose name corresponds the search.
      *
-     * @param array  $workspace
+     * @param array  $workspaces
      * @param string $search
      *
      * @return array[Group]
@@ -111,14 +111,15 @@ class GroupRepository extends EntityRepository
      *
      * @param bool   $executeQuery
      * @param string $orderedBy
+     * @param string $order
      *
-     * @return array[Group]|Query
+     * @return Group[]|Query
      */
     public function findAll($executeQuery = true, $orderedBy = 'id', $order = null)
     {
         if (!$executeQuery) {
             return $this->_em->createQuery(
-                "SELECT g, r, ws FROM Claroline\CoreBundle\Entity\Group g
+                "SELECT g, r, ws FROM Claroline\\CoreBundle\\Entity\\Group g
                  LEFT JOIN g.roles r
                  LEFT JOIN r.workspace ws
                  ORDER BY g.{$orderedBy} {$order}"
@@ -169,7 +170,7 @@ class GroupRepository extends EntityRepository
     {
         $dql = "
             SELECT g, r, ws
-            FROM Claroline\CoreBundle\Entity\Group g
+            FROM Claroline\\CoreBundle\\Entity\\Group g
             LEFT JOIN g.roles r
             LEFT JOIN r.workspace ws
             WHERE UPPER(g.name) LIKE :search
@@ -206,7 +207,7 @@ class GroupRepository extends EntityRepository
     /**
      * @param array $params
      *
-     * @return ArrayCollection
+     * @return array|ArrayCollection
      */
     public function extract($params)
     {
@@ -226,7 +227,7 @@ class GroupRepository extends EntityRepository
     public function findByRoles(array $roles, $getQuery = false, $orderedBy = 'id', $order = null)
     {
         $dql = "
-            SELECT u, ws, r FROM Claroline\CoreBundle\Entity\Group u
+            SELECT u, ws, r FROM Claroline\\CoreBundle\\Entity\\Group u
             JOIN u.roles r
             LEFT JOIN r.workspace ws
             WHERE r IN (:roles)
@@ -243,7 +244,7 @@ class GroupRepository extends EntityRepository
     {
         $search = strtoupper($name);
         $dql = "
-            SELECT u, ws, r FROM Claroline\CoreBundle\Entity\Group u
+            SELECT u, ws, r FROM Claroline\\CoreBundle\\Entity\\Group u
             JOIN u.roles r
             LEFT JOIN r.workspace ws
             WHERE r IN (:roles)
@@ -264,12 +265,9 @@ class GroupRepository extends EntityRepository
      * @param array $names
      *
      * @return Group[]
-     *
-     * @throws MissingObjectException if one or more groups cannot be found
      */
     public function findByNames(array $names)
     {
-        $nameCount = count($names);
         $dql = '
             SELECT g FROM Claroline\CoreBundle\Entity\Group g
             WHERE g.name IN (:names)
@@ -279,10 +277,6 @@ class GroupRepository extends EntityRepository
         $query->setParameter('names', $names);
 
         $result = $query->getResult();
-
-        if (($groupCount = count($result)) !== $nameCount) {
-            throw new MissingObjectException("{$groupCount} out of {$nameCount} groups were found");
-        }
 
         return $result;
     }
