aa2d788dc340c39d7dff15b56e2a1b9d7553e09b
Elorfin <penin.axel@gmail.com>
2019-12-17 10:38:36 +0100

    [VideoPlayerBundle] fixes player styles

--- plugin/video-player/Resources/modules/files/video/index.js
+++ plugin/video-player/Resources/modules/files/video/index.js
@@ -5,7 +5,8 @@ import {VideoPlayer} from '#/plugin/video-player/files/video/components/player'
 const fileType = {
   components: {
     player: VideoPlayer
-  }
+  },
+  styles: ['claroline-distribution-plugin-video-player-video-file']
 }
 
 export {
