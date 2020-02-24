77a9abad3f4f6f6f59c713006d8d9ca646b55621
Axel Penin <Elorfin@users.noreply.github.com>
2020-02-24 10:48:03 +0100

    [CoreBundle] fixes platform URL format

--- main/core/Library/Installation/Updater/Updater120532.php
+++ main/core/Library/Installation/Updater/Updater120532.php
@@ -11,12 +11,16 @@
 
 namespace Claroline\CoreBundle\Library\Installation\Updater;
 
+use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use Claroline\InstallationBundle\Updater\Updater;
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Updater120532 extends Updater
 {
+    /** @var PlatformConfigurationHandler */
+    private $config;
+
     /** @var Connection */
     private $conn;
 
@@ -24,11 +28,13 @@ class Updater120532 extends Updater
     {
         $this->logger = $logger;
         $this->conn = $container->get('doctrine.dbal.default_connection');
+        $this->config = $container->get(PlatformConfigurationHandler::class);
     }
 
     public function postUpdate()
     {
         $this->resetEvaluations();
+        $this->updatePlatformName();
     }
 
     private function resetEvaluations()
@@ -54,5 +60,17 @@ class Updater120532 extends Updater
             UPDATE claro_resource_user_evaluation SET nb_attempts = 0, nb_openings = 0
         ');
         $stmt->execute();
+    }
+
+    private function updatePlatformName()
+    {
+        $url = $this->config->getParameter('internet.platform_url');
+        if (!empty($url) && false === strpos('http', $url)) {
+            if ($this->config->getParameter('internet.ssl_enabled')) {
+                $this->config->setParameter('internet.platform_url', 'https://'.$url);
+            } else {
+                $this->config->setParameter('internet.platform_url', 'http://'.$url);
+            }
+        }
     }
 }
