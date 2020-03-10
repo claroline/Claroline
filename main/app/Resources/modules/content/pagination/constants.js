ea97d6b94bafffd3a698e19fcb7428c15ca241ee
Elorfin <penin.axel@gmail.com>
2020-03-10 15:42:27 +0100

    [CoreBundle] end fixes #5593 (5 results/page)

--- main/app/Resources/modules/content/pagination/constants.js
+++ main/app/Resources/modules/content/pagination/constants.js
@@ -1,6 +1,6 @@
 
-const AVAILABLE_PAGE_SIZES = [15, 30, 60, 120, -1] // -1 is for all
-const DEFAULT_PAGE_SIZE    = AVAILABLE_PAGE_SIZES[1]
+const AVAILABLE_PAGE_SIZES = [5, 15, 30, 60, 120, -1] // -1 is for all
+const DEFAULT_PAGE_SIZE    = AVAILABLE_PAGE_SIZES[2]
 
 export const constants = {
   AVAILABLE_PAGE_SIZES,
