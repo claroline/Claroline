2a893d3688fc9a590d7487eabdb977de3e8b49fb
Elorfin <penin.axel@gmail.com>
2020-05-22 11:33:07 +0200

    [CoreBundle] generates missing desktop rights on update

--- main/core/Library/Installation/Updater/Updater120543.php
+++ main/core/Library/Installation/Updater/Updater120543.php
@@ -11,6 +11,10 @@
 
 namespace Claroline\CoreBundle\Library\Installation\Updater;
 
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Role;
+use Claroline\CoreBundle\Entity\Tool\OrderedTool;
+use Claroline\CoreBundle\Entity\Tool\ToolRights;
 use Claroline\InstallationBundle\Updater\Updater;
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -20,15 +24,20 @@ class Updater120543 extends Updater
     /** @var Connection */
     private $conn;
 
+    /** @var ObjectManager */
+    private $om;
+
     public function __construct(ContainerInterface $container, $logger = null)
     {
         $this->logger = $logger;
         $this->conn = $container->get('doctrine.dbal.default_connection');
+        $this->om = $container->get(ObjectManager::class);
     }
 
     public function postUpdate()
     {
         $this->migrateDesktopToolsRights();
+        $this->addMissingDesktopRights();
     }
 
     private function migrateDesktopToolsRights()
@@ -47,6 +56,18 @@ class Updater120543 extends Updater
             ')
             ->execute();
 
+        // create rights for those who can access the tool
+        $this->conn
+            ->prepare('
+                INSERT INTO claro_tool_rights (role_id, mask, ordered_tool_id) 
+                SELECT r.id AS role_id, 1, ot.id 
+                FROM claro_tools_role AS tr
+                JOIN claro_tools AS t ON tr.tool_id = t.id
+                JOIN claro_ordered_tool AS ot ON (t.id = ot.tool_id AND ot.user_id IS NULL AND ot.workspace_id IS NULL)
+                WHERE tr.display IS NULL OR tr.display = "forced"
+            ')
+            ->execute();
+
         // create rights for those who cannot access the tool
         $this->conn
             ->prepare('
@@ -58,5 +79,45 @@ class Updater120543 extends Updater
                 WHERE tr.display = "hidden"
             ')
             ->execute();
+    }
+
+    private function addMissingDesktopRights()
+    {
+        $this->log('Add missing desktop tools rights...');
+
+        /** @var Role[] $platformRoles */
+        $platformRoles = $this->om->getRepository(Role::class)->findAllPlatformRoles();
+        /** @var OrderedTool[] $orderedTools */
+        $orderedTools = $this->om->getRepository(OrderedTool::class)->findByDesktop();
+        foreach ($orderedTools as $orderedTool) {
+            $results = $this->conn
+                ->query("SELECT * from claro_tools_role WHERE tool_id = {$orderedTool->getTool()->getId()}")
+                ->fetchAll();
+
+            foreach ($platformRoles as $platformRole) {
+                $found = false;
+                foreach ($results as $result) {
+                    if ($platformRole->getId() === $result['role_id']) {
+                        $found = true;
+                        break;
+                    }
+                }
+
+                if (!$found) {
+                    $rights = new ToolRights();
+                    $rights->setMask(1);
+                    $rights->setRole($platformRole);
+                    $rights->setOrderedTool($orderedTool);
+
+                    $orderedTool->addRight($rights);
+
+                    $this->om->persist($rights);
+                }
+            }
+
+            $this->om->persist($orderedTool);
+        }
+
+        $this->om->flush();
     }
 }
