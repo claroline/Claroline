fdaa6b4dc54ac8eed4c89a268fbbfeb9d4d6d718
Elorfin <penin.axel@gmail.com>
2020-06-25 15:03:41 +0200

    [ExoBundle] display full calendar for words item player

--- main/app/Resources/modules/content/components/help.jsx
+++ main/app/Resources/modules/content/components/help.jsx
@@ -4,7 +4,7 @@ import {PropTypes as T} from 'prop-types'
 import {toKey} from '#/main/core/scaffolding/text'
 
 const ContentHelp = props => Array.isArray(props.help) ?
-  <ul className="help-block-list">
+  <ul className="help-block-list" style={props.style}>
     {props.help.map(help =>
       <li key={toKey(help)} className="help-block">
         <span className="help-icon fa fa-fw fa-info-circle" />
@@ -13,12 +13,13 @@ const ContentHelp = props => Array.isArray(props.help) ?
     )}
   </ul>
   :
-  <div className="help-block">
+  <div className="help-block" style={props.style}>
     <span className="help-icon fa fa-fw fa-info-circle" />
     {props.help}
   </div>
 
 ContentHelp.propTypes = {
+  style: T.object,
   help: T.oneOfType([
     T.string,           // a single help message
     T.arrayOf(T.string) // a list of help messages
