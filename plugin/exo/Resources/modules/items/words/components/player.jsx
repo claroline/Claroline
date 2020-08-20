2df80c144a6ef2d13d8c8ffdbad4d6f584b8e012
Elorfin <penin.axel@gmail.com>
2020-08-20 13:44:25 +0200

    [ExoBundle] display date input for words item player

--- plugin/exo/Resources/modules/items/words/components/player.jsx
+++ plugin/exo/Resources/modules/items/words/components/player.jsx
@@ -1,30 +1,19 @@
 import React from 'react'
 import {PropTypes as T} from 'prop-types'
 
-import {trans, displayDate} from '#/main/app/intl'
+import {DateInput} from '#/main/app/data/types/date/components/input'
 import {HtmlInput} from '#/main/app/data/types/html/components/input'
-import {Calendar} from '#/main/core/layout/calendar/components/calendar'
-import {ContentHelp} from '#/main/app/content/components/help'
 
 const WordsPlayer = (props) => {
   switch (props.item.contentType) {
     case 'date':
       return (
-        <div className="text-center">
-          <h3 className="h4">{props.answer ? displayDate(props.answer) : trans('no_answer', {}, 'quiz')}</h3>
-
-          <ContentHelp
-            style={{textAlign: 'center'}}
-            help={trans('item_words_calendar_help', {}, 'quiz')}
-          />
-
-          <Calendar
-            id={`open-${props.item.id}-data`}
-            selected={props.answer}
-            disabled={props.disabled}
-            onChange={(value) => props.disabled ? false : props.onChange(value)}
-          />
-        </div>
+        <DateInput
+          id={`open-${props.item.id}-data`}
+          value={props.answer}
+          disabled={props.disabled}
+          onChange={(value) => props.disabled ? false : props.onChange(value)}
+        />
       )
 
     case 'text':
