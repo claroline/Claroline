614c10904cb41c3aeff9160a47ba4e32c59a7aa9
Elorfin <penin.axel@gmail.com>
2021-01-13 10:48:15 +0100

    [Badge] fixes assignation to organization

--- src/plugin/open-badge/Crud/BadgeClassCrud.php
+++ src/plugin/open-badge/Crud/BadgeClassCrud.php
@@ -4,6 +4,7 @@ namespace Claroline\OpenBadgeBundle\Crud;
 
 use Claroline\AppBundle\Event\Crud\CreateEvent;
 use Claroline\CoreBundle\API\Serializer\ParametersSerializer;
+use Claroline\CoreBundle\Entity\User;
 use Claroline\OpenBadgeBundle\Entity\BadgeClass;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -23,14 +24,24 @@ class BadgeClassCrud
         $this->parametersSerializer = $parametersSerializer;
     }
 
-    /**
-     * @param CreateEvent $event
-     */
     public function preCreate(CreateEvent $event)
     {
         /** @var BadgeClass $badge */
         $badge = $event->getObject();
-        $badge->setIssuer($this->tokenStorage->getToken()->getUser()->getMainOrganization());
+        if (empty($badge->getIssuer())) {
+            $organization = null;
+            if ($badge->getWorkspace()) {
+                if (count($badge->getWorkspace()->getOrganizations()) > 1) {
+                    $organization = $badge->getWorkspace()->getOrganizations()[0];
+                }
+            } else if ($this->tokenStorage->getToken()->getUser() instanceof User) {
+                $organization = $this->tokenStorage->getToken()->getUser()->getMainOrganization();
+            }
+
+            if (!empty($organization)) {
+                $badge->setIssuer($organization);
+            }
+        }
 
         if ($badge->getWorkspace()) {
             $badge->setEnabled($this->parametersSerializer->serialize()['badges']['enable_default']);
