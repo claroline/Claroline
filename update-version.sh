e7d3c5a77343d2d71bae8fc5f31560573479f3be
Robin Chalas <robin.chalas@gmail.com>
2021-01-21 17:57:12 +0100

    Create releases via Github Actions

--- update-version.sh
+++ update-version.sh
@@ -68,10 +68,10 @@ for COMMIT in $COMMITS
 do
     if [[ ${COMMITNAMES[$i]} == *"Merge"* ]]; then
         #we don't log them yet, but jenkins already do that
-        MERGESTRING="${MERGESTRING}\nclaroline/distribution@${COMMIT} - ${COMMITNAMES[$i]}"
+        MERGESTRING="${MERGESTRING}\nchalasr/claroline@${COMMIT} - ${COMMITNAMES[$i]}"
     else
         NAMELINKS=`echo ${COMMITNAMES[$i]} | sed -r 's/\(#([^)]*)\).*/[#\1]\(https:\/\/github.com\/claroline\/Distribution\/pull\/\1\)/'`
-        printf "[${COMMIT}](https://github.com/claroline/Distribution/commit/${COMMIT}) - ${NAMELINKS}  "$'\n' >> changelogs/${BRANCH_NAME}-${BASE_VERSION}.x.md
+        printf "[${COMMIT}](https://github.com/chalasr/Claroline/commit/${COMMIT}) - ${NAMELINKS}  "$'\n' >> changelogs/${BRANCH_NAME}-${BASE_VERSION}.x.md
     fi
     i=$((i + 1))
 done
