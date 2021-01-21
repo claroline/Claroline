596f4fd89c3387bca22df3d5071fceaed9e9cf4f
Elorfin <penin.axel@gmail.com>
2021-01-21 08:54:09 +0100

    [Dropzone] allows to delete user drops
    phpcs

---
+++ src/plugin/drop-zone/Installation/Migrations/pdo_mysql/Version20210120120829.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Claroline\DropZoneBundle\Installation\Migrations\pdo_mysql;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution.
+ *
+ * Generation date: 2021/01/20 12:08:30
+ */
+class Version20210120120829 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('
+            ALTER TABLE claro_dropzonebundle_dropzone 
+            ADD lock_drops TINYINT(1) NOT NULL
+        ');
+
+        $this->addSql('
+            UPDATE claro_dropzonebundle_dropzone SET lock_drops = 0
+        ');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('
+            ALTER TABLE claro_dropzonebundle_dropzone 
+            DROP lock_drops
+        ');
+    }
+}
