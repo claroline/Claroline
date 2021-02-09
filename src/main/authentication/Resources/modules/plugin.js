9296fb0bc5118f0a4efccf0faa2eaf00f4f5da67
Elorfin <penin.axel@gmail.com>
2021-02-09 08:16:25 +0100

    [Integration] fixes ips app alias

--- src/main/authentication/Resources/modules/plugin.js
+++ src/main/authentication/Resources/modules/plugin.js
@@ -7,8 +7,8 @@ import {registry} from '#/main/app/plugins/registry'
  */
 registry.add('ClarolineAuthenticationBundle', {
   integration: {
-    'tokens' : () => { return import(/* webpackChunkName: "authentication-integration-tokens" */ '#/main/authentication/integration/tokens')},
-    'ips' : () => { return import(/* webpackChunkName: "authentication-integration-tokens" */ '#/main/authentication/integration/ips')}
+    'tokens': () => { return import(/* webpackChunkName: "authentication-integration-tokens" */ '#/main/authentication/integration/tokens')},
+    'ips'   : () => { return import(/* webpackChunkName: "authentication-integration-ips" */    '#/main/authentication/integration/ips')}
   },
 
   /**
