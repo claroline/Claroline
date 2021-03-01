b229ddb1d02422d17fe687a8ffc3559f5c4573ca
Elorfin <penin.axel@gmail.com>
2021-03-01 06:50:23 +0100

    [Cursus] displays tags in course list

--- src/plugin/cursus/Resources/modules/course/components/card.jsx
+++ src/plugin/cursus/Resources/modules/course/components/card.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {PropTypes as T} from 'prop-types'
 import classes from 'classnames'
 import get from 'lodash/get'
+import isEmpty from 'lodash/isEmpty'
 
 import {trans} from '#/main/app/intl/translation'
 import {asset} from '#/main/app/config/asset'
@@ -25,6 +26,16 @@ const CourseCard = props =>
       get(props.data, 'restrictions.hidden')           && ['fa fa-eye-slash', trans('training_hidden', {}, 'cursus')],
       get(props.data, 'registration.selfRegistration') && ['fa fa-globe',     trans('training_public_registration', {}, 'cursus')]
     ].filter(flag => !!flag)}
+    footer={!isEmpty(props.data.tags) &&
+      <div className="tags">
+        {props.data.tags.map(tag =>
+          <span key={tag} className="tag label label-info">
+            <span className="fa fa-fw fa-tag icon-with-text-right" />
+            {tag}
+          </span>
+        )}
+      </div>
+    }
   />
 
 CourseCard.propTypes = {
