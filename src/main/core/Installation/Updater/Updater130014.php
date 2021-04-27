b0f1c4f6d96c9be96ba90482e155d824a7b2bd9d
Elorfin <penin.axel@gmail.com>
2021-04-27 08:57:06 +0200

    generates resource rights for ws managers

--- src/main/core/Installation/Updater/Updater130014.php
+++ src/main/core/Installation/Updater/Updater130014.php
@@ -3,8 +3,6 @@
 namespace Claroline\CoreBundle\Installation\Updater;
 
 use Claroline\AppBundle\Persistence\ObjectManager;
-use Claroline\CoreBundle\Entity\Tool\Tool;
-use Claroline\CoreBundle\Entity\Tool\ToolMaskDecoder;
 use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use Claroline\InstallationBundle\Updater\Updater;
 use Doctrine\DBAL\Connection;
@@ -44,6 +42,8 @@ class Updater130014 extends Updater
         /** @var Workspace[] $workspaces */
         $workspaces = $this->om->getRepository(Workspace::class)->findAll();
         foreach ($workspaces as $workspace) {
+            $this->log(sprintf('Processing Workspace "%s" (%s)', $workspace->getName(), $workspace->getUuid()));
+
             $sql = "
                 INSERT INTO claro_tool_rights (role_id, ordered_tool_id, mask)
                     (SELECT r.id, o.id, {$mask} AS mask
@@ -69,8 +69,55 @@ class Updater130014 extends Updater
     {
         $this->log('Generate resource rights for Workspace managers...');
 
-        // generate right
+        // For performances reason, we will set the same mask for all resources,
+        // this is the maximum rights found in platform atm, which is 255.
+        // It is the mask for the resource which has the more custom actions (eg. quiz, blog)
+        // For other resources extra bits will just be ignored by the decoder manager so it's ok to do it.
+        $mask = 255;
+
+        /** @var Workspace[] $workspaces */
+        $workspaces = $this->om->getRepository(Workspace::class)->findAll();
+        foreach ($workspaces as $workspace) {
+            $this->log(sprintf('Processing Workspace "%s" (%s)', $workspace->getName(), $workspace->getUuid()));
+
+            // generate right
+            $sql = "
+                INSERT INTO claro_resource_rights (role_id, resourceNode_id, mask)
+                    (SELECT r.id, n.id, {$mask} AS mask
+                    FROM (
+                        SELECT r.id
+                        FROM claro_role AS r
+                        WHERE r.name = 'ROLE_WS_MANAGER_{$workspace->getUuid()}'
+                    ) AS r, (
+                        SELECT n.id 
+                        FROM claro_resource_node AS n
+                        WHERE n.workspace_id = '{$workspace->getId()}'
+                    ) AS n)
+                ON DUPLICATE KEY UPDATE mask = {$mask}
+            ";
+
+            $stmt = $this->connection->prepare($sql);
+            $stmt->executeQuery();
+        }
 
         // generate creation list
+        $sql = "
+            INSERT IGNORE INTO claro_list_type_creation (resource_rights_id, resource_type_id)
+                (SELECT rr.id, t.id
+                FROM (
+                    SELECT rr.id
+                    FROM claro_resource_rights AS rr
+                    LEFT JOIN claro_role AS r ON (r.id = rr.role_id)
+                    LEFT JOIN claro_resource_node AS n ON (n.id = rr.resourceNode_id)
+                    WHERE r.name LIKE 'ROLE_WS_MANAGER_%'
+                      AND n.mime_type = 'custom/directory'
+                ) AS rr, (
+                    SELECT t.id 
+                    FROM claro_resource_type AS t
+                ) AS t)
+        ";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->executeQuery();
     }
 }
