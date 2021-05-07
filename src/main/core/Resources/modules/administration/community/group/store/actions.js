8ccf577cc0a37cbd9a4ecb17b571a1e0773df4e1
Elorfin <penin.axel@gmail.com>
2021-05-07 11:28:53 +0200

    [Community] fixes embedded lists invalidation

--- src/main/core/Resources/modules/administration/community/group/store/actions.js
+++ src/main/core/Resources/modules/administration/community/group/store/actions.js
@@ -10,16 +10,21 @@ import {Group as GroupTypes} from '#/main/core/user/prop-types'
 export const actions = {}
 
 actions.open = (formName, id = null) => (dispatch) => {
+  // invalidate embedded lists
+  dispatch(listActions.invalidateData(baseSelectors.STORE_NAME+'.groups.current.users'))
+  dispatch(listActions.invalidateData(baseSelectors.STORE_NAME+'.groups.current.organizations'))
+  dispatch(listActions.invalidateData(baseSelectors.STORE_NAME+'.groups.current.roles'))
+
   if (id) {
-    dispatch({
+    return dispatch({
       [API_REQUEST]: {
         url: ['apiv2_group_get', {id}],
         success: (response, dispatch) => dispatch(formActions.resetForm(formName, response, false))
       }
     })
-  } else {
-    dispatch(formActions.resetForm(formName, GroupTypes.defaultProps, true))
   }
+
+  return dispatch(formActions.resetForm(formName, GroupTypes.defaultProps, true))
 }
 
 actions.addUsers = (id, users) => ({
