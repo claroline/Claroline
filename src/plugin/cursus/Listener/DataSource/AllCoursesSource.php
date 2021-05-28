797a45ef2bfc1b376061dd73651987e2a7a6f79c
firepolo <anthony.pasche@gmail.com>
2021-05-28 11:20:31 +0200

    Add filtering by organisation

--- src/plugin/cursus/Listener/DataSource/AllCoursesSource.php
+++ src/plugin/cursus/Listener/DataSource/AllCoursesSource.php
@@ -13,21 +13,34 @@ namespace Claroline\CursusBundle\Listener\DataSource;
 
 use Claroline\AppBundle\API\FinderProvider;
 use Claroline\CoreBundle\Event\DataSource\GetDataEvent;
+use Claroline\CoreBundle\Entity\Organization\Organization;
 use Claroline\CursusBundle\Entity\Course;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class AllCoursesSource
 {
     /** @var FinderProvider */
     private $finder;
 
-    public function __construct(FinderProvider $finder)
-    {
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    public function __construct(
+        FinderProvider $finder,
+        TokenStorageInterface $tokenStorage
+    ) {
         $this->finder = $finder;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function getData(GetDataEvent $event)
     {
         $options = $event->getOptions();
+
+        $user = $this->tokenStorage->getToken()->getUser();
+        $options['organizations'] = array_map(function(Organization $organization) {
+            return $organization->getUuid();
+        }, $user->getOrganizations());
 
         $event->setData(
             $this->finder->search(Course::class, $options)
