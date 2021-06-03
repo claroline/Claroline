d9f108ca908f3c5baa595a47d50729b4d022e33a
Firepolo <anthony.pasche@gmail.com>
2021-06-03 11:11:33 +0200

    All courses (#1706)
    * Add data source configuration
    
    * Add data source translations
    
    * Add all course listener
    
    * fix name of listener
    
    * Add filtering by organisation
    
    * Remove feature from workspace context
    
    * Fill definition of course
    
    * Add authorisation
    
    * Fix error
    
    * Filter by default organisation
    
    * Fix default organization
    
    * Add space
    
    * Fix code style
    
    * Fix code style
    
    * Fix style code
    
    * Fix style php

---
+++ src/plugin/cursus/Listener/DataSource/AllCoursesSource.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\CursusBundle\Listener\DataSource;
+
+use Claroline\AppBundle\API\FinderProvider;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Organization\Organization;
+use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Event\DataSource\GetDataEvent;
+use Claroline\CursusBundle\Entity\Course;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class AllCoursesSource
+{
+    /** @var FinderProvider */
+    private $finder;
+
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    /** @var AuthorizationCheckerInterface */
+    private $authorization;
+
+    /** @var ObjectManager */
+    private $om;
+
+    public function __construct(
+        FinderProvider $finder,
+        ObjectManager $om,
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorization
+    ) {
+        $this->finder = $finder;
+        $this->om = $om;
+        $this->tokenStorage = $tokenStorage;
+        $this->authorization = $authorization;
+    }
+
+    public function getData(GetDataEvent $event)
+    {
+        $options = $event->getOptions();
+
+        if (!$this->authorization->isGranted('ROLE_ADMIN')) {
+            $user = $this->tokenStorage->getToken()->getUser();
+
+            $organizations = null;
+
+            if ($user instanceof User) {
+                $organizations = $user->getOrganizations();
+            } else {
+                $organizations = $this->om->getRepository(Organization::class)->findBy(['default' => true]);
+            }
+
+            $options['hiddenFilters']['organizations'] = array_map(function (Organization $organization) {
+                return $organization->getUuid();
+            }, $organizations);
+        }
+
+        $event->setData(
+            $this->finder->search(Course::class, $options)
+        );
+
+        $event->stopPropagation();
+    }
+}
--- src/plugin/cursus/Resources/modules/plugin.js
+++ src/plugin/cursus/Resources/modules/plugin.js
@@ -14,6 +14,7 @@ registry.add('ClarolineCursusBundle', {
       'training_session': () => { return import(/* webpackChunkName: "cursus-data-session" */ '#/plugin/cursus/data/types/session') }
     },
     sources: {
+      'all_courses': () => { return import(/* webpackChunkName: "cursus-data-all-courses" */ '#/plugin/cursus/data/sources/courses') },
       'public_course_sessions': () => { return import(/* webpackChunkName: "cursus-data-public-sessions" */ '#/plugin/cursus/data/sources/sessions') },
       'my_course_sessions'    : () => { return import(/* webpackChunkName: "cursus-data-my-sessions" */     '#/plugin/cursus/data/sources/my-sessions') }
     }
