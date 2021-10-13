33fb92b134b3f8f0636dfdb2da576b9b7c509ff0
Elorfin <penin.axel@gmail.com>
2021-10-13 10:32:22 +0200

    [Blog] adds meta in post card

--- src/plugin/blog/Resources/modules/data/components/post-card.jsx
+++ src/plugin/blog/Resources/modules/data/components/post-card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import get from 'lodash/get'
 
+import {displayDate, trans} from '#/main/app/intl'
 import {asset} from '#/main/app/config'
 import {getPlainText} from '#/main/app/data/types/html/utils'
 
 import {DataCard} from '#/main/app/data/components/card'
 import {UserAvatar} from '#/main/core/user/components/avatar'
+import {UserMicro} from '#/main/core/user/components/micro'
 
 const PostCard = (props) =>
   <DataCard
@@ -19,6 +21,24 @@ const PostCard = (props) =>
     }
     title={props.data.title}
     contentText={getPlainText(props.data.content)}
+    footer={
+      <span
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}
+      >
+        {props.data.meta.author ?
+          <UserMicro name={props.data.meta.author} /> :
+          <UserMicro {...props.data.meta.creator} />
+        }
+
+        {props.data.publicationDate ?
+          trans('published_at', {date: displayDate(props.data.publicationDate, false, true)}) : trans('not_published')
+        }
+      </span>
+    }
   />
 
 export {
