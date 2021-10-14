07680467cd05ba3bb06602eef73296b8f9d4f12e
firepolo <anthony.pasche@gmail.com>
2021-10-14 11:44:26 +0200

    fix limit of resume on courses & sessions

---
+++ src/plugin/cursus/Installation/Migrations/pdo_mysql/Version20211014093931.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Claroline\CursusBundle\Installation\Migrations\pdo_mysql;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated migration based on mapping information: modify it with caution.
+ *
+ * Generation date: 2021/10/14 09:39:34
+ */
+class Version20211014093931 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql("
+            ALTER TABLE claro_cursusbundle_course_session CHANGE plainDescription plainDescription LONGTEXT DEFAULT NULL
+        ");
+        $this->addSql("
+            ALTER TABLE claro_cursusbundle_course CHANGE plainDescription plainDescription LONGTEXT DEFAULT NULL
+        ");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("
+            ALTER TABLE claro_cursusbundle_course CHANGE plainDescription plainDescription VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci`
+        ");
+        $this->addSql("
+            ALTER TABLE claro_cursusbundle_course_session CHANGE plainDescription plainDescription VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci`
+        ");
+    }
+}
