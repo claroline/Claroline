12c53581da3b405a16a2931df590bd01d6519d20
Elorfin <penin.axel@gmail.com>
2021-10-18 14:37:28 +0200

    [Transfer] fixes user creation

--- src/main/core/API/Transfer/Action/User/Update.php
+++ src/main/core/API/Transfer/Action/User/Update.php
@@ -3,13 +3,73 @@
 namespace Claroline\CoreBundle\API\Transfer\Action\User;
 
 use Claroline\AppBundle\API\Transfer\Action\AbstractUpdateAction;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Group;
+use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Entity\User;
 
 class Update extends AbstractUpdateAction
 {
+    /** @var ObjectManager */
+    private $om;
+
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
     public function getAction()
     {
         return ['user', self::MODE_UPDATE];
+    }
+
+    public function execute(array $data, &$successData = [])
+    {
+        $groups = [];
+        if (isset($data['groups'])) {
+            foreach ($data['groups'] as $group) {
+                $object = $this->om->getObject($group, Group::class, array_keys($group));
+                if (!$object) {
+                    throw new \Exception('Group '.implode(',', $group).' does not exists');
+                }
+
+                $groups[] = $object;
+            }
+
+            // remove groups from input data to avoid the user serializer to process it
+            unset($data['groups']);
+        }
+
+        $roles = [];
+        if (isset($data['roles'])) {
+            foreach ($data['roles'] as $role) {
+                $object = $this->om->getObject($role, Role::class, array_keys($role));
+                if (!$object) {
+                    throw new \Exception('Role '.implode(',', $role).' does not exists');
+                }
+
+                $roles[] = $role;
+            }
+
+            // remove roles from input data to avoid the user serializer to process it
+            unset($data['roles']);
+        }
+
+        $user = $this->crud->update($this->getClass(), $data);
+        if ($user) {
+            if (!empty($groups)) {
+                $this->crud->patch($user, 'group', 'add', $groups);
+            }
+
+            if (!empty($roles)) {
+                $this->crud->patch($user, 'role', 'add', $roles);
+            }
+        }
+
+        $successData['update'][] = [
+            'data' => $data,
+            'log' => $this->getAction()[0].' updated.',
+        ];
     }
 
     public function getClass()
