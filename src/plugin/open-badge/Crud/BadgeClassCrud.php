c47a36079399469cb2c7eb6090c24e1696dac9b6
Elorfin <penin.axel@gmail.com>
2021-10-27 09:12:43 +0200

    [Badge] fixes default orga and assertion download

--- src/plugin/open-badge/Crud/BadgeClassCrud.php
+++ src/plugin/open-badge/Crud/BadgeClassCrud.php
@@ -2,32 +2,35 @@
 
 namespace Claroline\OpenBadgeBundle\Crud;
 
-use Claroline\AppBundle\Event\Crud\CreateEvent;
-use Claroline\CoreBundle\API\Serializer\ParametersSerializer;
+use Claroline\AppBundle\Event\Crud\CrudEvent;
 use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Manager\Organization\OrganizationManager;
 use Claroline\OpenBadgeBundle\Entity\BadgeClass;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class BadgeClassCrud
 {
-    /** @var ParametersSerializer */
-    private $parametersSerializer;
-
     /** @var TokenStorageInterface */
     private $tokenStorage;
+    /** @var OrganizationManager */
+    private $organizationManager;
 
     public function __construct(
         TokenStorageInterface $tokenStorage,
-        ParametersSerializer $parametersSerializer
+        OrganizationManager $organizationManager
     ) {
         $this->tokenStorage = $tokenStorage;
-        $this->parametersSerializer = $parametersSerializer;
+        $this->organizationManager = $organizationManager;
     }
 
-    public function preCreate(CreateEvent $event)
+    /**
+     * Auto link badge to the correct Organization if the user has not selected one.
+     */
+    public function checkOrganization(CrudEvent $event)
     {
         /** @var BadgeClass $badge */
         $badge = $event->getObject();
+
         if (empty($badge->getIssuer())) {
             $organization = null;
             if ($badge->getWorkspace()) {
@@ -40,11 +43,9 @@ class BadgeClassCrud
 
             if (!empty($organization)) {
                 $badge->setIssuer($organization);
+            } else {
+                $badge->setIssuer($this->organizationManager->getDefault(true));
             }
-        }
-
-        if ($badge->getWorkspace()) {
-            $badge->setEnabled($this->parametersSerializer->serialize()['badges']['enable_default']);
         }
     }
 }
