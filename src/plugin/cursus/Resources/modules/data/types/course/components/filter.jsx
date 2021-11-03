7cfd726c23682b3a3552f40b31360d470c861c8b
Elorfin <penin.axel@gmail.com>
2021-11-03 09:42:59 +0100

    [Training] adds search component for course data type

--- src/plugin/cursus/Resources/modules/data/types/course/components/filter.jsx
+++ src/plugin/cursus/Resources/modules/data/types/course/components/filter.jsx
@@ -7,20 +7,20 @@ import {trans} from '#/main/app/intl/translation'
 import {Button} from '#/main/app/action/components/button'
 import {CALLBACK_BUTTON, MODAL_BUTTON} from '#/main/app/buttons'
 
-import {MODAL_SESSIONS} from '#/plugin/cursus/modals/sessions'
+import {MODAL_COURSES} from '#/plugin/cursus/modals/courses'
 
-const SessionFilter = (props) =>
-  <span className="data-filter session-filter">
+const CourseFilter = (props) =>
+  <span className="data-filter course">
     {props.search}
 
     <Button
       className="btn btn-filter"
       type={MODAL_BUTTON}
       tooltip="left"
-      icon="fa fa-fw fa-calendar-week"
+      icon="fa fa-fw fa-graduation-cap"
       label={props.placeholder || trans('select', {}, 'actions')}
       size="sm"
-      modal={[MODAL_SESSIONS, {
+      modal={[MODAL_COURSES, {
         selectAction: (selected) => ({
           type: CALLBACK_BUTTON,
           label: trans('select', {}, 'actions'),
@@ -31,7 +31,7 @@ const SessionFilter = (props) =>
     />
   </span>
 
-implementPropTypes(SessionFilter, DataSearchTypes, {
+implementPropTypes(CourseFilter, DataSearchTypes, {
   /*search: T.shape({
    id: T.string.isRequired,
    name: T.string.isRequired
@@ -40,5 +40,5 @@ implementPropTypes(SessionFilter, DataSearchTypes, {
 })
 
 export {
-  SessionFilter
+  CourseFilter
 }
