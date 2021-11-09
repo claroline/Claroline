4da2f5fbc0cd81f76ac5dfdd6b74b5af2339dd7d
Elorfin <penin.axel@gmail.com>
2021-11-09 08:35:40 +0100

    [Text] adds PDF export

--- src/main/core/Controller/APINew/Resource/Types/TextController.php
+++ src/main/core/Controller/APINew/Resource/Types/TextController.php
@@ -13,13 +13,35 @@ namespace Claroline\CoreBundle\Controller\APINew\Resource\Types;
 
 use Claroline\AppBundle\Controller\AbstractCrudController;
 use Claroline\CoreBundle\Entity\Resource\Text;
+use Claroline\CoreBundle\Library\Normalizer\TextNormalizer;
+use Claroline\CoreBundle\Manager\Template\PlaceholderManager;
+use Claroline\CoreBundle\Security\PermissionCheckerTrait;
+use Dompdf\Dompdf;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * @Route("resource_text")
  */
 class TextController extends AbstractCrudController
 {
+    use PermissionCheckerTrait;
+
+    /** @var AuthorizationCheckerInterface */
+    private $authorization;
+    /** @var PlaceholderManager */
+    private $placeholderManager;
+
+    public function __construct(
+        AuthorizationCheckerInterface $authorization,
+        PlaceholderManager $placeholderManager
+    ) {
+        $this->authorization = $authorization;
+        $this->placeholderManager = $placeholderManager;
+    }
+
     public function getClass()
     {
         return Text::class;
@@ -33,5 +55,32 @@ class TextController extends AbstractCrudController
     public function getName()
     {
         return 'resource_text';
+    }
+
+    /**
+     * @Route("/{id}/pdf", name="apiv2_resource_text_download_pdf", methods={"GET"})
+     * @EXT\ParamConverter("text", class="Claroline\CoreBundle\Entity\Resource\Text", options={"mapping": {"id": "id"}})
+     */
+    public function downloadPdfAction(Text $text): StreamedResponse
+    {
+        $this->checkPermission('EXPORT', $text->getResourceNode(), [], true);
+
+        $domPdf = new Dompdf([
+            'isHtml5ParserEnabled' => true,
+            'isRemoteEnabled' => true,
+        ]);
+        $domPdf->loadHtml(
+            $this->placeholderManager->replacePlaceholders($text->getContent() ?? '')
+        );
+
+        // Render the HTML as PDF
+        $domPdf->render();
+
+        return new StreamedResponse(function () use ($domPdf) {
+            echo $domPdf->output();
+        }, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename='.TextNormalizer::toKey($text->getName()).'.pdf',
+        ]);
     }
 }
--- src/main/core/Resources/modules/resources/text/containers/resource.jsx
+++ src/main/core/Resources/modules/resources/text/containers/resource.jsx
@@ -1,11 +1,19 @@
-import {withRouter} from '#/main/app/router'
+import {connect} from 'react-redux'
+
 import {withReducer} from '#/main/app/store/components/withReducer'
+import {hasPermission} from '#/main/app/security'
+import {selectors as resourceSelectors} from '#/main/core/resource/store'
 
 import {TextResource as TextResourceComponent} from '#/main/core/resources/text/components/resource'
 import {reducer, selectors} from '#/main/core/resources/text/store'
 
-const TextResource = withRouter(
-  withReducer(selectors.STORE_NAME, reducer)(TextResourceComponent)
+const TextResource = withReducer(selectors.STORE_NAME, reducer)(
+  connect(
+    (state) => ({
+      canExport: hasPermission('export', resourceSelectors.resourceNode(state)),
+      text: selectors.text(state)
+    })
+  )(TextResourceComponent)
 )
 
 export {
