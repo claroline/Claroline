f31dd8b604cd1108874a16bde4e2f34cdccc3e00
Elorfin <penin.axel@gmail.com>
2021-11-09 08:35:40 +0100

    [Text] adds PDF export

--- src/main/core/Resources/modules/resources/text/components/resource.jsx
+++ src/main/core/Resources/modules/resources/text/components/resource.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import {PropTypes as T} from 'prop-types'
 
+import {trans} from '#/main/app/intl'
+import {URL_BUTTON} from '#/main/app/buttons'
 import {ResourcePage} from '#/main/core/resource/containers/page'
 
+import {Text as TextTypes} from '#/main/core/resources/text/prop-types'
 import {Player} from '#/main/core/resources/text/player/components/player'
 import {Editor} from '#/main/core/resources/text/editor/components/editor'
 
-const TextResource = () =>
+const TextResource = (props) =>
   <ResourcePage
+    customActions={[
+      {
+        name: 'export-pdf',
+        type: URL_BUTTON,
+        icon: 'fa fa-fw fa-file-pdf-o',
+        label: trans('export-pdf', {}, 'actions'),
+        displayed: props.canExport,
+        target: ['apiv2_resource_text_download_pdf', {id: props.text.id}],
+        group: trans('transfer')
+      }
+    ]}
     routes={[
       {
         path: '/',
@@ -18,6 +33,13 @@ const TextResource = () =>
       }
     ]}
   />
+
+TextResource.propTypes = {
+  canExport: T.bool.isRequired,
+  text: T.shape(
+    TextTypes.propTypes
+  ).isRequired
+}
 
 export {
   TextResource
