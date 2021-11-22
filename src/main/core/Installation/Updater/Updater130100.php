38cce80338497e6646e413e57eb15d0ba4f2f802
Elorfin <penin.axel@gmail.com>
2021-11-22 07:04:07 +0100

    [Template] fixes 13.1 upgrade

--- src/main/core/Installation/Updater/Updater130100.php
+++ src/main/core/Installation/Updater/Updater130100.php
@@ -3,7 +3,6 @@
 namespace Claroline\CoreBundle\Installation\Updater;
 
 use Claroline\AppBundle\Persistence\ObjectManager;
-use Claroline\CoreBundle\Entity\Template\TemplateType;
 use Claroline\CoreBundle\Entity\Tool\Tool;
 use Claroline\InstallationBundle\Updater\Updater;
 use Psr\Log\LoggerInterface;
@@ -23,33 +22,12 @@ class Updater130100 extends Updater
 
     public function preUpdate()
     {
-        $this->renameTemplateTool();
-
-        $this->renameTemplateType('claro_mail_layout', 'email_layout');
-        $this->renameTemplateType('claro_mail_registration', 'user_registration');
-        $this->renameTemplateType('claro_mail_validation', 'user_email_validation');
-    }
-
-    private function renameTemplateTool()
-    {
         $this->log('Renaming "templates_management" tool into "templates"`...');
 
         $tool = $this->om->getRepository(Tool::class)->findOneBy(['name' => 'templates_management']);
         if (!empty($tool)) {
             $tool->setName('templates');
             $this->om->persist($tool);
-            $this->om->flush();
-        }
-    }
-
-    private function renameTemplateType(string $oldName, string $newName)
-    {
-        $this->log(sprintf('Renaming "%s" template type into "%s"`...', $oldName, $newName));
-
-        $type = $this->om->getRepository(TemplateType::class)->findOneBy(['name' => $oldName]);
-        if (!empty($type)) {
-            $type->setName($newName);
-            $this->om->persist($type);
             $this->om->flush();
         }
     }
