d170d51b933ea32002864a56a4e04ca5a8a256fd
Elorfin <penin.axel@gmail.com>
2021-11-29 10:45:34 +0100

    [Template] fixes renaming

--- src/main/core/Resources/modules/administration/template/containers/tool.jsx
+++ src/main/core/Resources/modules/administration/template/containers/tool.jsx
@@ -1,13 +1,18 @@
 import {connect} from 'react-redux'
 
+import {actions as listActions} from '#/main/app/content/list/store'
+
 import {TemplateTool as TemplateToolComponent} from '#/main/core/administration/template/components/tool'
-import {actions} from '#/main/core/administration/template/store'
+import {actions, selectors} from '#/main/core/administration/template/store'
 
 const TemplateTool = connect(
   null,
   (dispatch) => ({
     open(id) {
       dispatch(actions.open(id))
+    },
+    invalidateList() {
+      dispatch(listActions.invalidateData(selectors.STORE_NAME + '.templates'))
     }
   })
 )(TemplateToolComponent)
