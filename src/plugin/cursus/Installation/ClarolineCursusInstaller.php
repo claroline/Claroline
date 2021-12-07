9e11ead94b7d07aeafd88f64a6be1052197c7fea
Elorfin <penin.axel@gmail.com>
2021-12-07 09:36:27 +0100

    [DataSource] adds icons and enhances some configs

--- src/plugin/cursus/Installation/ClarolineCursusInstaller.php
+++ src/plugin/cursus/Installation/ClarolineCursusInstaller.php
@@ -4,6 +4,7 @@ namespace Claroline\CursusBundle\Installation;
 
 use Claroline\CursusBundle\Installation\Updater\Updater130001;
 use Claroline\CursusBundle\Installation\Updater\Updater130013;
+use Claroline\CursusBundle\Installation\Updater\Updater130100;
 use Claroline\InstallationBundle\Additional\AdditionalInstaller;
 
 class ClarolineCursusInstaller extends AdditionalInstaller
@@ -13,6 +14,7 @@ class ClarolineCursusInstaller extends AdditionalInstaller
         return [
             '13.0.1' => Updater130001::class,
             '13.0.13' => Updater130013::class,
+            '13.1.0' => Updater130100::class,
         ];
     }
 
