efac6ab6872fa5949a5488d3347b6cfaa4d58522
Elorfin <penin.axel@gmail.com>
2022-01-28 11:02:08 +0100

    [PDF] fixes progression

--- src/plugin/pdf-player/Controller/UserEvaluationController.php
+++ src/plugin/pdf-player/Controller/UserEvaluationController.php
@@ -44,7 +44,7 @@ class UserEvaluationController
     /**
      * @Route("/{id}/progression/{page}/{total}", name="apiv2_pdf_progression_update", methods={"PUT"})
      * @EXT\ParamConverter("user", converter="current_user", options={"allowAnonymous"=false})
-     * @EXT\ParamConverter("pdf", class="Claroline\CoreBundle\Entity\Resource\File", options={"mapping": {"id": "id"}})
+     * @EXT\ParamConverter("pdf", class="Claroline\CoreBundle\Entity\Resource\File", options={"mapping": {"id": "uuid"}})
      *
      * @param int $page
      * @param int $total
