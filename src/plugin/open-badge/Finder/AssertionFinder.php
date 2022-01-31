d0e793ec04dacf9942686f42dd1f492aafaf7144
Elorfin <penin.axel@gmail.com>
2022-01-31 08:49:36 +0100

    [Badge] fixes assertions rights

--- src/plugin/open-badge/Finder/AssertionFinder.php
+++ src/plugin/open-badge/Finder/AssertionFinder.php
@@ -12,11 +12,27 @@
 namespace Claroline\OpenBadgeBundle\Finder;
 
 use Claroline\AppBundle\API\Finder\AbstractFinder;
+use Claroline\CoreBundle\Entity\Organization\Organization;
+use Claroline\CoreBundle\Entity\Tool\Tool;
+use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Manager\Tool\ToolMaskDecoderManager;
 use Claroline\OpenBadgeBundle\Entity\Assertion;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class AssertionFinder extends AbstractFinder
 {
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+    /** @var ToolMaskDecoderManager */
+    private $toolMaskDecoderManager;
+
+    public function __construct(TokenStorageInterface $tokenStorage, ToolMaskDecoderManager $toolMaskDecoderManager)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->toolMaskDecoderManager = $toolMaskDecoderManager;
+    }
+
     public static function getClass(): string
     {
         return Assertion::class;
@@ -25,6 +41,10 @@ class AssertionFinder extends AbstractFinder
     public function configureQueryBuilder(QueryBuilder $qb, array $searches = [], array $sortBy = null, array $options = [])
     {
         $userJoin = false;
+        $workspaceJoin = false;
+
+        /** @var User $user */
+        $user = $this->tokenStorage->getToken()->getUser();
 
         foreach ($searches as $filterName => $filterValue) {
             switch ($filterName) {
@@ -34,8 +54,12 @@ class AssertionFinder extends AbstractFinder
                     $qb->setParameter('badge', $filterValue);
                     break;
                 case 'workspace':
-                    $qb->join('obj.badge', 'b');
-                    $qb->join('b.workspace', 'w');
+                    if (!$workspaceJoin) {
+                        $qb->leftJoin('obj.badge', 'b');
+                        $qb->join('b.workspace', 'w');
+                        $workspaceJoin = true;
+                    }
+
                     $qb->andWhere('w.uuid = :workspace');
                     $qb->setParameter('workspace', $filterValue);
                     break;
@@ -47,6 +71,58 @@ class AssertionFinder extends AbstractFinder
                     }
                     $qb->andWhere('u.uuid = :user');
                     $qb->setParameter('user', $filterValue);
+                    break;
+                case 'fromGrantableBadges':
+                    $grantDecoder = $this->toolMaskDecoderManager->getMaskDecoderByToolAndName(
+                        $this->om->getRepository(Tool::class)->findOneBy(['name' => 'badges']),
+                        'grant'
+                    );
+
+                    if (!$workspaceJoin) {
+                        $qb->leftJoin('obj.badge', 'b');
+                        $qb->join('b.workspace', 'w');
+                        $workspaceJoin = true;
+                    }
+
+                    if (!$userJoin) {
+                        $qb->join('obj.recipient', 'u');
+                        $userJoin = true;
+                    }
+
+                    $qb->leftJoin('b.issuer', 'o');
+
+                    $subQb = $this->om->createQueryBuilder()
+                        ->select('ot')
+                        ->from('Claroline\CoreBundle\Entity\Tool\OrderedTool', 'ot')
+                        ->leftJoin('ot.workspace', 'otw')
+                        ->join('ot.rights', 'r')
+                        ->join('r.role', 'rr')
+                        ->where('ot.user IS NULL')
+                        ->andWhere('((w.id IS NULL AND ot.workspace IS NULL) OR otw.id = w.id)')
+                        ->andWhere('BIT_AND(r.mask, :grantMask) = :grantMask')
+                        ->andWhere('rr.name IN (:userRoles)')
+                        ->getDQL()
+                    ;
+
+                    $qb->andWhere($qb->expr()->orX(
+                        // always assignable by organization managers
+                        $qb->expr()->in('o.id', array_map(function (Organization $organization) {
+                            return $organization->getId();
+                        }, $user->getAdministratedOrganizations()->toArray())),
+
+                        // assignable by users with GRANT rights on the tool
+                        $qb->expr()->exists($subQb),
+
+                        // assignable by owners of the badge if enabled
+                        $qb->expr()->andX(
+                            $qb->expr()->eq('u.id', $user->getId()),
+                            $qb->expr()->eq('b.issuingPeer', true)
+                        )
+                    ))
+                        ->setParameter(':grantMask', $grantDecoder->getValue())
+                        ->setParameter(':userRoles', $this->tokenStorage->getToken()->getRoleNames())
+                    ;
+
                     break;
                 default:
                   $this->setDefaults($qb, $filterName, $filterValue);
