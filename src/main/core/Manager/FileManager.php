cad940946c8907f20e85718236367ef9901aa58b
Axel Penin <Elorfin@users.noreply.github.com>
2022-02-02 15:18:33 +0100

    Update FileManager.php

--- src/main/core/Manager/FileManager.php
+++ src/main/core/Manager/FileManager.php
@@ -38,12 +38,11 @@ class FileManager
 
     public function isStorageFull(): bool
     {
-        return false;
-        
         // TODO : enable when our storage management is fixed
-        //return $this->config->getParameter('restrictions.storage')
-        //    && $this->config->getParameter('restrictions.used_storage')
-        //    && $this->config->getParameter('restrictions.used_storage') >= $this->config->getParameter('restrictions.storage');
+        return false
+            && $this->config->getParameter('restrictions.storage')
+            && $this->config->getParameter('restrictions.used_storage')
+            && $this->config->getParameter('restrictions.used_storage') >= $this->config->getParameter('restrictions.storage');
     }
 
     /**
