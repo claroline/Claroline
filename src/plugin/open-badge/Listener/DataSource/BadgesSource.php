08ca2930acfc1c5d7577d2ca225e6b60f2c49611
Elorfin <penin.axel@gmail.com>
2022-03-30 11:02:45 +0200

    [Badge] filters list by organizations

--- src/plugin/open-badge/Listener/DataSource/BadgesSource.php
+++ src/plugin/open-badge/Listener/DataSource/BadgesSource.php
@@ -4,19 +4,29 @@ namespace Claroline\OpenBadgeBundle\Listener\DataSource;
 
 use Claroline\AppBundle\API\FinderProvider;
 use Claroline\CoreBundle\Entity\DataSource;
+use Claroline\CoreBundle\Entity\Organization\Organization;
+use Claroline\CoreBundle\Entity\User;
 use Claroline\CoreBundle\Event\DataSource\GetDataEvent;
 use Claroline\OpenBadgeBundle\Entity\BadgeClass;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class BadgesSource
 {
+    /** @var AuthorizationCheckerInterface */
+    private $authorization;
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
     /** @var FinderProvider */
     private $finder;
 
-    /**
-     * BadgesSource constructor.
-     */
-    public function __construct(FinderProvider $finder)
-    {
+    public function __construct(
+        AuthorizationCheckerInterface $authorization,
+        TokenStorageInterface $tokenStorage,
+        FinderProvider $finder
+    ) {
+        $this->authorization = $authorization;
+        $this->tokenStorage = $tokenStorage;
         $this->finder = $finder;
     }
 
@@ -27,6 +37,16 @@ class BadgesSource
 
         if (DataSource::CONTEXT_WORKSPACE === $event->getContext()) {
             $options['hiddenFilters']['workspace'] = $event->getWorkspace()->getUuid();
+        } elseif (!$this->authorization->isGranted('ROLE_ADMIN')) {
+            // only display badges for the current user organization
+            $options['hiddenFilters']['organizations'] = [];
+
+            $user = $this->tokenStorage->getToken()->getUser();
+            if ($user instanceof User) {
+                $options['hiddenFilters']['organizations'] = array_map(function (Organization $organization) {
+                    return $organization->getUuid();
+                }, $user->getOrganizations());
+            }
         }
 
         $event->setData(
