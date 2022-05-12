9b195adf9f6205cb45a53f9899940bbaf75778ef
Elorfin <penin.axel@gmail.com>
2022-05-12 10:30:21 +0200

    [Parameters] enhances translations

--- src/main/app/Resources/modules/layout/sections/home/constants.js
+++ src/main/app/Resources/modules/layout/sections/home/constants.js
@@ -6,10 +6,10 @@ const HOME_TYPE_HTML = 'html'
 const HOME_TYPE_TOOL = 'tool'
 
 const HOME_TYPES  = {
-  [HOME_TYPE_NONE]: trans('none'),
+  [HOME_TYPE_NONE]: trans('platform_home_none'),
   [HOME_TYPE_URL]: trans('url'),
   [HOME_TYPE_HTML]: trans('html'),
-  [HOME_TYPE_TOOL]: trans('tool')
+  [HOME_TYPE_TOOL]: trans('platform_home_tool')
 }
 
 export const constants = {
