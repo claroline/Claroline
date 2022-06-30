e89d621c68824dbc230fb4ec9a932c1ef62f5867
Elorfin <penin.axel@gmail.com>
2022-06-30 09:05:35 +0200

    [ClacoForm] fixes entry exports

--- src/plugin/claco-form/Controller/ClacoFormController.php
+++ src/plugin/claco-form/Controller/ClacoFormController.php
@@ -15,7 +15,6 @@ use Claroline\AppBundle\API\FinderProvider;
 use Claroline\AppBundle\API\Options;
 use Claroline\AppBundle\API\SerializerProvider;
 use Claroline\AppBundle\Controller\RequestDecoderTrait;
-use Claroline\AppBundle\Manager\PdfManager;
 use Claroline\AppBundle\Persistence\ObjectManager;
 use Claroline\ClacoFormBundle\Entity\ClacoForm;
 use Claroline\ClacoFormBundle\Entity\Comment;
@@ -23,13 +22,11 @@ use Claroline\ClacoFormBundle\Entity\Entry;
 use Claroline\ClacoFormBundle\Entity\EntryUser;
 use Claroline\ClacoFormBundle\Entity\Field;
 use Claroline\ClacoFormBundle\Manager\ClacoFormManager;
+use Claroline\ClacoFormBundle\Manager\ExportManager;
 use Claroline\CoreBundle\Entity\Facet\FieldFacet;
 use Claroline\CoreBundle\Entity\User;
-use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use Claroline\CoreBundle\Library\Normalizer\TextNormalizer;
-use Claroline\CoreBundle\Manager\LocationManager;
 use Claroline\CoreBundle\Security\PermissionCheckerTrait;
-use Ramsey\Uuid\Uuid;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -38,8 +35,6 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use Twig\Environment;
 
 /**
  * @Route("/claco/form", options = {"expose"=true})
@@ -57,47 +52,31 @@ class ClacoFormController
     private $finder;
     /** @var ClacoFormManager */
     private $clacoFormManager;
-    /** @var PlatformConfigurationHandler */
-    private $configHandler;
     /** @var string */
     private $filesDir;
-    /** @var LocationManager */
-    private $locationManager;
-    /** @var Environment */
-    private $templating;
-    /** @var TranslatorInterface */
-    private $translator;
     /** @var SerializerProvider */
     private $serializer;
-    /** @var PdfManager */
-    private $pdfManager;
+    /** @var ExportManager */
+    private $exportManager;
 
     public function __construct(
         AuthorizationCheckerInterface $authorization,
         ObjectManager $om,
         FinderProvider $finder,
         ClacoFormManager $clacoFormManager,
-        PlatformConfigurationHandler $configHandler,
-        $filesDir,
-        LocationManager $locationManager,
-        Environment $templating,
-        TranslatorInterface $translator,
+        string $filesDir,
         SerializerProvider $serializer,
         TokenStorageInterface $tokenStorage,
-        PdfManager $pdfManager
+        ExportManager $exportManager
     ) {
         $this->authorization = $authorization;
         $this->om = $om;
         $this->finder = $finder;
         $this->clacoFormManager = $clacoFormManager;
-        $this->configHandler = $configHandler;
         $this->filesDir = $filesDir;
-        $this->locationManager = $locationManager;
-        $this->templating = $templating;
-        $this->translator = $translator;
         $this->serializer = $serializer;
         $this->tokenStorage = $tokenStorage;
-        $this->pdfManager = $pdfManager;
+        $this->exportManager = $exportManager;
     }
 
     /**
@@ -306,9 +285,7 @@ class ClacoFormController
         $fileName = TextNormalizer::toKey($entry->getTitle());
 
         return new StreamedResponse(function () use ($entry, $user) {
-            echo $this->pdfManager->fromHtml(
-                $this->generatePdfForEntry($entry, $user)
-            );
+            echo $this->exportManager->generatePdfForEntry($entry, $user);
         }, 200, [
             'Content-Type' => 'application/pdf',
             'Content-Disposition' => 'attachment; filename='.$fileName.'.pdf',
@@ -387,10 +364,10 @@ class ClacoFormController
     {
         $this->checkPermission('EDIT', $clacoForm->getResourceNode(), [], true);
 
-        $content = $this->exportEntries($clacoForm);
+        $content = $this->exportManager->exportEntries($clacoForm);
 
         if ($this->clacoFormManager->hasFiles($clacoForm)) {
-            $file = $this->zipEntries($content, $clacoForm);
+            $file = $this->exportManager->zipEntries($content, $clacoForm);
 
             $response = new StreamedResponse();
             $response->setCallBack(
@@ -535,204 +512,5 @@ class ClacoFormController
         $countries = $this->clacoFormManager->getAllUsedCountriesCodes($clacoForm);
 
         return new JsonResponse($countries, 200);
-    }
-
-    private function exportEntries(ClacoForm $clacoForm)
-    {
-        $entriesData = [];
-        $fields = $clacoForm->getFields();
-        $entries = $this->clacoFormManager->getAllEntries($clacoForm);
-
-        foreach ($entries as $entry) {
-            $user = $entry->getUser();
-            $publicationDate = $entry->getPublicationDate();
-            $editionDate = $entry->getEditionDate();
-            $fieldValues = $entry->getFieldValues();
-            $data = [];
-            $data['id'] = $entry->getId();
-            $data['uuid'] = $entry->getUuid();
-            $data['title'] = $entry->getTitle();
-
-            $data['author'] = $this->translator->trans('anonymous', [], 'platform');
-            $data['author_username'] = null;
-            $data['author_email'] = null;
-            if (!empty($user)) {
-                $data['author'] = $user->getFirstName().' '.$user->getLastName();
-                $data['author_username'] = $user->getUsername();
-                $data['author_email'] = $user->getEmail();
-            }
-
-            $data['publicationDate'] = empty($publicationDate) ? '' : $publicationDate->format('d/m/Y');
-            $data['editionDate'] = empty($editionDate) ? '' : $editionDate->format('d/m/Y');
-
-            foreach ($fieldValues as $fiedValue) {
-                $field = $fiedValue->getField();
-                $fieldFacet = $field->getFieldFacet();
-                $fieldFacetValue = $fiedValue->getFieldFacetValue();
-                $val = $fieldFacetValue->getValue();
-
-                switch ($fieldFacet->getType()) {
-                    case FieldFacet::CHOICE_TYPE:
-                        $value = is_array($val) ? implode(', ', $val) : $val;
-                        break;
-                    case FieldFacet::CASCADE_TYPE:
-                        $value = is_array($val) ? implode(', ', $val) : '';
-                        break;
-                    case FieldFacet::COUNTRY_TYPE:
-                        $value = $this->locationManager->getCountryByCode($val);
-                        break;
-                    case FieldFacet::FILE_TYPE:
-                        if (!empty($val) && is_array($val)) {
-                            $fileUrl = preg_replace('#^\.\.\/files\/#', '', $val['url']); // TODO : files part should not be stored in the DB
-                            $filePath = $this->filesDir.DIRECTORY_SEPARATOR.$fileUrl;
-
-                            $ext = pathinfo($filePath, PATHINFO_EXTENSION);
-                            $value = TextNormalizer::toKey($entry->getTitle()).'-'.TextNormalizer::toKey($field->getLabel()).'.'.$ext;
-                        }
-                        break;
-                    default:
-                        $value = $val;
-                }
-                $data[$field->getId()] = $value;
-            }
-
-            $entriesData[] = $data;
-        }
-
-        return $this->templating->render(
-            '@ClarolineClacoForm/claco_form/entries_export.html.twig',
-            [
-                'fields' => $fields,
-                'entries' => $entriesData,
-            ]
-        );
-    }
-
-    private function generatePdfForEntry(Entry $entry, User $user)
-    {
-        $clacoForm = $entry->getClacoForm();
-        $fields = $clacoForm->getFields();
-        $fieldValues = [];
-
-        foreach ($entry->getFieldValues() as $fieldValue) {
-            $field = $fieldValue->getField();
-            $fieldValues[$field->getId()] = $fieldValue->getFieldFacetValue()->getValue();
-        }
-        $canEdit = $this->clacoFormManager->hasRight($clacoForm, 'EDIT');
-        $template = $clacoForm->getTemplate();
-        $useTemplate = $clacoForm->getUseTemplate();
-        $displayMeta = $clacoForm->getDisplayMetadata();
-        $isEntryManager = $user instanceof User && $this->clacoFormManager->isEntryManager($entry, $user);
-        $withMeta = $canEdit || 'all' === $displayMeta || ('manager' === $displayMeta && $isEntryManager);
-        $countries = $this->locationManager->getCountries();
-
-        if (!empty($template) && $useTemplate) {
-            $template = str_replace('%clacoform_entry_title%', $entry->getTitle(), $template);
-
-            foreach ($fields as $field) {
-                if (!$field->isHidden() && ($withMeta || !$field->isMetadata()) && isset($fieldValues[$field->getId()])) {
-                    $fieldFacet = $field->getFieldFacet();
-
-                    switch ($fieldFacet->getType()) {
-                        case FieldFacet::CASCADE_TYPE:
-                            $value = implode(', ', $fieldValues[$field->getId()]);
-                            break;
-                        case FieldFacet::COUNTRY_TYPE:
-                            $value = $this->locationManager->getCountryByCode($fieldValues[$field->getId()]);
-                            break;
-                        case FieldFacet::FILE_TYPE:
-                            $values = [];
-
-                            foreach ($fieldValues[$field->getId()] as $fileValue) {
-                                $values[] = '['.implode(', ', $fileValue).']';
-                            }
-                            $value = implode(', ', $values);
-                            break;
-                        case FieldFacet::BOOLEAN_TYPE:
-                            $value = $fieldValues[$field->getId()] ? $field->getLabel() : '';
-                            break;
-                        default:
-                            $value = $fieldValues[$field->getId()];
-                    }
-                } else {
-                    $value = '';
-                }
-
-                if (is_array($value)) {
-                    $value = implode(', ', $value);
-                }
-
-                $template = str_replace("%field_{$field->getUuid()}%", $value, $template);
-                // for retro-compatibility with very old templates
-                $template = str_replace("%field_{$field->getId()}%", $value, $template);
-            }
-        }
-        $canViewComments = $this->clacoFormManager->canViewComments($clacoForm);
-        $comments = [];
-
-        if ($canViewComments) {
-            $entryComments = $entry->getComments();
-
-            foreach ($entryComments as $comment) {
-                if (Comment::VALIDATED === $comment->getStatus()) {
-                    $comments[] = $comment;
-                }
-            }
-        }
-
-        return $this->templating->render(
-            '@ClarolineClacoForm/claco_form/entry.html.twig',
-            [
-                'entry' => $entry,
-                'template' => $template,
-                'useTemplate' => $useTemplate,
-                'withMeta' => $withMeta,
-                'fields' => $fields,
-                'fieldValues' => $fieldValues,
-                'countries' => $countries,
-                'canViewComments' => $canViewComments,
-                'comments' => $comments,
-            ]
-        );
-    }
-
-    private function zipEntries($content, ClacoForm $clacoForm)
-    {
-        $archive = new \ZipArchive();
-        $pathArch = $this->configHandler->getParameter('tmp_dir').DIRECTORY_SEPARATOR.Uuid::uuid4()->toString().'.zip';
-        $archive->open($pathArch, \ZipArchive::CREATE);
-        $archive->addFromString($clacoForm->getResourceNode()->getName().'.xls', $content);
-
-        $entries = $this->clacoFormManager->getAllEntries($clacoForm);
-
-        foreach ($entries as $entry) {
-            $fieldValues = $entry->getFieldValues();
-
-            foreach ($fieldValues as $fieldValue) {
-                $field = $fieldValue->getField();
-                $fieldFacetValue = $fieldValue->getFieldFacetValue();
-
-                if (FieldFacet::FILE_TYPE === $field->getType()) {
-                    /* TODO: change this when FILE_TYPE can accept an array of files again */
-                    $file = $fieldFacetValue->getValue();
-
-                    if (!empty($file) && is_array($file)) {
-                        $fileUrl = preg_replace('#^\.\.\/files\/#', '', $file['url']); // TODO : files part should not be stored in the DB
-                        $filePath = $this->filesDir.DIRECTORY_SEPARATOR.$fileUrl;
-
-                        $ext = pathinfo($filePath, PATHINFO_EXTENSION);
-                        $fileName = TextNormalizer::toKey($entry->getTitle()).'-'.TextNormalizer::toKey($field->getLabel()).'.'.$ext;
-
-                        $archive->addFile(
-                            $filePath,
-                            'files'.DIRECTORY_SEPARATOR.TextNormalizer::toKey($entry->getTitle()).DIRECTORY_SEPARATOR.$fileName
-                        );
-                    }
-                }
-            }
-        }
-        $archive->close();
-
-        return $pathArch;
     }
 }
