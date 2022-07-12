69cf79685f6d40d3a917a067b02963a5177dc7f5
Elorfin <penin.axel@gmail.com>
2022-07-12 09:14:06 +0200

    [Agenda] fixes outdated data in event form

--- src/plugin/agenda/Resources/modules/event/modals/parameters/components/modal.jsx
+++ src/plugin/agenda/Resources/modules/event/modals/parameters/components/modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {PropTypes as T} from 'prop-types'
+import isEmpty from 'lodash/isEmpty'
 import omit from 'lodash/omit'
 
 import {trans} from '#/main/app/intl'
@@ -11,21 +12,23 @@ import {selectors} from '#/plugin/agenda/event/modals/parameters/store/selectors
 
 const ParametersModal = props =>
   <Modal
-    {...omit(props, 'event', 'saveEnabled', 'loadEvent', 'update', 'save', 'onSave')}
+    {...omit(props, 'event', 'formData', 'saveEnabled', 'loadEvent', 'update', 'save', 'onSave')}
     icon={props.event.id ? 'fa fa-fw fa-cog' : 'fa fa-fw fa-plus'}
     title={props.event.id ? props.event.name : trans('new_event', {}, 'agenda')}
     subtitle={trans('parameters')}
     onEntering={() => props.loadEvent(props.event)}
   >
-    <EventParameters
-      name={selectors.STORE_NAME}
-      event={props.event}
-      update={props.update}
-      onSave={(response) => {
-        props.onSave(response)
-        props.fadeModal()
-      }}
-    />
+    {!isEmpty(props.formData) &&
+      <EventParameters
+        name={selectors.STORE_NAME}
+        event={props.formData}
+        update={props.update}
+        onSave={(response) => {
+          props.onSave(response)
+          props.fadeModal()
+        }}
+      />
+    }
   </Modal>
 
 ParametersModal.propTypes = {
@@ -34,6 +37,7 @@ ParametersModal.propTypes = {
   ),
   onSave: T.func,
   // from store
+  formData: T.object,
   loadEvent: T.func.isRequired,
   update: T.func.isRequired,
   // from modal
