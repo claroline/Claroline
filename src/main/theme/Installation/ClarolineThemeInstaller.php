3a076ff23ccd611a8945137049d89c36f26da2a5
Elorfin <penin.axel@gmail.com>
2022-10-26 11:25:40 +0200

    [Theme] removes obsolete themes

--- src/main/theme/Installation/ClarolineThemeInstaller.php
+++ src/main/theme/Installation/ClarolineThemeInstaller.php
@@ -3,7 +3,14 @@
 namespace Claroline\ThemeBundle\Installation;
 
 use Claroline\InstallationBundle\Additional\AdditionalInstaller;
+use Claroline\ThemeBundle\Installation\Updater\Updater130600;
 
 class ClarolineThemeInstaller extends AdditionalInstaller
 {
+    public static function getUpdaters(): array
+    {
+        return [
+            '13.6.0' => Updater130600::class,
+        ];
+    }
 }
