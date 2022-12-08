1ec865b5949a65833b721f2ddda8246d1a020bfd
Elorfin <penin.axel@gmail.com>
2022-12-08 11:10:01 +0100

    upgrades webpack

--- src/main/core/Resources/modules/scaffolding/id/index.js
+++ src/main/core/Resources/modules/scaffolding/id/index.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid'
+import {v4 as uuid} from 'uuid'
 
 function makeId() {
   return uuid()
--- webpack/plugins/dev/hot-module-replacement.js
+++
@@ -1,4 +0,0 @@
-
-const webpack = require('webpack')
-
-module.exports = () => new webpack.HotModuleReplacementPlugin()
