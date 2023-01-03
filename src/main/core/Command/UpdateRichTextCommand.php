529e375b32ef027bc2f851273725771526c844dd
Axel Penin <Elorfin@users.noreply.github.com>
2023-01-03 17:21:23 +0100

    Update UpdateRichTextCommand.php

--- src/main/core/Command/UpdateRichTextCommand.php
+++ src/main/core/Command/UpdateRichTextCommand.php
@@ -116,6 +116,7 @@ class UpdateRichTextCommand extends Command
             'Claroline\CursusBundle\Entity\Session' => ['description'],
             'HeVinci\UrlBundle\Entity\Url' => ['url'],
             'HeVinci\UrlBundle\Entity\Home\UrlTab' => ['url'],
+            'Icap\LessonBundle\Entity\Chapter' => ['description', 'internalNote'],
         ], function (string $className) {
             return class_exists($className);
         }, ARRAY_FILTER_USE_KEY);
