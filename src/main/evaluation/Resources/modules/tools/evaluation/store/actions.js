65ea3514b91e2443c27d3d17975de113bfbbd3b7
Elorfin <penin.axel@gmail.com>
2023-01-04 10:49:04 +0100

    [Evaluation] allows to download multiple certificates at once

--- src/main/evaluation/Resources/modules/tools/evaluation/store/actions.js
+++ src/main/evaluation/Resources/modules/tools/evaluation/store/actions.js
@@ -1,3 +1,5 @@
+import get from 'lodash/get'
+
 import {makeActionCreator} from '#/main/app/store/actions'
 import {API_REQUEST, url} from '#/main/app/api'
 import {actions as listActions} from '#/main/app/content/list/store/actions'
@@ -6,7 +8,6 @@ import {selectors} from '#/main/evaluation/tools/evaluation/store/selectors'
 
 export const USER_PROGRESSION_LOAD = 'USER_PROGRESSION_LOAD'
 export const USER_PROGRESSION_RESET = 'USER_PROGRESSION_RESET'
-export const LOAD_REQUIREMENTS = 'LOAD_REQUIREMENTS'
 
 export const actions = {}
 
@@ -29,5 +30,23 @@ actions.addRequiredResources = (workspaceId, resources) => (dispatch) => dispatc
       method: 'PATCH'
     },
     success: () => dispatch(listActions.invalidateData(selectors.STORE_NAME+'.requiredResources'))
+  }
+})
+
+actions.downloadParticipationCertificate = (evaluation) => ({
+  [API_REQUEST]: {
+    url: ['apiv2_workspace_download_participation_certificate', {workspace: get(evaluation, 'workspace.id'), user: get(evaluation, 'user.id')}],
+    request: {
+      method: 'GET'
+    }
+  }
+})
+
+actions.downloadSuccessCertificate = (evaluation) => ({
+  [API_REQUEST]: {
+    url: ['apiv2_workspace_download_success_certificate', {workspace: get(evaluation, 'workspace.id'), user: get(evaluation, 'user.id')}],
+    request: {
+      method: 'GET'
+    }
   }
 })
