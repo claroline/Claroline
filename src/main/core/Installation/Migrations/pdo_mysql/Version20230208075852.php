1638fa0bb340dc86987f4bba8fedb2fa9b97d381
Elorfin <penin.axel@gmail.com>
2023-02-14 09:08:23 +0100

    [Evaluation] fixes migration

--- src/main/core/Installation/Migrations/pdo_mysql/Version20230208075852.php
+++ src/main/core/Installation/Migrations/pdo_mysql/Version20230208075852.php
@@ -15,10 +15,21 @@ class Version20230208075852 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('
+            UPDATE claro_resource_evaluation SET evaluation_status = "unknown" WHERE evaluation_status IS NULL
+        ');
+        $this->addSql('
             ALTER TABLE claro_resource_evaluation CHANGE evaluation_status evaluation_status VARCHAR(255) NOT NULL
+        ');
+
+        $this->addSql('
+            UPDATE claro_resource_user_evaluation SET evaluation_status = "unknown" WHERE evaluation_status IS NULL
         ');
         $this->addSql('
             ALTER TABLE claro_resource_user_evaluation CHANGE evaluation_status evaluation_status VARCHAR(255) NOT NULL
+        ');
+
+        $this->addSql('
+            UPDATE claro_workspace_evaluation SET evaluation_status = "unknown" WHERE evaluation_status IS NULL
         ');
         $this->addSql('
             ALTER TABLE claro_workspace_evaluation CHANGE evaluation_status evaluation_status VARCHAR(255) NOT NULL
