0d41d93181a4ebb572222161bfac0936366b2664
Elorfin <penin.axel@gmail.com>
2023-02-17 07:03:28 +0100

    [Evaluation] rounds score to 100

--- src/main/evaluation/Resources/modules/resource/components/card.jsx
+++ src/main/evaluation/Resources/modules/resource/components/card.jsx
@@ -10,6 +10,7 @@ import {DataCard} from '#/main/app/data/components/card'
 
 import {constants} from '#/main/evaluation/constants'
 import {ResourceEvaluation as ResourceEvaluationTypes} from '#/main/evaluation/resource/prop-types'
+import {displayScore} from '#/main/evaluation/utils'
 
 const ResourceCard = (props) =>
   <DataCard
@@ -57,7 +58,7 @@ const ResourceCard = (props) =>
             icon: 'fa fa-fw fa-award',
             label: trans('score'),
             displayed: !!props.data.scoreMax,
-            value: (number(props.data.score) || 0) + ' / ' + number(props.data.scoreMax)
+            value: !!props.data.scoreMax && displayScore(props.data.scoreMax, props.data.score, 100) + ' / 100'
           }
         ]
           .filter(item => undefined === item.displayed || item.displayed)
