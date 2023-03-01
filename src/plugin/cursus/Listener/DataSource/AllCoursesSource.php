9ea396aa031bc6ba91e14ccf66799f48e27dfda5
Elorfin <penin.axel@gmail.com>
2023-03-01 08:24:01 +0100

    [Training] fixes organization filter for data source

--- src/plugin/cursus/Listener/DataSource/AllCoursesSource.php
+++ src/plugin/cursus/Listener/DataSource/AllCoursesSource.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Claroline Connect package.
- *
- * (c) Claroline Consortium <consortium@claroline.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Claroline\CursusBundle\Listener\DataSource;
 
 use Claroline\AppBundle\API\FinderProvider;
@@ -51,17 +42,8 @@ class AllCoursesSource
         $options = $event->getOptions();
         $options['hiddenFilters']['hidden'] = false;
 
-        if (!$this->authorization->isGranted('ROLE_ADMIN')) {
-            $user = $this->tokenStorage->getToken()->getUser();
-            if ($user instanceof User) {
-                $organizations = $user->getOrganizations();
-            } else {
-                $organizations = $this->om->getRepository(Organization::class)->findBy(['default' => true]);
-            }
-
-            $options['hiddenFilters']['organizations'] = array_map(function (Organization $organization) {
-                return $organization->getUuid();
-            }, $organizations);
+        if (!$this->authorization->isGranted('ROLE_ADMIN') && (empty($options['filters']) || empty($options['filters']['organizations']))) {
+            $options['hiddenFilters']['organizations'] = $this->getOrganizations();
         }
 
         $event->setData(
@@ -69,5 +51,17 @@ class AllCoursesSource
         );
 
         $event->stopPropagation();
+    }
+
+    private function getOrganizations(): array
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+        if ($user instanceof User) {
+            return array_map(function (Organization $organization) {
+                return $organization->getUuid();
+            }, $user->getOrganizations());
+        }
+
+        return [];
     }
 }
--- src/plugin/cursus/Resources/modules/data/sources/courses.js
+++ src/plugin/cursus/Resources/modules/data/sources/courses.js
@@ -67,6 +67,14 @@ export default {
         label: trans('order'),
         displayable: false,
         filterable: false
+      }, {
+        name: 'organizations',
+        type: 'organizations',
+        label: trans('organizations'),
+        displayable: false,
+        displayed: false,
+        sortable: false,
+        filterable: true
       }
     ],
     card: CourseCard
