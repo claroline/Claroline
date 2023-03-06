bd5c5dc7f3ad5e0c8cba19a77b2ae994c4c27939
Elorfin <penin.axel@gmail.com>
2023-03-06 09:15:24 +0100

    [BigBlueButton] implements evaluation
    php cs fixer

--- src/integration/big-blue-button/Listener/Resource/BBBListener.php
+++ src/integration/big-blue-button/Listener/Resource/BBBListener.php
@@ -11,30 +11,37 @@
 
 namespace Claroline\BigBlueButtonBundle\Listener\Resource;
 
+use Claroline\AppBundle\API\Serializer\SerializerInterface;
 use Claroline\AppBundle\API\SerializerProvider;
 use Claroline\BigBlueButtonBundle\Entity\BBB;
 use Claroline\BigBlueButtonBundle\Manager\BBBManager;
+use Claroline\BigBlueButtonBundle\Manager\EvaluationManager;
+use Claroline\CoreBundle\Entity\User;
 use Claroline\CoreBundle\Event\Resource\DeleteResourceEvent;
 use Claroline\CoreBundle\Event\Resource\LoadResourceEvent;
 use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class BBBListener
 {
-    /** @var PlatformConfigurationHandler */
-    private $config;
-    /** @var BBBManager */
-    private $bbbManager;
-    /** @var SerializerProvider */
-    private $serializer;
+    private TokenStorageInterface $tokenStorage;
+    private PlatformConfigurationHandler $config;
+    private SerializerProvider $serializer;
+    private BBBManager $bbbManager;
+    private EvaluationManager $evaluationManager;
 
     public function __construct(
+        TokenStorageInterface $tokenStorage,
         PlatformConfigurationHandler $config,
+        SerializerProvider $serializer,
         BBBManager $bbbManager,
-        SerializerProvider $serializer
+        EvaluationManager $evaluationManager
     ) {
+        $this->tokenStorage = $tokenStorage;
         $this->config = $config;
-        $this->bbbManager = $bbbManager;
         $this->serializer = $serializer;
+        $this->bbbManager = $bbbManager;
+        $this->evaluationManager = $evaluationManager;
     }
 
     public function onLoad(LoadResourceEvent $event)
@@ -60,7 +67,14 @@ class BBBListener
             }
         }
 
+        $userEvaluation = null;
+        if ($this->tokenStorage->getToken()->getUser() instanceof User) {
+            $attempt = $this->evaluationManager->update($bbb->getResourceNode(), $this->tokenStorage->getToken()->getUser());
+            $userEvaluation = $attempt->getResourceUserEvaluation();
+        }
+
         $event->setData([
+            'userEvaluation' => $this->serializer->serialize($userEvaluation, [SerializerInterface::SERIALIZE_MINIMAL]),
             'servers' => $this->bbbManager->getServers(),
             'bbb' => $this->serializer->serialize($bbb),
             'allowRecords' => $allowRecords,
