4978ae8545a27219b4936b10d5d8b0d7e7be4305
Elorfin <penin.axel@gmail.com>
2023-04-28 12:42:27 +0200

    [Video] updates progression on time update

--- src/integration/peertube/Resources/modules/resources/video/components/player.jsx
+++ src/integration/peertube/Resources/modules/resources/video/components/player.jsx
@@ -4,20 +4,35 @@ import {PropTypes as T} from 'prop-types'
 import {Video as VideoTypes} from '#/integration/peertube/prop-types'
 import {PeerTubePlayer} from '#/integration/peertube/components/player'
 
-const VideoPlayer = props =>
-  <PeerTubePlayer
-    url={props.video.embeddedUrl}
-    onPlay={(currentTime, duration) => {
-      if (props.currentUser) {
-        props.updateProgression(props.video.id, currentTime, duration)
-      }
-    }}
-    onPause={(currentTime, duration) => {
-      if (props.currentUser) {
-        props.updateProgression(props.video.id, currentTime, duration)
-      }
-    }}
-  />
+const VideoPlayer = props => {
+  let lastSaved = 0
+
+  return (
+    <PeerTubePlayer
+      url={props.video.embeddedUrl}
+      onPlay={(currentTime, duration) => {
+        if (props.currentUser) {
+          props.updateProgression(props.video.id, currentTime, duration)
+        }
+      }}
+      onPause={(currentTime, duration) => {
+        if (props.currentUser) {
+          props.updateProgression(props.video.id, currentTime, duration)
+        }
+      }}
+      onTimeUpdate={(currentTime, duration) => {
+        if (props.currentUser) {
+          const interval = Math.round((duration / 100) * 5)
+          const roundedTime = Math.round(currentTime)
+          if (roundedTime > lastSaved && 0 === roundedTime % interval) {
+            props.updateProgression(props.video.id, currentTime, duration)
+            lastSaved = roundedTime
+          }
+        }
+      }}
+    />
+  )
+}
 
 VideoPlayer.propTypes = {
   video: T.shape(
