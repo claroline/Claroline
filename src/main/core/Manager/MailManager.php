7dd7b8e91f19e9e0a823230d8515aac1b4d6b3e5
Noelle-M <monge.noelle@gmail.com>
2023-05-03 18:54:59 +0200

    re linter manager

--- src/main/core/Manager/MailManager.php
+++ src/main/core/Manager/MailManager.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Claroline\CoreBundle\Manager;
 
 use Claroline\AppBundle\Event\StrictDispatcher;
@@ -19,6 +20,7 @@ use Claroline\CoreBundle\Library\Mailing\Message;
 use Claroline\CoreBundle\Manager\Template\TemplateManager;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 class MailManager
 {
     /** @var Mailer */
@@ -35,6 +37,7 @@ class MailManager
     private $userManager;
     /** @var StrictDispatcher */
     private $dispatcher;
+
     public function __construct(
         Mailer $mailer,
         UrlGeneratorInterface $router,
@@ -52,34 +55,44 @@ class MailManager
         $this->userManager = $userManager;
         $this->dispatcher = $dispatcher;
     }
+
     public function isMailerAvailable(): bool
     {
         return $this->config->getParameter('mailer.enabled');
     }
+
     public function sendForgotPassword(User $user): bool
     {
         $this->dispatcher->dispatch(SecurityEvents::FORGOT_PASSWORD, ForgotPasswordEvent::class, [$user]);
+
         $locale = $this->localeManager->getLocale($user);
         $placeholders = [
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName(),
             'username' => $user->getUsername(),
         ];
+
         if (!$user->isEnabled()) {
             $subject = $this->templateManager->getTemplate('user_disabled', $placeholders, $locale, 'title');
             $body = $this->templateManager->getTemplate('user_disabled', $placeholders, $locale);
+
             return $this->send($subject, $body, [$user], null, [], true);
         }
+
         $this->userManager->initializePassword($user);
+
         $placeholders['password_reset_link'] = $this->router->generate(
                 'claro_index',
                 [],
                 UrlGeneratorInterface::ABSOLUTE_URL
             )."#/newpassword/{$user->getResetPasswordHash()}";
+
         $subject = $this->templateManager->getTemplate('forgotten_password', $placeholders, $locale, 'title');
         $body = $this->templateManager->getTemplate('forgotten_password', $placeholders, $locale);
+
         return $this->send($subject, $body, [$user], null, [], true);
     }
+
     public function sendInitPassword(User $user)
     {
         $this->userManager->initializePassword($user);
@@ -98,8 +111,10 @@ class MailManager
         ];
         $subject = $this->templateManager->getTemplate('password_initialization', $placeholders, $locale, 'title');
         $body = $this->templateManager->getTemplate('password_initialization', $placeholders, $locale);
+
         return $this->send($subject, $body, [$user], null, [], true);
     }
+
     public function sendEnableAccountMessage(User $user)
     {
         $hash = $user->getResetPasswordHash();
@@ -117,8 +132,10 @@ class MailManager
         ];
         $subject = $this->templateManager->getTemplate('user_activation', $placeholders, $locale, 'title');
         $body = $this->templateManager->getTemplate('user_activation', $placeholders, $locale);
+
         return $this->send($subject, $body, [$user], null, [], true);
     }
+
     public function sendValidateEmail(User $user)
     {
         $url = $this->router->generate(
@@ -135,8 +152,10 @@ class MailManager
         ];
         $subject = $this->templateManager->getTemplate('user_email_validation', $placeholders, $locale, 'title');
         $body = $this->templateManager->getTemplate('user_email_validation', $placeholders, $locale);
+
         $this->send($subject, $body, [$user], null, [], true);
     }
+
     /**
      * @return bool
      */
@@ -157,14 +176,17 @@ class MailManager
         ];
         $subject = $this->templateManager->getTemplate('user_registration', $placeholders, $locale, 'title');
         $body = $this->templateManager->getTemplate('user_registration', $placeholders, $locale);
+
         return $this->send($subject, $body, [$user], null, [], true);
     }
+
     public function send($subject, $body, array $users, $from = null, array $extra = [], $force = false, $replyToMail = null)
     {
         if (0 === count($users) && (!isset($extra['to']) || 0 === count($extra['to']))) {
             //obviously, if we're not going to send anything to anyone, it's better to stop
             return false;
         }
+
         if ($this->isMailerAvailable()) {
             $to = [];
 
@@ -174,7 +196,9 @@ class MailManager
             if (!$locale) {
                 $locale = $this->localeManager->getDefault();
             }
+
             $body = $this->templateManager->getTemplate('email_layout', ['content' => $body], $locale);
+
             if ($from) {
                 $body = str_replace('%first_name%', $from->getFirstName(), $body);
                 $body = str_replace('%last_name%', $from->getLastName(), $body);
@@ -182,14 +206,17 @@ class MailManager
                 $body = str_replace('%first_name%', $this->config->getParameter('display.name'), $body);
                 $body = str_replace('%last_name%', '', $body);
             }
+
             foreach ($users as $user) {
                 $email = $user->getEmail();
+
                 if ($user->isMailValidated() || $force) {
                     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                         $to[] = $email;
                     }
                 }
             }
+
             if (isset($extra['to'])) {
                 foreach ($extra['to'] as $email) {
                     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -197,27 +224,34 @@ class MailManager
                     }
                 }
             }
+
             $message = new Message();
             $message->subject($subject);
             $message->from($fromEmail);
             $message->body($body);
+
             (null !== $from && filter_var($from->getEmail(), FILTER_VALIDATE_EMAIL)) ?
                 $message->replyTo($from->getEmail()) :
                 $message->replyTo($replyToMail);
+
             if (count($to) > 1) {
                 $message->bcc($to);
             } else {
                 $message->to($to);
             }
+
             if (isset($extra['attachments'])) {
                 foreach ($extra['attachments'] as $attachment) {
                     $message->attach($attachment['name'], $attachment['url'], $attachment['type']);
                 }
             }
+
             return $this->mailer->send($message);
         }
+
         return false;
     }
+
     public function getMailerFrom()
     {
         $from = $this->config->getParameter('mailer.from');
@@ -226,11 +260,14 @@ class MailManager
                 return $from;
             }
         }
+
         if ($this->config->getParameter('internet.domain_name') && '' !== trim($this->config->getParameter('internet.domain_name'))) {
             return 'noreply@'.$this->config->getParameter('internet.domain_name');
         }
+
         return $this->config->getParameter('help.support_email');
     }
+
     public function sendSimpleMailOneToOne($subject, $body, $to, $from = null, $replyToMail = null)
     {
         if ($this->isMailerAvailable()) {
