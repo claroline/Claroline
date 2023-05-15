8d3e1479f7fec241bd4981c510c49b55f6872259
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-05-15 16:38:14 +0200

    PHPCSFixer error

--- src/main/theme/Resources/modules/administration/appearance/modals/color-chart-parameters/store/actions.js
+++ src/main/theme/Resources/modules/administration/appearance/modals/color-chart-parameters/store/actions.js
@@ -7,13 +7,13 @@ import {selectors} from '#/main/theme/administration/appearance/modals/color-cha
 export const actions = {}
 
 actions.save = (data) => (dispatch) => {
-  const isNew = typeof data.id === "undefined";
+  const isNew = typeof data.id === 'undefined'
 
   return dispatch({
     [API_REQUEST]: {
       url: isNew ? ['apiv2_color_collection_create'] : ['apiv2_color_collection_update', {id: data.id}],
       request: {
-        method: isNew ? "POST" : "PUT",
+        method: isNew ? 'POST' : 'PUT',
         body: JSON.stringify(data)
       },
       error: (errors) => dispatch(formActions.errors(selectors.STORE_NAME, errors))
