87535da739585e6fc6b9f3ef363d62e92e8f3a1b
N.M <monge.noelle@gmail.com>
2023-05-17 11:46:39 +0200

    adds privacy plugin (#2484)
    * commit du soir bonsoir
    
    * PrivacyBundle
    
    * corrections suite review PR
    
    * Update bundles.ini-dist

---
+++ src/main/privacy/Resources/modules/administration/privacy/components/tool.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import {PropTypes as T} from 'prop-types'
+
+import {LINK_BUTTON} from '#/main/app/buttons'
+import {FormData} from '#/main/app/content/form/containers/data'
+import {ToolPage} from '#/main/core/tool/containers/page'
+import {selectors} from '#/main/privacy/administration/privacy/store'
+import get from 'lodash/get'
+
+import {trans} from '#/main/app/intl/translation'
+
+const PrivacyTool = (props) =>
+  <ToolPage>
+    <FormData
+      level={2}
+      name={selectors.FORM_NAME}
+      target={['apiv2_privacy_update']}
+      buttons={true}
+      cancel={{
+        type: LINK_BUTTON,
+        target: props.path,
+        exact: true
+      }}
+      locked={props.lockedParameters}
+      sections={[
+        {
+          title: trans('general'),
+          primary: true,
+          fields: [
+            {
+              name: 'privacy.countryStorage',
+              label: trans('Pays de stockage des données'),
+              type: 'country'
+            }
+          ]
+        }, {
+          icon: 'fa fa-fw fa-user-shield',
+          title: trans('dpo'),
+          fields: [
+            {
+              name: 'privacy.dpo.name',
+              label: trans('name'),
+              type: 'string'
+            }, {
+              name: 'privacy.dpo.email',
+              label: trans('email'),
+              type: 'email'
+            }, {
+              name: 'privacy.dpo.phone',
+              label: trans('phone'),
+              type: 'string'
+            }, {
+              name: 'privacy.dpo.address',
+              label: trans('address'),
+              type: 'address'
+            }
+          ]
+        }, {
+          icon: 'fa fa-fw fa-copyright',
+          title: trans('terms_of_service'),
+          fields: [
+            {
+              name: 'tos.enabled',
+              type: 'boolean',
+              label: trans('terms_of_service_activation_message'),
+              help: trans('terms_of_service_activation_help'),
+              linked: [
+                {
+                  name: 'tos.text',
+                  type: 'translated',
+                  label: trans('terms_of_service'),
+                  required: true,
+                  displayed: get(props.parameters, 'tos.enabled')
+                }
+              ]
+            }
+          ]
+        }
+      ]}
+    />
+  </ToolPage>
+
+PrivacyTool.propTypes = {
+  path: T.string.isRequired,
+  lockedParameters: T.arrayOf(T.string),
+  parameters: T.shape({
+    tos: T.shape({
+      enabled: T.bool
+    })
+  })
+}
+
+export {
+  PrivacyTool
+}
