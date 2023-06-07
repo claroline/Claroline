d85cf27202a77b6492adab05e8a63e9d2caa0bcf
Noelle-M <monge.noelle@gmail.com>
2023-06-07 15:25:18 +0200

    PHPCSFixer

--- src/main/privacy/Resources/modules/account/privacy/containers/tool.jsx
+++ src/main/privacy/Resources/modules/account/privacy/containers/tool.jsx
@@ -2,7 +2,6 @@ import {connect} from 'react-redux'
 import {withReducer} from '#/main/app/store/components/withReducer'
 import {PrivacyTool as PrivacyToolComponent} from '#/main/privacy/account/privacy/components/tool'
 import {selectors, reducer} from '#/main/privacy/account/privacy/store'
-import {actions as formActions, selectors as formSelectors} from '#/main/app/content/form/store'
 
 const PrivacyTool = withReducer(selectors.STORE_NAME, reducer)(
   connect(
