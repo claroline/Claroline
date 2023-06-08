9128abf3b9f3c6e2b848cf64f238bc618a911fe5
Noelle-M <monge.noelle@gmail.com>
2023-06-08 15:53:04 +0200

    correction chemin privacy/modal/index

--- src/main/privacy/Resources/modules/modals/terms/index.js
+++ src/main/privacy/Resources/modules/modals/terms/index.js
@@ -1,5 +1,5 @@
 import { registry } from '#/main/app/modals/registry'
-import { TermsModal } from '#/main/privacy/account/privacy/modals/terms/containers/modal'
+import { TermsModal } from '#/main/privacy/modals/terms/containers/modal'
 
 const MODAL_TERMS_OF_SERVICE = 'MODAL_THERM_OF_SERVICE'
 
