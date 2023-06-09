be6f5f51add39418306e239f83ebaeff26467400
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-06-09 12:46:32 +0200

    Video editor redirection

--- src/integration/youtube/Resources/modules/resources/video/containers/editor.jsx
+++ src/integration/youtube/Resources/modules/resources/video/containers/editor.jsx
@@ -1,15 +1,16 @@
 import {connect} from 'react-redux'
 
-import {selectors as toolSelectors} from '#/main/core/tool/store/selectors'
+import {selectors as resourceSelectors} from '#/main/core/resource/store'
 
 import {VideoEditor as VideoEditorComponent} from '#/integration/youtube/resources/video/components/editor'
 import {selectors} from '#/integration/youtube/resources/video/store'
 
 const VideoEditor = connect(
   (state) => ({
-    path: toolSelectors.path(state),
-    video: selectors.video(state)
-  })
+    path: resourceSelectors.path(state),
+    workspace: resourceSelectors.workspace(state),
+    video: selectors.video(state),
+  }),
 )(VideoEditorComponent)
 
 export {
