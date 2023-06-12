a5d3a7fe90067cc7f68ad28cad12a551a025ae4e
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2023-06-12 15:47:12 +0200

    display password strength in password input (#2509)
    * added messages lvl security password
    
    * ESLint checks
    
    * style changes
    
    * review
    
    * added trad
    
    * password styles
    
    * bug fragment
    
    * review changes
    
    * delete div in .less
    
    * last changes

--- src/main/app/Resources/modules/data/types/password/components/input.jsx
+++ src/main/app/Resources/modules/data/types/password/components/input.jsx
@@ -7,66 +7,113 @@ import {DataInput as DataInputTypes} from '#/main/app/data/types/prop-types'
 
 import {Button} from '#/main/app/action/components/button'
 import {CALLBACK_BUTTON} from '#/main/app/buttons'
+import {ProgressBar} from '#/main/app/content/components/progress-bar'
 
 class PasswordInput extends PureComponent {
   constructor(props) {
     super(props)
 
     this.state = {
-      visible: false
+      visible: false,
+      passwordStrength: 0
     }
 
     this.onChange = this.onChange.bind(this)
     this.toggleVisibility = this.toggleVisibility.bind(this)
+    this.estimatePasswordStrength = this.estimatePasswordStrength.bind(this)
   }
-
   onChange(e) {
     this.props.onChange(e.target.value)
+    this.estimatePasswordStrength(e.target.value)
   }
 
   toggleVisibility() {
     this.setState({visible: !this.state.visible})
   }
 
+  estimatePasswordStrength(password) {
+    const conditions = [
+      /[a-z]/,
+      /[A-Z]/,
+      /[0-9]/,
+      /[^a-zA-Z0-9]/,
+      /^.{8,}$/
+    ]
+
+    const strengthSum = conditions.reduce((sum, regex) => {
+      return regex.test(password) ? sum + 1 : sum
+    }, 0)
+
+    this.setState({
+      passwordStrength: strengthSum
+    })
+  }
+
   render() {
+    const progressBarTypes = ['danger', 'warning', 'info', 'success']
+    const progressBarType = this.state.passwordStrength > 0 ? progressBarTypes[this.state.passwordStrength - 1] : 'danger'
+    const labels = [
+      trans('password_strength.very_weak', {}, 'security'),
+      trans('password_strength.weak', {}, 'security'),
+      trans('password_strength.medium', {}, 'security'),
+      trans('password_strength.strong', {}, 'security'),
+      trans('password_strength.very_strong', {}, 'security')
+    ]
+
     return (
-      <div className={classes('input-group', this.props.className, {
-        [`input-group-${this.props.size}`]: !!this.props.size
-      })}>
-        <span className="input-group-addon">
-          <span className="fa fa-fw fa-lock" role="presentation" />
-        </span>
+      <>
+        <div className={classes('input-group', this.props.className, {
+          [`input-group-${this.props.size}`]: !!this.props.size
+        })}>
 
-        <input
-          id={this.props.id}
-          type={this.state.visible ? 'text':'password'}
-          className="form-control"
-          value={this.props.value || ''}
-          disabled={this.props.disabled}
-          onChange={this.onChange}
-          placeholder={this.props.placeholder}
-          autoComplete={this.props.autoComplete}
-        />
+          <span className="input-group-addon">
+            <span className="fa fa-fw fa-lock" role="presentation" />
+          </span>
 
-        <span className="input-group-btn">
-          <Button
-            className="btn"
-            type={CALLBACK_BUTTON}
-            icon={classes('fa fa-fw', {
-              'fa-eye'      : !this.state.visible,
-              'fa-eye-slash': this.state.visible
-            })}
-            label={trans(this.state.visible ? 'hide_password':'show_password')}
+          <input
+            id={this.props.id}
+            type={this.state.visible ? 'text':'password'}
+            className="form-control"
+            value={this.props.value || ''}
             disabled={this.props.disabled}
-            callback={this.toggleVisibility}
-            tooltip="left"
+            onChange={this.onChange}
+            placeholder={this.props.placeholder}
+            autoComplete={this.props.autoComplete}
           />
-        </span>
-      </div>
+
+          <span className="input-group-btn">
+            <Button
+              className="btn"
+              type={CALLBACK_BUTTON}
+              icon={classes('fa fa-fw', {
+                'fa-eye'      : !this.state.visible,
+                'fa-eye-slash': this.state.visible
+              })}
+              label={trans(this.state.visible ? 'hide_password':'show_password')}
+              disabled={this.props.disabled}
+              callback={this.toggleVisibility}
+              tooltip="left"
+            />
+          </span>
+        </div>
+
+        {!this.props.hideStrength && this.props.value &&
+          <>
+            <ProgressBar
+              className="password-strength"
+              value={this.state.passwordStrength * 25}
+              size="sm"
+              type={progressBarType}
+            />
+            <span className={`text-${progressBarType}`}>
+              {labels[this.state.passwordStrength]}
+            </span>
+          </>
+        }
+      </>
     )
   }
 }
-
 
 implementPropTypes(PasswordInput, DataInputTypes, {
   value: T.string
