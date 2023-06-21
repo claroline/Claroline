7017b51eab96d4f1b91f387b840a1fefd4f8b235
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-06-21 17:54:06 +0200

    ESLint

--- src/main/authentication/Resources/modules/administration/authentication/components/tool.jsx
+++ src/main/authentication/Resources/modules/administration/authentication/components/tool.jsx
@@ -5,51 +5,51 @@ import {trans} from '#/main/app/intl/translation'
 import {FormData} from '#/main/app/content/form/containers/data'
 import {selectors} from '#/main/authentication/administration/authentication/store/selectors'
 const AuthenticationTool = (props) => {
-  return (
-      <FormData
-        name={selectors.FORM_NAME}
-        target={['apiv2_authentication_update']}
-        buttons={true}
-        cancel={{
-          type: LINK_BUTTON,
-          target: props.path,
-          exact: true
-        }}
-        definition={[
-          {
-            title: trans('general'),
-            primary: true,
-            fields: [
-              {
-                name: 'minLength',
-                type: 'number',
-                label: trans('minLength', {}, 'authentication'),
-              },
-              {
-                name: 'requireLowercase',
-                type: 'boolean',
-                label: trans('requireLowercase', {}, 'authentication'),
-              },
-              {
-                name: 'requireUppercase',
-                type: 'boolean',
-                label: trans('requireUppercase', {}, 'authentication'),
-              },
-              {
-                name: 'requireNumber',
-                type: 'boolean',
-                label: trans('requireNumber', {}, 'authentication'),
-              },
-              {
-                name: 'requireSpecialChar',
-                type: 'boolean',
-                label: trans('requireSpecialChar', {}, 'authentication'),
-              },
-            ],
-          }
-        ]}
+    return (
+        <FormData
+            name={selectors.FORM_NAME}
+            target={['apiv2_authentication_update']}
+            buttons={true}
+            cancel={{
+                type: LINK_BUTTON,
+                target: props.path,
+                exact: true
+            }}
+            definition={[
+                {
+                    title: trans('general'),
+                    primary: true,
+                    fields: [
+                        {
+                            name: 'minLength',
+                            type: 'number',
+                            label: trans('minLength', {}, 'authentication')
+                        },
+                        {
+                            name: 'requireLowercase',
+                            type: 'boolean',
+                            label: trans('requireLowercase', {}, 'authentication')
+                        },
+                        {
+                            name: 'requireUppercase',
+                            type: 'boolean',
+                            label: trans('requireUppercase', {}, 'authentication')
+                        },
+                        {
+                            name: 'requireNumber',
+                            type: 'boolean',
+                            label: trans('requireNumber', {}, 'authentication')
+                        },
+                        {
+                            name: 'requireSpecialChar',
+                            type: 'boolean',
+                            label: trans('requireSpecialChar', {}, 'authentication')
+                        }
+                    ]
+                }
+            ]}
       />
-  )
+    )
 }
 
 export {
