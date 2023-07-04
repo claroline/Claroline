c3b3b61aba848bec4c1850a834f986e704285942
Noelle-M <monge.noelle@gmail.com>
2023-07-04 12:03:58 +0200

    PHPCSFixer

--- src/main/app/Resources/modules/modals/terms-of-service/components/modal.jsx
+++ src/main/app/Resources/modules/modals/terms-of-service/components/modal.jsx
@@ -8,28 +8,28 @@ import {ContentLoader} from '#/main/app/content/components/loader'
 import {ContentHtml} from '#/main/app/content/components/html'
 
 const TermsOfServiceModal = props =>
-    <Modal
-      {...omit(props, 'fetch')}
-      icon="fa fa-fw fa-shield"
-      title={trans('terms_of_service')}
-      bsSize="lg"
-      onEntering={() => {
-        if (!props.loaded) {
-          props.fetch()
-        }
-      }}
-    >
-      {!props.loaded &&
-        <ContentLoader
-          size="lg"
-          description="Nous chargeons les conditions d'utilisation..."
-        />
+  <Modal
+    {...omit(props, 'fetch')}
+    icon="fa fa-fw fa-shield"
+    title={trans('terms_of_service')}
+    bsSize="lg"
+    onEntering={() => {
+      if (!props.loaded) {
+        props.fetch()
       }
+    }}
+  >
+    {!props.loaded &&
+      <ContentLoader
+        size="lg"
+        description="Nous chargeons les conditions d'utilisation..."
+      />
+    }
 
-      {(props.loaded && props.content) &&
-        <ContentHtml className="modal-body">{props.content}</ContentHtml>
-      }
-    </Modal>
+    {(props.loaded && props.content) &&
+      <ContentHtml className="modal-body">{props.content}</ContentHtml>
+    }
+  </Modal>
 
 TermsOfServiceModal.propTypes = {
   fetch: T.func.isRequired,
