32b5a814c1632b8b2db4e84b705a93372ee60928
Noelle-M <monge.noelle@gmail.com>
2023-07-05 09:25:30 +0200

    restauration privacyExtension

--- src/main/privacy/DependencyInjection/ClarolinePrivacyExtension.php
+++ src/main/privacy/DependencyInjection/ClarolinePrivacyExtension.php
@@ -11,6 +11,7 @@
 
 namespace Claroline\PrivacyBundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -18,6 +19,11 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ClarolinePrivacyExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @throws Exception
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $locator = new FileLocator(__DIR__.'/../Resources/config');
