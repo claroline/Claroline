bb77bddf4aa8f7fb4b445ad4623ca7604bcfefd6
Elorfin <penin.axel@gmail.com>
2023-07-10 16:40:03 +0200

    [Lesson] resets migrations

--- src/plugin/lesson/Installation/Migrations/Version20230426080000.php
+++ src/plugin/lesson/Installation/Migrations/Version20230426080000.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Icap\LessonBundle\Installation\Migrations\pdo_mysql;
+namespace Icap\LessonBundle\Installation\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -8,9 +8,9 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated migration based on mapping information: modify it with caution.
  *
- * Generation date: 2020/07/01 08:50:19
+ * Generation date: 2023/07/10 02:38:35
  */
-class Version20180606143209 extends AbstractMigration
+final class Version20230426080000 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
@@ -21,30 +21,34 @@ class Version20180606143209 extends AbstractMigration
                 parent_id INT DEFAULT NULL, 
                 title VARCHAR(255) NOT NULL, 
                 text LONGTEXT DEFAULT NULL, 
+                internalNote LONGTEXT DEFAULT NULL, 
                 slug VARCHAR(128) NOT NULL, 
                 lft INT NOT NULL, 
                 lvl INT NOT NULL, 
                 rgt INT NOT NULL, 
                 root INT DEFAULT NULL, 
                 uuid VARCHAR(36) NOT NULL, 
+                poster VARCHAR(255) DEFAULT NULL, 
                 UNIQUE INDEX UNIQ_3D7E3C8C989D9B62 (slug), 
                 UNIQUE INDEX UNIQ_3D7E3C8CD17F50A6 (uuid), 
                 INDEX IDX_3D7E3C8CCDF80196 (lesson_id), 
                 INDEX IDX_3D7E3C8C727ACA70 (parent_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
             CREATE TABLE icap__lesson (
                 id INT AUTO_INCREMENT NOT NULL, 
                 root_id INT DEFAULT NULL, 
+                description LONGTEXT DEFAULT NULL, 
+                show_overview TINYINT(1) DEFAULT 1 NOT NULL, 
                 uuid VARCHAR(36) NOT NULL, 
                 resourceNode_id INT DEFAULT NULL, 
                 UNIQUE INDEX UNIQ_D9B36130D17F50A6 (uuid), 
                 UNIQUE INDEX UNIQ_D9B3613079066886 (root_id), 
                 UNIQUE INDEX UNIQ_D9B36130B87FAB32 (resourceNode_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
             ALTER TABLE icap__lesson_chapter 
@@ -76,6 +80,10 @@ class Version20180606143209 extends AbstractMigration
     {
         $this->addSql('
             ALTER TABLE icap__lesson_chapter 
+            DROP FOREIGN KEY FK_3D7E3C8CCDF80196
+        ');
+        $this->addSql('
+            ALTER TABLE icap__lesson_chapter 
             DROP FOREIGN KEY FK_3D7E3C8C727ACA70
         ');
         $this->addSql('
@@ -83,8 +91,8 @@ class Version20180606143209 extends AbstractMigration
             DROP FOREIGN KEY FK_D9B3613079066886
         ');
         $this->addSql('
-            ALTER TABLE icap__lesson_chapter 
-            DROP FOREIGN KEY FK_3D7E3C8CCDF80196
+            ALTER TABLE icap__lesson 
+            DROP FOREIGN KEY FK_D9B36130B87FAB32
         ');
         $this->addSql('
             DROP TABLE icap__lesson_chapter
