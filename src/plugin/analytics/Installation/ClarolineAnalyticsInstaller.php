25c12412bb8b6f452446659216a342f51d131f36
Elorfin <penin.axel@gmail.com>
2023-07-10 15:44:48 +0200

    [Analytics] resets updaters

--- src/plugin/analytics/Installation/ClarolineAnalyticsInstaller.php
+++ src/plugin/analytics/Installation/ClarolineAnalyticsInstaller.php
@@ -2,18 +2,10 @@
 
 namespace Claroline\AnalyticsBundle\Installation;
 
-use Claroline\AnalyticsBundle\Installation\Updater\Updater130300;
 use Claroline\InstallationBundle\Additional\AdditionalInstaller;
 
 class ClarolineAnalyticsInstaller extends AdditionalInstaller
 {
-    public static function getUpdaters(): array
-    {
-        return [
-            '13.1.1' => Updater130300::class,
-        ];
-    }
-
     public function hasMigrations(): bool
     {
         return false;
