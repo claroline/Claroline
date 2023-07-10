0685945bf2b11d7930b000158a0a8fcd75e680b3
Elorfin <penin.axel@gmail.com>
2023-07-10 16:09:39 +0200

    [Competency] resets migrations and updaters

--- src/plugin/competency/Installation/HeVinciCompetencyInstaller.php
+++ src/plugin/competency/Installation/HeVinciCompetencyInstaller.php
@@ -3,16 +3,7 @@
 namespace HeVinci\CompetencyBundle\Installation;
 
 use Claroline\InstallationBundle\Additional\AdditionalInstaller;
-use HeVinci\CompetencyBundle\Installation\Updater\Updater130600;
-use HeVinci\CompetencyBundle\Installation\Updater\Updater130700;
 
 class HeVinciCompetencyInstaller extends AdditionalInstaller
 {
-    public static function getUpdaters(): array
-    {
-        return [
-            '13.6.0' => Updater130600::class,
-            '13.7.0' => Updater130700::class,
-        ];
-    }
 }
