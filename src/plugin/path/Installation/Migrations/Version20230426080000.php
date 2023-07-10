0b14e638e85c0e7caa690221c5803cfdea4efb01
Elorfin <penin.axel@gmail.com>
2023-07-10 17:04:53 +0200

    [Path] resets migrations

--- src/plugin/path/Installation/Migrations/Version20230426080000.php
+++ src/plugin/path/Installation/Migrations/Version20230426080000.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Innova\PathBundle\Installation\Migrations\pdo_mysql;
+namespace Innova\PathBundle\Installation\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -8,59 +8,33 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated migration based on mapping information: modify it with caution.
  *
- * Generation date: 2020/07/01 09:02:51
+ * Generation date: 2023/07/10 02:57:41
  */
-class Version20190821151113 extends AbstractMigration
+final class Version20230426080000 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         $this->addSql('
             CREATE TABLE innova_step (
                 id INT AUTO_INCREMENT NOT NULL, 
-                activity_id INT DEFAULT NULL, 
-                parameters_id INT DEFAULT NULL, 
                 parent_id INT DEFAULT NULL, 
                 path_id INT DEFAULT NULL, 
                 resource_id INT DEFAULT NULL, 
-                activity_height INT DEFAULT NULL, 
                 step_order INT NOT NULL, 
                 title VARCHAR(255) DEFAULT NULL, 
                 description LONGTEXT DEFAULT NULL, 
                 numbering VARCHAR(255) DEFAULT NULL, 
                 showResourceHeader TINYINT(1) NOT NULL, 
-                evaluated TINYINT(1) NOT NULL, 
                 slug VARCHAR(128) NOT NULL, 
                 uuid VARCHAR(36) NOT NULL, 
                 poster VARCHAR(255) DEFAULT NULL, 
                 UNIQUE INDEX UNIQ_86F48567D17F50A6 (uuid), 
-                INDEX IDX_86F4856781C06096 (activity_id), 
-                INDEX IDX_86F4856788BD9C1F (parameters_id), 
                 INDEX IDX_86F48567727ACA70 (parent_id), 
                 INDEX IDX_86F48567D96C566B (path_id), 
                 INDEX IDX_86F4856789329D25 (resource_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
-        $this->addSql("
-            CREATE TABLE innova_path (
-                id INT AUTO_INCREMENT NOT NULL, 
-                structure LONGTEXT DEFAULT NULL, 
-                modified TINYINT(1) NOT NULL, 
-                numbering VARCHAR(255) NOT NULL, 
-                description LONGTEXT DEFAULT NULL, 
-                manual_progression_allowed TINYINT(1) NOT NULL, 
-                show_overview TINYINT(1) DEFAULT '1' NOT NULL, 
-                secondaryResourcesTarget VARCHAR(255) DEFAULT '_self' NOT NULL, 
-                score_total DOUBLE PRECISION DEFAULT '100' NOT NULL, 
-                success_score DOUBLE PRECISION DEFAULT NULL, 
-                show_score TINYINT(1) NOT NULL, 
-                uuid VARCHAR(36) NOT NULL, 
-                resourceNode_id INT DEFAULT NULL, 
-                UNIQUE INDEX UNIQ_CE19F054D17F50A6 (uuid), 
-                UNIQUE INDEX UNIQ_CE19F054B87FAB32 (resourceNode_id), 
-                PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
-        ");
         $this->addSql('
             CREATE TABLE innova_step_secondary_resource (
                 id INT AUTO_INCREMENT NOT NULL, 
@@ -70,33 +44,48 @@ class Version20190821151113 extends AbstractMigration
                 INDEX IDX_4E895FCB73B21E9C (step_id), 
                 INDEX IDX_4E895FCB89329D25 (resource_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
+        $this->addSql("
+            CREATE TABLE innova_path (
+                id INT AUTO_INCREMENT NOT NULL, 
+                resource_id INT DEFAULT NULL, 
+                end_back_target_id INT DEFAULT NULL, 
+                numbering VARCHAR(255) NOT NULL, 
+                manual_progression_allowed TINYINT(1) NOT NULL, 
+                secondaryResourcesTarget VARCHAR(255) DEFAULT '_self' NOT NULL, 
+                score_total DOUBLE PRECISION DEFAULT '100' NOT NULL, 
+                success_score DOUBLE PRECISION DEFAULT NULL, 
+                show_score TINYINT(1) NOT NULL, 
+                uuid VARCHAR(36) NOT NULL, 
+                show_overview TINYINT(1) NOT NULL, 
+                description LONGTEXT DEFAULT NULL, 
+                show_end_page TINYINT(1) NOT NULL, 
+                end_message LONGTEXT DEFAULT NULL, 
+                end_navigation TINYINT(1) NOT NULL, 
+                end_back_type LONGTEXT DEFAULT NULL, 
+                end_back_label LONGTEXT DEFAULT NULL, 
+                show_workspace_certificates TINYINT(1) NOT NULL, 
+                success_message LONGTEXT DEFAULT NULL, 
+                failure_message LONGTEXT DEFAULT NULL, 
+                resourceNode_id INT DEFAULT NULL, 
+                UNIQUE INDEX UNIQ_CE19F054D17F50A6 (uuid), 
+                INDEX IDX_CE19F05489329D25 (resource_id), 
+                UNIQUE INDEX UNIQ_CE19F054B87FAB32 (resourceNode_id), 
+                INDEX IDX_CE19F05448FD0A1B (end_back_target_id), 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ");
         $this->addSql('
             CREATE TABLE innova_path_progression (
                 id INT AUTO_INCREMENT NOT NULL, 
                 step_id INT DEFAULT NULL, 
                 user_id INT DEFAULT NULL, 
                 progression_status VARCHAR(255) NOT NULL, 
-                authorized_access TINYINT(1) NOT NULL, 
-                locked_access TINYINT(1) NOT NULL, 
-                lockedcall_access TINYINT(1) NOT NULL, 
                 INDEX IDX_960F966A73B21E9C (step_id), 
                 INDEX IDX_960F966AA76ED395 (user_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
-        ');
-        $this->addSql('
-            ALTER TABLE innova_step 
-            ADD CONSTRAINT FK_86F4856781C06096 FOREIGN KEY (activity_id) 
-            REFERENCES claro_activity (id) 
-            ON DELETE SET NULL
-        ');
-        $this->addSql('
-            ALTER TABLE innova_step 
-            ADD CONSTRAINT FK_86F4856788BD9C1F FOREIGN KEY (parameters_id) 
-            REFERENCES claro_activity_parameters (id) 
-            ON DELETE SET NULL
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
             ALTER TABLE innova_step 
@@ -107,19 +96,14 @@ class Version20190821151113 extends AbstractMigration
         $this->addSql('
             ALTER TABLE innova_step 
             ADD CONSTRAINT FK_86F48567D96C566B FOREIGN KEY (path_id) 
-            REFERENCES innova_path (id)
+            REFERENCES innova_path (id) 
+            ON DELETE CASCADE
         ');
         $this->addSql('
             ALTER TABLE innova_step 
             ADD CONSTRAINT FK_86F4856789329D25 FOREIGN KEY (resource_id) 
             REFERENCES claro_resource_node (id) 
             ON DELETE SET NULL
-        ');
-        $this->addSql('
-            ALTER TABLE innova_path 
-            ADD CONSTRAINT FK_CE19F054B87FAB32 FOREIGN KEY (resourceNode_id) 
-            REFERENCES claro_resource_node (id) 
-            ON DELETE CASCADE
         ');
         $this->addSql('
             ALTER TABLE innova_step_secondary_resource 
@@ -132,6 +116,24 @@ class Version20190821151113 extends AbstractMigration
             ADD CONSTRAINT FK_4E895FCB89329D25 FOREIGN KEY (resource_id) 
             REFERENCES claro_resource_node (id) 
             ON DELETE CASCADE
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            ADD CONSTRAINT FK_CE19F05489329D25 FOREIGN KEY (resource_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE SET NULL
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            ADD CONSTRAINT FK_CE19F054B87FAB32 FOREIGN KEY (resourceNode_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE CASCADE
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            ADD CONSTRAINT FK_CE19F05448FD0A1B FOREIGN KEY (end_back_target_id) 
+            REFERENCES claro_resource_node (id) 
+            ON DELETE SET NULL
         ');
         $this->addSql('
             ALTER TABLE innova_path_progression 
@@ -154,25 +156,49 @@ class Version20190821151113 extends AbstractMigration
             DROP FOREIGN KEY FK_86F48567727ACA70
         ');
         $this->addSql('
+            ALTER TABLE innova_step 
+            DROP FOREIGN KEY FK_86F48567D96C566B
+        ');
+        $this->addSql('
+            ALTER TABLE innova_step 
+            DROP FOREIGN KEY FK_86F4856789329D25
+        ');
+        $this->addSql('
             ALTER TABLE innova_step_secondary_resource 
             DROP FOREIGN KEY FK_4E895FCB73B21E9C
+        ');
+        $this->addSql('
+            ALTER TABLE innova_step_secondary_resource 
+            DROP FOREIGN KEY FK_4E895FCB89329D25
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            DROP FOREIGN KEY FK_CE19F05489329D25
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            DROP FOREIGN KEY FK_CE19F054B87FAB32
+        ');
+        $this->addSql('
+            ALTER TABLE innova_path 
+            DROP FOREIGN KEY FK_CE19F05448FD0A1B
         ');
         $this->addSql('
             ALTER TABLE innova_path_progression 
             DROP FOREIGN KEY FK_960F966A73B21E9C
         ');
         $this->addSql('
-            ALTER TABLE innova_step 
-            DROP FOREIGN KEY FK_86F48567D96C566B
+            ALTER TABLE innova_path_progression 
+            DROP FOREIGN KEY FK_960F966AA76ED395
         ');
         $this->addSql('
             DROP TABLE innova_step
         ');
         $this->addSql('
-            DROP TABLE innova_path
+            DROP TABLE innova_step_secondary_resource
         ');
         $this->addSql('
-            DROP TABLE innova_step_secondary_resource
+            DROP TABLE innova_path
         ');
         $this->addSql('
             DROP TABLE innova_path_progression
