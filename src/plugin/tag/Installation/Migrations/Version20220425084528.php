fc44e916a8de0a1dda8a79942a4447ee8988df15
Elorfin <penin.axel@gmail.com>
2023-07-10 17:06:06 +0200

    [Tag] resets migrations and updaters

--- src/plugin/tag/Installation/Migrations/Version20220425084528.php
+++ src/plugin/tag/Installation/Migrations/Version20220425084528.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Claroline\TagBundle\Installation\Migrations\pdo_mysql;
+namespace Claroline\TagBundle\Installation\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -8,9 +8,9 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated migration based on mapping information: modify it with caution.
  *
- * Generation date: 2020/07/01 08:23:20
+ * Generation date: 2023/07/10 03:02:30
  */
-class Version20181212090304 extends AbstractMigration
+final class Version20220425084528 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
@@ -24,32 +24,24 @@ class Version20181212090304 extends AbstractMigration
                 INDEX IDX_1EA1E15DBAD26311 (tag_id), 
                 UNIQUE INDEX `unique` (object_id, object_class, tag_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
             CREATE TABLE claro_tagbundle_tag (
                 id INT AUTO_INCREMENT NOT NULL, 
-                user_id INT DEFAULT NULL, 
                 tag_name VARCHAR(255) NOT NULL, 
                 uuid VARCHAR(36) NOT NULL, 
                 description LONGTEXT DEFAULT NULL, 
                 color VARCHAR(255) DEFAULT NULL, 
+                UNIQUE INDEX UNIQ_6E5EC9DB02CC1B0 (tag_name), 
                 UNIQUE INDEX UNIQ_6E5EC9DD17F50A6 (uuid), 
-                INDEX IDX_6E5EC9DA76ED395 (user_id), 
-                UNIQUE INDEX `unique` (tag_name, user_id), 
                 PRIMARY KEY(id)
-            ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         ');
         $this->addSql('
             ALTER TABLE claro_tagbundle_tagged_object 
             ADD CONSTRAINT FK_1EA1E15DBAD26311 FOREIGN KEY (tag_id) 
             REFERENCES claro_tagbundle_tag (id) 
-            ON DELETE CASCADE
-        ');
-        $this->addSql('
-            ALTER TABLE claro_tagbundle_tag 
-            ADD CONSTRAINT FK_6E5EC9DA76ED395 FOREIGN KEY (user_id) 
-            REFERENCES claro_user (id) 
             ON DELETE CASCADE
         ');
     }
