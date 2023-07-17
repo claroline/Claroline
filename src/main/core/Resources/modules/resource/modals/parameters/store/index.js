881e7b6a8da70f1dfbb6743e7f3234c170cbd3fb
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2023-07-17 17:25:43 +0200

    [Resource] full node loading in resource config (#2541)
    * config resourceNode
    
    * bug endpoint
    
    * typo
    
    * bug endpoint fixed
    
    * PHPSC fixes

--- src/main/core/Resources/modules/resource/modals/parameters/store/index.js
+++ src/main/core/Resources/modules/resource/modals/parameters/store/index.js
@@ -4,9 +4,11 @@
 
 import {reducer} from '#/main/core/resource/modals/parameters/store/reducer'
 import {selectors} from '#/main/core/resource/modals/parameters/store/selectors'
+import {actions} from '#/main/core/resource/modals/parameters/store/actions'
 
 // export store module
 export {
   reducer,
-  selectors
+  selectors,
+  actions
 }
