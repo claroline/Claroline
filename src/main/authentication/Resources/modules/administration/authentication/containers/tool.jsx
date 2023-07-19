a5b750a7a13909319f1c32ee74ed2dd3beb0a953
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-07-19 10:40:53 +0200

    ESLint fixes

--- src/main/authentication/Resources/modules/administration/authentication/containers/tool.jsx
+++ src/main/authentication/Resources/modules/administration/authentication/containers/tool.jsx
@@ -1,6 +1,5 @@
 import {connect} from 'react-redux'
 
-import {actions as formActions} from '#/main/app/content/form/store'
 import {selectors as toolSelectors} from '#/main/core/tool/store'
 
 import {AuthenticationTool as AuthenticationComponent} from '#/main/authentication/administration/authentication/components/tool'
@@ -9,11 +8,6 @@ const AuthenticationTool =
   connect(
     (state) => ({
       path: toolSelectors.path(state)
-    }),
-    dispatch => ({
-      updateProp(prop, value) {
-        dispatch(formActions.updateProp(selectors.FORM_NAME, prop, value))
-      }
     })
   )(AuthenticationComponent)
 
