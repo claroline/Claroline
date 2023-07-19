3876ee0d037e40252597c0c4c1af6971ba238909
Elorfin <penin.axel@gmail.com>
2023-07-19 14:42:28 +0200

    fixes 14.0 installation

--- src/main/core/Installation/Updater/Updater140000.php
+++ src/main/core/Installation/Updater/Updater140000.php
@@ -2,20 +2,29 @@
 
 namespace Claroline\CoreBundle\Installation\Updater;
 
+use Claroline\InstallationBundle\Bundle\InstallableInterface;
 use Claroline\InstallationBundle\Updater\Updater;
 use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class Updater140000 extends Updater
 {
+    private KernelInterface $kernel;
     private Connection $connection;
 
-    public function __construct(Connection $connection)
-    {
+    public function __construct(
+        KernelInterface $kernel,
+        Connection $connection
+    ) {
+        $this->kernel = $kernel;
         $this->connection = $connection;
     }
 
     public function preUpdate()
     {
+        // Adds migration FQCN in the versions tables (required by new doctrine migrations version)
+        $this->renameMigrations();
+
         // the namespace of the migrations has changed (eg. removed the `pdo_mysql` part)
         // Doctrine will try to re-execute migrations because of the renaming
         // we need to update the version classnames in the DB to avoid breaking updates
@@ -65,29 +74,49 @@ class Updater140000 extends Updater
                     ]);
                 }
             }
+        }
+    }
 
-            /*$lastVersion = array_pop($versions);
-            if ($lastVersion) {
-                // clean table (we only need to keep the last version)
-                $deleteQuery = $this->connection->prepare("
-                    DELETE FROM {$table}
-                    WHERE `version` != :lastVersion
-                ");
+    private function renameMigrations(): void
+    {
+        $toMigrate = [];
 
-                $deleteQuery->executeQuery(['lastVersion' => $lastVersion['version']]);
-
-                if (false !== strpos($lastVersion['version'], 'pdo_mysql') && !class_exists($lastVersion['version'])) {
-                    // version has been renamed
-                    $updateVersionQuery = $this->connection->prepare("
-                        UPDATE {$table} SET `version` = :updatedVersion WHERE version = :version
-                    ");
-
-                    $updateVersionQuery->executeQuery([
-                        'version' => $lastVersion['version'],
-                        'updatedVersion' => str_replace('\\pdo_mysql', '', $lastVersion['version']),
-                    ]);
+        $bundles = $this->kernel->getBundles();
+        foreach ($bundles as $bundle) {
+            if ($bundle instanceof InstallableInterface) {
+                $installer = $bundle->getAdditionalInstaller();
+                if ($installer && $installer->hasMigrations()) {
+                    $toMigrate[] = $bundle;
                 }
-            }*/
+            }
+        }
+
+        foreach ($toMigrate as $bundleToMigrate) {
+            $migrationsTableName = 'doctrine_'.strtolower($bundleToMigrate->getName()).'_versions';
+
+            $createQuery = $this->connection->prepare("
+                CREATE TABLE IF NOT EXISTS {$migrationsTableName} (
+                  `version` VARCHAR(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+                  `executed_at` DATETIME DEFAULT NULL,
+                  `execution_time` INT DEFAULT NULL,
+                  PRIMARY KEY (`version`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci
+            ");
+            $createQuery->executeQuery();
+
+            $alterQuery = $this->connection->prepare("
+                ALTER TABLE {$migrationsTableName} CHANGE `version` `version` VARCHAR(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL
+            ");
+
+            $alterQuery->executeQuery();
+
+            $updateVersionQuery = $this->connection->prepare("
+                UPDATE {$migrationsTableName} SET `version` = CONCAT(:migrationNamespace, `version`) WHERE `version` NOT LIKE '%Migrations%'
+            ");
+
+            $updateVersionQuery->executeQuery([
+                'migrationNamespace' => $bundleToMigrate->getNamespace().'\\Installation\\Migrations\\pdo_mysql\\Version',
+            ]);
         }
     }
 }
