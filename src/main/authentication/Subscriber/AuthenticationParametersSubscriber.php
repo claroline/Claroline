82ce1c3c8940b7693fa9cf6ab73a78fc69ed1e5c
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-07-25 11:31:18 +0200

    fixe bug empty data

--- src/main/authentication/Subscriber/AuthenticationParametersSubscriber.php
+++ src/main/authentication/Subscriber/AuthenticationParametersSubscriber.php
@@ -3,26 +3,25 @@
 namespace Claroline\AuthenticationBundle\Subscriber;
 
 use Claroline\AppBundle\API\SerializerProvider;
-use Claroline\AppBundle\Persistence\ObjectManager;
-use Claroline\AuthenticationBundle\Entity\AuthenticationParameters;
+use Claroline\AuthenticationBundle\Manager\AuthenticationManager;
 use Claroline\CoreBundle\Event\GenericDataEvent;
 use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class AuthenticationParametersSubscriber implements EventSubscriberInterface
 {
-    private SerializerProvider $serializer;
-    private ObjectManager $objectManager;
     private PlatformConfigurationHandler $config;
+    private SerializerProvider $serializer;
+    private AuthenticationManager $authenticationManager;
 
     public function __construct(
         PlatformConfigurationHandler $config,
         SerializerProvider $serializer,
-        ObjectManager $objectManager
+        AuthenticationManager $authenticationManager
     ) {
         $this->config = $config;
         $this->serializer = $serializer;
-        $this->objectManager = $objectManager;
+        $this->authenticationManager = $authenticationManager;
     }
 
     public static function getSubscribedEvents(): array
@@ -34,11 +33,26 @@ class AuthenticationParametersSubscriber implements EventSubscriberInterface
 
     public function onPopulateConfig(GenericDataEvent $event): void
     {
-        $authenticationParameters = $this->objectManager->getRepository(AuthenticationParameters::class)->findOneBy([]);
+        $authenticationParameters = $this->authenticationManager->getParameters();
+        if ($authenticationParameters->getMinLength() === null) {
+
+            $authenticationParameters->setMinLength(0);
+            $authenticationParameters->setRequireLowercase(false);
+            $authenticationParameters->setRequireUppercase(false);
+            $authenticationParameters->setRequireSpecialChar(false);
+            $authenticationParameters->setRequireNumber(false);
+
+            $authenticationParameters->setHelpMessage('Default help message');
+            $authenticationParameters->setChangePassword(true);
+            $authenticationParameters->setInternalAccount(true);
+            $authenticationParameters->setShowClientIp(false);
+            $authenticationParameters->setRedirectAfterLoginOption('default_option');
+            $authenticationParameters->setRedirectAfterLoginUrl(null);
+
+            $this->authenticationManager->updateParameters($authenticationParameters);
+        }
+
         $serializedParameters = $this->serializer->serialize($authenticationParameters);
-        $event->setResponse([
-            'password' => $serializedParameters['password'],
-            'login' => $serializedParameters['login'],
-        ]);
+        $event->setResponse($serializedParameters);
     }
 }
