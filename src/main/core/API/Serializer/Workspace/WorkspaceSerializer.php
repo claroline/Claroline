7377a64a2eb2b434ede4ae33792259edea093a56
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-07-27 16:00:26 +0200

    PHPCSFixer fixes

--- src/main/core/API/Serializer/Workspace/WorkspaceSerializer.php
+++ src/main/core/API/Serializer/Workspace/WorkspaceSerializer.php
@@ -90,7 +90,7 @@ class WorkspaceSerializer
                 'meta' => [
                     // move outside meta
                     'model' => $workspace->isModel(),
-                ]
+                ],
             ];
         }
 
