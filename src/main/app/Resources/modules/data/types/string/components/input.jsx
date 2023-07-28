9fd8ee849fd30426bbd8c83fc384e4e951dff097
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-07-28 18:32:46 +0200

    maximum and minimum length on string field

--- src/main/app/Resources/modules/data/types/string/components/input.jsx
+++ src/main/app/Resources/modules/data/types/string/components/input.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react'
 import classes from 'classnames'
 
+import {trans} from '#/main/app/intl'
 import {PropTypes as T, implementPropTypes} from '#/main/app/prop-types'
 import {DataInput as DataInputTypes} from '#/main/app/data/types/prop-types'
 
@@ -12,7 +13,9 @@ class StringInput extends PureComponent {
   }
 
   onChange(e) {
-    this.props.onChange(e.target.value)
+    if (!this.props.maxLength || e.target.value.length <= this.props.maxLength) {
+      this.props.onChange(e.target.value)
+    }
   }
 
   render() {
@@ -28,20 +31,60 @@ class StringInput extends PureComponent {
       autoComplete: this.props.autoComplete
     }
 
+    const charsTyped = this.props.value ? this.props.value.length : 0
+    const minLength = this.props.minLength
+    const maxLength = this.props.maxLength
+
     if (this.props.long) {
       return (
-        <textarea
-          {...commonProps}
-          rows={this.props.minRows}
-        />
+        <div>
+          <textarea
+            {...commonProps}
+            rows={this.props.minRows}
+          />
+        </div>
       )
     }
 
     return (
-      <input
-        {...commonProps}
-        type="text"
-      />
+      <div>
+        <input
+          {...commonProps}
+          type="text"
+        />
+
+        {(minLength || maxLength) && charsTyped !== 0 &&
+          <div className="chars-remaining">
+            {charsTyped} {
+            trans('characters_typed', {}, 'platform')
+          }
+          </div>
+        }
+
+        <div className="length-rules">
+          {this.props.minLength &&
+            <div className="px-2">
+              <span className="icon-with-text-right">
+                <i className="fa fa-circle-exclamation" />
+              </span>
+              <span>
+                {minLength} {trans('charsMin_length', {}, 'platform')}
+              </span>
+            </div>
+          }
+
+          {this.props.maxLength &&
+            <div className="px-2">
+              <span className="icon-with-text-right">
+                <i className="fa fa-circle-exclamation" />
+              </span>
+              <span>
+                {maxLength} {trans('charsMax_length', {}, 'platform')}
+              </span>
+            </div>
+          }
+        </div>
+      </div>
     )
   }
 }
