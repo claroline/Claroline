b1c23897514b8ce3a90bd758bf4af73babcbc2c2
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-09-07 11:30:43 +0200

    use youtube video thumbnail as a resource thumbnail

--- src/integration/youtube/Manager/YouTubeManager.php
+++ src/integration/youtube/Manager/YouTubeManager.php
@@ -2,8 +2,22 @@
 
 namespace Claroline\YouTubeBundle\Manager;
 
+use Claroline\AppBundle\Manager\File\TempFileManager;
+use Claroline\CoreBundle\Manager\CurlManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class YouTubeManager
 {
+    /** @var CurlManager */
+    private $curlManager;
+    private $tempManager;
+
+    public function __construct(CurlManager $curlManager, TempFileManager $tempManager)
+    {
+        $this->curlManager = $curlManager;
+        $this->tempManager = $tempManager;
+    }
+
     public function checkUrl(string $url): ?string
     {
         $videoId = $this->extractVideoId($url);
@@ -28,5 +42,47 @@ class YouTubeManager
         }
 
         return $query['v'];
+    }
+
+    public function getThumbnailUrl(string $url): ?string
+    {
+        $videoId = $this->extractVideoId($url);
+
+        if (empty($videoId)) {
+            return null;
+        }
+
+        return "https://img.youtube.com/vi/{$videoId}/mqdefault.jpg";
+    }
+
+    public function downloadThumbnail(string $url): ?string
+    {
+        $thumbnailUrl = $this->getThumbnailUrl($url);
+
+        if (!$thumbnailUrl) {
+            return null;
+        }
+
+        try {
+            $thumbnailFile = $this->curlManager->exec($thumbnailUrl);
+        } catch (\Exception) {
+            return null;
+        }
+
+        return $thumbnailFile;
+    }
+
+    public function getTemporaryThumbnailFile(string $url): ?UploadedFile
+    {
+        $thumbnailData = $this->downloadThumbnail($url);
+
+        if (!$thumbnailData) {
+            return null;
+        }
+
+        $tempFileName = $this->tempManager->generate();
+        file_put_contents($tempFileName, $thumbnailData);
+
+        return new UploadedFile($tempFileName, 'youtube_thumbnail.jpg', 'image/jpeg', null, true);
     }
 }
--- src/integration/youtube/Subscriber/Resource/VideoSubscriber.php
+++ src/integration/youtube/Subscriber/Resource/VideoSubscriber.php
@@ -2,12 +2,19 @@
 
 namespace Claroline\YouTubeBundle\Subscriber\Resource;
 
+use Claroline\AppBundle\API\Crud;
 use Claroline\AppBundle\API\Serializer\SerializerInterface;
 use Claroline\AppBundle\API\SerializerProvider;
+use Claroline\AppBundle\Event\Crud\CreateEvent;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\File\PublicFile;
+use Claroline\CoreBundle\Entity\Resource\ResourceNode;
 use Claroline\CoreBundle\Entity\User;
 use Claroline\CoreBundle\Event\Resource\LoadResourceEvent;
+use Claroline\CoreBundle\Manager\FileManager;
 use Claroline\YouTubeBundle\Entity\Video;
 use Claroline\YouTubeBundle\Manager\EvaluationManager;
+use Claroline\YouTubeBundle\Manager\YouTubeManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Twig\Environment;
@@ -22,23 +29,40 @@ class VideoSubscriber implements EventSubscriberInterface
     private $serializer;
     /** @var EvaluationManager */
     private $evaluationManager;
+    /** @var FileManager */
+    private $fileManager;
+    /** @var YouTubeManager */
+    private $youtubeManager;
+    /** @var Crud */
+    private $crud;
+    /** @var ObjectManager */
+    private $om;
 
     public function __construct(
         TokenStorageInterface $tokenStorage,
         Environment $templating,
         SerializerProvider $serializer,
-        EvaluationManager $evaluationManager
+        EvaluationManager $evaluationManager,
+        FileManager $fileManager,
+        YouTubeManager $youtubeManager,
+        Crud $crud,
+        ObjectManager $om
     ) {
         $this->tokenStorage = $tokenStorage;
         $this->templating = $templating;
         $this->serializer = $serializer;
         $this->evaluationManager = $evaluationManager;
+        $this->fileManager = $fileManager;
+        $this->youtubeManager = $youtubeManager;
+        $this->crud = $crud;
+        $this->om = $om;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
             'resource.youtube_video.load' => 'onLoad',
+            Crud::getEventName('create', 'post', Video::class) => 'onCreate',
         ];
     }
 
@@ -57,5 +81,26 @@ class VideoSubscriber implements EventSubscriberInterface
             'url' => $video->getUrl(),
         ]);
         $event->stopPropagation();
+    }
+
+    public function onCreate(CreateEvent $event): void
+    {
+        $video = $event->getObject();
+        $resourceNode = $video->getResourceNode();
+
+        if (!$resourceNode->getThumbnail()) {
+            $uploadedFile = $this->youtubeManager->getTemporaryThumbnailFile($video->getUrl());
+
+            if ($uploadedFile) {
+                $publicFile = $this->crud->create(PublicFile::class, [], ['file' => $uploadedFile]);
+
+                $resourceNode->setThumbnail($publicFile->getUrl());
+                $this->om->persist($resourceNode);
+
+                $this->fileManager->linkFile(ResourceNode::class, $resourceNode->getUuid(), $publicFile->getUrl());
+
+                $this->om->flush();
+            }
+        }
     }
 }
