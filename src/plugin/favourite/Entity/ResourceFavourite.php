ce7cda7e4f9c9f0098b57e993d7eee5c7e5ba4df
Elorfin <penin.axel@gmail.com>
2023-09-07 10:33:49 +0200

    [Favourites] fixes typo

--- src/plugin/favourite/Entity/ResourceFavourite.php
+++ src/plugin/favourite/Entity/ResourceFavourite.php
@@ -7,14 +7,17 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
+ *
  * @ORM\Table(name="claro_resource_favourite", uniqueConstraints={
- *     @ORM\uniqueConstraint(columns={"user_id", "resource_node_id"})
+ *
+ *     @ORM\UniqueConstraint(columns={"user_id", "resource_node_id"})
  * })
  */
 class ResourceFavourite extends AbstractFavourite
 {
     /**
      * @ORM\ManyToOne(targetEntity="Claroline\CoreBundle\Entity\Resource\ResourceNode")
+     *
      * @ORM\JoinColumn(name="resource_node_id", onDelete="CASCADE")
      *
      * @var ResourceNode
