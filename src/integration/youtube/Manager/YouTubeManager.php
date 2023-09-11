6068f1bc4a01eedc6275303e311b7018076daac7
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2023-09-11 09:32:04 +0200

    [YouTube] use youtube video thumbnail as a resource thumbnail (#2586)
    * use youtube video thumbnail as a resource thumbnail
    
    * pr review

--- src/integration/youtube/Manager/YouTubeManager.php
+++ src/integration/youtube/Manager/YouTubeManager.php
@@ -2,8 +2,38 @@
 
 namespace Claroline\YouTubeBundle\Manager;
 
+use Claroline\AppBundle\API\Crud;
+use Claroline\AppBundle\Manager\File\TempFileManager;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\File\PublicFile;
+use Claroline\CoreBundle\Entity\Resource\ResourceNode;
+use Claroline\CoreBundle\Manager\CurlManager;
+use Claroline\CoreBundle\Manager\FileManager;
+use Claroline\YouTubeBundle\Entity\Video;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class YouTubeManager
 {
+    private CurlManager $curlManager;
+    private TempFileManager $tempManager;
+    private Crud $crud;
+    private ObjectManager $om;
+    private FileManager $fileManager;
+
+    public function __construct(
+        CurlManager $curlManager,
+        TempFileManager $tempManager,
+        Crud $crud,
+        ObjectManager $om,
+        FileManager $fileManager
+    ) {
+        $this->curlManager = $curlManager;
+        $this->tempManager = $tempManager;
+        $this->crud = $crud;
+        $this->om = $om;
+        $this->fileManager = $fileManager;
+    }
+
     public function checkUrl(string $url): ?string
     {
         $videoId = $this->extractVideoId($url);
@@ -28,5 +58,66 @@ class YouTubeManager
         }
 
         return $query['v'];
+    }
+
+    public function handleThumbnailForVideo(Video $video): void
+    {
+        $resourceNode = $video->getResourceNode();
+        if (!$resourceNode->getThumbnail()) {
+            $uploadedFile = $this->getTemporaryThumbnailFile($video->getUrl());
+
+            if ($uploadedFile) {
+                $publicFile = $this->crud->create(PublicFile::class, [], ['file' => $uploadedFile]);
+
+                $resourceNode->setThumbnail($publicFile->getUrl());
+                $this->om->persist($resourceNode);
+
+                $this->fileManager->linkFile(ResourceNode::class, $resourceNode->getUuid(), $publicFile->getUrl());
+
+                $this->om->flush();
+            }
+        }
+    }
+
+    public function getThumbnailUrl(string $url): ?string
+    {
+        $videoId = $this->extractVideoId($url);
+
+        if (empty($videoId)) {
+            return null;
+        }
+
+        return "https://img.youtube.com/vi/{$videoId}/mqdefault.jpg";
+    }
+
+    public function downloadThumbnail(string $url): ?string
+    {
+        $thumbnailUrl = $this->getThumbnailUrl($url);
+
+        if (!$thumbnailUrl) {
+            return null;
+        }
+
+        try {
+            $thumbnailFile = $this->curlManager->exec($thumbnailUrl);
+        } catch (\Exception) {
+            return null;
+        }
+
+        return $thumbnailFile;
+    }
+
+    public function getTemporaryThumbnailFile(string $url): ?UploadedFile
+    {
+        $thumbnailData = $this->downloadThumbnail($url);
+
+        if (!$thumbnailData) {
+            return null;
+        }
+
+        $tempFileName = $this->tempManager->generate();
+        file_put_contents($tempFileName, $thumbnailData);
+
+        return new UploadedFile($tempFileName, 'youtube_thumbnail.jpg', 'image/jpeg', null, true);
     }
 }
