2969ceb0df15b473a03c6ee09c8707598e09aad4
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2023-09-11 11:00:50 +0200

    [Community] Improving registration process to workspaces (#2590)
    * register users
    
    * register groups

--- src/main/community/Resources/modules/actions/workspace/register-groups.js
+++ src/main/community/Resources/modules/actions/workspace/register-groups.js
@@ -2,10 +2,7 @@ import {trans} from '#/main/app/intl/translation'
 import {hasPermission} from '#/main/app/security'
 import {ASYNC_BUTTON, MODAL_BUTTON} from '#/main/app/buttons'
 
-import {actions as modalActions} from '#/main/app/overlays/modal/store'
-
 import {MODAL_GROUPS} from '#/main/community/modals/groups'
-import {MODAL_WORKSPACE_ROLES} from '#/main/core/workspace/modals/roles'
 
 /**
  * Registers selected groups to some workspaces.
@@ -16,7 +13,6 @@ export default (workspaces, refresher) => ({
   icon: 'fa fa-fw fa-users',
   label: trans('register_groups'),
   displayed: -1 !== workspaces.findIndex(workspace => !workspace.meta.model && !workspace.meta.archived && hasPermission('administrate', workspace)),
-  // open a modal to select the list of groups to register
   modal: [MODAL_GROUPS, {
     title: trans('register_groups'),
 
@@ -24,30 +20,14 @@ export default (workspaces, refresher) => ({
     selectAction: (groups) => ({
       type: ASYNC_BUTTON,
       request: {
-        url: ['apiv2_workspace_roles_common', {
-          workspaces: workspaces.map(workspace => workspace.id)
+        url: ['apiv2_workspace_bulk_register_groups', {
+          workspaces: workspaces.map(workspace => workspace.id),
+          groups: groups.map(group => group.id)
         }],
-        success: (response, dispatch) => dispatch(modalActions.showModal(MODAL_WORKSPACE_ROLES, {
-          icon: 'fa fa-fw fa-users',
-          title: trans('register_groups'),
-          roles: response,
-          // send registration request for selected role and groups
-          selectAction: (role) => ({
-            type: ASYNC_BUTTON,
-            label: trans('register', {}, 'actions'),
-            request: {
-              url: ['apiv2_workspace_bulk_register_groups', {
-                role: role.translationKey,
-                workspaces: workspaces.map(workspace => workspace.id),
-                groups: groups.map(group => group.id)
-              }],
-              request: {
-                method: 'PATCH'
-              },
-              success: () => refresher.update(workspaces)
-            }
-          })
-        }))
+        request: {
+          method: 'PATCH'
+        },
+        success: () => refresher.update(workspaces)
       }
     })
   }],
--- src/main/community/Resources/modules/tools/community/user/containers/list.jsx
+++ src/main/community/Resources/modules/tools/community/user/containers/list.jsx
@@ -20,8 +20,8 @@ const UserList = connect(
     unregister(users, workspace) {
       dispatch(actions.unregister(users, workspace))
     },
-    addUsersToRoles(roles, users) {
-      roles.map(role => dispatch(actions.addUsersToRole(role, users)))
+    registerUsers(users) {
+      dispatch(actions.registerUsers(users))
     }
   })
 )(UserListComponent)
