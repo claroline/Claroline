7602f741548c26914058478ff4741e129d190260
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-09-12 13:36:45 +0200

    display the visible side in the editor instead of the question and change the portrait => landscape format

--- src/plugin/flashcard/Resources/modules/resources/flashcard/components/cards.jsx
+++ src/plugin/flashcard/Resources/modules/resources/flashcard/components/cards.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import {PropTypes as T} from 'prop-types'
+
+import {ContentHtml} from '#/main/app/content/components/html'
 import {Toolbar} from '#/main/app/action/components/toolbar'
 import {Card as CardTypes} from '#/plugin/flashcard/resources/flashcard/prop-types'
 
 const Cards = props =>
   <ul className="flashcards">
     {props.cards.map((card) =>
-      <li key={card.id} className="flashcard-preview">
-        {props.actions && (
-          <div className="flashcard-actions">
-            <Toolbar
-              id={`${card.id}-btn`}
-              className="flashcard-actions"
-              buttonName="btn"
-              tooltip="bottom"
-              size="sm"
-              toolbar="more"
-              actions={props.actions(card)}
-            />
+      <li key={card.id} className="flashcard-preview card">
+        <div className="flashcard-content">
+          {props.actions && (
+            <div className="flashcard-actions">
+              <Toolbar
+                id={`${card.id}-btn`}
+                className="flashcard-actions"
+                buttonName="btn"
+                tooltip="bottom"
+                size="sm"
+                toolbar="more"
+                actions={props.actions(card)}
+              />
+            </div>
+          )}
+          <div className="card-body">
+            <ContentHtml>{card.visibleContent}</ContentHtml>
           </div>
-        )}
-        <div className="flashcard-header">
-          <strong>{card.question}</strong>
         </div>
       </li>
     )}
