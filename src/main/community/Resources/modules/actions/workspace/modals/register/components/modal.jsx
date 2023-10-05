453294eee869eb568a253b6f08d70501983c186c
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-10-05 11:05:00 +0200

    Addition of checkbox enabling the choice of roles for user and group registrations from the left-hand menu action

--- src/main/community/Resources/modules/actions/workspace/modals/register/components/modal.jsx
+++ src/main/community/Resources/modules/actions/workspace/modals/register/components/modal.jsx
@@ -1,20 +1,66 @@
-import React from 'react'
-import {PropTypes as T} from 'prop-types'
+import React, {useState} from 'react'
+import {PropTypes as T}  from 'prop-types'
 import omit from 'lodash/omit'
-
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 
+import {url} from '#/main/app/api'
 import {trans} from '#/main/app/intl/translation'
 import {Button} from '#/main/app/action/components/button'
+import {ASYNC_BUTTON, MODAL_BUTTON} from '#/main/app/buttons'
 import {Modal} from '#/main/app/overlays/modal/components/modal'
+import {Checkbox} from '#/main/app/input/components/checkbox'
+import {actions as roleActions} from '#/main/community/tools/community/role/store/actions'
 
 import {selectors} from '#/main/community/actions/workspace/modals/register/store'
 import {UserList}  from '#/main/community/user/components/list'
 import {GroupList} from '#/main/community/group/components/list'
+import {MODAL_ROLES} from '#/main/community/modals/roles'
 
 const RegisterModal = props => {
-  const selectAction = props.selectAction(props.selectedGroups, props.selectedUsers)
+  const [roleSelectionEnabled, setRoleSelectionEnabled] = useState(false)
+  const registerAction = ( roles = '' ) => {
+    const role = typeof roles === 'string' ? roles : (roles[0] && roles[0].translationKey) || undefined
+
+    return ({
+      type: ASYNC_BUTTON,
+      request: {
+        url: url(['apiv2_workspace_register', {role: role}]),
+        request: {
+          method: 'PATCH',
+          body: JSON.stringify({
+            workspaces: props.workspaces.map(workspace => workspace.id),
+            groups: props.selectedGroups.map(group => group.id),
+            users: props.selectedUsers.map(user => user.id)
+          })
+        },
+        success: (workspaces, dispatch) => {
+          if( roles && typeof roles !== 'string' ) {
+            roles.map(role => {
+              if( props.selectedGroups.length > 0 ) {
+                dispatch(roleActions.addGroups(role.id, props.selectedGroups, true))
+              }
+              if( props.selectedUsers.length > 0 ) {
+                dispatch(roleActions.addUsers(role.id, props.selectedUsers, true))
+              }
+            })
+          }
+        }
+      }
+    })
+  }
+
+  const registerWithRoleAction = () => ({
+    label: trans('workspace_register_select_roles'),
+    type: MODAL_BUTTON,
+    modal: [MODAL_ROLES, {
+      title: trans('roles'),
+      url: ['apiv2_workspace_list_roles', {id: props.workspaces[0].id}],
+      filters: [],
+      selectAction: registerAction
+    }]
+  })
+
   return (
     <Modal
       icon="fa fa-fw fa-user-plus"
@@ -46,14 +92,24 @@ const RegisterModal = props => {
         </Tab>
       </Tabs>
 
+      <div className="modal-footer">
+        <Checkbox
+          id="select-role"
+          label={trans('choose_role', {}, 'actions')}
+          className="form-switch form-check-reverse"
+          checked={roleSelectionEnabled}
+          onChange={setRoleSelectionEnabled}
+        />
+      </div>
+
       <Button
-        label={trans('save', {}, 'actions')}
-        {...selectAction}
+        label={trans('register', {}, 'actions')}
+        {...(roleSelectionEnabled ? registerWithRoleAction() : registerAction() )}
         className="modal-btn"
         variant="btn"
         size="lg"
         primary={true}
-        disabled={0 === props.selectedUsers.length && 0 === props.selectedGroups}
+        disabled={props.selectedUsers.length === 0 && props.selectedGroups.length === 0}
         onClick={props.fadeModal}
       />
     </Modal>
@@ -61,9 +117,10 @@ const RegisterModal = props => {
 }
 
 RegisterModal.propTypes = {
+  title: T.string,
+  workspaces: T.array.isRequired,
   selectedUsers: T.array.isRequired,
   selectedGroups: T.array.isRequired,
-  selectAction: T.func.isRequired,
   resetGroups: T.func.isRequired,
   resetUsers: T.func.isRequired,
   fadeModal: T.func.isRequired
--- src/main/community/Resources/modules/actions/workspace/register.js
+++ src/main/community/Resources/modules/actions/workspace/register.js
@@ -1,13 +1,13 @@
 import {trans} from '#/main/app/intl/translation'
 import {hasPermission} from '#/main/app/security'
-import {ASYNC_BUTTON, MODAL_BUTTON} from '#/main/app/buttons'
+import {MODAL_BUTTON} from '#/main/app/buttons'
 
 import {MODAL_REGISTER} from '#/main/community/actions/workspace/modals/register'
 
 /**
  * Registers selected users groups to some workspaces.
  */
-export default (workspaces, refresher) => ({
+export default (workspaces) => ({
   name: 'register-users-groups',
   type: MODAL_BUTTON,
   icon: 'fa fa-fw fa-users',
@@ -15,22 +15,7 @@ export default (workspaces, refresher) => ({
   displayed: -1 !== workspaces.findIndex(workspace => !workspace.meta.model && !workspace.meta.archived && hasPermission('administrate', workspace)),
   modal: [MODAL_REGISTER, {
     title: trans('register_users_groups'),
-
-    selectAction: (groups, users) => ({
-      type: ASYNC_BUTTON,
-      request: {
-        url: ['apiv2_workspace_register'],
-        request: {
-          method: 'PATCH',
-          body: JSON.stringify({
-            workspaces: workspaces.map(workspace => workspace.id),
-            groups: groups.map(group => group.id),
-            users: users.map(user => user.id)
-          })
-        },
-        success: () => refresher.update(workspaces)
-      }
-    })
+    workspaces: workspaces
   }],
   group: trans('registration'),
   scope: ['object', 'collection']
