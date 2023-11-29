8b9dc70411bbb2c3287757aa488415e4e410b1a1
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-11-29 11:23:14 +0100

    widget list my workspace evaluations

---
+++ src/main/evaluation/Resources/modules/data/sources/my-workspace-evaluations.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import {trans} from '#/main/app/intl/translation'
+import {URL_BUTTON} from '#/main/app/buttons'
+
+import {constants} from '#/main/evaluation/constants'
+import {route} from '#/main/core/workspace/routing'
+import {WorkspaceCard} from '#/main/evaluation/workspace/components/card'
+
+export default {
+  name: 'my_workspace_evaluations',
+  parameters: {
+    primaryAction: (evaluation) => ({
+      type: URL_BUTTON,
+      target: `#${route(evaluation.workspace)}`
+    }),
+    definition: [
+      {
+        name: 'status',
+        type: 'choice',
+        label: trans('status'),
+        options: {
+          choices: constants.EVALUATION_STATUSES_SHORT
+        },
+        displayed: true,
+        render: (row) => (
+          <span className={`badge text-bg-${constants.EVALUATION_STATUS_COLOR[row.status]}`}>
+            {constants.EVALUATION_STATUSES_SHORT[row.status]}
+          </span>
+        )
+      }, {
+        name: 'user',
+        type: 'user',
+        label: trans('user'),
+        displayed: true
+      }, {
+        name: 'workspace',
+        type: 'workspace',
+        label: trans('workspace'),
+        displayed: true
+      }, {
+        name: 'date',
+        label: trans('last_activity'),
+        type: 'date',
+        options: {time: true},
+        displayed: true,
+        primary: true
+      }, {
+        name: 'duration',
+        type: 'time',
+        label: trans('duration'),
+        displayed: true,
+        filterable: false
+      }, {
+        name: 'progression',
+        label: trans('progression'),
+        type: 'progression',
+        displayed: true,
+        filterable: false,
+        options: {
+          type: 'learning'
+        }
+      }, {
+        name: 'score',
+        type: 'score',
+        label: trans('score'),
+        calculated: (row) => {
+          if (row.scoreMax) {
+            return {
+              current: row.score,
+              total: row.scoreMax
+            }
+          }
+
+          return null
+        },
+        displayed: true,
+        filterable: false
+      }, {
+        name: 'userDisabled',
+        label: trans('user_disabled', {}, 'community'),
+        type: 'boolean',
+        displayable: false,
+        sortable: false,
+        filterable: true
+      }
+    ],
+    card: WorkspaceCard
+  }
+}
---
+++ src/main/evaluation/Subscriber/DataSource/MyResourceEvaluationSource.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Claroline\EvaluationBundle\Subscriber\DataSource;
+
+use Claroline\AppBundle\API\FinderProvider;
+use Claroline\CoreBundle\Entity\DataSource;
+use Claroline\CoreBundle\Entity\Resource\ResourceUserEvaluation;
+use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Event\DataSource\GetDataEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class MyResourceEvaluationSource implements EventSubscriberInterface
+{
+    private TokenStorageInterface $tokenStorage;
+    private FinderProvider $finder;
+
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        FinderProvider $finder
+    ) {
+        $this->tokenStorage = $tokenStorage;
+        $this->finder = $finder;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'data_source.my_resource_evaluations.load' => 'getData',
+        ];
+    }
+
+    public function getData(GetDataEvent $event): void
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        $options = $event->getOptions();
+
+        if ($user instanceof User) {
+            $options['hiddenFilters']['user'] = $user->getUuid();
+            if (DataSource::CONTEXT_WORKSPACE === $event->getContext()) {
+                $options['hiddenFilters']['workspace'] = $event->getWorkspace()->getUuid();
+            }
+        } else {
+            throw new AccessDeniedException();
+        }
+
+        $event->setData(
+            $this->finder->search(ResourceUserEvaluation::class, $options)
+        );
+
+        $event->stopPropagation();
+    }
+}
