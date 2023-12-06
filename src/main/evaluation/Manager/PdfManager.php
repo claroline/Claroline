c3c6001f770530cd07ad8931ca5c071a3f7ad30b
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2023-12-06 07:55:22 +0100

    [Workspace Evaluation] certificates of achievement in zip format (#2663)
    * zip success certificate
    
    * PR review
    
    * pr review
    
    * bug fix
    
    ---------
    
    Co-authored-by: Elorfin <penin.axel@gmail.com>

--- src/main/evaluation/Manager/PdfManager.php
+++ src/main/evaluation/Manager/PdfManager.php
@@ -2,34 +2,40 @@
 
 namespace Claroline\EvaluationBundle\Manager;
 
+use Claroline\AppBundle\API\Utils\FileBag;
+use Claroline\AppBundle\Manager\File\ArchiveManager;
+use Claroline\AppBundle\Manager\File\TempFileManager;
 use Claroline\AppBundle\Manager\PdfManager as BasePdfManager;
 use Claroline\AppBundle\Manager\PlatformManager;
 use Claroline\CoreBundle\Entity\Workspace\Evaluation;
+use Claroline\CoreBundle\Library\Normalizer\TextNormalizer;
 use Claroline\CoreBundle\Manager\Template\TemplateManager;
 use Claroline\EvaluationBundle\Entity\AbstractEvaluation;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PdfManager
 {
-    /** @var TranslatorInterface */
-    private $translator;
-    /** @var PlatformManager */
-    private $platformManager;
-    /** @var BasePdfManager */
-    private $pdfManager;
-    /** @var TemplateManager */
-    private $templateManager;
+    private TranslatorInterface $translator;
+    private PlatformManager $platformManager;
+    private BasePdfManager $pdfManager;
+    private TemplateManager $templateManager;
+    private TempFileManager $tempFileManager;
+    private ArchiveManager $archiveManager;
 
     public function __construct(
         TranslatorInterface $translator,
         PlatformManager $platformManager,
         BasePdfManager $pdfManager,
-        TemplateManager $templateManager
+        TemplateManager $templateManager,
+        TempFileManager $tempFileManager,
+        ArchiveManager $archiveManager
     ) {
         $this->translator = $translator;
         $this->platformManager = $platformManager;
         $this->pdfManager = $pdfManager;
         $this->templateManager = $templateManager;
+        $this->tempFileManager = $tempFileManager;
+        $this->archiveManager = $archiveManager;
     }
 
     public function getWorkspaceParticipationCertificate(Evaluation $evaluation): ?string
@@ -85,5 +91,52 @@ class PdfManager
             'evaluation_duration' => round($evaluation->getDuration() / 60, 2), // in minutes
             'evaluation_status' => $this->translator->trans('evaluation_'.$evaluation->getStatus().'_status', [], 'workspace'),
         ], $this->templateManager->formatDatePlaceholder('evaluation', $evaluation->getDate()));
+    }
+
+    public function getWorkspaceParticipationCertificates(array $evaluations): ?array
+    {
+        $certificates = [];
+        foreach ($evaluations as $evaluation) {
+            $certificateFilename = TextNormalizer::toKey($evaluation->getWorkspace()->getName().'-'.TextNormalizer::toKey($evaluation->getUser()->getFullName()).'.pdf');
+            $certificates[$certificateFilename] = $this->getWorkspaceParticipationCertificate($evaluation);
+        }
+
+        return $this->generateCertificates($certificates);
+    }
+
+    public function getWorkspaceSuccessCertificates(array $evaluations): ?array
+    {
+        $certificates = [];
+        foreach ($evaluations as $evaluation) {
+            $certificateFilename = TextNormalizer::toKey($evaluation->getWorkspace()->getName().'-'.TextNormalizer::toKey($evaluation->getUser()->getFullName()).'.pdf');
+            $certificates[$certificateFilename] = $this->getWorkspaceSuccessCertificate($evaluation);
+        }
+
+        return $this->generateCertificates($certificates);
+    }
+
+    private function generateCertificates(array $certificates): ?array
+    {
+        if (empty($certificates)) {
+            return null;
+        }
+
+        $tmpFile = $this->tempFileManager->generate();
+
+        if (1 === count($certificates)) {
+            $certificateNames = array_keys($certificates);
+            file_put_contents($tmpFile, $certificates[$certificateNames[0]]);
+
+            return [$certificateNames[0], $tmpFile];
+        }
+
+        $archive = $this->archiveManager->create($tmpFile, new FileBag());
+        foreach ($certificates as $certificateName => $certificateData) {
+            $archive->addFromString($certificateName, $certificateData);
+        }
+
+        $archive->close();
+
+        return [TextNormalizer::toKey('certificates.zip'), $tmpFile];
     }
 }
