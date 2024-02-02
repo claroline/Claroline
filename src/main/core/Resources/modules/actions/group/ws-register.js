55119f3d52785565e40783619228250ed7bf930c
Elorfin <penin.axel@gmail.com>
2024-02-02 07:44:00 +0100

    [Community] fixes ws registration rights

--- src/main/core/Resources/modules/actions/group/ws-register.js
+++ src/main/core/Resources/modules/actions/group/ws-register.js
@@ -1,30 +1,36 @@
 import {trans} from '#/main/app/intl/translation'
+import {hasPermission} from '#/main/app/security'
 import {ASYNC_BUTTON, MODAL_BUTTON} from '#/main/app/buttons'
 
 import {MODAL_WORKSPACES} from '#/main/core/modals/workspaces'
 
-export default (groups, refresher) => ({
-  name: 'ws-register',
-  type: MODAL_BUTTON,
-  icon: 'fa fa-fw fa-book',
-  label: trans('ws-register', {}, 'actions'),
-  modal: [MODAL_WORKSPACES, {
+export default (groups, refresher) => {
+  const processable = groups.filter(group => hasPermission('administrate', group))
 
-    selectAction: (workspaces) => ({
-      type: ASYNC_BUTTON,
-      request: {
-        url: ['apiv2_workspace_register'],
+  return {
+    name: 'ws-register',
+    type: MODAL_BUTTON,
+    icon: 'fa fa-fw fa-book',
+    label: trans('ws-register', {}, 'actions'),
+    displayed: 0 !== processable.length,
+    modal: [MODAL_WORKSPACES, {
+      url: ['apiv2_workspace_list'],
+      selectAction: (workspaces) => ({
+        type: ASYNC_BUTTON,
         request: {
-          method: 'PATCH',
-          body: JSON.stringify({
-            workspaces: workspaces.map(workspace => workspace.id),
-            groups: groups.map(group => group.id)
-          })
-        },
-        success: () => refresher.update(groups)
-      }
-    })
-  }],
-  group: trans('registration'),
-  scope: ['object', 'collection']
-})
+          url: ['apiv2_workspace_register'],
+          request: {
+            method: 'PATCH',
+            body: JSON.stringify({
+              workspaces: workspaces.map(workspace => workspace.id),
+              groups: processable.map(group => group.id)
+            })
+          },
+          success: () => refresher.update(processable)
+        }
+      })
+    }],
+    group: trans('registration'),
+    scope: ['object', 'collection']
+  }
+}
