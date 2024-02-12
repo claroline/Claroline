946766b61c529d3cfb061f4d94531e7eb104c530
Elorfin <penin.axel@gmail.com>
2024-02-12 16:12:56 +0100

    fixes forms

--- src/main/core/Resources/modules/widget/editor/modals/creation/store/reducer.js
+++ src/main/core/Resources/modules/widget/editor/modals/creation/store/reducer.js
@@ -1,10 +1,10 @@
 import {makeFormReducer} from '#/main/app/content/form/store/reducer'
 
-import {Widget as WidgetTypes} from '#/main/core/widget/prop-types'
+import {WidgetContainer as WidgetContainerTypes} from '#/main/core/widget/prop-types'
 import {selectors} from '#/main/core/widget/editor/modals/creation/store/selectors'
 
 const reducer = makeFormReducer(selectors.STORE_NAME, {
-  data: WidgetTypes.defaultProps
+  data: WidgetContainerTypes.defaultProps
 })
 
 export {
