4a14e1c48a44f6dfedc64ad71addff7b7446568d
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2024-02-19 13:38:48 +0100

    Added youtube player parameters

--- src/integration/youtube/Entity/Video.php
+++ src/integration/youtube/Entity/Video.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity()
+ *
  * @ORM\Table(name="claro_youtube_video")
  */
 class Video extends AbstractResource
@@ -15,6 +16,36 @@ class Video extends AbstractResource
      * @ORM\Column(name="video_id", type="string", nullable=false)
      */
     private string $videoId;
+
+    /**
+     * @ORM\Column(name="timecode_start", type="integer", nullable=true)
+     */
+    private ?int $timecodeStart = null;
+
+    /**
+     * @ORM\Column(name="timecode_end", type="integer", nullable=true)
+     */
+    private ?int $timecodeEnd = null;
+
+    /**
+     * @ORM\Column(name="autoplay", type="boolean", nullable=false)
+     */
+    private bool $autoplay = false;
+
+    /**
+     * @ORM\Column(name="looping", type="boolean", nullable=false)
+     */
+    private bool $looping = false;
+
+    /**
+     * @ORM\Column(name="controls", type="boolean", nullable=false)
+     */
+    private bool $controls = true;
+
+    /**
+     * @ORM\Column(name="resume", type="boolean", nullable=false)
+     */
+    private bool $resume = false;
 
     public function getVideoId(): string
     {
@@ -29,5 +60,65 @@ class Video extends AbstractResource
     public function getUrl(): string
     {
         return 'https://www.youtube.com/watch?v='.$this->videoId;
+    }
+
+    public function getTimecodeStart(): ?int
+    {
+        return $this->timecodeStart;
+    }
+
+    public function setTimecodeStart(?int $timecodeStart): void
+    {
+        $this->timecodeStart = $timecodeStart;
+    }
+
+    public function getTimecodeEnd(): ?int
+    {
+        return $this->timecodeEnd;
+    }
+
+    public function setTimecodeEnd(?int $timecodeEnd): void
+    {
+        $this->timecodeEnd = $timecodeEnd;
+    }
+
+    public function getAutoplay(): bool
+    {
+        return $this->autoplay;
+    }
+
+    public function setAutoplay(bool $autoplay): void
+    {
+        $this->autoplay = $autoplay;
+    }
+
+    public function getLooping(): bool
+    {
+        return $this->looping;
+    }
+
+    public function setLooping(bool $looping): void
+    {
+        $this->looping = $looping;
+    }
+
+    public function getControls(): bool
+    {
+        return $this->controls;
+    }
+
+    public function setControls(bool $controls): void
+    {
+        $this->controls = $controls;
+    }
+
+    public function getResume(): bool
+    {
+        return $this->resume;
+    }
+
+    public function setResume(bool $resume): void
+    {
+        $this->resume = $resume;
     }
 }
--- src/integration/youtube/Resources/modules/prop-types.js
+++ src/integration/youtube/Resources/modules/prop-types.js
@@ -3,10 +3,15 @@ import {PropTypes as T} from 'prop-types'
 const Video = {
   propTypes: {
     videoId: T.string,
-    url: T.string
+    url: T.string,
+    timecodeStart: T.number,
+    timecodeEnd: T.number,
+    autoplay: T.bool,
+    looping: T.bool,
+    controls: T.bool,
+    resume: T.bool
   },
   defaultProps: {
-
   }
 }
 
