72dc158a208cb17d75a0aedda6bb8ddd17536800
Elorfin <penin.axel@gmail.com>
2024-02-26 08:57:14 +0100

    [Tags] fixes creation right

--- src/plugin/tag/Resources/modules/modals/object-tags/containers/modal.jsx
+++ src/plugin/tag/Resources/modules/modals/object-tags/containers/modal.jsx
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux'
 
 import {withReducer} from '#/main/app/store/components/withReducer'
-import {param} from '#/main/app/config'
+import {selectors as configSelectors} from '#/main/app/config/store'
 
 import {ObjectTagsModal as ObjectTagsModalComponent} from '#/plugin/tag/modals/object-tags/components/modal'
 import {actions, reducer, selectors} from '#/plugin/tag/modals/object-tags/store'
@@ -10,7 +10,7 @@ const ObjectTagsModal = withReducer(selectors.STORE_NAME, reducer)(
   connect(
     (state) => ({
       tags: selectors.tags(state),
-      canCreate: param('canCreateTags')
+      canCreate: configSelectors.param(state, 'canCreateTags', false)
     }),
     (dispatch) => ({
       loadTags(objectClass, objects) {
