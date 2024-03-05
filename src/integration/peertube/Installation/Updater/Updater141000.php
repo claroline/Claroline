83a5bcc015bbab3a6c1447949c1f935e937c72b0
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2024-03-05 14:28:46 +0100

    updater video controls

---
+++ src/integration/peertube/Installation/Updater/Updater141000.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Claroline\PeerTubeBundle\Installation\Updater;
+
+use Claroline\InstallationBundle\Updater\Updater;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+
+class Updater141000 extends Updater
+{
+    private Connection $connection;
+
+    public function __construct(
+        Connection $connection
+    ) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function postUpdate(): void
+    {
+        $updateControls = $this->connection->prepare('
+            UPDATE claro_peertube_video
+            AS ptv 
+            SET ptv.controls = true
+        ');
+
+        $updateControls->executeQuery();
+    }
+}
