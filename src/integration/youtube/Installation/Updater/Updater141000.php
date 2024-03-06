a2ce89ae5882809d7ebc7349d96d70a76c00d031
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2024-03-06 08:39:28 +0100

    [Youtube] updater video controls (#2752)

---
+++ src/integration/youtube/Installation/Updater/Updater141000.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Claroline\YouTubeBundle\Installation\Updater;
+
+use Claroline\InstallationBundle\Updater\Updater;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+
+class Updater141000 extends Updater
+{
+    private Connection $connection;
+
+    public function __construct(
+        Connection $connection
+    ) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function postUpdate(): void
+    {
+        $updateControls = $this->connection->prepare('
+            UPDATE claro_youtube_video 
+            AS ytv 
+            SET ytv.controls = true
+        ');
+
+        $updateControls->executeQuery();
+    }
+}
