70547add629ea61224623cd29feb73e66ad45660
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2024-03-06 08:40:34 +0100

    [Peertube] updater video controls (#2753)

--- src/integration/peertube/Installation/ClarolinePeerTubeInstaller.php
+++ src/integration/peertube/Installation/ClarolinePeerTubeInstaller.php
@@ -3,11 +3,19 @@
 namespace Claroline\PeerTubeBundle\Installation;
 
 use Claroline\InstallationBundle\Additional\AdditionalInstaller;
+use Claroline\PeerTubeBundle\Installation\Updater\Updater141000;
 
 class ClarolinePeerTubeInstaller extends AdditionalInstaller
 {
     public function hasMigrations(): bool
     {
         return true;
+    }
+
+    public static function getUpdaters(): array
+    {
+        return [
+            '14.1.0' => Updater141000::class,
+        ];
     }
 }
