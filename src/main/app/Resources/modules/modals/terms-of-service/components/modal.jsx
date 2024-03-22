df6f8cebeba66f325f73cba92d71f524263d2fcd
Elorfin <penin.axel@gmail.com>
2024-03-22 09:16:14 +0100

    displays terms of service on login

--- src/main/app/Resources/modules/modals/terms-of-service/components/modal.jsx
+++ src/main/app/Resources/modules/modals/terms-of-service/components/modal.jsx
@@ -6,6 +6,8 @@ import {trans} from '#/main/app/intl/translation'
 import {Modal} from '#/main/app/overlays/modal/components/modal'
 import {ContentLoader} from '#/main/app/content/components/loader'
 import {ContentHtml} from '#/main/app/content/components/html'
+import {Button} from '#/main/app/action'
+import {CALLBACK_BUTTON} from '#/main/app/buttons'
 
 const TermsOfServiceModal = props =>
   <Modal
@@ -13,6 +15,8 @@ const TermsOfServiceModal = props =>
     icon="fa fa-fw fa-shield"
     title={trans('terms_of_service')}
     bsSize="lg"
+    backdrop="static"
+    closeButton={false}
     onEntering={() => {
       if (!props.loaded) {
         props.fetch()
@@ -29,6 +33,30 @@ const TermsOfServiceModal = props =>
     {(props.loaded && props.content) &&
       <ContentHtml className="modal-body">{props.content}</ContentHtml>
     }
+
+    {props.loaded && props.validate &&
+      <div className="modal-footer">
+        <Button
+          type={CALLBACK_BUTTON}
+          className="btn btn-primary"
+          label={trans('accept_terms_of_service')}
+          callback={() => props.accept().then(() => {
+            props.onAccept()
+            props.fadeModal()
+          })}
+        />
+        <Button
+          className="btn btn-danger"
+          type={CALLBACK_BUTTON}
+          label={trans('refuse_terms_of_service')}
+          dangerous={true}
+          callback={() => {
+            props.onRefuse()
+            props.fadeModal()
+          }}
+        />
+      </div>
+    }
   </Modal>
 
 TermsOfServiceModal.propTypes = {
@@ -36,7 +64,19 @@ TermsOfServiceModal.propTypes = {
   loaded: T.bool.isRequired,
   content: T.string,
 
+  // validation props
+  validate: T.bool,
+  accept: T.func.isRequired,
+  onAccept: T.func,
+  onRefuse: T.func,
+
   fadeModal: T.func.isRequired
+}
+
+TermsOfServiceModal.defaultProps = {
+  validate: false,
+  onAccept: () => true,
+  onRefuse: () => true
 }
 
 export {
