d316fb5d341cac34bd35708f4fcf5bcb4b682161
Rebecca Tinchon <58392030+WolfyWin@users.noreply.github.com>
2024-04-16 07:22:53 +0200

    [Privacy] tos modal (#2781)

--- src/main/privacy/Resources/modules/modals/terms-of-service/components/modal.jsx
+++ src/main/privacy/Resources/modules/modals/terms-of-service/components/modal.jsx
@@ -3,6 +3,8 @@ import {PropTypes as T} from 'prop-types'
 import omit from 'lodash/omit'
 
 import {trans} from '#/main/app/intl/translation'
+import {Button} from '#/main/app/action'
+import {CALLBACK_BUTTON} from '#/main/app/buttons'
 import {Modal} from '#/main/app/overlays/modal/components/modal'
 import {ContentLoader} from '#/main/app/content/components/loader'
 import {ContentHtml} from '#/main/app/content/components/html'
@@ -13,6 +15,8 @@ const TermsOfServiceModal = props =>
     icon="fa fa-fw fa-file-shield"
     title={trans('terms_of_service',{},'privacy')}
     size="lg"
+    backdrop="static"
+    closeButton={false}
     onEntering={() => {
       if (!props.loaded) {
         props.fetch()
@@ -29,14 +33,51 @@ const TermsOfServiceModal = props =>
     {(props.loaded && props.content) &&
       <ContentHtml className="modal-body">{props.content}</ContentHtml>
     }
+
+    {props.loaded && props.validate &&
+      <div className="modal-footer">
+        <Button
+          type={CALLBACK_BUTTON}
+          className="btn btn-primary"
+          label={trans('terms_of_service_accept', {}, 'privacy')}
+          callback={() => props.accept().then(() => {
+            props.onAccept()
+            props.fadeModal()
+          })}
+        />
+        <Button
+          className="btn btn-danger"
+          type={CALLBACK_BUTTON}
+          label={trans('terms_of_service_refuse', {}, 'privacy')}
+          dangerous={true}
+          callback={() => {
+            props.onRefuse()
+            props.fadeModal()
+          }}
+        />
+      </div>
+    }
   </Modal>
 
 TermsOfServiceModal.propTypes = {
   fetch: T.func.isRequired,
   loaded: T.bool.isRequired,
   content: T.string,
+
+  // validation props
+  validate: T.bool,
+  accept: T.func.isRequired,
+  onAccept: T.func,
+  onRefuse: T.func,
+
   fadeModal: T.func.isRequired,
   formData: T.object.isRequired
+}
+
+TermsOfServiceModal.defaultProps = {
+  validate: false,
+  onAccept: () => true,
+  onRefuse: () => true
 }
 
 export {
