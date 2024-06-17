386718cd3afb0637d1690b8caac66cdb46dccb49
Axel Penin <Elorfin@users.noreply.github.com>
2024-06-17 09:56:27 +0200

    fixes ci

--- src/plugin/slideshow/Installation/Migrations/Version20240617074835.php
+++ src/plugin/slideshow/Installation/Migrations/Version20240617074835.php
@@ -14,28 +14,28 @@ final class Version20240617074835 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_slide 
             DROP FOREIGN KEY FK_9F1DBC778B14E343
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_slide 
             ADD CONSTRAINT FK_F8EC39708B14E343 FOREIGN KEY (slideshow_id) 
             REFERENCES claro_slideshow (id) 
             ON DELETE CASCADE
-        ");
+        ');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_slide 
             DROP FOREIGN KEY FK_F8EC39708B14E343
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_slide 
             ADD CONSTRAINT FK_9F1DBC778B14E343 FOREIGN KEY (slideshow_id) 
             REFERENCES claro_slideshow (id)
-        ");
+        ');
     }
 }
