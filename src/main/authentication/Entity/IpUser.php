f63f3193c2a89acc0ca0903370076e1e099b8c63
Axel Penin <Elorfin@users.noreply.github.com>
2024-08-26 13:37:58 +0200

    [Authentication] fixes known ip deletion (#2835)
    * fixes delete by auto id
    
    * Update RequestDecoderTrait.php

--- src/main/authentication/Entity/IpUser.php
+++ src/main/authentication/Entity/IpUser.php
@@ -3,6 +3,7 @@
 namespace Claroline\AuthenticationBundle\Entity;
 
 use Claroline\AppBundle\Entity\Identifier\Id;
+use Claroline\AppBundle\Entity\Identifier\Uuid;
 use Claroline\AppBundle\Entity\Restriction\Locked;
 use Claroline\CoreBundle\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
@@ -12,11 +13,13 @@ use Doctrine\ORM\Mapping as ORM;
  * Used with the IpAuthenticator.
  *
  * @ORM\Table(name="claro_ip_user")
+ *
  * @ORM\Entity
  */
 class IpUser
 {
     use Id;
+    use Uuid;
     use Locked;
 
     /**
@@ -25,7 +28,7 @@ class IpUser
     private ?string $ip = null;
 
     /**
-     * If true, the $ip field contains to ips separated by a , to define the range.
+     * If true, the $ip field contains to ips separated by a comma (,) to define the range.
      *
      * @ORM\Column(name="is_range", type="boolean")
      */
@@ -33,9 +36,15 @@ class IpUser
 
     /**
      * @ORM\ManyToOne(targetEntity="Claroline\CoreBundle\Entity\User")
+     *
      * @ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")
      */
     private ?User $user = null;
+
+    public function __construct()
+    {
+        $this->refreshUuid();
+    }
 
     public function getIp(): ?string
     {
