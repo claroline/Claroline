1cf86bae40cb674222cde66f103d62cbb0a8504e
Axel Penin <Elorfin@users.noreply.github.com>
2024-09-04 12:05:02 +0200

    [DevOps] updates php cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,6 +10,7 @@ return $config
         '@Symfony' => true,
         'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
         'array_syntax' => ['syntax' => 'short'],
-        'nullable_type_declaration_for_default_null_value' => false
+        'nullable_type_declaration_for_default_null_value' => false, // this is deprecated and will be treated as true
+        'trailing_comma_in_multiline' => ['elements' => ['array_destructuring', 'arrays', 'match']], // excludes func arguments and parameters
     ])
     ->setFinder($finder);
