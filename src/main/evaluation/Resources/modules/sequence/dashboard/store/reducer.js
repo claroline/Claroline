15378776ea37d78bbb6e455fb8304e492e8bdfa1
Elorfin <penin.axel@gmail.com>
2025-08-26 09:49:57 +0200

    [Sequence] reloads evaluation when changing sequence

--- src/main/evaluation/Resources/modules/sequence/dashboard/store/reducer.js
+++ src/main/evaluation/Resources/modules/sequence/dashboard/store/reducer.js
@@ -1,4 +1,6 @@
-import {combineReducers} from '#/main/app/store/reducer'
+import {combineReducers, makeReducer} from '#/main/app/store/reducer'
+import {makeInstanceAction} from '#/main/app/store/actions'
+import {API_FETCH_PENDING} from '#/main/app/api/fetch/store/actions'
 import {makeListReducer} from '#/main/app/content/list/store'
 
 import {selectors} from '#/main/evaluation/sequence/dashboard/store/selectors'
@@ -6,9 +8,17 @@ import {selectors} from '#/main/evaluation/sequence/dashboard/store/selectors'
 const reducer = combineReducers({
   evaluations: makeListReducer(selectors.STORE_NAME+'.evaluations', {
     sortBy: { property: 'lastActivityAt', direction: -1 }
+  }, {
+    loaded: makeReducer(false, {
+      [makeInstanceAction(API_FETCH_PENDING, 'evaluationSequence')]: () => false
+    })
   }),
   logs: makeListReducer(selectors.STORE_NAME + '.logs', {
     sortBy: { property: 'date', direction: -1 }
+  }, {
+    loaded: makeReducer(false, {
+      [makeInstanceAction(API_FETCH_PENDING, 'evaluationSequence')]: () => false
+    })
   })
 })
 
