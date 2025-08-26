ebfb6c4a24263d09e5ed1baf66568b59ea7be58b
Elorfin <penin.axel@gmail.com>
2025-08-26 09:37:01 +0200

    [Evaluation] fixes sequence path in evaluations lists

--- src/main/evaluation/Resources/modules/actions/sequence_evaluation/open-sequence.js
+++ src/main/evaluation/Resources/modules/actions/sequence_evaluation/open-sequence.js
@@ -4,12 +4,12 @@ import {LINK_BUTTON} from '#/main/app/buttons'
 import {route} from '#/main/evaluation/sequence'
 import {declareAction} from '#/main/app/action'
 
-export default declareAction((evaluations, refresher, path) => ({
+export default declareAction((evaluations) => ({
   name: 'open-sequence',
   type: LINK_BUTTON,
   icon: 'fa fa-fw fa-route',
   label: trans('open_sequence', {}, 'actions'),
-  target: route(evaluations[0].sequence, null, path),
+  target: route(evaluations[0].sequence),
   scope: ['object'],
   exact: true,
   default: true
--- src/main/evaluation/Resources/modules/sequence/routing.js
+++ src/main/evaluation/Resources/modules/sequence/routing.js
@@ -1,15 +1,7 @@
-import {route as toolRoute} from '#/main/core/tool/routing'
+import {route as workspaceRoute} from '#/main/core/workspace/routing'
 
-function route(sequence, step = null, basePath = null) {
-  let sequencePath
-
-  if (basePath) {
-    sequencePath = basePath
-  } else {
-    sequencePath = toolRoute('progression')
-  }
-
-  sequencePath = `${sequencePath}/sequences/${sequence.id}`
+function route(sequence, step = null) {
+  const sequencePath = `${workspaceRoute(sequence.workspace, 'progression')}/sequences/${sequence.id}`
 
   if (step) {
     return `${sequencePath}/play/${step.id}`
