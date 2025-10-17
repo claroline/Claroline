03b7d550c3596df321f107c19947f8a5e01faa90
Elorfin <penin.axel@gmail.com>
2025-10-17 11:27:48 +0200

    [Lesson] fixes tags migration

--- src/plugin/lesson/Installation/Migrations/Version20251013120000.php
+++ src/plugin/lesson/Installation/Migrations/Version20251013120000.php
@@ -11,10 +11,11 @@ final class Version20251013120000 extends AbstractMigration
     {
         // migrates old blog tags
         $this->addSql('
-            UPDATE claro_tagbundle_tagged_object AS t
+            UPDATE IGNORE claro_tagbundle_tagged_object AS t
             LEFT JOIN icap__lesson_chapter AS c ON (t.object_name = c.title)
             SET t.object_id = c.uuid, t.object_class = "Icap\\\LessonBundle\\\Entity\\\Chapter"
             WHERE t.object_class = "Icap\\\BlogBundle\\\Entity\\\Post"
+              AND c.uuid IS NOT NULL
         ');
     }
 
