53ef2233c54d3daf674c7bba2600a773bc6ecbf0
Elorfin <penin.axel@gmail.com>
2026-01-06 09:24:02 +0100

    [Community] fixes user search by roles (includes groups)

--- src/main/community/Finder/UserType.php
+++ src/main/community/Finder/UserType.php
@@ -12,7 +12,6 @@ use Claroline\AppBundle\API\Finder\Type\EntityType;
 use Claroline\AppBundle\API\Finder\Type\RelatedEntityType;
 use Claroline\AppBundle\API\Finder\Type\TextType;
 use Claroline\CommunityBundle\Entity\Team;
-use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Entity\User;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
@@ -39,35 +38,51 @@ class UserType extends AbstractType
             ->add('created', DateType::class)
             ->add('disabled', BooleanType::class, ['default' => false])
             ->add('groups', RelatedEntityType::class)
-            ->add('roles', RelatedEntityType::class, [
-                'joinQuery' => static function (QueryBuilder $queryBuilder, FinderInterface $finder): void {
+            ->add('roles', ClosureType::class, [
+                'buildQuery' => static function (QueryBuilder $queryBuilder, FinderInterface $finder): void {
+                    if (null === $finder->getFilterValue()) {
+                        return;
+                    }
+
                     $alias = $finder->getAlias();
                     if (!$finder->isRoot()) {
                         $alias = $finder->getParent()->getAlias();
                     }
 
-                    $queryBuilder->leftJoin(Role::class, $finder->getAlias(), Join::WITH, "$alias MEMBER OF {$finder->getAlias()}.users");
+                    $queryBuilder->andWhere("EXISTS (
+                        SELECT r.id
+                        FROM Claroline\CoreBundle\Entity\Role AS r
+                        LEFT JOIN Claroline\CoreBundle\Entity\User AS ur WITH (ur.id = $alias.id AND r MEMBER OF ur.roles)
+                        LEFT JOIN Claroline\CoreBundle\Entity\Group AS gr WITH (r MEMBER OF gr.roles AND gr MEMBER OF $alias.groups)
+                        WHERE r.uuid IN (:roles)
+                          AND (ur IS NOT NULL OR gr IS NOT NULL)
+                    )");
+
+                    $filterValue = is_array($finder->getFilterValue()) ? $finder->getFilterValue() : [$finder->getFilterValue()];
+                    $queryBuilder->setParameter('roles', $filterValue);
                 },
             ])
             ->add('workspace', ClosureType::class, [
                 'buildQuery' => static function (QueryBuilder $queryBuilder, FinderInterface $finder): void {
-                    if (null !== $finder->getFilterValue()) {
-                        $alias = $finder->getAlias();
-                        if (!$finder->isRoot()) {
-                            $alias = $finder->getParent()->getAlias();
-                        }
-
-                        $queryBuilder->andWhere("EXISTS (
-                            SELECT wsr.id
-                            FROM Claroline\CoreBundle\Entity\Role AS wsr
-                            LEFT JOIN Claroline\CoreBundle\Entity\User AS wsru WITH (wsru.id = $alias.id AND wsr MEMBER OF wsru.roles)
-                            LEFT JOIN Claroline\CoreBundle\Entity\Group AS wsrg WITH (wsr MEMBER OF wsrg.roles AND wsrg MEMBER OF $alias.groups)
-                            LEFT JOIN wsr.workspace AS wsrw
-                            WHERE wsrw.uuid = :workspace
-                              AND (wsru IS NOT NULL OR wsrg IS NOT NULL)
-                        )");
-                        $queryBuilder->setParameter('workspace', $finder->getFilterValue());
+                    if (null === $finder->getFilterValue()) {
+                        return;
                     }
+
+                    $alias = $finder->getAlias();
+                    if (!$finder->isRoot()) {
+                        $alias = $finder->getParent()->getAlias();
+                    }
+
+                    $queryBuilder->andWhere("EXISTS (
+                        SELECT wsr.id
+                        FROM Claroline\CoreBundle\Entity\Role AS wsr
+                        LEFT JOIN Claroline\CoreBundle\Entity\User AS wsru WITH (wsru.id = $alias.id AND wsr MEMBER OF wsru.roles)
+                        LEFT JOIN Claroline\CoreBundle\Entity\Group AS wsrg WITH (wsr MEMBER OF wsrg.roles AND wsrg MEMBER OF $alias.groups)
+                        LEFT JOIN wsr.workspace AS wsrw
+                        WHERE wsrw.uuid = :workspace
+                          AND (wsru IS NOT NULL OR wsrg IS NOT NULL)
+                    )");
+                    $queryBuilder->setParameter('workspace', $finder->getFilterValue());
                 },
             ])
             ->add('teams', RelatedEntityType::class, [
