69a7d025f47b1f1f0917d6e6aabeade79878cccc
Elorfin <penin.axel@gmail.com>
2026-01-20 11:13:30 +0100

    [UI] fixes actions menu memoization

--- src/main/app/Resources/modules/buttons/menu/components/button.jsx
+++ src/main/app/Resources/modules/buttons/menu/components/button.jsx
@@ -43,7 +43,7 @@ const StandardMenu = (props) => {
       groups: groupActions,
       dangerous: dangerousActions
     }
-  }, [props.menu.items.map(item => item.name).join('-')])
+  }, [props.menu.items])
 
   return (
     <Menu
