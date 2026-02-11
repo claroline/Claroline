9270c9776a1310dfd46303097ea027bd7d4d9d46
Logan Fromont <logan.fromont@sinfony.eu>
2026-02-11 14:42:33 +0100

    [CURSUS] coding style

--- src/plugin/cursus/Resources/modules/tools/presence/components/tool.jsx
+++ src/plugin/cursus/Resources/modules/tools/presence/components/tool.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import omit from 'lodash/omit'
 
 import {Tool, ToolPage} from '#/main/core/tool'
-import {withReducer} from '#/main/app/store/reducer'
-import {selectors as securitySelectors} from '#/main/app/security/store'
-
 import {EventPresence} from '#/plugin/cursus/presence/components/event'
 import {SignPresence} from '#/plugin/cursus/presence/components/signing'
-import {actions, reducer, selectors} from '#/plugin/cursus/presence/store'
 import {trans} from '#/main/app/intl'
 import {PageContent} from '#/main/app/page'
 
