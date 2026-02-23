429082d6141c3ada64a8f9bb2bee746de90054f0
Logan Fromont <logan.fromont@sinfony.eu>
2026-02-23 11:55:33 +0100

    [EVALUATION]: add button to set the estimated duration on sequence + remove the step estimated duration in editor

--- src/main/evaluation/Resources/modules/sequence/editor/components/overview.jsx
+++ src/main/evaluation/Resources/modules/sequence/editor/components/overview.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import get from 'lodash/get'
 
 import {trans, transChoice} from '#/main/app/intl'
 
 import {selectors} from '#/main/evaluation/sequence/editor/store'
 import {EditorOverview} from '#/main/app/editor/components/overview'
+import {actions as formActions} from '#/main/app/content/form/store'
 
 const SequenceEditorOverview = () => {
   const workspace = useSelector(selectors.workspace)
   const sequence = useSelector(selectors.data)
   const totalEstimatedDuration = useSelector(selectors.totalEstimatedDuration)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const onClick = (e) => {
+      const target = e.target
+      const btn = target && target.closest ? target.closest('[data-fill-estimated-duration]') : null
+      if (!btn) return
+
+      e.preventDefault()
+
+      dispatch(
+        formActions.updateProp(
+          selectors.STORE_NAME,
+          'estimatedDuration',
+          totalEstimatedDuration
+        )
+      )
+    }
+    document.addEventListener('click', onClick)
+    return () => document.removeEventListener('click', onClick)
+  }, [dispatch, totalEstimatedDuration])
+
 
   return (
     <EditorOverview
@@ -64,18 +87,22 @@ const SequenceEditorOverview = () => {
               name: 'estimatedDuration',
               label: trans('estimated_duration'),
               help:
-                `${trans('estimated_duration_help')}<br/>` +
+                trans('estimated_duration_help') +
+                '<br/>' +
                 transChoice(
                   'estimated_resource_duration',
                   totalEstimatedDuration,
                   { duration: totalEstimatedDuration },
                   'sequence'
-                ),
+                ) +
+                ' <button type="button" class="btn btn-subtle-primary btn-sm" data-fill-estimated-duration="1">' +
+                trans('use_calculated_duration', {}, 'sequence') +
+                '</button>',
               type: 'number',
               options: {
                 unit: trans('minutes')
               }
-            },{
+            }, {
               name: 'meta.descriptionHtml',
               label: trans('description_long'),
               type: 'html',
