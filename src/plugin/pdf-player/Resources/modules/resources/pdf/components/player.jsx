015b883f4072770833b2a51fd6f9c7df68bbbce8
Logan Fromont <logan.fromont@sinfony.eu>
2026-03-04 10:58:58 +0100

    eslint ignore import.meta.url

--- src/plugin/pdf-player/Resources/modules/resources/pdf/components/player.jsx
+++ src/plugin/pdf-player/Resources/modules/resources/pdf/components/player.jsx
@@ -13,11 +13,12 @@ import {
   ScrollMode
 } from 'pdfjs-dist/web/pdf_viewer'
 
-// eslint-disable-next-line
+/* eslint-disable */
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url
 ).toString()
+/* eslint-enable */
 
 import {url} from '#/main/app/api'
 import {toKey} from '#/main/app/utils/text'
