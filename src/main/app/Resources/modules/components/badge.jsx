4f2604873db2b61c406b0b0789c60fd029be2f1a
Elorfin <penin.axel@gmail.com>
2026-03-19 12:46:45 +0100

    [UI] add style support in badge component

--- src/main/app/Resources/modules/components/badge.jsx
+++ src/main/app/Resources/modules/components/badge.jsx
@@ -6,14 +6,16 @@ const Badge = (props) =>
   <span className={classes('badge', props.className, {
     [`text-bg-${props.variant}`]: !props.subtle,
     [`bg-${props.variant}-subtle text-${props.variant}-emphasis`]: props.subtle
-  })}>
+  })} style={props.style}>
     {props.children}
   </span>
 
 Badge.propTypes = {
   className: T.string,
   subtle: T.bool.isRequired,
-  variant: T.oneOf(['primary', 'secondary', 'success', 'warning', 'danger', 'info']).isRequired
+  variant: T.oneOf(['primary', 'secondary', 'success', 'warning', 'danger', 'info']).isRequired,
+  style: T.object,
+  children: T.any
 }
 
 Badge.defaultProps = {
