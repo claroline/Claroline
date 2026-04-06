e465f638a57d38d68de47f62d5bba1ecf278b1fd
Laurent Gruber <laurent.gruber@sinfony.eu>
2026-04-06 14:23:10 +0200

    init plugin prject

---
+++ src/plugin/project/ClarolineProjectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Claroline\ProjectBundle;
+
+use Claroline\KernelBundle\Bundle\DistributionPluginBundle;
+
+class ClarolineProjectBundle extends DistributionPluginBundle
+{
+}
---
+++ src/plugin/project/Entity/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\ProjectBundle\Entity;
+
+use Claroline\CoreBundle\Entity\Resource\AbstractResource;
+use Claroline\CoreBundle\Entity\User;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'claro_project')]
+#[ORM\Entity]
+class Project extends AbstractResource
+{
+
+}
